bca8fb26d9337ad489a1ad54cc2ce26cc17daffd
Hasan Ozovali <ozovalihasan@gmail.com>
2022-07-05 19:03:42 +0300

    Update the solution of "Valid Sudoku" challenge

--- 36_Valid_Sudoku/solution.rb
+++ 36_Valid_Sudoku/solution.rb
@@ -1,26 +1,25 @@
 # @param {Character[][]} board
 # @return {Boolean}
 def is_valid_sudoku(board)
-  return false unless check_rows(board)
   return false unless check_rows(board.transpose)
-
-  grouped_board = [[[],[],[]],[[],[],[]],[[],[],[]]]
-  board.each_with_index do | row, index |
-    row.each_with_index do | number, index2 |
-      grouped_board[index / 3][index2 / 3] << number
+  return false unless check_rows(board)
+  
+  grouped_board = [[{},{},{}],[{},{},{}],[{},{},{}]]
+  board.each_with_index do | row, index_row |
+    row.each_with_index do | number, index_number |
+      next if number == "."
+      return false if grouped_board[index_row / 3][index_number / 3][number]
+        
+      grouped_board[index_row / 3][index_number / 3][number] = true
     end
   end
-
-  return false unless check_rows(grouped_board.flatten 1)
-
+  
   true
 end
 
 def check_rows(board)
   board.each do |row|
-    
-    return false unless (row - ["."]).uniq.size == (row - ["."]).size
-    
+    return false unless (row.uniq - ["."]) == (row - ["."])
   end
   true
 end
