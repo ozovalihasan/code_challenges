4a5c6c05c4ed2d235af125f3615331b5fcc0dd69
Hasan Ozovali <ozovalihasan@gmail.com>
2022-07-08 16:36:36 +0300

    Add a solution to the "45_Jump_Game_II" challenge

--- 45_Jump_Game_II/solution.rb
+++ 45_Jump_Game_II/solution.rb
@@ -1,5 +1,18 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def jump(nums)
+  return 0 if nums.size == 1
 
+  nums.map!.with_index { |num, index| num + index }
+  index, jump = 0, 0 
+  loop do
+    num = nums[index]
+    return jump + 1 if num >= (nums.size - 1)
+
+    limited_nums = nums[index + 1..num]
+    index += limited_nums.rindex(limited_nums.max) + 1
+    jump += 1
+  end
+
+  jump
 end
