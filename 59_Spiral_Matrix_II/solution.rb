606f82aadfd125d1f3e3dd755dcc5c26883864bb
ozovalihasan <ozovalihasan@gmail.com>
2022-07-12 12:08:49 +0300

    Add a solution to the "Spiral Matrix II" challenge

--- 59_Spiral_Matrix_II/solution.rb
+++ 59_Spiral_Matrix_II/solution.rb
@@ -1,5 +1,29 @@
 # @param {Integer} n
 # @return {Integer[][]}
 def generate_matrix(n)
-    
+  matrix = [(1..n).to_a]
+  (n - 1).times { matrix << [] }
+
+  column = n - 1
+  row = 0
+  count = n
+  step = 1
+
+  (n - 1).downto(1) do |number|
+    number.times do
+      row += step
+      count += 1
+      matrix[row][column] = count
+    end
+
+    number.times do
+      column -= step
+      count += 1
+      matrix[row][column] = count
+    end
+
+    step *= -1
+  end
+
+  matrix
 end
