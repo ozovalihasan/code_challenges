79282721e3c43476b785aaa2ad0be2b27b9d3820
ozovalihasan <ozovalihasan@gmail.com>
2022-07-13 14:12:44 +0300

    Add a test for the "Valid Number" challenge

---
+++ 65_Valid_Number/test_solution.rb
@@ -0,0 +1,16 @@
+require_relative './solution'
+require_relative '../read_test_files'
+
+describe 'Solution' do
+  examples, results = read_test_files.values_at(:examples, :results)
+
+  describe '#is_number' do
+    examples.each_with_index do |str, index|
+
+      it 'returns whether the given string is a number or not' do
+        expect(is_number(str)).to eq(results[index])
+      end
+      
+    end
+  end
+end
