e1dc5cfb79d3762fd54d86f3145c3fd7d2ebc732
ozovalihasan <ozovalihasan@gmail.com>
2022-07-19 11:53:36 +0300

    Add a solution to the "Minimum Depth of Binary Tree" challenge

--- 111_Minimum_Depth_of_Binary_Tree/solution.rb
+++ 111_Minimum_Depth_of_Binary_Tree/solution.rb
@@ -9,6 +9,45 @@
 # end
 # @param {TreeNode} root
 # @return {Integer}
+
+################
+# Solution 1
+################
 def min_depth(root)
-    
+  return 0 if root.nil?
+  @depth = Float::INFINITY
+  check_nodes(root, 1)
+  @depth
 end
+
+def check_nodes(node, count)
+  return if node.nil?
+  
+  if node.left.nil? && node.right.nil? 
+    @depth = count if count < @depth
+  end
+  check_nodes(node.left, count + 1)
+  check_nodes(node.right, count + 1)
+  
+end
+
+################
+# Solution 2
+################
+# def min_depth(root)
+#   return 0 if root.nil?
+#   nodes = [root]
+#   count = 1
+#   while true
+#     new_nodes = []
+#     nodes.each do |node|
+#       if node.left.nil? && node.right.nil?
+#         return count
+#       end
+#       new_nodes << node.left
+#       new_nodes << node.right
+#     end
+#     nodes = new_nodes.compact
+#     count +=1
+#   end
+# end
