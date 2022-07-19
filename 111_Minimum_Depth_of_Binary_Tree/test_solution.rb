93aaa45150d0796a2c071cdec377d948e4586ecc
ozovalihasan <ozovalihasan@gmail.com>
2022-07-19 11:56:37 +0300

    Refactor the test of the "Minimum Depth of Binary Tree" challenge

--- 111_Minimum_Depth_of_Binary_Tree/test_solution.rb
+++ 111_Minimum_Depth_of_Binary_Tree/test_solution.rb
@@ -6,9 +6,9 @@ describe 'Solution' do
   examples, results = read_test_files.values_at(:examples, :results)
 
   describe '#min_depth' do
-    examples.each_with_index do |root, index|
+    examples.each_with_index do |arr, index|
       it 'returns the minimum depth of a binary tree' do
-        tree = Tree.new(root)
+        tree = Tree.new(arr)
         expect(min_depth(tree.root)).to eq(results[index])
       end
     end
