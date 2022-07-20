98cbb23261b637a4ca2c0ba40a5d3168a608df82
ozovalihasan <ozovalihasan@gmail.com>
2022-07-20 13:25:23 +0300

    Add a solution to the "Path Sum" challenge

--- 112_Path_Sum/solution.rb
+++ 112_Path_Sum/solution.rb
@@ -11,5 +11,15 @@
 # @param {Integer} target_sum
 # @return {Boolean}
 def has_path_sum(root, target_sum)
+  return true if check_sum(root, 0, target_sum)
+  false
+end
+
+def check_sum(node, sum, target_sum)
+  return if node.nil?
   
+  sum += node.val
+  return true if sum == target_sum && node.left.nil? && node.right.nil?
+  return true if check_sum(node.left, sum, target_sum)
+  return true if check_sum(node.right, sum, target_sum)
 end
