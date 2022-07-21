68fcadc8bee2d9badc8ca209e5e87d55440dba73
ozovalihasan <ozovalihasan@gmail.com>
2022-07-21 11:09:03 +0300

    Add a solution to the "Plus One" challenge

--- 66_Plus_One/solution.rb
+++ 66_Plus_One/solution.rb
@@ -1,4 +1,15 @@
 # @param {Integer[]} digits
 # @return {Integer[]}
 def plus_one(digits)
+  digits.reverse_each.with_index do |digit, index|
+    if digit == 9
+      digits[ - index - 1 ] = 0
+    else
+      digits[ - index - 1 ] += 1 
+      return digits
+    end
+
+  end
+  digits.unshift 1
+  
 end
