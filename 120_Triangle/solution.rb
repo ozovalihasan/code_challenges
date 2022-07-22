f50433957c943feed24cedb8274928142ee4a571
ozovalihasan <ozovalihasan@gmail.com>
2022-07-22 11:31:36 +0300

    Add a solution to the "Triangle" challenge

--- 120_Triangle/solution.rb
+++ 120_Triangle/solution.rb
@@ -2,4 +2,16 @@
 # @return {Integer}
 def minimum_total(triangle)
   
+  triangle[1..].each_with_index do |arr, index|
+
+    triangle[index+1][0] += triangle[index].first
+    
+    1.upto( arr.size-2 ) do |index2|
+      triangle[index+1][index2] += triangle[index][index2 - 1..index2].min
+    end
+
+    triangle[index+1][-1] += triangle[index].last
+
+  end
+  triangle.last.min
 end
