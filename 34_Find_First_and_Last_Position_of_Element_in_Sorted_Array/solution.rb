1af38e9f8e0b00f03bdf3efbaf6cd0119906eff1
ozovalihasan <ozovalihasan@gmail.com>
2022-07-25 11:05:02 +0300

    Add a solution to the "Find First and Last Position of Element in Sorted Array" challenge

--- 34_Find_First_and_Last_Position_of_Element_in_Sorted_Array/solution.rb
+++ 34_Find_First_and_Last_Position_of_Element_in_Sorted_Array/solution.rb
@@ -2,5 +2,12 @@
 # @param {Integer} target
 # @return {Integer[]}
 def search_range(nums, target)
-    
+  first = nums.bsearch_index {|num| num >= target}
+  if first && nums[first] == target
+    last = (nums.bsearch_index {|num| num > target} || nums.size) - 1
+  else
+    return [-1, -1]
+  end
+  
+  [first, last]
 end
