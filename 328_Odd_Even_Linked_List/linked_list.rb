ae30894437d984ca15878616cb9b9486eb8154a0
ozovalihasan <ozovalihasan@gmail.com>
2022-07-26 10:29:34 +0300

    Add a test for the "Odd Even Linked List" challenge

---
+++ 328_Odd_Even_Linked_List/linked_list.rb
@@ -0,0 +1,36 @@
+class ListNode
+  attr_accessor :val, :next
+
+  def initialize(val = 0, _next = nil)
+    @val = val
+    @next = _next
+  end
+end
+
+class LinkedList
+  attr_accessor :head
+
+  def initialize(arr)
+    if arr.empty?
+      @head = nil
+    else
+      @head = ListNode.new(arr.first)
+      node = @head
+
+      arr[1..].each do |val|
+        node.next = ListNode.new(val)
+        node = node.next
+      end
+    end
+  end
+
+  def to_a
+    node = @head
+    arr = []
+    until node.nil?
+      arr << node.val
+      node = node.next
+    end
+    arr
+  end
+end
