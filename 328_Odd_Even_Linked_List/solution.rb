04b03ac7ad53b9cf7d226c483a16cec4f054811c
ozovalihasan <ozovalihasan@gmail.com>
2022-07-26 11:34:41 +0300

    Add a solution to the "Odd Even Linked List" challenge

--- 328_Odd_Even_Linked_List/solution.rb
+++ 328_Odd_Even_Linked_List/solution.rb
@@ -9,5 +9,20 @@
 # @param {ListNode} head
 # @return {ListNode}
 def odd_even_list(head)
-  
+  return nil if head.nil?
+
+  node = head
+  node2 = head.next
+  head2 = node2
+
+  until node2&.next.nil?
+    node.next = node2.next
+    node2.next = node2.next.next
+    node.next.next = head2
+
+    node = node.next
+    node2 = node2.next
+  end
+
+  head
 end
