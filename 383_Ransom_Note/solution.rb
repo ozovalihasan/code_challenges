4a505a751f396e594ac169ae47b3a786ad234743
ozovalihasan <ozovalihasan@gmail.com>
2022-07-27 11:23:36 +0300

    Add a solution to the "Ransom Note" challenge

--- 383_Ransom_Note/solution.rb
+++ 383_Ransom_Note/solution.rb
@@ -2,5 +2,16 @@
 # @param {String} magazine
 # @return {Boolean}
 def can_construct(ransom_note, magazine)
+  hash = Hash.new(0)
+  magazine.each_char do |char|
+    hash[char] += 1
+  end
+  
+  ransom_note.each_char do |char|
+    hash[char] -= 1
+    return false if hash[char].negative?
+  end
+
+  true
 
 end
