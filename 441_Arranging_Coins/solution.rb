909cfe4a21949cdab7af80113a411a432facd227
ozovalihasan <ozovalihasan@gmail.com>
2022-07-31 21:52:35 +0300

    Add a solution to the "Arranging Coins" challenge

--- 441_Arranging_Coins/solution.rb
+++ 441_Arranging_Coins/solution.rb
@@ -1,5 +1,11 @@
 # @param {Integer} n
 # @return {Integer}
 def arrange_coins(n)
-    
+  count = Math.sqrt(n+1).floor
+  n -= (count * count + count) / 2
+  until n <= count
+    count += 1
+    n -= count
+  end
+  count
 end
