c017284a818a4c11d508da75b856ece1fb0c80fc
ozovalihasan <ozovalihasan@gmail.com>
2022-08-01 10:30:36 +0300

    Add a solution to the "Rotate Array" challenge

--- 189_Rotate_Array/solution.rb
+++ 189_Rotate_Array/solution.rb
@@ -2,5 +2,6 @@
 # @param {Integer} k
 # @return {Void} Do not return anything, modify nums in-place instead.
 def rotate(nums, k)
-    
+  k = k % nums.size
+  nums.concat(nums.shift(nums.size - k))
 end
