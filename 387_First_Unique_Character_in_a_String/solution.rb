95a06b89d1e7f3be5eebd7472f16f53fcbd4205a
ozovalihasan <ozovalihasan@gmail.com>
2022-08-02 10:49:54 +0300

    Add a solution to the "First Unique Character in a String" challenge

--- 387_First_Unique_Character_in_a_String/solution.rb
+++ 387_First_Unique_Character_in_a_String/solution.rb
@@ -1,5 +1,17 @@
 # @param {String} s
 # @return {Integer}
 def first_uniq_char(s)
-    
+  hash = {}
+
+  s.chars.each do |char|
+    if hash[char] 
+      next
+    elsif s.count(char) == 1
+      return s.index(char)
+    else
+      hash[char] = true
+    end
+  end
+  
+  -1
 end
