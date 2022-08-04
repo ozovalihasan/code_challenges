49c71d7897f5c0c1ebb01e163a901f90192396f8
ozovalihasan <ozovalihasan@gmail.com>
2022-08-04 11:47:39 +0300

    Add a solution to the "Repeated String Match" challenge

--- 686_Repeated_String_Match/solution.rb
+++ 686_Repeated_String_Match/solution.rb
@@ -2,5 +2,21 @@
 # @param {String} b
 # @return {Integer}
 def repeated_string_match(a, b)
-    
+  reference = b[0...a.length]
+  return -1 unless (a * 2).include? reference
+  
+  index = 0
+  while index < b.length
+    return -1 unless reference.match(/^#{b.slice(index, a.length)}/)
+    index += a.length
+  end
+
+  repeat = b.length / a.length
+  if (a * repeat).include? b
+    repeat
+  elsif (a * (repeat + 1)).include? b
+    repeat + 1
+  else
+    repeat + 2
+  end
 end
