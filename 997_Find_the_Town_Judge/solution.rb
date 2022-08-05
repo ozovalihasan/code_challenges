ef01fa5a87e17082787495dedba2139401aae3e9
ozovalihasan <ozovalihasan@gmail.com>
2022-08-05 11:29:47 +0300

    Add a solution to the "Find the Town Judge" challenge

--- 997_Find_the_Town_Judge/solution.rb
+++ 997_Find_the_Town_Judge/solution.rb
@@ -2,5 +2,12 @@
 # @param {Integer[][]} trust
 # @return {Integer}
 def find_judge(n, trust)
-    
+  return n if n == 1
+
+  possible_judge = trust.group_by(&:last).find {|_, pair| pair.size == (n - 1)}&.first
+  if possible_judge && trust.none? {|arr| arr.first == possible_judge}
+    return possible_judge
+  end
+  
+  -1
 end
