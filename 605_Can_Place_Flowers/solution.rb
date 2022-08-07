7c496b8dcb6eb4816dab9e7cd57c4cea846d6c5d
ozovalihasan <ozovalihasan@gmail.com>
2022-08-07 22:49:53 +0300

    Update the solution of the "Can Place Flowers" challenge

--- 605_Can_Place_Flowers/solution.rb
+++ 605_Can_Place_Flowers/solution.rb
@@ -1,36 +1,15 @@
-# #############
-# 1. Solution
-# #############
 # @param {Integer[]} flowerbed
 # @param {Integer} n
 # @return {Boolean}
 def can_place_flowers(flowerbed, n)
-  flowerbed.unshift(0) << 0
-  result = [""]
-  
-  flowerbed.each do |plot|
-    if plot.zero?
-      result.last << "0"
+  n *= 2
+  while flowerbed.first do
+    if flowerbed.pop.zero?
+      n -= 1
     else
-      result << ""
+      n += (n % 2) + 1
     end
   end
-  
-  result.inject(0) do |sum, pair|
-    sum + (pair.length-1) / 2
-  end >= n
-end
 
-# #############
-# 2. Solution
-# #############
-# @param {Integer[]} flowerbed
-# @param {Integer} n
-# @return {Boolean}
-# def can_place_flowers(flowerbed, n)
-#   flowerbed.unshift(0) << 0
-#   flowerbed
-#     .chunk(&:zero?)
-#     .filter_map {|result| (result.last.size - 1) / 2 if result.first }
-#     .sum >= n
-# end
+  0 >= n - 1
+end
