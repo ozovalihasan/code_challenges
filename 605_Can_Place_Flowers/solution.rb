2c2937b5a8f4d7db67f87d25d3c26d583d1dbbd5
ozovalihasan <ozovalihasan@gmail.com>
2022-08-07 23:31:49 +0300

    Update the solution of the "Can Place Flowers" challenge

--- 605_Can_Place_Flowers/solution.rb
+++ 605_Can_Place_Flowers/solution.rb
@@ -2,14 +2,14 @@
 # @param {Integer} n
 # @return {Boolean}
 def can_place_flowers(flowerbed, n)
-  n *= 2
+  needed_plots = n * 2
   while flowerbed.first do
     if flowerbed.pop.zero?
-      n -= 1
+      needed_plots -= 1
     else
-      n += (n % 2) + 1
+      needed_plots += (needed_plots % 2) + 1
     end
   end
 
-  0 >= n - 1
+  0 >= needed_plots - 1
 end
