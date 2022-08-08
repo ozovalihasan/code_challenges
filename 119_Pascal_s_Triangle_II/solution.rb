c2f5cd6da8baad713f23fce20e73281fff6ea868
ozovalihasan <ozovalihasan@gmail.com>
2022-08-08 11:04:01 +0300

    Add a solution to the "Pascal's Triangle II" challenge

--- 119_Pascal_s_Triangle_II/solution.rb
+++ 119_Pascal_s_Triangle_II/solution.rb
@@ -1,5 +1,11 @@
 # @param {Integer} row_index
 # @return {Integer[]}
 def get_row(row_index)
-    
+  result = [1]
+  
+  1.upto(row_index) do |index|
+    result =  result.each_cons(2).map(&:sum).unshift(1) << 1
+  end
+  
+  result
 end
