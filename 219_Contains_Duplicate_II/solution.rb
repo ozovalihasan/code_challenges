1dfc8e09146813627dd199f753041360e477224a
ozovalihasan <ozovalihasan@gmail.com>
2022-08-09 10:48:54 +0300

    Add a solution to the "Contains Duplicate II" challenge

--- 219_Contains_Duplicate_II/solution.rb
+++ 219_Contains_Duplicate_II/solution.rb
@@ -2,5 +2,14 @@
 # @param {Integer} k
 # @return {Boolean}
 def contains_nearby_duplicate(nums, k)
-    
+  return false if nums.uniq == nums
+  
+  hash = {}
+  hash.default = -k - 1
+  nums.each_with_index do |num, index|
+    return true if (index - hash[num]) <= k
+    hash[num] = index
+  end
+  
+  false
 end
