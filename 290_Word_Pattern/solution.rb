a4ac3b444b1074c8d319668e338fd36e05d66d04
ozovalihasan <ozovalihasan@gmail.com>
2022-08-10 12:48:35 +0300

    Add a solution to the "Word Pattern" challenge

--- 290_Word_Pattern/solution.rb
+++ 290_Word_Pattern/solution.rb
@@ -2,5 +2,8 @@
 # @param {String} s
 # @return {Boolean}
 def word_pattern(pattern, s)
-    
+  pattern = pattern.chars
+  s = s.split
+
+  s.size == pattern.size && s.uniq.size == pattern.uniq.size && s.zip(pattern).uniq.size == s.uniq.size
 end
