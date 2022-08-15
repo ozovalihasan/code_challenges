fda811961353c604e561789457e9ee817b7d3db6
ozovalihasan <ozovalihasan@gmail.com>
2022-08-15 10:22:13 +0300

    Add a solution to the "Add Digits" challenge

--- 258_Add_Digits/solution.rb
+++ 258_Add_Digits/solution.rb
@@ -1,5 +1,11 @@
 # @param {Integer} num
 # @return {Integer}
 def add_digits(num)
-    
+  loop do
+    digits = num.digits
+    break if digits.size == 1
+    num = digits.sum
+  end
+  
+  num
 end
