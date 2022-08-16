6465dd3bd7db86a2ca0a0f832a33a1f402637edf
ozovalihasan <ozovalihasan@gmail.com>
2022-08-16 10:52:19 +0300

    Add a solution to the "Rectangle Overlap" challenge

--- 836_Rectangle_Overlap/solution.rb
+++ 836_Rectangle_Overlap/solution.rb
@@ -2,5 +2,8 @@
 # @param {Integer[]} rec2
 # @return {Boolean}
 def is_rectangle_overlap(rec1, rec2)
-    
+  (rec1.first < rec2[2]) &&
+  (rec2.first < rec1[2]) &&
+  (rec1[1] < rec2.last) &&
+  (rec2[1] < rec1.last) 
 end
