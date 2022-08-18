452258ac80c0cf04e12d0cc15d62c85ccd87d49b
ozovalihasan <ozovalihasan@gmail.com>
2022-08-18 10:39:26 +0300

    Add a solution to the "Self Dividing Numbers" challenge

--- 728_Self_Dividing_Numbers/solution.rb
+++ 728_Self_Dividing_Numbers/solution.rb
@@ -2,5 +2,8 @@
 # @param {Integer} right
 # @return {Integer[]}
 def self_dividing_numbers(left, right)
-    
+  left.upto(right).reject do |num| 
+    digits = num.digits
+    digits.include?(0) || digits.any? {|digit| (num % digit) != 0 } 
+  end
 end
