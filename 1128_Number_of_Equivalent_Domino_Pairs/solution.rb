1622bb7b7c98b322452577e1e03b5d60a54366dd
ozovalihasan <ozovalihasan@gmail.com>
2022-08-19 11:50:24 +0300

    Add a solution to the "Number of Equivalent Domino Pairs" challenge

--- 1128_Number_of_Equivalent_Domino_Pairs/solution.rb
+++ 1128_Number_of_Equivalent_Domino_Pairs/solution.rb
@@ -1,5 +1,17 @@
 # @param {Integer[][]} dominoes
 # @return {Integer}
 def num_equiv_domino_pairs(dominoes)
-    
+  dominoes.group_by(&:sort)
+          .values.map(&:size).sum {|val| val * (val - 1) / 2}
 end
+
+
+##########
+# 2. solution
+##########
+
+# # @param {Integer[][]} dominoes
+# # @return {Integer}
+# def num_equiv_domino_pairs(dominoes)
+#   dominoes.map(&:sort).tally.values.sum {|val| val * (val - 1) / 2}
+# end
