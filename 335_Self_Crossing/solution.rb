a9e7221351af8f60bdd9694d2b4bef221baea193
ozovalihasan <ozovalihasan@gmail.com>
2022-08-20 18:26:50 +0300

    Add a solution to the "Self Crossing" challenge

--- 335_Self_Crossing/solution.rb
+++ 335_Self_Crossing/solution.rb
@@ -1,5 +1,33 @@
+# Thanks to KuangYuan to share the approach (https://leetcode.com/problems/self-crossing/discuss/79131/Java-Oms-with-explanation)
+
 # @param {Integer[]} distance
 # @return {Boolean}
 def is_self_crossing(distance)
-    
+  return false if distance.size < 4
+
+  return true if check_four_element(*distance[-4...])
+
+  return false if distance.size == 4
+
+  return true if check_five_element(*distance[-5...])
+
+  distance.each_cons(6) do |long_pair|
+    if check_six_element(*long_pair)
+      return true 
+    end
+  end
+
+  false
+end
+
+def check_four_element(x1, x2, x3, x4)
+  x1 >= x3 && x4 >= x2
+end
+
+def check_five_element(x1, x2, x3, x4, x5)
+  (x1 + x5) >= x3 && x4 == x2 || check_four_element(x1, x2, x3, x4)
+end
+
+def check_six_element(x1, x2, x3, x4, x5, x6 )
+  ((x1 + x5) >= x3 && x3 >= x5 && (x6 + x2) >= x4 && x4 >= x2) || check_five_element(x1, x2, x3, x4, x5)
 end
