aa2243b7bd647496dc25c43768c0844dcec8406d
ozovalihasan <ozovalihasan@gmail.com>
2022-08-23 11:57:52 +0300

    Add a solution to the "Positions of Large Groups" challenge

--- 830_Positions_of_Large_Groups/solution.rb
+++ 830_Positions_of_Large_Groups/solution.rb
@@ -1,5 +1,15 @@
 # @param {String} s
 # @return {Integer[][]}
 def large_group_positions(s)
-    
+  result = [[-1]]
+  s += "#"
+  s.each_char.with_index do |char, index|
+    unless char == s[index - 1]
+      result.last << index - 1
+      result << [index]
+    end
+  end  
+  
+  result.pop
+  result.reject! {|pair| pair.last - pair.first < 2 }
 end
