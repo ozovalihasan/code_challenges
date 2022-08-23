123775a7dfc3fa1a4bd586d4e43ae7326a64d701
ozovalihasan <ozovalihasan@gmail.com>
2022-08-23 22:39:45 +0300

    Add a solution to the "Binary Search" challenge

--- 704_Binary_Search/solution.rb
+++ 704_Binary_Search/solution.rb
@@ -1,6 +1,8 @@
+# Thanks to @Stefan for the sharing(https://stackoverflow.com/questions/63181259/strange-behavior-with-bsearch-index#comment111734948_63181661)
+
 # @param {Integer[]} nums
 # @param {Integer} target
 # @return {Integer}
 def search(nums, target)
-    
+  nums.bsearch_index {|item| target <=> item} || -1
 end
