a6f0f6bd88cbc5cbf50b9bd426060b185138e34d
ozovalihasan <ozovalihasan@gmail.com>
2022-08-25 11:45:20 +0300

    Add a solution to the "Squares of a Sorted Array" challenge

--- 977_Squares_of_a_Sorted_Array/solution.rb
+++ 977_Squares_of_a_Sorted_Array/solution.rb
@@ -1,5 +1,32 @@
 # @param {Integer[]} nums
 # @return {Integer[]}
 def sorted_squares(nums)
-    
+  return nums.map{|num| num * num } if nums.first >= 0
+  return nums.reverse.map{|num| num * num } if nums.last < 0
+  
+  result = []
+  nums.map!{|num| num * num }
+  index1 = 0
+  index2 = nums.size - 1
+  
+  until index1 > index2
+    if nums[index2] > nums[index1]
+      result << nums[index2]
+      index2 -= 1
+    else
+      result << nums[index1]
+      index1 += 1
+    end
+  end
+  
+  result.reverse
 end
+
+##################
+## 2. Solution
+##################
+# # @param {Integer[]} nums
+# # @return {Integer[]}
+# def sorted_squares(nums)
+#   nums.map {|num| num * num}.sort
+# end
