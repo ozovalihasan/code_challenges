96082fbc6cd41c79b4165eda13ec80e20936b620
ozovalihasan <ozovalihasan@gmail.com>
2022-08-26 12:32:48 +0300

    Add two solutions to the "Move Zeroes" challenge

--- 283_Move_Zeroes/solution.rb
+++ 283_Move_Zeroes/solution.rb
@@ -1,5 +1,16 @@
 # @param {Integer[]} nums
 # @return {Void} Do not return anything, modify nums in-place instead.
 def move_zeroes(nums)
-    
+  nums.sort! {|first| first.zero? ? 1 : -1}
 end
+
+#############
+## 2. Solution
+#############
+# # @param {Integer[]} nums
+# # @return {Void} Do not return anything, modify nums in-place instead.
+# def move_zeroes(nums)
+#   count = nums.count 0
+#   nums.delete 0
+#   nums.concat([0] * count)
+# end
