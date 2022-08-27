31c26ccf7bc68812e5ee21093ba18a0d6af896af
ozovalihasan <ozovalihasan@gmail.com>
2022-08-27 13:50:58 +0300

    Add a solution to the "Reverse String" challenge

--- 344_Reverse_String/solution.rb
+++ 344_Reverse_String/solution.rb
@@ -1,5 +1,16 @@
 # @param {Character[]} s
 # @return {Void} Do not return anything, modify s in-place instead.
 def reverse_string(s)
-
+  (s.length/2).times do |index|
+    s[index], s[~index] = s[~index], s[index]
+  end
 end
+
+###############
+## 2. Solution
+###############
+# # @param {Character[]} s
+# # @return {Void} Do not return anything, modify s in-place instead.
+# def reverse_string(s)
+#   s.reverse!
+# end
