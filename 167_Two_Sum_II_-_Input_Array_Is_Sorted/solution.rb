90d2e5d3e83482fd345b694daab0e81fcdc2539d
ozovalihasan <ozovalihasan@gmail.com>
2022-08-27 11:40:07 +0300

    Add a solution to the "Two Sum II - Input Array Is Sorted" challenge

--- 167_Two_Sum_II_-_Input_Array_Is_Sorted/solution.rb
+++ 167_Two_Sum_II_-_Input_Array_Is_Sorted/solution.rb
@@ -2,5 +2,9 @@
 # @param {Integer} target
 # @return {Integer[]}
 def two_sum(numbers, target)
-    
+  
+  numbers.each_with_index.with_object do |(num, index), hash|
+    return [hash[target - num] + 1, index + 1] if hash[target - num]
+    hash[num] = index
+  end
 end
