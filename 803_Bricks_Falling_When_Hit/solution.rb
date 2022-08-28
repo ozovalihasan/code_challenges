5a4d9a8f7c68181512d2f5a2f2c029685ffc89cf
ozovalihasan <ozovalihasan@gmail.com>
2022-08-28 23:20:08 +0300

    Add a solution to the "Bricks Falling When Hit" challenge

--- 803_Bricks_Falling_When_Hit/solution.rb
+++ 803_Bricks_Falling_When_Hit/solution.rb
@@ -2,5 +2,99 @@
 # @param {Integer[][]} hits
 # @return {Integer[]}
 def hit_bricks(grid, hits)
+  hits.each do |(row, col)|
+    grid[row][col] = "hit" if grid[row][col] == 1 
+  end 
+  
+  hits.reverse!
+  
+  grid.first.each_with_index do |item, col|
+    check_coordinates(grid, 0, col)
+  end
+  
+  result = []
+
+  hits.each do |row, col|
+    result << 0
+
+    next if grid[row][col] == 0
+
+    update = false
+    if grid[row][col] == "hit"
+      if (row > 0 && grid[row - 1][col] == :strong) ||
+        (row < grid.size - 1 && grid[row + 1][col] == :strong) ||
+        (col > 0 && grid[row][col - 1] == :strong) ||
+        (col < grid.first.size - 1 && grid[row][col + 1] == :strong) ||
+        row == 0
+        update = true
+      else
+        grid[row][col] = 1
+        next
+      end
+      
+    end
     
+    grid[row][col] = 1
+    
+    count_coordinates(grid, row, col, result)
+    
+    result[-1] -= 1
+
+    if row == 0 || update == true
+      check_coordinates(grid, row, col)
+    end
+
+  end
+
+  result.reverse
+
+end
+
+def count_coordinates(grid, row, col, result)
+
+  return unless grid[row][col] == 1
+  
+  result[-1] += 1
+  grid[row][col] = :checked
+
+  if row > 0
+    count_coordinates(grid, row - 1, col, result)
+  end
+  
+  if row < grid.size - 1
+    count_coordinates(grid, row + 1, col, result)
+  end
+  
+  if col > 0
+    count_coordinates(grid, row, col - 1 , result)
+  end
+  
+  if col < grid.first.size - 1
+    count_coordinates(grid, row, col + 1 , result)
+  end
+  
+end
+
+
+def check_coordinates(grid, row, col)
+  return unless grid[row][col] == 1 || grid[row][col] == :checked
+
+  grid[row][col] = :strong
+  
+  if row > 0
+    check_coordinates(grid, row - 1, col)
+  end
+  
+  if row < grid.size - 1
+    check_coordinates(grid, row + 1, col)
+  end
+  
+  if col > 0
+    check_coordinates(grid, row, col - 1 )
+  end
+  
+  if col < grid.first.size - 1
+    check_coordinates(grid, row, col + 1 )
+  end
+
 end
