bae5e383448ba99f1878d02a372d7290faf693c1
ozovalihasan <ozovalihasan@gmail.com>
2022-08-30 15:12:32 +0300

    Add a solution to the "Flood Fill" challenge

--- 733_Flood_Fill/solution.rb
+++ 733_Flood_Fill/solution.rb
@@ -4,5 +4,25 @@
 # @param {Integer} color
 # @return {Integer[][]}
 def flood_fill(image, sr, sc, color)
-    
+  return image if image[sr][sc] == color
+
+  check_cell(image, sr, sc, image[sr][sc], color)
+  image
+end
+
+def check_cell(image, sr, sc, previous_color, color)
+  return if image.check(sr).check(sc) != previous_color
+  
+  image[sr][sc] = color
+  [[-1,0], [1,0], [0,-1], [0, 1]].each do |change_row, change_col| 
+    check_cell(image, sr + change_row, sc + change_col, previous_color, color)
+  end
+  
+end
+
+class Array
+  def check(index)
+    return [] unless index >= 0 && self[index]
+    self[index]
+  end
 end
