9aa4c8c6269aba81064c2d3472a71721428ae211
ozovalihasan <ozovalihasan@gmail.com>
2022-08-31 11:57:13 +0300

    Add a solution to the "Find Common Characters" challenge

--- 1002_Find_Common_Characters/solution.rb
+++ 1002_Find_Common_Characters/solution.rb
@@ -1,5 +1,16 @@
 # @param {String[]} words
 # @return {String[]}
 def common_chars(words)
-    
+  words.map! {|word| word.chars.tally}
+
+  result = {}
+  words.first.keys.each {|key| result[key] = []} 
+
+  words.each do |word| 
+    result.each do |key, value| 
+      word[key] ? result[key] << word[key] : result.delete(key) 
+    end
+  end
+
+  result.map {|key, value| [key] * value.min }.flatten
 end
