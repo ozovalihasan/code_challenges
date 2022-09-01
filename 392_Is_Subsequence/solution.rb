a16082607a1ba940e0e5d210434fbe3ef08519b8
ozovalihasan <ozovalihasan@gmail.com>
2022-09-01 14:01:47 +0300

    Add a solution to the "Is Subsequence" challenge

--- 392_Is_Subsequence/solution.rb
+++ 392_Is_Subsequence/solution.rb
@@ -2,5 +2,13 @@
 # @param {String} t
 # @return {Boolean}
 def is_subsequence(s, t)
-    
+  index_s = s.size - 1
+  index_t = t.size - 1
+  
+  until index_t.negative? || index_s.negative?
+    index_s -= 1 if t[index_t] == s[index_s]
+    index_t -= 1
+  end
+  
+  index_s.negative?
 end
