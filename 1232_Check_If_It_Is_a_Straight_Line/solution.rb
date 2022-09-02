6130cfc28398fe90602ea07153d4b5ad6737b3d7
ozovalihasan <ozovalihasan@gmail.com>
2022-09-02 11:13:40 +0300

    Add a solution to the "Check If It Is a Straight Line" challenge

--- 1232_Check_If_It_Is_a_Straight_Line/solution.rb
+++ 1232_Check_If_It_Is_a_Straight_Line/solution.rb
@@ -1,5 +1,17 @@
 # @param {Integer[][]} coordinates
 # @return {Boolean}
 def check_straight_line(coordinates)
-    
+  first = coordinates.first
+  slope = calculate_slope(first, coordinates.last)
+  
+  coordinates[1...-1].all? {|point| slope == calculate_slope(first, point) }
+end
+
+def calculate_slope(point1, point2)
+  x1, y1 = point1
+  x2, y2 = point2
+
+  return Float::INFINITY if x2 == x1
+  
+  (y2 - y1).to_f / ( x2-x1 ).to_f
 end
