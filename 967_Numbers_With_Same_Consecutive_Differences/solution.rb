82c76d131873d80490603a13256a99b53e3c23ad
ozovalihasan <ozovalihasan@gmail.com>
2022-09-03 14:15:17 +0300

    Add a solution to the "Numbers With Same Consecutive Differences" challenge

--- 967_Numbers_With_Same_Consecutive_Differences/solution.rb
+++ 967_Numbers_With_Same_Consecutive_Differences/solution.rb
@@ -2,5 +2,30 @@
 # @param {Integer} k
 # @return {Integer[]}
 def nums_same_consec_diff(n, k)
-    
+  result = []
+  1.upto(9) do |number|
+    update_digits([number], n-1, k, result)
+  end  
+  result
+end
+
+def update_digits(digits, digits_size, difference, result)
+  
+  if digits_size.zero?
+    result << digits.join.to_i
+    return
+  end
+  
+  digits_size -= 1
+  
+  if digits.last >= difference
+    update_digits(digits << (digits.last - difference), digits_size, difference, result)
+    digits.pop
+  end
+
+  if digits.last + difference < 10 && difference.nonzero?
+    update_digits(digits << (digits.last + difference), digits_size, difference, result)
+    digits.pop
+  end
+
 end
