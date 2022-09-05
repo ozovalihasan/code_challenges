a6890d87d79ea26968e28b7455bbba403817fee2
ozovalihasan <ozovalihasan@gmail.com>
2022-09-05 10:39:18 +0300

    Add a solution to the "Sum of Left Leaves" challenge

--- 404_Sum_of_Left_Leaves/solution.rb
+++ 404_Sum_of_Left_Leaves/solution.rb
@@ -9,6 +9,18 @@
 # end
 # @param {TreeNode} root
 # @return {Integer}
-def sum_of_left_leaves(root)
-  
+def sum_of_left_leaves(root, left_node = false)
+  result = [0]
+  check_nodes(root.left, true, result)
+  check_nodes(root.right, false, result)
+  result.first
+end
+
+def check_nodes(node, left_node, result)
+  return if node.nil? 
+
+  result[0] += node.val if left_node && node.left.nil? && node.right.nil? 
+    
+  check_nodes(node.left, true, result)
+  check_nodes(node.right, false, result)
 end
