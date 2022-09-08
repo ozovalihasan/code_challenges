ee15bc79b8bf543b7ac724062dc0212275457182
ozovalihasan <ozovalihasan@gmail.com>
2022-09-08 11:18:35 +0300

    Add a solution to the "Count the Number of Consistent Strings" challenge

--- 1684_Count_the_Number_of_Consistent_Strings/solution.rb
+++ 1684_Count_the_Number_of_Consistent_Strings/solution.rb
@@ -2,5 +2,10 @@
 # @param {String[]} words
 # @return {Integer}
 def count_consistent_strings(allowed, words)
+  hash = {}
 
+  allowed.each_char {|char| hash[char] = true}
+  
+  words.count {|word| word.each_char.all? {|char| hash[char]} }
+  
 end
