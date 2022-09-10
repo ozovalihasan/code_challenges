3db661ffbda79b352a0172986d881fe3d335090b
ozovalihasan <ozovalihasan@gmail.com>
2022-09-10 15:44:47 +0300

    Add a solution to the "Surface Area of 3D Shapes" challenge

--- 892_Surface_Area_of_3D_Shapes/solution.rb
+++ 892_Surface_Area_of_3D_Shapes/solution.rb
@@ -1,5 +1,27 @@
 # @param {Integer[][]} grid
 # @return {Integer}
 def surface_area(grid)
-    
+  result = 0
+
+  grid.each_with_index do |row, row_index|
+    row.each_with_index do |el, col_index|
+
+      unless el.zero?
+        result += 2
+      end
+      
+      result += (el - find_value(find_value(grid, row_index - 1), col_index)).abs 
+      result += (el - find_value(find_value(grid, row_index), col_index - 1)).abs
+
+    end
+
+  end
+  
+  result += grid.map(&:last).sum
+  result += grid.last.sum 
+end
+
+def find_value(arr_value, index)
+  return 0 if arr_value == 0 || index == -1
+  arr_value[index]      
 end
