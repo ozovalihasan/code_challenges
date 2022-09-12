8179f3b1f152b3d40415c33036a473dd0be4a4ec
ozovalihasan <ozovalihasan@gmail.com>
2022-09-12 12:07:28 +0300

    Add a solution to the "Maximum Population Year" challenge

--- 1854_Maximum_Population_Year/solution.rb
+++ 1854_Maximum_Population_Year/solution.rb
@@ -1,5 +1,13 @@
 # @param {Integer[][]} logs
 # @return {Integer}
 def maximum_population(logs)
-    
+  hash = {}
+  logs.each do |birth, _|
+    unless hash[birth] 
+      hash[birth] = logs.count {|first, second| birth.between?(first, second - 1)}
+    end
+  end
+  
+  max = hash.values.max
+  hash.filter_map {|key, value| key if value == max}.min
 end
