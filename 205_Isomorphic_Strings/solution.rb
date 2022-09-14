51f6032406190fc91279d9f4b09d6843359315f7
ozovalihasan <ozovalihasan@gmail.com>
2022-09-14 11:39:33 +0300

    Add a solution to the "Isomorphic Strings" challenge

--- 205_Isomorphic_Strings/solution.rb
+++ 205_Isomorphic_Strings/solution.rb
@@ -2,5 +2,9 @@
 # @param {String} t
 # @return {Boolean}
 def is_isomorphic(s, t)
-    
+  (hash_chars(s) - hash_chars(t)).empty?
+end
+
+def hash_chars(str)
+  str.each_char.with_index.with_object({}) {|(char, index), hash| (hash[char] ||= []) << index }.values
 end
