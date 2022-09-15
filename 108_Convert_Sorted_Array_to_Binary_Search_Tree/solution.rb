522060f1655f5947c7ccdc8f6d651b51fbc2a5af
ozovalihasan <ozovalihasan@gmail.com>
2022-09-15 19:39:00 +0300

    Add a solution to the "Convert Sorted Array to Binary Search Tree" challenge

--- 108_Convert_Sorted_Array_to_Binary_Search_Tree/solution.rb
+++ 108_Convert_Sorted_Array_to_Binary_Search_Tree/solution.rb
@@ -9,6 +9,14 @@
 # end
 # @param {Integer[]} nums
 # @return {TreeNode}
-def sorted_array_to_bst(nums)
-
+def sorted_array_to_bst(nums, min = -1, max = nums.size)
+  return nil if (min + 1 == max)
+  
+  middle = (min + max) / 2
+  TreeNode.new(
+    nums[middle],
+    sorted_array_to_bst(nums, min, middle),
+    sorted_array_to_bst(nums, middle, max)
+  )
+  
 end
