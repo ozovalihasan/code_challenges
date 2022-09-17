76721046730354aa0201c6daca87f7e08070792f
ozovalihasan <ozovalihasan@gmail.com>
2022-09-17 15:21:07 +0300

    Update the solution of the "Longest Increasing Path in a Matrix" challenge

--- 329_Longest_Increasing_Path_in_a_Matrix/solution.rb
+++ 329_Longest_Increasing_Path_in_a_Matrix/solution.rb
@@ -1,35 +1,38 @@
 # @param {Integer[][]} matrix
 # @return {Integer}
 def longest_increasing_path(matrix)
-  @hash = Array.new(matrix.size) {Array.new(matrix.first.size, nil)}
-
+  @matrix = matrix
   @size = matrix.size
   @row_size = matrix.first.size
+  @hash = Array.new(@size) {Array.new(@row_size, nil)}
   @steps = [[1,0], [-1, 0], [0, 1], [0, -1]]
-  matrix.each_with_index do |row, index1|
-    row.each_with_index do |_, index2|
-      check_neighbours(matrix, index1, index2)  
+  @max = 0
+  
+  matrix.each_with_index do |row, index_1|
+    row.each_with_index do |_, index_2|
+      check_neighbours(index_1, index_2)  
     end
   end
 
-  @hash.map(&:max).max
+  @max
 end
 
-def check_neighbours(matrix, index1, index2)
-  return @hash[index1][index2] if @hash[index1][index2]
+def check_neighbours(index_1, index_2)
+  return @hash[index_1][index_2] if @hash[index_1][index_2]
   
-  cell = matrix[index1][index2]
+  cell = @matrix[index_1][index_2]
   result = 1
   
   @steps.each do |step_1, step_2|
-    if in_range?(index1 + step_1, index2 + step_2) && matrix[index1 + step_1][index2 + step_2] > cell
-      result = [check_neighbours(matrix, index1 + step_1, index2 + step_2 ) + 1, result].max
+    if in_range?(index_1 + step_1, index_2 + step_2) && @matrix[index_1 + step_1][index_2 + step_2] > cell
+      result = [check_neighbours(index_1 + step_1, index_2 + step_2 ) + 1, result].max
     end
   end
 
-  @hash[index1][index2] = result
+  @max = [@max, result].max
+  @hash[index_1][index_2] = result
 end
 
-def in_range?(index1, index2)
-  index1 >= 0 && index1 < @size && index2 >= 0 && index2 < @row_size
+def in_range?(index_1, index_2)
+  index_1 >= 0 && index_1 < @size && index_2 >= 0 && index_2 < @row_size
 end
