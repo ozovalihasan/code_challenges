61a3e164539afbdd4f578911ac0b04e944a232b3
ozovalihasan <ozovalihasan@gmail.com>
2022-09-19 10:53:09 +0300

    Add a solution to the "Tallest Billboard" challenge

--- 956_Tallest_Billboard/solution.rb
+++ 956_Tallest_Billboard/solution.rb
@@ -1,5 +1,27 @@
+# Thanks to [yidong_w](https://leetcode.com/yidong_w) for [the solution](https://leetcode.com/problems/tallest-billboard/discuss/219700/Python-DP-clean-solution(1D))
+
 # @param {Integer[]} rods
 # @return {Integer}
 def tallest_billboard(rods)
+  dp = {}
+  dp.default = 0
+  dp[0] = 0
+
+  half_sum = rods.sum / 2
+
+  rods.each do |rod|
     
+    cur = {}
+    cur.default = 0
+
+    dp.keys.each do |s|
+      cur[s + rod] = [dp[s] + rod, cur[s + rod]].max unless (s + rod) > half_sum
+      cur[s] = [dp[s], cur[s]].max 
+      cur[s - rod] = [dp[s], cur[s - rod]].max unless (s - rod) < -half_sum
+    end
+    
+    dp = cur
+  end
+
+  dp[0]
 end
