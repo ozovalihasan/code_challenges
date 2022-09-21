826a197332a481d0c9bd8d501252b9f743de016d
ozovalihasan <ozovalihasan@gmail.com>
2022-09-21 18:35:42 +0300

    Add a solution to the "Long Pressed Name" challenge

--- 925_Long_Pressed_Name/solution.rb
+++ 925_Long_Pressed_Name/solution.rb
@@ -2,5 +2,14 @@
 # @param {String} typed
 # @return {Boolean}
 def is_long_pressed_name(name, typed)
-    
+  name = slice_consecutive_chars(name)
+  typed = slice_consecutive_chars(typed)
+
+  return false if name.size != typed.size
+
+  name.zip(typed).all? {|arr_name, arr_typed| arr_name[0] == arr_typed[0] && arr_name.size <= arr_typed.size }
+end
+
+def slice_consecutive_chars(str)
+  str.each_char.chunk(&:itself).map(&:last)
 end
