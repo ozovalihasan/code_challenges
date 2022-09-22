a2426ee3450e996cf5dbf2b087fca9e94058a6cd
ozovalihasan <ozovalihasan@gmail.com>
2022-09-22 11:36:53 +0300

    Add a solution to the "Happy Number" challenge

--- 202_Happy_Number/solution.rb
+++ 202_Happy_Number/solution.rb
@@ -1,5 +1,27 @@
 # @param {Integer} n
 # @return {Boolean}
 def is_happy(n)
-    
+  touched = {}
+  loop do
+    return true if n == 1 
+    return false if touched[n]
+    touched[n] = true
+    n = n.digits.sum {|digit| digit ** 2}
+  end
 end
+
+################
+# Solution 2 (Not recommended)
+################
+# # @param {Integer} n
+# # @return {Boolean}
+# def is_happy(n)
+#   known = {1=>true, 7=>true, 10=>true}
+ 
+#   loop do
+#     return true if known[n]
+#     return false if n < 10
+    
+#     n = n.digits.sum {|digit| digit ** 2}
+#   end
+# end
