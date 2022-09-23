aac790ea375ce04de36929d7cc6a416b8ff59c50
ozovalihasan <ozovalihasan@gmail.com>
2022-09-23 13:17:59 +0300

    Add a solution to the "Valid Mountain Array" challenge

--- 941_Valid_Mountain_Array/solution.rb
+++ 941_Valid_Mountain_Array/solution.rb
@@ -1,5 +1,18 @@
 # @param {Integer[]} arr
 # @return {Boolean}
 def valid_mountain_array(arr)
-    
+  return false unless arr[1] && arr.first < arr[1] && arr[-2] > arr.last
+
+  index = 0
+  arr << Float::INFINITY
+
+  loop do 
+    arr[index] < arr[index+1] ? index += 1 : break
+  end
+
+  loop do
+    arr[index] > arr[index+1] ? index += 1 : break
+  end
+
+  index == (arr.size - 2)
 end
