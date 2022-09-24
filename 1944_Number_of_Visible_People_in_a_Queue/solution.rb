61e8ee5abdaf9ced0e1cbcbc795b50fb69df02bf
ozovalihasan <ozovalihasan@gmail.com>
2022-09-24 18:40:27 +0300

    Add a solution to the "Number of Visible People in a Queue" challenge

--- 1944_Number_of_Visible_People_in_a_Queue/solution.rb
+++ 1944_Number_of_Visible_People_in_a_Queue/solution.rb
@@ -1,5 +1,21 @@
 # @param {Integer[]} heights
 # @return {Integer[]}
 def can_see_persons_count(heights)
+  possible_heights = [ heights.pop, Float::INFINITY ]
+  result = [0]
+
+  loop do
+    height = heights.pop
+    break unless height
     
+    index = possible_heights.bsearch_index {|pos_height| pos_height > height }
+    
+    result.unshift(index + ((index == possible_heights.size - 1) ? 0 : 1))
+    
+    possible_heights.slice!(0, index )
+    
+    possible_heights.unshift height        
+  end
+
+  result
 end
