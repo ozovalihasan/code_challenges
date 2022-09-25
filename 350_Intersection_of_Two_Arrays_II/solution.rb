d5eb1c43c6319d648a44dddaa12847390a8c83d1
ozovalihasan <ozovalihasan@gmail.com>
2022-09-25 13:08:10 +0300

    Add a solution to the "Intersection of Two Arrays II" challenge

--- 350_Intersection_of_Two_Arrays_II/solution.rb
+++ 350_Intersection_of_Two_Arrays_II/solution.rb
@@ -2,5 +2,12 @@
 # @param {Integer[]} nums2
 # @return {Integer[]}
 def intersect(nums1, nums2)
-
+  nums1 = nums1.group_by(&:itself) 
+  nums2 = nums2.group_by(&:itself) 
+  
+  result = []
+  
+  nums2.each { |num, value| result.concat( value.first( nums1[num].size ) ) if nums1[num] }
+  
+  result
 end
