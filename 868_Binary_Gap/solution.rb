e1748554cb3e9cd28a4bc92f69fc61d30c603e8f
ozovalihasan <ozovalihasan@gmail.com>
2022-09-26 10:57:48 +0300

    Add a solution to the "Binary Gap" challenge

--- 868_Binary_Gap/solution.rb
+++ 868_Binary_Gap/solution.rb
@@ -1,5 +1,16 @@
 # @param {Integer} n
 # @return {Integer}
 def binary_gap(n)
-    
+  n = n.to_s(2);
+  distance = 0;
+  last_index = 0;
+  
+  n.each_char.with_index do |char, index|
+    if char == "1" 
+      distance = index - last_index if (index - last_index) > distance 
+      last_index = index
+    end
+  end
+  
+  distance
 end
