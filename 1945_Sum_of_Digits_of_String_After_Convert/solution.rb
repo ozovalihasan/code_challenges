c40d60d3a95bd255e629e6863824d5a260c9582d
ozovalihasan <ozovalihasan@gmail.com>
2022-09-27 10:59:35 +0300

    Add a solution to the "Sum of Digits of String After Convert" challenge

--- 1945_Sum_of_Digits_of_String_After_Convert/solution.rb
+++ 1945_Sum_of_Digits_of_String_After_Convert/solution.rb
@@ -1,6 +1,20 @@
 # @param {String} s
 # @param {Integer} k
 # @return {Integer}
-def get_lucky(s, k)
-    
+def get_lucky(str, k)
+  s = transform_chars_to_nums(s)
+  
+  k.times do
+    s = sum_digits(s)
+  end
+  s
+end
+
+def transform_chars_to_nums(str)
+  hash = {}
+  str.chars.map {|char| hash[char] ||= char.ord - 96}.join.to_i
+end
+
+def sum_digits(num)
+  num.digits.sum
 end
