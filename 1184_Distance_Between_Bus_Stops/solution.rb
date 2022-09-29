cc35c2250e69d9bdf8380c50cf1f59c79667608c
ozovalihasan <ozovalihasan@gmail.com>
2022-09-29 10:52:55 +0300

    Add a solution to the "Distance Between Bus Stops" challenge

--- 1184_Distance_Between_Bus_Stops/solution.rb
+++ 1184_Distance_Between_Bus_Stops/solution.rb
@@ -3,5 +3,8 @@
 # @param {Integer} destination
 # @return {Integer}
 def distance_between_bus_stops(distance, start, destination)
-    
+  [
+    distance.slice!([start, destination].min, (destination - start).abs).sum, 
+    distance.sum
+  ].min
 end
