7d0ad126b68b521daf7e7355e46a109a62719a08
ozovalihasan <ozovalihasan@gmail.com>
2022-09-30 11:44:05 +0300

    Add a solution to the "Shortest Completing Word" challenge

--- 748_Shortest_Completing_Word/solution.rb
+++ 748_Shortest_Completing_Word/solution.rb
@@ -2,5 +2,8 @@
 # @param {String[]} words
 # @return {String}
 def shortest_completing_word(license_plate, words)
-    
+  letters = license_plate.downcase.scan(/[A-Za-z]/).tally
+
+  words.select {|word| letters.keys.all? {|letter| word.count(letter) >= letters[letter]}}
+       .min_by(&:length)
 end
