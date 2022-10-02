b0b57028e2def9214f6048d42c798340d4d67ed3
ozovalihasan <ozovalihasan@gmail.com>
2022-10-02 14:22:26 +0300

    Add a solution to the "Diagonal Traverse" challenge

--- 498_Diagonal_Traverse/solution.rb
+++ 498_Diagonal_Traverse/solution.rb
@@ -1,5 +1,23 @@
 # @param {Integer[][]} mat
 # @return {Integer[]}
 def find_diagonal_order(mat)
-    
+  result = []
+  
+  mat.each_with_index do |row, index|
+    row.each_with_index do |cell, index2|
+      result[index + index2] = [] unless result[index + index2]
+      result[index + index2] << cell
+    end
+  end
+
+  result2 = []
+  result.each_with_index do |arr, index| 
+    if index.even?
+      result2.concat arr.reverse
+    else
+      result2.concat arr
+    end
+  end
+  result2
+
 end
