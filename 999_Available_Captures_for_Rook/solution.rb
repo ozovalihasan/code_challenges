829d46de8b6cc55e6cb1a6317e044fc4b26a798e
ozovalihasan <ozovalihasan@gmail.com>
2022-10-05 11:46:13 +0300

    Update the solution of the "Available Captures for Rook" challenge

--- 999_Available_Captures_for_Rook/solution.rb
+++ 999_Available_Captures_for_Rook/solution.rb
@@ -1,25 +1,27 @@
 # @param {Character[][]} board
 # @return {Integer}
 def num_rook_captures(board)
-  rook = nil
+  rook_column = nil
+  rook_row = nil
   
   board.each_with_index do |row, index|
     row.each_with_index do |cell, index2|
       if cell == "R"
-        rook = [index, index2]
+        rook_row = index
+        rook_column = index2
         break
       end
     end
-    break if rook
+    break if rook_row
   end
 
   result = 0
 
-  row = board.map { |row| row[rook.last] }.join
+  row = board.map { |row| row[rook_column] }.join
   result += 1 if row.match?(/R\.*p/)
   result += 1 if row.match?(/p\.*R/)
 
-  column = board[rook.first].join 
+  column = board[rook_row].join 
   result += 1 if column.match?(/R\.*p/)
   result += 1 if column.match?(/p\.*R/)
 
