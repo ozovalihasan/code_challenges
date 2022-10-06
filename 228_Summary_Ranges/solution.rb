6140491c2abb10e28050e6ee67b7e2e68ca716b3
ozovalihasan <ozovalihasan@gmail.com>
2022-10-06 12:04:10 +0300

    Add a solution to the "Summary Ranges" challenge

--- 228_Summary_Ranges/solution.rb
+++ 228_Summary_Ranges/solution.rb
@@ -1,5 +1,36 @@
 # @param {Integer[]} nums
 # @return {String[]}
 def summary_ranges(nums)
-    
+  return [] if nums.empty?
+  last = nums.first
+  result = []
+
+  nums.each_cons(2) do |first, second| 
+    unless first.next.eql? second 
+      result << last.to_s
+      result.last << "->#{first}" unless first.eql? last
+      last = second
+    end
+  end
+
+  result << last.to_s
+  result.last << "->#{ nums.last }" unless nums.last.eql? last
+
+  result
 end
+
+
+
+
+######################
+## More clear, but slower solution
+######################
+# @param {Integer[]} nums
+# @return {String[]}
+# def summary_ranges(nums)
+#   nums.chunk_while {|first, second| (first + 1) == second}
+#       .map do |arr| 
+#         arr.first.to_s + (arr.size > 1 ? "->#{ arr.last }" : "" )
+#       end
+# end
+
