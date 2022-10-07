41908f1c06b23d6ed4f89594d84d2f490a4f735b
ozovalihasan <ozovalihasan@gmail.com>
2022-10-07 10:52:48 +0300

    Add a solution to the "Max Consecutive Ones" challenge

--- 485_Max_Consecutive_Ones/solution.rb
+++ 485_Max_Consecutive_Ones/solution.rb
@@ -1,5 +1,23 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def find_max_consecutive_ones(nums)
-    
+  nums.each_with_object([0]) {|num, result| num == 1 ? result[-1] += 1 : result << 0 }.max
 end
+
+#################
+## Solution 2
+#################
+## @param {Integer[]} nums
+## @return {Integer}
+#def find_max_consecutive_ones(nums)
+#  nums.slice_after(&:zero?).map {|arr| arr.count 1}.max
+#end
+
+#################
+## Solution 3
+#################
+## @param {Integer[]} nums
+## @return {Integer}
+#def find_max_consecutive_ones(nums)
+#  (nums.join.split("0").max_by(&:length) || "").length
+#end
