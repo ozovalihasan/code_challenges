a3442acabd6b7a74708bc895afb401f7d85320b1
ozovalihasan <ozovalihasan@gmail.com>
2022-10-11 11:16:49 +0300

    Add a solution to the "Shortest Distance to a Character" challenge

--- 821_Shortest_Distance_to_a_Character/solution.rb
+++ 821_Shortest_Distance_to_a_Character/solution.rb
@@ -3,4 +3,19 @@
 # @return {Integer[]}
 def shortest_to_char(s, c)
   
+  result = []
+  
+  previous_index = -Float::INFINITY
+  next_index = s.index c
+  
+  0.upto(s.length - 1) do |index|
+    result << [ index - previous_index, next_index - index ].min
+
+    if next_index == index
+      previous_index = next_index
+      next_index = s.index(c, previous_index + 1) || Float::INFINITY
+    end
+  end
+
+  result
 end
