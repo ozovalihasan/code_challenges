906a7acfa11a12f0cfc808216ba800d741fe3e91
ozovalihasan <ozovalihasan@gmail.com>
2022-10-13 10:52:45 +0300

    Add two solutions to the "Teemo Attacking" challenge

--- 495_Teemo_Attacking/solution.rb
+++ 495_Teemo_Attacking/solution.rb
@@ -2,5 +2,24 @@
 # @param {Integer} duration
 # @return {Integer}
 def find_poisoned_duration(time_series, duration)
-    
+
+  0.upto( time_series.size - 2 ).sum do |i|
+    [time_series[i + 1] - time_series[i], duration].min
+  end + duration
+
 end
+
+
+####################
+## 2. Solution
+####################
+# # @param {Integer[]} time_series
+# # @param {Integer} duration
+# # @return {Integer}
+# def find_poisoned_duration(time_series, duration)
+#   time_series << Float::INFINITY
+
+#   time_series.each_cons(2).sum do |first, second|
+#     [second - first, duration].min
+#   end
+# end
