0087d7a5fc6ae288c9bf3b6320819bbab109a64d
ozovalihasan <ozovalihasan@gmail.com>
2022-10-16 11:49:50 +0300

    Add a test for the "Maximum Swap" challenge

--- 670_Maximum_Swap/solution.rb
+++ 670_Maximum_Swap/solution.rb
@@ -1,5 +1,15 @@
 # @param {Integer} num
 # @return {Integer}
 def maximum_swap(num)
-    
+  num_digits = num.digits.reverse
+
+  return num_digits.join.to_i if num_digits.each_cons(2).all? {|first, second| first >= second }
+  
+  sorted_digits = num_digits.sort
+
+  index = num_digits.index { |digit| digit != sorted_digits.pop }
+  index_max_value = num_digits.rindex( num_digits[(index + 1)...].max )
+  num_digits[index], num_digits[index_max_value] = num_digits[index_max_value], num_digits[index]
+
+  num_digits.join.to_i
 end
