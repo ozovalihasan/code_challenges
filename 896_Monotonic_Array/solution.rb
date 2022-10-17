2b1a110cd29cf8f2e0ae8f73eb4c57a4a6e58bbd
ozovalihasan <ozovalihasan@gmail.com>
2022-10-17 10:44:14 +0300

    Add a solution to the "Monotonic Array" challenge

--- 896_Monotonic_Array/solution.rb
+++ 896_Monotonic_Array/solution.rb
@@ -1,5 +1,6 @@
 # @param {Integer[]} nums
 # @return {Boolean}
 def is_monotonic(nums)
-    
+  nums.each_cons(2).all? {|first, second| first >= second } ||
+    nums.each_cons(2).all? {|first, second| first <= second }
 end
