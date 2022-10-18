1a3ce45759b44ff18b006c75860bdb25482158e8
ozovalihasan <ozovalihasan@gmail.com>
2022-10-18 11:15:09 +0300

    Add a solution to the "Matrix Diagonal Sum" challenge

--- 1572_Matrix_Diagonal_Sum/solution.rb
+++ 1572_Matrix_Diagonal_Sum/solution.rb
@@ -1,5 +1,12 @@
 # @param {Integer[][]} mat
 # @return {Integer}
 def diagonal_sum(mat)
-    
+  sum = 0
+  
+  0.upto(mat.size - 1) do |index|
+    sum += mat[ index ][ index ] + mat[ -index-1 ][ index ]
+  end
+  
+  sum -= mat[ mat.size / 2 ][ mat.size / 2 ] if mat.size.odd?
+  sum
 end
