d1e72a7e3f8cdd77fd8a374f874e823522745dd3
ozovalihasan <ozovalihasan@gmail.com>
2022-10-19 21:03:54 +0300

    Add a solution to the "Repeated DNA Sequences" challenge

--- 187_Repeated_DNA_Sequences/solution.rb
+++ 187_Repeated_DNA_Sequences/solution.rb
@@ -1,5 +1,12 @@
 # @param {String} s
 # @return {String[]}
 def find_repeated_dna_sequences(s)
-    
+  hash = Hash.new(0)
+  
+  0.upto(s.size - 10) do |index|
+    hash[ s[index...index+10] ] += 1 
+  end
+
+  hash.keys.select {|key| hash[key] > 1}
+
 end
