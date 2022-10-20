88109709e5bc7bf8d66195c55a9517699ec3a4aa
ozovalihasan <ozovalihasan@gmail.com>
2022-10-20 15:10:37 +0300

    Add a solution to the "Minimum Absolute Difference" challenge

--- 1200_Minimum_Absolute_Difference/solution.rb
+++ 1200_Minimum_Absolute_Difference/solution.rb
@@ -1,5 +1,7 @@
+# Thanks to @k_duder(https://leetcode.com/k_duder/) to share own solution(https://leetcode.com/problems/minimum-absolute-difference/discuss/1636767/ruby-1-liner)
+
 # @param {Integer[]} arr
 # @return {Integer[][]}
 def minimum_abs_difference(arr)
-    
+  arr.sort.each_cons(2).group_by {|first, second| second - first }.min.pop
 end
