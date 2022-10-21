489152051b728b9569ee5dcbc2db70c9d1886fbc
ozovalihasan <ozovalihasan@gmail.com>
2022-10-21 10:53:14 +0300

    Add a solution to the "Unique Number of Occurrences" challenge

--- 1207_Unique_Number_of_Occurrences/solution.rb
+++ 1207_Unique_Number_of_Occurrences/solution.rb
@@ -1,5 +1,16 @@
 # @param {Integer[]} arr
 # @return {Boolean}
 def unique_occurrences(arr)
-    
+  arr.tally.values.tally.values.all? 1
+end
+
+###################
+## Solution 2
+###################
+# @param {Integer[]} arr
+# @return {Boolean}
+def unique_occurrences(arr)
+  hash2 = {}
+  arr.tally.values.each { |num2| hash2[num2] ? (return false) : hash2[num2] = true }
+  true
 end
