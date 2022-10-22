f3e19ba382b8362a449a64a6a18f5f12c7f0dc6f
ozovalihasan <ozovalihasan@gmail.com>
2022-10-22 18:22:36 +0300

    Add a solution to the "Shifting Letters II" challenge

--- 2381_Shifting_Letters_II/solution.rb
+++ 2381_Shifting_Letters_II/solution.rb
@@ -2,5 +2,17 @@
 # @param {Integer[][]} shifts
 # @return {String}
 def shifting_letters(s, shifts)
-    
+  arr = Array.new( s.length + 1, 0 )
+  
+  shifts.each do |start_index, end_index, step|
+    arr[ start_index ] += step * 2 - 1
+    arr[ end_index + 1] -= step * 2 - 1
+  end
+
+  change = 0
+  s.each_char.with_index do |char, index|
+    change += arr[index]
+    s[index] = ((char.ord - 97 + change) % 26 + 97).chr
+  end
+  
 end
