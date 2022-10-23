5a90a9faa8894e7bdc1f162e4ec93b1cec818190
ozovalihasan <ozovalihasan@gmail.com>
2022-10-23 20:47:31 +0300

    Add a solution to the "Count Largest Group" challenge

--- 1399_Count_Largest_Group/solution.rb
+++ 1399_Count_Largest_Group/solution.rb
@@ -1,5 +1,9 @@
 # @param {Integer} n
 # @return {Integer}
 def count_largest_group(n)
-    
+  hash = {}
+  1.upto(n) {|num| (hash[num.digits.sum] ||= []) << num}
+  
+  max_size = hash.values.map(&:size).max
+  hash.values.count {|arr| arr.size == max_size}
 end
