34687e5f56fd03415f8c8b6cb21d43d40630f89a
ozovalihasan <ozovalihasan@gmail.com>
2022-10-24 14:26:26 +0300

    Add a solution to the "Maximum Length of a Concatenated String with Unique Characters" challenge

--- 1239_Maximum_Length_of_a_Concatenated_String_with_Unique_Characters/solution.rb
+++ 1239_Maximum_Length_of_a_Concatenated_String_with_Unique_Characters/solution.rb
@@ -1,5 +1,33 @@
 # @param {String[]} arr
 # @return {Integer}
 def max_length(arr)
-    
+  max = 0
+
+  1.upto(arr.size) do |size|
+    stop = true
+
+    arr.combination(size).each do |comb|
+      comb_total_length = comb.sum(&:length)
+
+      next if comb_total_length <= max
+
+      if all_uniq?(comb.join.chars) 
+        stop = false
+        max = comb_total_length
+  
+        return 26 if max == 26
+  
+      end
+  
+    end
+  
+    break if stop
+  end
+
+  max
 end
+
+def all_uniq?(str)
+  str.all? {|char| str.count(char).eql? 1}
+end
+
