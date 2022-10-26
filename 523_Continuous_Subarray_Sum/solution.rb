3201a643c3b166eafa78f89bc875d600bc34672d
ozovalihasan <ozovalihasan@gmail.com>
2022-10-26 13:48:17 +0300

    Add a solution to the "Continuous Subarray Sum" challenge

--- 523_Continuous_Subarray_Sum/solution.rb
+++ 523_Continuous_Subarray_Sum/solution.rb
@@ -2,5 +2,18 @@
 # @param {Integer} k
 # @return {Boolean}
 def check_subarray_sum(nums, k)
-    
+  last = 0
+  hash = Hash.new(0)
+  hash[0] = 1
+
+  while num = nums.pop
+    prev, last = last, ((last + num) % k)
+    hash[last] += 1
+
+    next if hash[last] == 1
+
+    return true if hash[last] > 2 || prev != last
+  end
+
+  false
 end
