18ab28cdeff65859a153ae5e339128d9a166d459
ozovalihasan <ozovalihasan@gmail.com>
2022-10-29 16:14:59 +0300

    Add a solution to the "Earliest Possible Day of Full Bloom" challenge

--- 2136_Earliest_Possible_Day_of_Full_Bloom/solution.rb
+++ 2136_Earliest_Possible_Day_of_Full_Bloom/solution.rb
@@ -2,5 +2,14 @@
 # @param {Integer[]} grow_time
 # @return {Integer}
 def earliest_full_bloom(plant_time, grow_time)
-    
+  rest_day = 0
+  more_necessary_days = []
+  plant_time.zip(grow_time).sort_by(&:last).each do |plant_day, grow_day|
+    if  rest_day < grow_day
+      more_necessary_days << (grow_day - rest_day)
+    end
+    rest_day += plant_day
+  end
+
+  plant_time.sum + more_necessary_days.max
 end
