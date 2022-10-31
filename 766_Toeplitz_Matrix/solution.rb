5bf06cf719cdb080fcdfc60fd4f59ef0e124b3b5
ozovalihasan <ozovalihasan@gmail.com>
2022-10-31 13:54:04 +0300

    Add a solution to the "Toeplitz Matrix" challenge

--- 766_Toeplitz_Matrix/solution.rb
+++ 766_Toeplitz_Matrix/solution.rb
@@ -1,5 +1,9 @@
 # @param {Integer[][]} matrix
 # @return {Boolean}
 def is_toeplitz_matrix(matrix)
-    
+  matrix[1..].each_with_index do |row, index|
+    return false unless row[1..].eql? matrix[index][..-2]  
+  end  
+  
+  true
 end
