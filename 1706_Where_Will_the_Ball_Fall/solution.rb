e2056f38b8b1b9373d12fe9090b15404f082bc1c
ozovalihasan <ozovalihasan@gmail.com>
2022-11-01 13:23:18 +0300

    Add a solution to the "Where Will the Ball Fall" challenge

--- 1706_Where_Will_the_Ball_Fall/solution.rb
+++ 1706_Where_Will_the_Ball_Fall/solution.rb
@@ -1,5 +1,23 @@
 # @param {Integer[][]} grid
 # @return {Integer[]}
 def find_ball(grid)
-    
+
+  total_col = grid.first.size
+  
+  0.upto(total_col - 1).map do |index_col|
+
+    grid.each do |row|
+      if row[index_col] == 1 && row[index_col + 1] == 1
+        index_col += 1
+      elsif row[index_col] == -1 && row[index_col - 1] == -1 && index_col > 0
+        index_col -= 1
+      else
+        index_col = -1
+        break
+      end
+    end
+
+    index_col
+  end
+  
 end
