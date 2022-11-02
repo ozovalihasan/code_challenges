53358e2d45692faeafc2503ae092966faaf0d8cd
ozovalihasan <ozovalihasan@gmail.com>
2022-11-03 00:22:55 +0300

    Update the solution of the "Minimum Genetic Mutation" challenge

--- 433_Minimum_Genetic_Mutation/solution.rb
+++ 433_Minimum_Genetic_Mutation/solution.rb
@@ -4,43 +4,39 @@
 # @return {Integer}
 def min_mutation(start_gene, end_gene, bank)
   return -1 unless bank.include? end_gene    
-  @min_step = Float::INFINITY
+
   bank.map! do |gene|
-    transform_as_number(gene)
+    gene.chars
   end
 
-  @start_gene = transform_as_number(start_gene)
-  end_gene = transform_as_number(end_gene)
+  @start_gene = start_gene.chars
+  end_gene = end_gene.chars
+
   bank << @start_gene unless bank.include? @start_gene
-
-  hash = {}
   
-  bank.map do |gene|
-    hash[gene] = bank.select {|temp_gene| diff_count_between_genes(temp_gene, gene) == 1 }
-  end
-
-  check_genes(hash, end_gene, 0, [end_gene])
-
-  @min_step == Float::INFINITY ? -1 : @min_step
+  hash = bank.map do |gene|
+    [ gene, bank.select {|temp_gene| valid_mutation?(temp_gene, gene) } ]
+  end.to_h
+  
+  valid_steps = []
+  check_genes(hash, end_gene, 0, [end_gene], valid_steps)
+  valid_steps.min || -1 
   
 end
 
-def check_genes(hash, current_gene, step, used_genes)
-  if current_gene == @start_gene && step < @min_step
-    @min_step = step 
+def check_genes(hash, current_gene, step, used_genes, valid_steps)
+  if current_gene == @start_gene 
+    valid_steps << step 
     return
   end
   
-  hash[current_gene].each do |temp_gene|
-    next if used_genes.include?(temp_gene)
-    check_genes(hash, temp_gene, step + 1, used_genes + [current_gene])
+  (hash[current_gene] - used_genes).each do |temp_gene|
+    used_genes.push current_gene
+    check_genes(hash, temp_gene, step + 1, used_genes, valid_steps)
+    used_genes.pop
   end
 end
 
-def transform_as_number(gene)
-  gene.chars
-end
-
-def diff_count_between_genes(gene1, gene2)
-  gene1.zip(gene2).count {|pair| pair.last != pair.first }
+def valid_mutation?(gene1, gene2)
+  gene1.zip(gene2).count {|pair| pair.last != pair.first } == 1
 end
