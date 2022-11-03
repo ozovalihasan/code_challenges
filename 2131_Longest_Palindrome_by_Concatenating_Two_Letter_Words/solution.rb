33dc1198f3ef78cb281a2d5003b550e8692dadda
ozovalihasan <ozovalihasan@gmail.com>
2022-11-03 11:27:32 +0300

    Add a solution to the "Longest Palindrome by Concatenating Two Letter Words" challenge

--- 2131_Longest_Palindrome_by_Concatenating_Two_Letter_Words/solution.rb
+++ 2131_Longest_Palindrome_by_Concatenating_Two_Letter_Words/solution.rb
@@ -1,5 +1,19 @@
 # @param {String[]} words
 # @return {Integer}
 def longest_palindrome(words)
-    
+  hash = Hash.new(0)
+  total_length = 0
+
+  words.each  do |word| 
+    if hash[word] > 0 
+      total_length += 4
+      hash[word] -= 1
+    else
+      hash[word.reverse] += 1
+    end
+  end
+
+  total_length += 2 if hash.reject {|_, val| val.zero? }.keys.any? {|word| word[0] == word[1]}
+  
+  total_length
 end
