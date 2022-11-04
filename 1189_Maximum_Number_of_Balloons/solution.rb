a774bbdc43b9213c9341b5e2fab13275cc8aa62a
ozovalihasan <ozovalihasan@gmail.com>
2022-11-04 11:34:34 +0300

    Add a solution to the "Maximum Number of Balloons" challenge

--- 1189_Maximum_Number_of_Balloons/solution.rb
+++ 1189_Maximum_Number_of_Balloons/solution.rb
@@ -1,5 +1,7 @@
 # @param {String} text
 # @return {Integer}
 def max_number_of_balloons(text)
-    
+  {"b" => 1, "a" => 1, "l" => 2, "o" => 2, "n" => 1}.map do |char, val| 
+    text.count(char) / val 
+  end.min
 end
