0bdbd8077317e3ce67d59f329705847abb6e4805
ozovalihasan <ozovalihasan@gmail.com>
2022-11-06 11:44:35 +0300

    Add a solution to the "Orderly Queue" challenge

--- 899_Orderly_Queue/solution.rb
+++ 899_Orderly_Queue/solution.rb
@@ -2,5 +2,24 @@
 # @param {Integer} k
 # @return {String}
 def orderly_queue(s, k)
+  s_sorted = s.chars.sort
+
+  if k == 1
     
+    possible_words = []
+    index = -1
+    first_char = s_sorted.first
+    while index = s.index(first_char, index + 1)
+      possible_words << s[index..] + s[0...index]
+    end
+
+    result = possible_words.sort.first
+    
+  else
+
+    result = s_sorted.join
+
+  end 
+
+  result
 end
