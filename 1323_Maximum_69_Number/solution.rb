327b5841faec2f0650bd7f21e6e79be4d695b7c9
ozovalihasan <ozovalihasan@gmail.com>
2022-11-07 11:38:03 +0300

    Add a solution to the "Maximum 69 Number" challenge

--- 1323_Maximum_69_Number/solution.rb
+++ 1323_Maximum_69_Number/solution.rb
@@ -1,5 +1,10 @@
 # @param {Integer} num
 # @return {Integer}
 def maximum69_number(num)
-    
+  num = num.to_s
+  num.match(/6/) do |match| 
+    num[ match.end(0) - 1 ] = "9" 
+  end
+
+  num.to_i
 end
