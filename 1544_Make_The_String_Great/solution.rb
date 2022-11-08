eae1d6bdb905225028eda89617b7f43445398f01
ozovalihasan <ozovalihasan@gmail.com>
2022-11-08 12:16:31 +0300

    Add a solution to the "Make The String Great" challenge

--- 1544_Make_The_String_Great/solution.rb
+++ 1544_Make_The_String_Great/solution.rb
@@ -1,5 +1,17 @@
 # @param {String} s
 # @return {String}
 def make_good(s)
-    
+  index = 0
+  s = s.chars
+  
+  while s[index + 1]
+    if (s[index] != s[index + 1]) && s[index].downcase == s[index + 1].downcase
+      s.slice!(index, 2)
+      index -= 1 if index.positive?
+    else
+      index += 1
+    end
+  end
+  
+  s.join
 end
