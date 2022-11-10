3576d47ec5073a7c324a821416ee52b90c066d96
ozovalihasan <ozovalihasan@gmail.com>
2022-11-10 11:17:43 +0300

    Add a solution to the "Remove All Adjacent Duplicates In String" challenge

--- 1047_Remove_All_Adjacent_Duplicates_In_String/solution.rb
+++ 1047_Remove_All_Adjacent_Duplicates_In_String/solution.rb
@@ -1,5 +1,11 @@
 # @param {String} s
 # @return {String}
 def remove_duplicates(s)
-    
+  stack = []
+  
+  s.each_char do |char|
+    char == stack.last ? stack.pop : stack << char
+  end  
+  
+  stack.join
 end
