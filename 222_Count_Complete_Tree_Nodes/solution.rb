2258e223162417cdee4730446b4bb2b238358385
ozovalihasan <ozovalihasan@gmail.com>
2022-11-15 10:15:10 +0300

    Add a solution to the "Count Complete Tree Nodes" challenge

--- 222_Count_Complete_Tree_Nodes/solution.rb
+++ 222_Count_Complete_Tree_Nodes/solution.rb
@@ -10,5 +10,15 @@
 # @param {TreeNode} root
 # @return {Integer}
 def count_nodes(root)
-    
+  @count = 0
+  check_node(root)
+  @count
+end
+
+def check_node(node)
+  return if node.nil?
+
+  @count += 1
+  check_node(node.left)
+  check_node(node.right)
 end
