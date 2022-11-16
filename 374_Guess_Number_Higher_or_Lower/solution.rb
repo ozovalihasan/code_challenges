3c4d03d53d1eb51777c438d36eb7e4383c64376d
ozovalihasan <ozovalihasan@gmail.com>
2022-11-16 10:36:57 +0300

    Add two solutions to the "Guess Number Higher or Lower" challenge

--- 374_Guess_Number_Higher_or_Lower/solution.rb
+++ 374_Guess_Number_Higher_or_Lower/solution.rb
@@ -6,8 +6,33 @@
 # def guess(num)
 
 def guessNumber(n)
+  right = n
+  left = 0
+  selected_num = (right + left) / 2
+
+  loop do
+    result = guess(selected_num)
+    if  result == 1
+      left = selected_num
+      selected_num = (right + left) / 2 + 1
+    elsif result.zero?
+      return selected_num
+    else
+      right = selected_num
+      selected_num = (right + left) / 2 
+    end
     
+  end
+
+  selected_num
 end
+
+###############
+## 2. solution(simpler one)
+###############
+# def guessNumber(n)
+# (1..n).bsearch {|num| guess(num)}
+# end
 
 def guess_check_number(n, picked)
   @picked = picked
