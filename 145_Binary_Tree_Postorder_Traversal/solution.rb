9c096aab8db46dd4f8f93097e8e954627c201fa8
ozovalihasan <ozovalihasan@gmail.com>
2022-11-17 10:30:26 +0300

    Add a solution to the "Binary Tree Postorder Traversal" challenge

--- 145_Binary_Tree_Postorder_Traversal/solution.rb
+++ 145_Binary_Tree_Postorder_Traversal/solution.rb
@@ -10,5 +10,14 @@
 # @param {TreeNode} root
 # @return {Integer[]}
 def postorder_traversal(root)
-    
+  @result = []
+  check_node(root)
+  @result
+end
+
+def check_node(node)
+  return if node.nil? 
+  check_node(node.left)
+  check_node(node.right)
+  @result << node.val
 end
