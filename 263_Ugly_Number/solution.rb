32eb9c5a5c022c7addee4ec67c35109e3ee78934
ozovalihasan <ozovalihasan@gmail.com>
2022-11-18 10:23:43 +0300

    Add a solution to the "Ugly Number" challenge

--- 263_Ugly_Number/solution.rb
+++ 263_Ugly_Number/solution.rb
@@ -1,5 +1,14 @@
 # @param {Integer} n
 # @return {Boolean}
+
 def is_ugly(n)
-    
+  return false if n.zero?
+
+  [2, 3, 5].each do |num|
+    while (n % num).zero?    
+      n /= num
+    end  
+  end
+  
+  n == 1
 end
