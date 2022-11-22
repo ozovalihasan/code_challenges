9b04529d9e15d0191f29690bcaf02ece32920a28
ozovalihasan <ozovalihasan@gmail.com>
2022-11-22 11:17:28 +0300

    Add a solution to the "Perfect Squares" challenge

--- 279_Perfect_Squares/solution.rb
+++ 279_Perfect_Squares/solution.rb
@@ -1,5 +1,19 @@
 # @param {Integer} n
 # @return {Integer}
 def num_squares(n)
-    
+  @found = {0 => 0}
+  check_num(n)
+end
+
+def check_num(num)
+  return @found[num] if @found[num]
+
+  max = Math.sqrt(num).floor()
+  result = []
+  
+  max.downto(1) do |counter|
+    result << ((num  / ( counter ** 2 )) + check_num( num % (counter ** 2) ))
+  end
+
+  @found[num] = result.min
 end
