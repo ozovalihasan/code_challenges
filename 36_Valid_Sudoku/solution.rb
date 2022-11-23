70ec8fafbb7f2d1204a6f48ae859e0c709727f88
ozovalihasan <ozovalihasan@gmail.com>
2022-11-23 12:26:00 +0300

    Update the solution of the "Valid Sudoku" challenge

--- 36_Valid_Sudoku/solution.rb
+++ 36_Valid_Sudoku/solution.rb
@@ -1,25 +1,23 @@
 # @param {Character[][]} board
 # @return {Boolean}
 def is_valid_sudoku(board)
-  return false unless check_rows(board.transpose)
-  return false unless check_rows(board)
+  valid_rows?(board) && valid_rows?(board.transpose) && valid_3_by_3?(board)
+end
 
-  grouped_board = [[{}, {}, {}], [{}, {}, {}], [{}, {}, {}]]
-  board.each_with_index do |row, index_row|
-    row.each_with_index do |number, index_number|
-      next if number == '.'
-      return false if grouped_board[index_row / 3][index_number / 3][number]
+def valid_rows?(board)
+  board.none? {|row| repeated_cell?(row) }
+end
 
-      grouped_board[index_row / 3][index_number / 3][number] = true
+def valid_3_by_3?(board)
+  board.each_slice(3).each do |rows|
+    0.upto(2) do |index|
+      return false if repeated_cell?( rows[0].pop(3).concat( rows[1].pop(3) ).concat( rows[2].pop(3) ) )
     end
   end
-
+  
   true
 end
 
-def check_rows(board)
-  board.each do |row|
-    return false unless (row.uniq - ['.']) == (row - ['.'])
-  end
-  true
+def repeated_cell?(arr)
+  arr.group_by(&:itself).tap{|groups| groups.delete(".")}.any? {|key, cells| cells.size > 1 }
 end
