370951efa5fa713ac297b33c6fc3facdc2e56a60
ozovalihasan <ozovalihasan@gmail.com>
2022-11-25 13:52:41 +0300

    Add a solution to the "Sum of Subarray Minimums" challenge

--- 907_Sum_of_Subarray_Minimums/solution.rb
+++ 907_Sum_of_Subarray_Minimums/solution.rb
@@ -1,5 +1,23 @@
+MOD = 10**9 + 7
+
 # @param {Integer[]} arr
 # @return {Integer}
 def sum_subarray_mins(arr)
-    
+  used = [-1, arr.size]
+  
+  arr.map!.with_index do |num, index|
+    [num, index]
+  end
+
+  result = 0
+  arr.sort_by(&:first).each do |num, index|
+    used_index = used.bsearch_index {|used_index| used_index > index }
+
+    result += num * (used[used_index] - index - 1) * (index - used[used_index - 1] - 1) 
+    result += num * (used[used_index] - used[used_index - 1] - 1 )
+
+    used.insert(used_index, index)
+  end
+
+  result % MOD
 end
