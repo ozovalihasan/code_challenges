4ef21ed15ce09f6700e0a51eacadca021a4b3c64
ozovalihasan <ozovalihasan@gmail.com>
2022-11-26 13:02:04 +0300

    Add a solution to the "Maximum Profit in Job Scheduling" challenge

--- 1235_Maximum_Profit_in_Job_Scheduling/solution.rb
+++ 1235_Maximum_Profit_in_Job_Scheduling/solution.rb
@@ -3,5 +3,14 @@
 # @param {Integer[]} profit
 # @return {Integer}
 def job_scheduling(start_time, end_time, profit)
-    
+  jobs = start_time.zip(end_time, profit).sort_by(&:first).reverse
+  previous_total_profits = [[0, 0]]
+
+  while (time_now, end_time, profit = jobs.pop)
+    max_profit_until_now = previous_total_profits.select { |end_time, _| end_time <= time_now }.max_by(&:last).last
+    previous_total_profits.select! {|end_time, _| end_time > time_now}
+    previous_total_profits << [ time_now, max_profit_until_now ] << [ end_time, max_profit_until_now + profit ]
+  end
+
+  previous_total_profits.max_by(&:last).last
 end
