6573646653131d8c9394a160be3f0ebe663fed71
ozovalihasan <ozovalihasan@gmail.com>
2022-11-29 10:33:53 +0300

    Add a solution to the "Find Players With Zero or One Losses" challenge

--- 2225_Find_Players_With_Zero_or_One_Losses/solution.rb
+++ 2225_Find_Players_With_Zero_or_One_Losses/solution.rb
@@ -1,5 +1,10 @@
 # @param {Integer[][]} matches
 # @return {Integer[][]}
 def find_winners(matches)
-    
+  loser_list = matches.map(&:last).tally
+  winners_array = matches.map(&:first)
+  
+  winners_without_lost = ( winners_array - loser_list.keys ).uniq.sort
+  lost_only_one_match = loser_list.filter_map {|loser, total_lost| loser if total_lost.eql? 1}.sort
+  [ winners_without_lost, lost_only_one_match ]
 end
