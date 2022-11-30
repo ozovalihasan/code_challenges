9210b393bdbc62c063908465b2057e0dec769da9
ozovalihasan <ozovalihasan@gmail.com>
2022-11-30 10:37:01 +0300

    Add a solution to the "Number of Students Doing Homework at a Given Time" challenge

--- 1450_Number_of_Students_Doing_Homework_at_a_Given_Time/solution.rb
+++ 1450_Number_of_Students_Doing_Homework_at_a_Given_Time/solution.rb
@@ -3,5 +3,7 @@
 # @param {Integer} query_time
 # @return {Integer}
 def busy_student(start_time, end_time, query_time)
-    
+  start_time.each_with_index.count do |starting, index|
+    query_time.between? starting, end_time[index]
+  end
 end
