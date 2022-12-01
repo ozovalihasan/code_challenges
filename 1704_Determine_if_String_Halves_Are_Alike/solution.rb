b521e52970065cff69b2916d990bdf8a02f19c1a
ozovalihasan <ozovalihasan@gmail.com>
2022-12-01 11:07:37 +0300

    Add a solution to the "Determine if String Halves Are Alike" challenge

--- 1704_Determine_if_String_Halves_Are_Alike/solution.rb
+++ 1704_Determine_if_String_Halves_Are_Alike/solution.rb
@@ -1,5 +1,10 @@
 # @param {String} s
 # @return {Boolean}
 def halves_are_alike(s)
-    
+  count_vowels( s.slice!(0, s.size / 2) ) == count_vowels( s )
+end
+
+VOWELS = /[aeiou]/i
+def count_vowels(str)
+  str.scan(VOWELS).size 
 end
