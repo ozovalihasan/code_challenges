3b77daf80517630f6460307ed9c654880a00cb5d
ozovalihasan <ozovalihasan@gmail.com>
2022-12-02 11:45:38 +0300

    Add a solution to the "Determine if Two Strings Are Close" challenge

--- 1657_Determine_if_Two_Strings_Are_Close/solution.rb
+++ 1657_Determine_if_Two_Strings_Are_Close/solution.rb
@@ -2,5 +2,9 @@
 # @param {String} word2
 # @return {Boolean}
 def close_strings(word1, word2)
-    
+  word1, word2 = word1.chars, word2.chars
+
+  return false unless word1.uniq.sort.eql? word2.uniq.sort 
+  
+  word1.tally.values.sort.eql? word2.tally.values.sort
 end
