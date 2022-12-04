7c10ef9494206680b4008a0e228f17bdb4776ac7
ozovalihasan <ozovalihasan@gmail.com>
2022-12-04 17:46:02 +0300

    Add a solution to the "Minimum Average Difference" challenge

--- 2256_Minimum_Average_Difference/solution.rb
+++ 2256_Minimum_Average_Difference/solution.rb
@@ -1,5 +1,14 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def minimum_average_difference(nums)
-    
+  sum1 = 0
+  sum2 = nums.sum
+
+  diff = 1.upto(nums.size - 1).map do |index|
+    sum1 += nums[index - 1]
+    sum2 -= nums[index - 1]
+    ((sum1 / index) - (sum2 / (nums.size - index))).abs
+  end << (nums.sum / nums.size).abs
+  
+  diff.index(diff.min)
 end
