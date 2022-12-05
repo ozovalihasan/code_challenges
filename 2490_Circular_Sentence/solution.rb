e8856878998fd4365f4dc3f5a85f055b24336dc2
ozovalihasan <ozovalihasan@gmail.com>
2022-12-05 11:35:18 +0300

    Add a solution to the "Circular Sentence" challenge

--- 2490_Circular_Sentence/solution.rb
+++ 2490_Circular_Sentence/solution.rb
@@ -1,5 +1,11 @@
 # @param {String} sentence
 # @return {Boolean}
 def is_circular_sentence(sentence)
-    
+  sentence = sentence << " " << sentence[0]
+
+  sentence.each_char.with_index do |char, index|
+    return false if char == " " && sentence[index - 1] != sentence[index + 1]
+  end
+  
+  true
 end
