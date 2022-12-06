829f3aa5a1397b3cd187f75ce27fba8f797c6a44
ozovalihasan <ozovalihasan@gmail.com>
2022-12-06 11:33:49 +0300

    Add a solution to the "Bulls and Cows" challenge

--- 299_Bulls_and_Cows/solution.rb
+++ 299_Bulls_and_Cows/solution.rb
@@ -2,5 +2,13 @@
 # @param {String} guess
 # @return {String}
 def get_hint(secret, guess)
-    
+  count_all = secret.chars.tally.each.sum do |char, count|
+    [guess.count(char), count].min 
+  end
+
+  count_A = secret.chars.each_with_index.count do |char,index|
+    char == guess[index]
+  end
+  
+  count_A.to_s << "A" << (count_all - count_A).to_s << "B"
 end
