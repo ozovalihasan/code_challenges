44c6db2ee4f0f68af35e8af4f31c0099747ab5b3
ozovalihasan <ozovalihasan@gmail.com>
2022-12-07 11:26:40 +0300

    Add a solution to the "Range Sum of BST" challenge

--- 938_Range_Sum_of_BST/solution.rb
+++ 938_Range_Sum_of_BST/solution.rb
@@ -11,6 +11,34 @@
 # @param {Integer} low
 # @param {Integer} high
 # @return {Integer}
+
+##############
+## Solution 1
+##############
 def range_sum_bst(root, low, high)
-    
+  return 0 if root.nil?
+  result = root.val.between?(low, high) ? root.val : 0
+  result += range_sum_bst(root.right, low, high) 
+  result += range_sum_bst(root.left, low, high)
 end
+
+##############
+## Solution 2
+##############
+# def range_sum_bst(root, low, high)
+#   return 0 if root.nil?
+#   stack = [root]
+  
+#   result = 0
+#   while stack.any?
+#     node = stack.pop
+    
+#     next if node.nil?
+    
+#     result += node.val if node.val.between? low, high
+#     stack << node.right
+#     stack << node.left
+#   end
+
+#   result
+# end
