2700b7f431a7ca1ef9bd877d684d92c6a3180250
ozovalihasan <ozovalihasan@gmail.com>
2022-12-08 10:45:18 +0300

    Add a solution to the "Leaf-Similar Trees" challenge

--- 872_Leaf-Similar_Trees/solution.rb
+++ 872_Leaf-Similar_Trees/solution.rb
@@ -10,6 +10,44 @@
 # @param {TreeNode} root1
 # @param {TreeNode} root2
 # @return {Boolean}
+################
+## Solution 1
+################
 def leaf_similar(root1, root2)
-    
+  leaves(root1) == leaves(root2)
 end
+
+def leaves(root)
+  stack = [root]
+  result = []
+
+  while node = stack.pop
+    result << node.val if node.right.nil? && node.left.nil?
+    stack << node.right if node.right
+    stack << node.left if node.left
+  end
+  
+  result
+end
+
+################
+## Solution 2
+################
+# def leaf_similar(root1, root2)
+#   leaves(root1) == leaves(root2)
+# end
+
+# def leaves(root)
+#   result = []
+#   check_node(root, result)
+#   result
+# end
+
+# def check_node(node, result)
+#   return if node.nil?
+  
+#   result << node.val if node.right.nil? && node.left.nil?
+  
+#   check_node(node.right, result)
+#   check_node(node.left, result)
+# end
