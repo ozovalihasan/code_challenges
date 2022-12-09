a8d378df118b33ae71152aa929cf70375ee2650f
ozovalihasan <ozovalihasan@gmail.com>
2022-12-09 10:39:10 +0300

    Add a solution to the "Maximum Difference Between Node and Ancestor" challenge

--- 1026_Maximum_Difference_Between_Node_and_Ancestor/solution.rb
+++ 1026_Maximum_Difference_Between_Node_and_Ancestor/solution.rb
@@ -10,5 +10,18 @@
 # @param {TreeNode} root
 # @return {Integer}
 def max_ancestor_diff(root)
-    
+  @result = []
+  check_node(root, root.val, root.val)
+  @result.max
+end
+
+def check_node(node, min, max)
+  return if node.nil?
+  
+  @result << (node.val - min)
+  @result << (max - node.val)
+  
+  min, max = [min, node.val, max].minmax unless node.val.between? min, max
+  check_node(node.left, min, max)
+  check_node(node.right, min, max)
 end
