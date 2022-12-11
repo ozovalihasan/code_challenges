5120843cf22af98742c103e6df4e27b8542c0865
ozovalihasan <ozovalihasan@gmail.com>
2022-12-11 11:41:12 +0300

    Add a solution to the "Binary Tree Maximum Path Sum" challenge

--- 124_Binary_Tree_Maximum_Path_Sum/solution.rb
+++ 124_Binary_Tree_Maximum_Path_Sum/solution.rb
@@ -10,5 +10,18 @@
 # @param {TreeNode} root
 # @return {Integer}
 def max_path_sum(root)
-    
+  @possible_path_sums = []
+  check_node(root)
+  @possible_path_sums.max
+end
+
+def check_node(node)
+  return 0 if node.nil?
+
+  left = [check_node(node.left), 0].max
+  right = [check_node(node.right), 0].max
+
+  @possible_path_sums << (left + right + node.val)
+
+  [left, right].max + node.val
 end
