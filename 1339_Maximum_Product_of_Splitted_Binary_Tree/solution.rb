c1db7312e51311f45eb80eab0a6dc42973ab7cda
ozovalihasan <ozovalihasan@gmail.com>
2022-12-11 11:18:56 +0300

    Add a solution to the "Maximum Product of Splitted Binary Tree" challenge

--- 1339_Maximum_Product_of_Splitted_Binary_Tree/solution.rb
+++ 1339_Maximum_Product_of_Splitted_Binary_Tree/solution.rb
@@ -10,5 +10,16 @@
 # @param {TreeNode} root
 # @return {Integer}
 def max_product(root)
-    
+  @values = []
+  check_node(root)
+  sum_of_all_values = @values.last
+  max_product = @values.map { |sum_of_one_tree| (sum_of_all_values - sum_of_one_tree) * sum_of_one_tree }.max 
+  max_product % (10**9 + 7)
+end
+
+def check_node(node)
+  return 0 if node.nil?
+
+  @values << (check_node(node.left) + check_node(node.right) + node.val)
+  @values.last
 end
