79d4a99b70a325407fc3518ea389396206eb1cd2
ozovalihasan <ozovalihasan@gmail.com>
2022-12-13 11:19:37 +0300

    Add a solution to the "Minimum Falling Path Sum" challenge

--- 931_Minimum_Falling_Path_Sum/solution.rb
+++ 931_Minimum_Falling_Path_Sum/solution.rb
@@ -1,5 +1,18 @@
 # @param {Integer[][]} matrix
 # @return {Integer}
 def min_falling_path_sum(matrix)
+  current = matrix.first
+  
+  1.upto(matrix.size - 1).each do |row_index|
+    previous = current
+    previous.unshift Float::INFINITY
+    current = []
     
+    matrix[row_index].each_with_index do |cell, index_col|
+      current[index_col] = cell + previous.slice(index_col, 3).min
+    end
+    
+  end
+
+  current.min
 end
