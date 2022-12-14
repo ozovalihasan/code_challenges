bcd29d4c8efcf4b15d1400db1c4ed1f442ea8a07
ozovalihasan <ozovalihasan@gmail.com>
2022-12-14 10:07:07 +0300

    Add a solution to the "House Robber" challenge

--- 198_House_Robber/solution.rb
+++ 198_House_Robber/solution.rb
@@ -1,5 +1,15 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def rob(nums)
-    
+  skipped_house = 0
+  waited_house = 0
+  entered_house = 0
+  
+  nums.each do |house_val|
+    skipped_house = [skipped_house, waited_house].max
+    waited_house = entered_house
+    entered_house = skipped_house + house_val
+  end
+
+  [skipped_house, waited_house, entered_house].max
 end
