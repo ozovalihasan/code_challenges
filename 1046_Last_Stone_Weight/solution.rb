5eda801b1ca58112231383dc7d7017f4eb507c56
ozovalihasan <ozovalihasan@gmail.com>
2022-12-14 10:24:45 +0300

    Add a solution to the "Last Stone Weight" challenge

--- 1046_Last_Stone_Weight/solution.rb
+++ 1046_Last_Stone_Weight/solution.rb
@@ -1,5 +1,12 @@
 # @param {Integer[]} stones
 # @return {Integer}
 def last_stone_weight(stones)
-    
+  stones.sort!
+
+  until stones.size == 1
+    result = stones.pop - stones.pop
+    stones.insert(stones.bsearch_index {|val| val >= result} || stones.size, result)
+  end
+  
+  stones.last
 end
