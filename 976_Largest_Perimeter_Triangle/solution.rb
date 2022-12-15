c0f27c217e8df33c9494ee02c354082ef1f2196f
ozovalihasan <ozovalihasan@gmail.com>
2022-12-15 21:54:36 +0300

    Add two solutions to the "Largest Perimeter Triangle" challenge

--- 976_Largest_Perimeter_Triangle/solution.rb
+++ 976_Largest_Perimeter_Triangle/solution.rb
@@ -1,5 +1,31 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def largest_perimeter(nums)
+  nums.sort.reverse.each_cons(3) do |longest, medium, shortest|
+    return longest + medium  + shortest if (shortest + medium) > longest
+  end
 
+  0
 end
+
+
+##############
+## Solution 2
+##############
+# # @param {Integer[]} nums
+# # @return {Integer}
+# def largest_perimeter(nums)
+  
+#   nums.sort!
+#   sum = nums.last(2).sum
+#   last = 0
+
+#   while nums[2]
+#     sum += nums[-3] - last
+#     last = nums.pop
+
+#     return sum if (sum - last) > last
+#   end
+
+#   0
+# end
