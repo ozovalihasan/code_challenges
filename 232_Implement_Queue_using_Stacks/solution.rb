a7d5bd3c842d114025e7268afe919a2d87b2c1fa
ozovalihasan <ozovalihasan@gmail.com>
2022-12-16 11:40:33 +0300

    Update the solution of the "Implement Queue using Stacks" challenge

--- 232_Implement_Queue_using_Stacks/solution.rb
+++ 232_Implement_Queue_using_Stacks/solution.rb
@@ -1,6 +1,7 @@
 class MyQueue
   def initialize()
-    @arr = []
+    @in = []
+    @out = []
   end
 
 
@@ -9,7 +10,7 @@ class MyQueue
   :rtype: Void
 =end
   def push(x)
-    @arr << x
+    @in << x
   end
 
 
@@ -17,7 +18,8 @@ class MyQueue
   :rtype: Integer
 =end
   def pop()
-    @arr.shift
+    transform_items
+    @out.pop
   end
 
 
@@ -25,7 +27,8 @@ class MyQueue
   :rtype: Integer
 =end
   def peek()
-    @arr.first
+    transform_items
+    @out.last
   end
 
 
@@ -33,10 +36,17 @@ class MyQueue
   :rtype: Boolean
 =end
   def empty()
-    @arr.empty?
+    @in.empty? && @out.empty?
   end
 
+  private
 
+  def transform_items
+    if @out.empty? 
+      @out << (@in.pop) until @in.empty?
+    end
+  end
+  
 end
 
 # Your MyQueue object will be instantiated and called as such:
