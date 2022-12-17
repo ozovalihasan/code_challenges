b4256b9084aaa60f79d1be01e3137b4c1470ce68
ozovalihasan <ozovalihasan@gmail.com>
2022-12-17 12:34:13 +0300

    Add a solution to the "Evaluate Reverse Polish Notation" challenge

--- 150_Evaluate_Reverse_Polish_Notation/solution.rb
+++ 150_Evaluate_Reverse_Polish_Notation/solution.rb
@@ -1,5 +1,27 @@
 # @param {String[]} tokens
 # @return {Integer}
 def eval_rpn(tokens)
+  stack = []
+
+  while item = tokens.shift
+    unless ["+", "-", "*", "/"].include? item
+      stack << item.to_i
+      next 
+    end
+      
+    num1, num2 = stack.pop(2)  
+
+    if item == "/"
+      stack << (num1.to_f / num2.to_f).to_i 
+    elsif item == "+"
+      stack << (num1 + num2)
+    elsif item == "-"
+      stack << (num1 - num2)
+    elsif item == "*"
+      stack << (num1 * num2)
+    end
     
+  end
+
+  stack.last
 end
