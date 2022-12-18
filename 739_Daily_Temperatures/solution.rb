a249ed626ee2809b79e7f2bac9f3bff0f6593a82
ozovalihasan <ozovalihasan@gmail.com>
2022-12-18 21:18:40 +0300

    Add a solution to the "Daily Temperatures" challenge

--- 739_Daily_Temperatures/solution.rb
+++ 739_Daily_Temperatures/solution.rb
@@ -1,5 +1,16 @@
 # @param {Integer[]} temperatures
 # @return {Integer[]}
 def daily_temperatures(temperatures)
+  future_temperatures = []
+  result = Array.new(temperatures.size, 0)
 
+  temperatures.reverse_each.with_index do |temperature, index|
+    until future_temperatures.empty? || future_temperatures.last.first > temperature 
+      future_temperatures.pop
+    end
+    result[index] = (index - future_temperatures.last.last) if future_temperatures.any?
+    future_temperatures << [temperature, index]
+  end
+  
+  result.reverse
 end
