ec899579607365069877e71cd7dc179e8718425e
ozovalihasan <ozovalihasan@gmail.com>
2022-12-19 11:30:09 +0300

    Add a solution to the "Find if Path Exists in Graph" challenge

--- 1971_Find_if_Path_Exists_in_Graph/solution.rb
+++ 1971_Find_if_Path_Exists_in_Graph/solution.rb
@@ -4,5 +4,22 @@
 # @param {Integer} destination
 # @return {Boolean}
 def valid_path(n, edges, source, destination)
-    
+  return true if source == destination || 
+                 edges.include?([source, destination]) || 
+                 edges.include?([destination, source])
+  
+  graph = {}
+  until edges.empty?
+    num1, num2 = edges.pop
+    ( graph[num1] ||= [] ) << num2
+    ( graph[num2] ||= [] ) << num1
+  end
+
+  current = [source]
+  until current.empty?
+    current = current.map { |num| graph.delete(num) }.compact.flatten
+    return true if current.include? destination
+  end
+  
+  false
 end
