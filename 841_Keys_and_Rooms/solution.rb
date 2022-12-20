ea745679a168cec6a2a630d2e9b5323bf65cae6c
ozovalihasan <ozovalihasan@gmail.com>
2022-12-20 11:03:57 +0300

    Add a solution to the "Keys and Rooms" challenge

--- 841_Keys_and_Rooms/solution.rb
+++ 841_Keys_and_Rooms/solution.rb
@@ -1,5 +1,21 @@
 # @param {Integer[][]} rooms
 # @return {Boolean}
 def can_visit_all_rooms(rooms)
-    
+  open_room = 0
+  current = rooms[open_room]
+  entered_rooms = [0]
+
+  until current.empty?
+    current.map! do |key| 
+      if entered_rooms.include? key
+        next [] 
+      else 
+        entered_rooms << key
+        next rooms[key] 
+      end
+    end
+    current.flatten!
+  end
+
+  entered_rooms.size == rooms.size
 end
