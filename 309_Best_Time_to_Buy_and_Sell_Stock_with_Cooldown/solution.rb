c86a1b47087c236131c9fe751a7c8ec049cc69ec
ozovalihasan <ozovalihasan@gmail.com>
2022-12-23 12:19:09 +0300

    Add a solution to the "Best Time to Buy and Sell Stock with Cooldown" challenge

--- 309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown/solution.rb
+++ 309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown/solution.rb
@@ -1,5 +1,28 @@
 # @param {Integer[]} prices
 # @return {Integer}
 def max_profit(prices)
-    
+  product = Product.new
+  prices.each do |price|
+    product.update(price)
+  end
+  product.profit
+end
+
+class Product
+  attr_accessor :buy, :sell, :cooldown
+  
+  def initialize
+    @buy = - Float::INFINITY
+    @sell = 0
+    @cooldown = 0
+  end
+
+  def update(price)
+    self.buy, self.sell, self.cooldown = [ [cooldown - price, buy].max, buy + price, [ sell, cooldown].max ]
+  end
+
+  def profit
+    [sell, cooldown].max
+  end
+  
 end
