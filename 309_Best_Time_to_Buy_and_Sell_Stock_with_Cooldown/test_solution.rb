8c2b656269c4fc16924966dbe1eb56c0a3320c4d
ozovalihasan <ozovalihasan@gmail.com>
2022-12-23 11:35:48 +0300

    Add a test for the "Best Time to Buy and Sell Stock with Cooldown" challenge

---
+++ 309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown/test_solution.rb
@@ -0,0 +1,14 @@
+require_relative './solution'
+require_relative '../read_test_files'
+
+describe 'Solution' do
+  examples, results = read_test_files.values_at(:examples, :results)
+
+  describe '#max_profit' do
+    examples.each_with_index do |prices, index|
+      it 'returns the maximum profit you can achieve' do
+        expect(max_profit(prices)).to eq(results[index])
+      end
+    end
+  end
+end
