5332d75f64b03adbc5b4f508be8cad5b6871ed5d
ozovalihasan <ozovalihasan@gmail.com>
2022-12-24 11:59:08 +0300

    Add a solution to the "Domino and Tromino Tiling" challenge

--- 790_Domino_and_Tromino_Tiling/solution.rb
+++ 790_Domino_and_Tromino_Tiling/solution.rb
@@ -1,5 +1,11 @@
 # @param {Integer} n
 # @return {Integer}
 def num_tilings(n)
-    
+  result = {1 => 1, 2 => 2, 3 => 5}
+
+  4.upto(n) do |num|
+    result[num] = result[num - 1] * 2 + result[num - 3]
+  end
+  
+  result[n] % (10**9 + 7)
 end
