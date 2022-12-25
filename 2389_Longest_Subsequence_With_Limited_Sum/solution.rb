0db8aacb5b6e3915120373d2ccae6c78a6ca59b5
ozovalihasan <ozovalihasan@gmail.com>
2022-12-25 11:56:33 +0300

    Add a solution to the "Longest Subsequence With Limited Sum" challenge

--- 2389_Longest_Subsequence_With_Limited_Sum/solution.rb
+++ 2389_Longest_Subsequence_With_Limited_Sum/solution.rb
@@ -2,5 +2,15 @@
 # @param {Integer[]} queries
 # @return {Integer[]}
 def answer_queries(nums, queries)
-    
+  sum_arr = sum_arr_of_sorted_arr(nums)
+  
+  queries.map { |num| find_index_greater_than(num, sum_arr) }
+end
+
+def find_index_greater_than(num, arr)
+  arr.bsearch_index {|val| val > num} || arr.size
+end
+
+def sum_arr_of_sorted_arr(arr)
+  arr.sort.each_with_object([]) { |num, sum_arr| sum_arr << (sum_arr.last || 0) + num }
 end
