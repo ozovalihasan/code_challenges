d866cda14c4bb5fbc045b7a1fb5fbe64abe4974b
ozovalihasan <ozovalihasan@gmail.com>
2022-12-26 11:07:41 +0300

    Add a solution to the "Jump Game" challenge

--- 55_Jump_Game/solution.rb
+++ 55_Jump_Game/solution.rb
@@ -1,5 +1,14 @@
 # @param {Integer[]} nums
 # @return {Boolean}
 def can_jump(nums)
-    
+  return true unless nums.include? 0
+
+  possible_max_index = 0
+  0.upto(nums.size - 1) do |index|
+   
+    return false if index > possible_max_index
+   
+    possible_max_index = [nums[index] + index, possible_max_index].max
+  end
+  true
 end
