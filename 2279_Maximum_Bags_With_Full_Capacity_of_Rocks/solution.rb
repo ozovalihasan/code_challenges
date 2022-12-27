cc3a8e5603b527a948e9a67681fb96c8781b42eb
ozovalihasan <ozovalihasan@gmail.com>
2022-12-27 11:07:42 +0300

    Add a solution to the "Maximum Bags With Full Capacity of Rocks" challenge

--- 2279_Maximum_Bags_With_Full_Capacity_of_Rocks/solution.rb
+++ 2279_Maximum_Bags_With_Full_Capacity_of_Rocks/solution.rb
@@ -3,5 +3,15 @@
 # @param {Integer} additional_rocks
 # @return {Integer}
 def maximum_bags(capacity, rocks, additional_rocks)
-    
+  return capacity.size if (rocks.sum + additional_rocks) >= capacity.sum
+
+  needed_rocks = rocks.map.with_index { |rock, index| capacity[index] - rock }
+  
+  needed_rocks.sort!.each_with_index.inject(0) do |total_need, (need, index)|
+
+    return index if (total_need += need) > additional_rocks
+
+    total_need
+  end
+  
 end
