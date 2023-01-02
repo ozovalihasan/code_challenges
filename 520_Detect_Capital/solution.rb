2dac4b349e45a2023bd0472a0a29e19bd894bc80
ozovalihasan <ozovalihasan@gmail.com>
2023-01-02 10:16:16 +0300

    Update the solution of the "Detect Capital" challenge

--- 520_Detect_Capital/solution.rb
+++ 520_Detect_Capital/solution.rb
@@ -1,12 +1,5 @@
 # @param {String} word
 # @return {Boolean}
 def detect_capital_use(word)
-  return true if word.length == 1
-
-  if word[0] == word[0].upcase
-    word[0] = ''
-    word == ( word[0] == word[0].upcase ? word.upcase : word.downcase )
-  else
-    word == word.downcase
-  end
+  word[1..] == word[1..].downcase || word == word.upcase
 end
