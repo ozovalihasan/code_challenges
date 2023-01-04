ef60a5875accb11bbc2e8a56c0d417a262a976da
ozovalihasan <ozovalihasan@gmail.com>
2023-01-04 10:56:58 +0300

    Add a solution to the "Minimum Rounds to Complete All Tasks" challenge

--- 2244_Minimum_Rounds_to_Complete_All_Tasks/solution.rb
+++ 2244_Minimum_Rounds_to_Complete_All_Tasks/solution.rb
@@ -1,5 +1,9 @@
 # @param {Integer[]} tasks
 # @return {Integer}
 def minimum_rounds(tasks)
-    
+  tasks_values = tasks.tally.values
+
+  return -1 if tasks_values.include? 1
+
+  tasks_values.sum { |val| (val + 2) / 3 }
 end
