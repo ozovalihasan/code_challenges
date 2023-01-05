5887d596572fe9e5616f82f2ce79f86336d7c11e
ozovalihasan <ozovalihasan@gmail.com>
2023-01-05 14:18:54 +0300

    Add a solution to the "Minimum Number of Arrows to Burst Balloons" challenge

--- 452_Minimum_Number_of_Arrows_to_Burst_Balloons/solution.rb
+++ 452_Minimum_Number_of_Arrows_to_Burst_Balloons/solution.rb
@@ -1,5 +1,15 @@
 # @param {Integer[][]} points
 # @return {Integer}
 def find_min_arrow_shots(points)
-    
+  points.sort_by!(&:first)
+  shoot = 0
+  index = points.size - 1
+  
+  while index >= 0
+    shoot += 1
+    shoot_point = points[index].first
+    index -= 1 while index >= 0 && shoot_point <= points[index].last
+  end
+
+  shoot
 end
