3a4669ee6b8beadee095321411c73f2adcc94c27
ozovalihasan <ozovalihasan@gmail.com>
2023-01-06 10:56:16 +0300

    Add a solution to the "Maximum Ice Cream Bars" challenge

--- 1833_Maximum_Ice_Cream_Bars/solution.rb
+++ 1833_Maximum_Ice_Cream_Bars/solution.rb
@@ -2,5 +2,11 @@
 # @param {Integer} coins
 # @return {Integer}
 def max_ice_cream(costs, coins)
-    
+  return costs.size if coins >= costs.sum
+
+  sum = 0
+  costs.sort!.each_with_index do |cost, index|
+    sum += cost
+    return index if sum > coins
+  end
 end
