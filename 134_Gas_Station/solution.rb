191b07a3ca8e7557f41fbf4f3ab624594e2e6ef8
ozovalihasan <ozovalihasan@gmail.com>
2023-01-07 14:41:11 +0300

    Add a solution to the "Gas Station" challenge

--- 134_Gas_Station/solution.rb
+++ 134_Gas_Station/solution.rb
@@ -2,5 +2,17 @@
 # @param {Integer[]} cost
 # @return {Integer}
 def can_complete_circuit(gas, cost)
-    
+  return -1 if cost.sum > gas.sum
+
+  tank = 0
+  start_index = 0
+  gas.size.times do |index|
+    tank += gas[index] - cost[index]
+    if tank < 0
+      tank = 0
+      start_index = index + 1
+    end
+  end
+  
+  start_index
 end
