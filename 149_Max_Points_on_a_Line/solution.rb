70a602b63e0703fdc37a656628cb54e1e911ada4
ozovalihasan <ozovalihasan@gmail.com>
2023-01-08 13:57:51 +0300

    Add a solution to the "Max Points on a Line" challenge

--- 149_Max_Points_on_a_Line/solution.rb
+++ 149_Max_Points_on_a_Line/solution.rb
@@ -1,5 +1,23 @@
 # @param {Integer[][]} points
 # @return {Integer}
 def max_points(points)
+  return points.size if points.size <= 2
+
+  points_found = 0
+  until points[points_found].nil?
+    point1 = points.pop
+
+    slopes = Hash.new(0)
+    points.each { |point2| slopes[ slope(point1, point2) ] += 1 }
     
+    points_found = [points_found, slopes.values.max + 1].max
+  end    
+
+  points_found 
+end
+
+def slope(point1, point2)
+  return Float::INFINITY if point1.first == point2.first
+
+  (point1.last - point2.last) / (point1.first - point2.first).to_f
 end
