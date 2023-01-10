caf489ca40699be3a40582268d44def2149899ca
ozovalihasan <ozovalihasan@gmail.com>
2023-01-10 11:00:23 +0300

    Add a solution to the "Duplicate Zeros" challenge

--- 1089_Duplicate_Zeros/solution.rb
+++ 1089_Duplicate_Zeros/solution.rb
@@ -1,5 +1,11 @@
 # @param {Integer[]} arr
 # @return {Void} Do not return anything, modify arr in-place instead.
 def duplicate_zeros(arr)
+  
+  (arr.size - 1).downto(0) do |index| 
+
+    arr.insert(index, 0).pop if arr[index].zero? 
     
+  end
+
 end
