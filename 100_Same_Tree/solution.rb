6a7fe21651cbabd65db4946ac75ecf669e0c5d2b
ozovalihasan <ozovalihasan@gmail.com>
2023-01-10 10:10:15 +0300

    Add a solution to the "Same Tree" challenge

--- 100_Same_Tree/solution.rb
+++ 100_Same_Tree/solution.rb
@@ -7,9 +7,15 @@
 #         @right = right
 #     end
 # end
-# @param {TreeNode} p
-# @param {TreeNode} q
+# @param {TreeNode} node1
+# @param {TreeNode} node2
 # @return {Boolean}
-def is_same_tree(p, q)
-    
+def is_same_tree(node1, node2)
+  return node1.nil? && node2.nil? if node1.nil? || node2.nil?
+
+  return false unless node1.val == node2.val && 
+                      is_same_tree(node1.left, node2.left) && 
+                      is_same_tree(node1.right, node2.right)
+
+  true
 end
