179047412b72a38e2b284877e20d55793f6450a0
ozovalihasan <ozovalihasan@gmail.com>
2023-01-11 12:49:29 +0300

    Add a solution to the "Minimum Time to Collect All Apples in a Tree" challenge

--- 1443_Minimum_Time_to_Collect_All_Apples_in_a_Tree/solution.rb
+++ 1443_Minimum_Time_to_Collect_All_Apples_in_a_Tree/solution.rb
@@ -3,5 +3,30 @@
 # @param {Boolean[]} has_apple
 # @return {Integer}
 def min_time(n, edges, has_apple)
+  return 0 if has_apple.none?
+  
+  @tree = { 0 => []}
+  @has_apple = has_apple
+  @result = 0
+
+  edges.each do |start_node, end_node|
+    if @tree[start_node]
+      @tree[start_node] << end_node
+      @tree[end_node] = []
+    else
+      @tree[end_node] << start_node
+      @tree[start_node] = []
+    end
     
+  end
+
+  starting_node = 0
+  check_node(starting_node)
+  @result
+end
+
+def check_node(node_val)
+  @tree[node_val].map! { |subnode_val| @result += 2 if check_node(subnode_val) }
+  
+  @tree[node_val].any? || @has_apple[node_val]
 end
