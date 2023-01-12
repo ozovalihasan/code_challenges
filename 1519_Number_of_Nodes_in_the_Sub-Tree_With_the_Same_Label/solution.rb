8e5e160249be773869cdd22b22593e999857b570
ozovalihasan <ozovalihasan@gmail.com>
2023-01-12 12:43:09 +0300

    Add a solution to the "Number of Nodes in the Sub-Tree With the Same Label" challenge

--- 1519_Number_of_Nodes_in_the_Sub-Tree_With_the_Same_Label/solution.rb
+++ 1519_Number_of_Nodes_in_the_Sub-Tree_With_the_Same_Label/solution.rb
@@ -3,5 +3,40 @@
 # @param {String} labels
 # @return {Integer[]}
 def count_sub_trees(n, edges, labels)
-    
+  @tree = build_tree(edges)  
+
+  count_labels(labels)  
+end
+
+def build_tree(edges)
+  tree = { 0 => []}
+  
+  edges.each do |start_node, end_node|
+    if tree[start_node]
+      tree[start_node] << end_node
+      tree[end_node] = []
+    else
+      tree[end_node] << start_node
+      tree[start_node] = []
+    end
+  end
+
+  tree
+end
+
+def count_labels(labels)
+  @result = []
+  @labels = labels.split("")
+  @label_values = Hash.new(0)
+
+  starting_node = 0
+  check_node(starting_node, @labels[starting_node])
+  @result
+end
+
+def check_node(node_val, label)
+  label_val = @label_values[ label ]
+  @label_values[ label ] += 1
+  @tree[node_val].each { |node| check_node(node, @labels[node]) }
+  @result[node_val] = @label_values[label] - label_val
 end
