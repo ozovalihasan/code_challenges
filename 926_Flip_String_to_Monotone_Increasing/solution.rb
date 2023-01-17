331fbf1f469d191a943a2fd64157a8dc4f78e108
ozovalihasan <ozovalihasan@gmail.com>
2023-01-17 11:18:18 +0300

    Add a solution to the "Flip String to Monotone Increasing" challenge

--- 926_Flip_String_to_Monotone_Increasing/solution.rb
+++ 926_Flip_String_to_Monotone_Increasing/solution.rb
@@ -1,5 +1,13 @@
 # @param {String} s
 # @return {Integer}
 def min_flips_mono_incr(s)
-    
+  min_flip = s.count("1")
+  total_flip = s.count("0")
+
+  s.each_char do |char|
+    min_flip = total_flip if min_flip > total_flip
+    total_flip += char == "1" ? 1 : -1
+  end
+  
+  min_flip
 end
