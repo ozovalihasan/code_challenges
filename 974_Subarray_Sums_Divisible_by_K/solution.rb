c64da51798a11c217e55f45ca9ca36722bd21bf1
ozovalihasan <ozovalihasan@gmail.com>
2023-01-19 12:56:01 +0300

    Add a solution to the "Subarray Sums Divisible by K" challenge

--- 974_Subarray_Sums_Divisible_by_K/solution.rb
+++ 974_Subarray_Sums_Divisible_by_K/solution.rb
@@ -1,6 +1,19 @@
+# Thank [namasteDuniya](https://leetcode.com/namasteDuniya/) for [the clear explanation](https://leetcode.com/problems/subarray-sums-divisible-by-k/solutions/217979/pictured-explanation-python-o-n-clean-solution-8-lines/?orderBy=most_votes)
+
 # @param {Integer[]} nums
 # @param {Integer} k
 # @return {Integer}
 def subarrays_div_by_k(nums, k)
-    
+  result = 0
+  sum = 0
+  hash = Hash.new(0)
+  hash[0] = 1
+
+  nums.each do |num|
+    sum += num
+    result += hash[sum % k]
+    hash[sum % k] += 1
+  end
+
+  result
 end
