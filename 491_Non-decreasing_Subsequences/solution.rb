bd0a2be2abe1dc42ab4a1a1c7632abbb2be95f9b
ozovalihasan <ozovalihasan@gmail.com>
2023-01-20 15:45:31 +0300

    Add a solution to the "Non-decreasing Subsequences" challenge

--- 491_Non-decreasing_Subsequences/solution.rb
+++ 491_Non-decreasing_Subsequences/solution.rb
@@ -1,5 +1,10 @@
 # @param {Integer[]} nums
 # @return {Integer[][]}
 def find_subsequences(nums)
-    
+  result = nums.each_with_object( Set.new ) do |num, subresult|
+    subresult.merge( subresult.filter_map { |arr| arr.clone << num if arr.last <= num } )
+    subresult << [ num ]
+  end
+  
+  result.select! {|arr| arr.size > 1}.to_a
 end
