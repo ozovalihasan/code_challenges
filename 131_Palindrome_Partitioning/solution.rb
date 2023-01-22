8b31a174f112f611bde44c7d3faa6cfc3bad6708
ozovalihasan <ozovalihasan@gmail.com>
2023-01-22 19:15:51 +0300

    Add a solution to the "Palindrome Partitioning" challenge

--- 131_Palindrome_Partitioning/solution.rb
+++ 131_Palindrome_Partitioning/solution.rb
@@ -1,5 +1,48 @@
 # @param {String} s
 # @return {String[][]}
 def partition(s)
-    
+  palindromes_starting_from = set_palindromes(s)
+  
+  @max_size = s.size
+  @result = []
+  starting_index = 0
+  find_result(
+    starting_index, 
+    [], 
+    palindromes_starting_from
+  )
+  @result
+end
+
+def find_result(index, array, palindromes_starting_from)
+  return @result << array if index == @max_size
+
+  palindromes_starting_from[index].each do |str|
+    find_result(
+      index + str.size, 
+      array.clone << str, 
+      palindromes_starting_from
+    )
+  end
+end
+
+def set_palindromes(s)
+  
+  s.size.times.with_object({}) do |index, palindromes_starting_from|
+    palindromes_starting_from[index] = []
+
+    [0, 1].each do |difference|
+      
+      left_index = index
+      right_index = index + difference
+
+      while s[left_index] == s[right_index] && left_index >= 0
+        palindromes_starting_from[left_index] << s[left_index..right_index]
+        
+        left_index -= 1
+        right_index += 1
+      end
+    end
+
+  end
 end
