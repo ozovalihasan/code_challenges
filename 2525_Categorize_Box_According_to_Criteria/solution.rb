45f5086440d35d7159c445bf70b92977f8f6656d
ozovalihasan <ozovalihasan@gmail.com>
2023-01-23 18:25:35 +0300

    Add a solution to the "Minimum Rounds to Complete All Tasks" challenge

--- 2525_Categorize_Box_According_to_Criteria/solution.rb
+++ 2525_Categorize_Box_According_to_Criteria/solution.rb
@@ -4,5 +4,23 @@
 # @param {Integer} mass
 # @return {String}
 def categorize_box(length, width, height, mass)
-    
+  bulky = bulky?(length, width, height)
+  heavy = heavy?(mass)
+
+  if heavy && bulky
+    "Both"
+  elsif heavy
+    "Heavy"
+  elsif bulky
+    "Bulky"
+  else
+    "Neither"
+  end
 end
+
+def bulky?(length, width, height)
+  return true if [length, width, height].any? {|dimension| dimension >= 10**4}
+  length * width * height >= 10**9
+end
+
+def heavy?(mass) = mass >= 100
