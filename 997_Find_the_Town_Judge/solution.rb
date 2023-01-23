ee3dffefd23407bb05c78cd27a8a2f0381db4a85
ozovalihasan <ozovalihasan@gmail.com>
2023-01-23 18:08:28 +0300

    Update the solution of the "Find the Town Judge" challenge

--- 997_Find_the_Town_Judge/solution.rb
+++ 997_Find_the_Town_Judge/solution.rb
@@ -2,15 +2,12 @@
 # @param {Integer[][]} trust
 # @return {Integer}
 def find_judge(n, trust)
-  return n if n == 1
+  return 1 if n == 1
+  return -1 if trust.empty?
   
-  hash = Hash.new(0)
-  trust.each do |(first,second)|
-    hash[second] += 1
-    hash[first] -= 1
-  end
+  trust = trust.transpose
 
-  possible_judge = hash.find{|_, val| val == n-1}
-  return possible_judge.first if possible_judge
-  -1
+  possible_judges = trust.last.tally.find {|_, total_people| total_people == n - 1} || []
+  
+  (possible_judges - trust.first).first || -1
 end
