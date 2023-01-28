24df0824cd4fb8767412ac65e44700816ffeaa32
ozovalihasan <ozovalihasan@gmail.com>
2023-01-28 13:24:07 +0300

    Add a solution to the "Data Stream as Disjoint Intervals" challenge

--- 352_Data_Stream_as_Disjoint_Intervals/solution.rb
+++ 352_Data_Stream_as_Disjoint_Intervals/solution.rb
@@ -1,25 +1,54 @@
 class SummaryRanges
   def initialize()
-      
+    @ranges = []
   end
-
 
 =begin
   :type value: Integer
   :rtype: Void
 =end
   def add_num(value)
+    if @ranges.empty?
+      @ranges << [value, value]
+    else
+      index = @ranges.bsearch_index {|first, last| last >= value}
+
+      if index
+        return if value.between?(*@ranges[index])
+      else
+        index = @ranges.size
+      end
+
+      @ranges.insert(index, [value, value])
+
+      if index > 0 && necessary_merge_with_previous_one?(index)
+        merge_interval_with_previous_one(index)
+      else
+        index += 1
+      end
       
+      if @ranges[index] && necessary_merge_with_previous_one?(index)
+        merge_interval_with_previous_one(index)
+      end
+      
+    end
+
   end
 
+  def necessary_merge_with_previous_one?(index)
+    (@ranges[index].first - @ranges[index - 1].last) <= 1
+  end
 
+  def merge_interval_with_previous_one(index)
+    @ranges[index - 1][1] = @ranges.delete_at(index).last
+  end
+  
 =begin
   :rtype: Integer[][]
 =end
   def get_intervals()
-      
+    @ranges
   end
-
 
 end
 
