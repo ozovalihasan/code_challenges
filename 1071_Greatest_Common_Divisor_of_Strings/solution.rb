053b5cd9eb13b9b19518e118887e7e3de3017d60
ozovalihasan <ozovalihasan@gmail.com>
2023-02-01 11:39:37 +0300

    Add a solution to the "Greatest Common Divisor of Strings" challenge

--- 1071_Greatest_Common_Divisor_of_Strings/solution.rb
+++ 1071_Greatest_Common_Divisor_of_Strings/solution.rb
@@ -2,5 +2,11 @@
 # @param {String} str2
 # @return {String}
 def gcd_of_strings(str1, str2)
+  return str1 if str1 == str2
+
+  shortest = str1.match(/^(.*?)\1*$/)[1]
+
+  return "" unless str2.match?(/^(#{shortest})+$/)
   
+  shortest * (str2.size / shortest.size).gcd(str1.size / shortest.size)
 end
