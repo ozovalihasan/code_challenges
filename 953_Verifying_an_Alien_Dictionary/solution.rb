2151d0187480ea27123de17b6fd37be735cc6815
ozovalihasan <ozovalihasan@gmail.com>
2023-02-02 10:54:44 +0300

    Update the solution of the "Verifying an Alien Dictionary" challenge

--- 953_Verifying_an_Alien_Dictionary/solution.rb
+++ 953_Verifying_an_Alien_Dictionary/solution.rb
@@ -2,16 +2,5 @@
 # @param {String} order
 # @return {Boolean}
 def is_alien_sorted(words, order)
-  words.each_cons(2) do |first, second| 
-    next if second.start_with? first
-    return false if first.start_with? second
-
-    index = 0
-    index += 1 while second[index] == first[index]
-      
-    return false if order.index(first[index]) >  order.index(second[index])
-    
-  end 
-
-  true
+  words.sort_by {|word| word.tr(order, "a-z") } == words
 end
