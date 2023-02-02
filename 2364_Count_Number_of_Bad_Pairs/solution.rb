27c6c5ec07b16ce4324d2fada36928ebb78067e4
ozovalihasan <ozovalihasan@gmail.com>
2023-02-02 12:21:45 +0300

    Add a solution to the "Count Number of Bad Pairs" challenge

--- 2364_Count_Number_of_Bad_Pairs/solution.rb
+++ 2364_Count_Number_of_Bad_Pairs/solution.rb
@@ -1,5 +1,16 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def count_bad_pairs(nums)
-    
+  nums.map!.with_index do |num, index|
+    num - index
+  end
+
+  good_pairs_count = nums.tally.values.sum {|value| count_pairs(value)}
+  all_pairs_count = count_pairs(nums.size)
+  
+  all_pairs_count - good_pairs_count
+end
+
+def count_pairs(size)
+  size * (size - 1) / 2
 end
