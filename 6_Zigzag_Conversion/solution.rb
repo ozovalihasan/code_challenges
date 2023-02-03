0975a68a0d710bab5ea4c420b2f2e0e60df8ab64
ozovalihasan <ozovalihasan@gmail.com>
2023-02-03 11:41:04 +0300

    Add a solution to the "Zigzag Conversion" challenge

--- 6_Zigzag_Conversion/solution.rb
+++ 6_Zigzag_Conversion/solution.rb
@@ -2,5 +2,16 @@
 # @param {Integer} num_rows
 # @return {String}
 def convert(s, num_rows)
-    
+  return s if num_rows == 1
+  
+  total_indices = (num_rows - 1) * 2
+  result = Array.new(num_rows) { "" }
+  
+  s.each_char.with_index do |char, index|
+    index %= total_indices
+    index = total_indices - index if index >= num_rows
+    result[index] << char
+  end
+
+  result.join
 end
