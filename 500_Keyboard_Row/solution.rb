26e18ed9ab694291ef0e0d6251e8194f6cdd0901
ozovalihasan <ozovalihasan@gmail.com>
2023-02-03 12:42:41 +0300

    Add a solution to the "Keyboard Row" challenge

--- 500_Keyboard_Row/solution.rb
+++ 500_Keyboard_Row/solution.rb
@@ -1,5 +1,13 @@
 # @param {String[]} words
 # @return {String[]}
 def find_words(words)
-    
+  rows = ["qwertyuiop", "asdfghjkl", "zxcvbnm"]
+
+  result = []
+  
+  while chars = rows.pop
+    result.concat( words.select {|word| word.downcase.tr(chars, "").empty? } )
+  end
+
+  result
 end
