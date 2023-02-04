f1533b4aabf476a4ba8a3cd9abbc38c75b6fc20d
ozovalihasan <ozovalihasan@gmail.com>
2023-02-04 12:34:50 +0300

    Add a solution to the "Permutation in String" challenge

--- 567_Permutation_in_String/solution.rb
+++ 567_Permutation_in_String/solution.rb
@@ -1,6 +1,27 @@
-# @param {String} s1
-# @param {String} s2
+# @param {String} str_first
+# @param {String} str_second
 # @return {Boolean}
-def check_inclusion(s1, s2)
-    
+def check_inclusion(str_first, str_second)
+  return false if str_first.size > str_second.size
+
+  window_size = str_first.size
+  chars = str_first.split("").tally
+  chars.default = 0
+
+  str_second = str_second.split("")
+
+  str_second.take(window_size).each do |char|
+    chars[ char ] -= 1
+  end
+  
+  return true if chars.values.all? 0
+
+  str_second.drop(window_size).each_with_index do |char, index|
+    chars[ char ] -= 1
+    chars[ str_second[ index ] ] += 1
+
+    return true if chars.values.all? 0
+  end
+  
+  false
 end
