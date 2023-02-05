fa0c8d04d0ccb5479ac5bc9489969931b064a909
ozovalihasan <ozovalihasan@gmail.com>
2023-02-05 11:36:45 +0300

    Add a solution to the "Find All Anagrams in a String" challenge

--- 438_Find_All_Anagrams_in_a_String/solution.rb
+++ 438_Find_All_Anagrams_in_a_String/solution.rb
@@ -1,6 +1,30 @@
-# @param {String} s
-# @param {String} p
+# @param {String} str_first
+# @param {String} str_second
 # @return {Integer[]}
-def find_anagrams(s, p)
-    
+def find_anagrams(str_first, str_second)
+  window_size = str_second.size
+  chars = str_second.split("").tally
+  chars.default = 0
+
+  str_first = str_first.split("")
+
+  str_first.take(window_size).each do |char|
+    chars[ char ] -= 1
+  end
+  
+  result = []
+  result << 0 if chars.values.all? 0
+
+  check_inclusion_by_shifting_window(str_first, window_size, chars, result)
+
+  result
 end
+
+def check_inclusion_by_shifting_window(str_first, window_size, chars, result)
+  str_first.drop(window_size).each_with_index do |char, index|
+    chars[ char ] -= 1
+    chars[ str_first[ index ] ] += 1
+
+    result << (index + 1) if chars.values.all? 0
+  end
+end    
