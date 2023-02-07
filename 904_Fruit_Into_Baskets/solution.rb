5e349855d4dac5a517e23cb6ee94469eb290aee9
ozovalihasan <ozovalihasan@gmail.com>
2023-02-07 12:46:47 +0300

    Add a solution to the "Fruit Into Baskets" challenge

--- 904_Fruit_Into_Baskets/solution.rb
+++ 904_Fruit_Into_Baskets/solution.rb
@@ -1,5 +1,29 @@
 # @param {Integer[]} fruits
 # @return {Integer}
 def total_fruit(fruits)
-    
+  fruits = fruits.chunk_while {|type1, type2| type1 == type2}.map {|types| Fruit.new(types)}
+  counted_fruits = fruits.first(2)
+  total_counts = [ counted_fruits.sum(&:count) ]
+  
+  fruits[2..]&.each do |fruit|
+    unless fruit.type == counted_fruits.at(-2).type
+      counted_fruits = counted_fruits.last(1)
+      total_counts << counted_fruits.last.count
+    end
+
+    total_counts[-1] += fruit.count
+    counted_fruits << fruit
+  end
+
+  total_counts.max
+end
+
+class Fruit
+  attr_accessor :count
+  attr_reader :type
+  
+  def initialize(arr)
+    @type = arr.first
+    @count = arr.size
+  end
 end
