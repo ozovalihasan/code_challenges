ba2b6e6b8d07e22f21df73017f07dce18f578956
ozovalihasan <ozovalihasan@gmail.com>
2023-02-08 11:43:54 +0300

    Add a solution to the "Validate Stack Sequences" challenge

--- 946_Validate_Stack_Sequences/solution.rb
+++ 946_Validate_Stack_Sequences/solution.rb
@@ -2,5 +2,18 @@
 # @param {Integer[]} popped
 # @return {Boolean}
 def validate_stack_sequences(pushed, popped)
-    
+  arr = [] 
+  popped.reverse!
+
+  pushed.each do |num|
+    arr << num
+    while arr.last == popped.last
+      arr.pop
+      popped.pop
+  
+      return true if popped.empty?
+    end
+  end
+
+  false
 end
