09c8fc87593b250a0e40dbe6c36f1a64d59684a6
ozovalihasan <ozovalihasan@gmail.com>
2023-02-12 11:54:52 +0300

    Add a solution to the "Minimum Fuel Cost to Report to the Capital" challenge

--- 2477_Minimum_Fuel_Cost_to_Report_to_the_Capital/solution.rb
+++ 2477_Minimum_Fuel_Cost_to_Report_to_the_Capital/solution.rb
@@ -2,5 +2,33 @@
 # @param {Integer} seats
 # @return {Integer}
 def minimum_fuel_cost(roads, seats)
-    
+  @seats = seats
+  capital_city = 0
+
+  cities = { capital_city => []}
+  roads.each do |(first_city, second_city)|
+    cities[first_city] ||= []
+    cities[first_city] << second_city
+
+    cities[second_city] ||= []
+    cities[second_city] << first_city
+  end
+
+  @result = 0
+  cities[capital_city].each do |city|
+    cities[city].delete(capital_city)
+    check_passengers(city, cities)  
+  end
+  
+  @result
+end
+
+def check_passengers(city, cities)
+  passengers = cities[city].sum do |next_city|
+                 cities[next_city].delete(city)
+                 check_passengers(next_city, cities)
+               end
+  
+  @result += passengers / @seats + 1
+  passengers + 1
 end
