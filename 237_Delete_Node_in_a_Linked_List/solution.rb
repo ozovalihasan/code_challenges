f32b4577019e31db10666bb5329c0f1c68e28cac
ozovalihasan <ozovalihasan@gmail.com>
2023-02-14 10:36:27 +0300

    Update the solution of the "Delete Node in a Linked List" challenge

--- 237_Delete_Node_in_a_Linked_List/solution.rb
+++ 237_Delete_Node_in_a_Linked_List/solution.rb
@@ -10,9 +10,6 @@
 # @param {ListNode} node
 # @return {Void} Do not return anything, modify node in-place instead.
 def delete_node(node)
-  until node.nil?
-    node.val = node.next.val
-    node.next = nil if node.next.next.nil?
-    node = node.next
-  end
+  node.val = node.next.val
+  node.next = node.next.next
 end
