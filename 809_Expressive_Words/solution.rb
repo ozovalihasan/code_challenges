8e51453f5fda0d5dbd21c1beb273996debbc6730
ozovalihasan <ozovalihasan@gmail.com>
2023-02-16 11:51:35 +0300

    Add a solution to the "Expressive Words" challenge

--- 809_Expressive_Words/solution.rb
+++ 809_Expressive_Words/solution.rb
@@ -2,5 +2,25 @@
 # @param {String[]} words
 # @return {Integer}
 def expressive_words(s, words)
-    
+  s = s.chars.chunk(&:itself).to_a
+
+  @result = 0
+
+  words.each do |word|
+    word = word.chars.chunk(&:itself).to_a
+    compare(s, word)
+  end
+
+  @result
+end
+
+def compare(s, word)
+  return unless s.size == word.size
+
+  s.zip(word).each do |(s_char, s_char_arr), (char, char_arr)|
+    return unless s_char == char && s_char_arr.size >= char_arr.size
+    return if (s_char_arr.size == 2) && (char_arr.size == 1)
+  end
+
+  @result += 1
 end
