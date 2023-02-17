7f3cc8b8bcc6d7b8b6c3c53f0fcb3edf2b0751f6
ozovalihasan <ozovalihasan@gmail.com>
2023-02-17 10:38:31 +0300

    Add a solution to the "Minimum Distance Between BST Nodes" challenge

--- 783_Minimum_Distance_Between_BST_Nodes/solution.rb
+++ 783_Minimum_Distance_Between_BST_Nodes/solution.rb
@@ -10,5 +10,22 @@
 # @param {TreeNode} root
 # @return {Integer}
 def min_diff_in_bst(root)
-    
+
+  @node_values = [ ]
+  @diff = []
+  
+  check_node(root)
+  
+  @diff.min
+end
+
+def check_node(node)
+  return if node.nil?
+
+  check_node(node.left)
+  
+  @diff << node.val - @node_values.last unless @node_values.empty?
+  @node_values << node.val
+  
+  check_node(node.right)
 end
