cc12868b13e88e5465b0fbb75eb93cc815ac0bcc
ozovalihasan <ozovalihasan@gmail.com>
2023-02-19 10:08:00 +0300

    Add a solution to the "Binary Tree Zigzag Level Order Traversal" challenge

--- 103_Binary_Tree_Zigzag_Level_Order_Traversal/solution.rb
+++ 103_Binary_Tree_Zigzag_Level_Order_Traversal/solution.rb
@@ -10,5 +10,22 @@
 # @param {TreeNode} root
 # @return {Integer[][]}
 def zigzag_level_order(root)
-    
+  @result = []
+  depth = 0
+  check_node(root, depth)
+  @result
+end
+
+def check_node(node, depth)
+  return if node.nil?
+
+  @result[depth] ||= []
+  if depth.even?
+    @result[depth].push node.val
+  else
+    @result[depth].unshift node.val
+  end
+  
+  check_node(node.left, depth + 1)
+  check_node(node.right, depth + 1)
 end
