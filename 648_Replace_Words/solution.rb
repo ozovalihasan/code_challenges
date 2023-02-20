441248261a37051c13901eb27209c4ad4b7a50c0
ozovalihasan <ozovalihasan@gmail.com>
2023-02-20 10:25:40 +0300

    Add a solution to the "Replace Words" challenge

--- 648_Replace_Words/solution.rb
+++ 648_Replace_Words/solution.rb
@@ -2,5 +2,17 @@
 # @param {String} sentence
 # @return {String}
 def replace_words(dictionary, sentence)
-    
+  dictionary = dictionary.group_by {|word| word[0]}.transform_values(&:sort!)
+  dictionary.default = []
+
+  words = sentence.split(" ")
+  words.map! {|word| update_word(word, dictionary) }.join(" ")
+end
+
+def update_word(word, dictionary)
+  dictionary[ word[0] ].each do |root|
+    return root if word.start_with? root
+  end
+
+  word
 end
