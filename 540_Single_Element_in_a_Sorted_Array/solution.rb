620bfe7bc99904c4bc5edb5a9d49b2ce0228a478
ozovalihasan <ozovalihasan@gmail.com>
2023-02-21 10:54:42 +0300

    Add a solution to the "Single Element in a Sorted Array" challenge

--- 540_Single_Element_in_a_Sorted_Array/solution.rb
+++ 540_Single_Element_in_a_Sorted_Array/solution.rb
@@ -1,5 +1,30 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def single_non_duplicate(nums)
-    
+  return nums.first if nums.size == 1
+  
+  left = 0
+  right = nums.size - 1
+  index = (left + right) / 2
+
+  loop do
+    if nums[index - 1] == nums[index] 
+      left, index, right = update_boundaries(index.even?, left, index, right)
+    elsif nums[index + 1] == nums[index]
+      left, index, right = update_boundaries(index.odd?, left, index, right)
+    else
+      return nums[index]
+    end
+  end
+end
+
+def update_boundaries(statement, left, index, right)
+  if statement
+    right = index
+  else
+    left = index + 1
+  end
+  index = (left + right) / 2
+
+  [left, index, right]
 end
