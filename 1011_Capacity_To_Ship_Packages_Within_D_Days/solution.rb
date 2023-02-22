c2290dd8a43077a02d8271f4739404eac7073381
ozovalihasan <ozovalihasan@gmail.com>
2023-02-22 12:24:30 +0300

    Add a solution to the "Capacity To Ship Packages Within D Days" challenge

--- 1011_Capacity_To_Ship_Packages_Within_D_Days/solution.rb
+++ 1011_Capacity_To_Ship_Packages_Within_D_Days/solution.rb
@@ -2,5 +2,26 @@
 # @param {Integer} days
 # @return {Integer}
 def ship_within_days(weights, days)
+  sum = weights.sum
+  min_capacity = [sum / days, weights.max].max
+
+  (min_capacity..min_capacity * 2).bsearch do |checked_capacity|
+    given_capacity_enough?(checked_capacity, weights, days)
+  end
+end
+
+def given_capacity_enough?(checked_capacity, weights, days)
+  capacity_rest = checked_capacity
+  total_day = 1
+
+  weights.each do |weight| 
+    if capacity_rest < weight
+      capacity_rest = checked_capacity
+      total_day += 1
+    end
     
+    capacity_rest -= weight
+  end
+
+  total_day <= days 
 end
