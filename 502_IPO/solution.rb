fc58fb46dd5cc76fc411ed3332dfc62a5c99574b
ozovalihasan <ozovalihasan@gmail.com>
2023-02-23 12:24:49 +0300

    Add a solution to the "IPO" challenge

--- 502_IPO/solution.rb
+++ 502_IPO/solution.rb
@@ -1,8 +1,40 @@
-# @param {Integer} k
-# @param {Integer} w
+# @param {Integer} projects_until_deadline
+# @param {Integer} total_capital
 # @param {Integer[]} profits
 # @param {Integer[]} capital
 # @return {Integer}
-def find_maximized_capital(k, w, profits, capital)
+def find_maximized_capital(projects_until_deadline, total_capital, profits, capital)
+  
+  project_groups = capital.zip(profits).group_by(&:first).each {|_, projects| projects.map!(&:last)}
+  
+  project_groups = project_groups.sort_by(&:first).reverse
+
+  project_groups.each do |cap, profits|
+    profits.sort!
+  end
+  
+  until project_groups.empty? || projects_until_deadline <= 0
     
+    last_project_index = project_groups.bsearch_index do |cap, _|
+                                          cap <= total_capital
+                                        end
+
+    if last_project_index.nil?
+      break
+    elsif last_project_index.zero?
+      return project_groups.map(&:last).flatten.sort.last(projects_until_deadline).sum + total_capital
+    end
+    
+    project_group = project_groups[last_project_index..].max_by {|_, profits| profits.last}
+    
+    profit = project_group.last.pop
+    if project_group.last.empty?
+      project_groups.delete(project_group)
+    end
+
+    total_capital += profit
+    projects_until_deadline -= 1
+  end
+  
+  total_capital
 end
