54bfb52cd8966812fb491af33c75673ffa2c9e5d
ozovalihasan <ozovalihasan@gmail.com>
2023-02-25 13:47:05 +0300

    Add a solution to the "Best Time to Buy and Sell Stock" challenge

--- 121_Best_Time_to_Buy_and_Sell_Stock/solution.rb
+++ 121_Best_Time_to_Buy_and_Sell_Stock/solution.rb
@@ -1,5 +1,13 @@
 # @param {Integer[]} prices
 # @return {Integer}
 def max_profit(prices)
-    
+  min_price_so_far = prices.shift
+  profit = 0
+
+  prices.each do |price|
+    profit = price - min_price_so_far if price - min_price_so_far > profit
+    min_price_so_far = price if price < min_price_so_far 
+  end
+
+  profit
 end
