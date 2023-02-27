d0dae69f7ccacafcd45a73918bfe5547ed6bb42e
ozovalihasan <ozovalihasan@gmail.com>
2023-02-27 11:44:14 +0300

    Add a solution to the "Construct Quad Tree" challenge

--- 427_Construct_Quad_Tree/solution.rb
+++ 427_Construct_Quad_Tree/solution.rb
@@ -14,5 +14,26 @@
 # @param {Integer[][]} grid
 # @return {Node}
 def construct(grid)
-	
+  sample_cell = grid.first.first
+  grid.each do |row|
+    unless row.all? sample_cell  
+      return divide_into_four(grid)
+    end
+  end
+
+  Node.new(sample_cell == 1, true)
+end
+
+def divide_into_four(grid)
+  size = grid.size
+
+  grid_top, grid_bottom = grid.each_slice(size / 2).to_a
+  
+  grid_top_right = []
+  grid_top.each {|row| grid_top_right << row.slice!(size / 2, size / 2)}
+
+  grid_bottom_right = []
+  grid_bottom.each {|row| grid_bottom_right << row.slice!(size / 2, size / 2)}
+  
+  Node.new(0, false, construct(grid_top), construct(grid_top_right), construct(grid_bottom), construct(grid_bottom_right) )
 end
