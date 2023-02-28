6f83a69332a8f6b9639ffca57898ed60a1a8631e
ozovalihasan <ozovalihasan@gmail.com>
2023-02-28 12:30:44 +0300

    Add a solution to the "Find Duplicate Subtrees" challenge

--- 652_Find_Duplicate_Subtrees/solution.rb
+++ 652_Find_Duplicate_Subtrees/solution.rb
@@ -10,5 +10,18 @@
 # @param {TreeNode} root
 # @return {TreeNode[]}
 def find_duplicate_subtrees(root)
-    
+  @subtrees = Hash.new(0)
+  
+  @result = []
+  check_node(root)    
+  @result
+end
+
+def check_node(node)
+  return [nil] if node.nil?
+
+  arr = [node.val].concat(check_node(node.left)).concat(check_node(node.right))
+  @subtrees[ arr ] += 1
+  @result << node if @subtrees[ arr ] == 2
+  arr
 end
