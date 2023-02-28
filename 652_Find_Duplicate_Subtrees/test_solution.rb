3377f0325f08edf6ad76fd323c2746d846a61264
ozovalihasan <ozovalihasan@gmail.com>
2023-02-28 11:16:26 +0300

    Add a test for the "Find Duplicate Subtrees" challenge

---
+++ 652_Find_Duplicate_Subtrees/test_solution.rb
@@ -0,0 +1,16 @@
+require_relative './solution'
+require_relative '../read_test_files'
+require_relative './binary_tree'
+
+describe 'Solution' do
+  examples, results = read_test_files.values_at(:examples, :results)
+
+  describe '#find_duplicate_subtrees' do
+    examples.each_with_index do |arr, index|
+      it 'returns all duplicate subtrees' do
+        tree = Tree.new(arr)
+        expect(find_duplicate_subtrees(tree.root)).to eq(results[index])
+      end
+    end
+  end
+end
