ab79533faa37e5e2bf13a70bfe7e70c9fa90cc1f
ozovalihasan <ozovalihasan@gmail.com>
2023-03-01 10:57:44 +0300

    Add a solution to the "Sort an Array" challenge

--- 912_Sort_an_Array/solution.rb
+++ 912_Sort_an_Array/solution.rb
@@ -1,5 +1,11 @@
 # @param {Integer[]} nums
 # @return {Integer[]}
 def sort_array(nums)
-    
+  result = []
+  nums.each do |num|
+    index = result.bsearch_index {|num2| num2 > num} || result.size
+    result.insert(index, num)
+  end
+
+  result
 end
