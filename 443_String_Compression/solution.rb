1ae7a627aefba688b0ec5186dc735741f1ef5095
ozovalihasan <ozovalihasan@gmail.com>
2023-03-02 11:45:09 +0300

    Add a solution to the "String Compression" challenge

--- 443_String_Compression/solution.rb
+++ 443_String_Compression/solution.rb
@@ -1,5 +1,22 @@
 # @param {Character[]} chars
 # @return {Integer}
 def compress(chars)
-    
+  count = 1
+
+  (chars.size - 1).downto(1) do |index|
+    unless chars[index] == chars[index - 1]
+      update_chars(chars, index, count)      
+
+      count = 0
+    end
+
+    count += 1 
+  end
+
+  update_chars(chars, 0, count)      
+  chars.size
+end
+
+def update_chars(chars, index, count)
+  chars[index + 1...index + count] = count.to_s.split("") if count != 1
 end
