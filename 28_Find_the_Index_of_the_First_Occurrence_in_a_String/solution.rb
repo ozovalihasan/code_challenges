6bf24be1be4d4151a1b3cd61af225a9a82e35690
ozovalihasan <ozovalihasan@gmail.com>
2023-03-03 10:46:19 +0300

    Add a solution to the "Find the Index of the First Occurrence in a String" challenge

--- 28_Find_the_Index_of_the_First_Occurrence_in_a_String/solution.rb
+++ 28_Find_the_Index_of_the_First_Occurrence_in_a_String/solution.rb
@@ -2,5 +2,14 @@
 # @param {String} needle
 # @return {Integer}
 def str_str(haystack, needle)
-    
+  haystack = haystack.split("")
+  needle = needle.split("")
+  
+  needle_length = needle.length
+  
+  0.upto(haystack.length - needle_length) do |index|
+    return index if haystack[index] == needle.first && haystack.slice(index, needle_length) == needle
+  end
+
+  -1
 end
