8388a72de970e308b71100ec3e5f812b7fe03922
ozovalihasan <ozovalihasan@gmail.com>
2023-03-05 21:29:59 +0300

    Add a solution to the "Jump Game IV" challenge

--- 1345_Jump_Game_IV/solution.rb
+++ 1345_Jump_Game_IV/solution.rb
@@ -1,5 +1,28 @@
 # @param {Integer[]} arr
 # @return {Integer}
 def min_jumps(arr)
-    
+  hash = {}
+  arr.each_with_index { |num, index| (hash[num] ||= []) << index }
+  
+  max_step = hash.size * 2
+  
+  last_index = arr.size - 1
+  indices = [0]
+  jump = 0
+  checked = []
+
+  loop do
+    return jump if indices.include? last_index
+
+    indices = indices.flat_map do |index|
+                        if checked[index]
+                          []
+                        else
+                          checked[index] = true
+                          hash[arr[index]].last(max_step) << index + 1 << index - 1
+                        end
+                      end.uniq
+              
+    jump += 1
+  end
 end
