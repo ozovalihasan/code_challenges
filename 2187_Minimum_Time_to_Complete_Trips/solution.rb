5cd6dbb1b9ab9f832ce4da83a60a470d06761c71
ozovalihasan <ozovalihasan@gmail.com>
2023-03-07 11:33:48 +0300

    Add a solution to the "Minimum Time to Complete Trips" challenge

--- 2187_Minimum_Time_to_Complete_Trips/solution.rb
+++ 2187_Minimum_Time_to_Complete_Trips/solution.rb
@@ -2,5 +2,11 @@
 # @param {Integer} total_trips
 # @return {Integer}
 def minimum_time(time, total_trips)
-    
+  max_trip = time.max
+  min_time = (total_trips / time.sum {|trip_time| (max_trip / trip_time) + 1}) * max_trip
+  max_time = ((total_trips / time.sum {|trip_time| (max_trip / trip_time) }) + 1) * max_trip
+
+  (min_time..max_time).bsearch do |current_time|
+    time.sum {|trip_time| current_time / trip_time} >= total_trips
+  end
 end
