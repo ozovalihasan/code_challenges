93e8d10d90cc86d08dbceb02a470e1693a5ddfd7
ozovalihasan <ozovalihasan@gmail.com>
2023-03-08 11:37:13 +0300

    Add a solution to the "Koko Eating Bananas" challenge

--- 875_Koko_Eating_Bananas/solution.rb
+++ 875_Koko_Eating_Bananas/solution.rb
@@ -2,5 +2,9 @@
 # @param {Integer} h
 # @return {Integer}
 def min_eating_speed(piles, h)
-    
+  piles.map!(&:to_f)
+  
+  (1..piles.max.to_i).bsearch do |current_k|
+    piles.sum {|pile| (pile / current_k).ceil} <= h
+  end
 end
