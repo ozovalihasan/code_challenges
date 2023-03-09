140a38e097e78a07c58c620cd2a5a0108288e86e
ozovalihasan <ozovalihasan@gmail.com>
2023-03-09 22:51:33 +0300

    Add a solution to the "Water Bottles" challenge

--- 1518_Water_Bottles/solution.rb
+++ 1518_Water_Bottles/solution.rb
@@ -2,5 +2,13 @@
 # @param {Integer} num_exchange
 # @return {Integer}
 def num_water_bottles(num_bottles, num_exchange)
-    
+  total_bottles = num_bottles
+  
+  until num_bottles < num_exchange
+    full_bottles, empty_bottles = num_bottles.divmod(num_exchange)
+    total_bottles += full_bottles
+    num_bottles = full_bottles + empty_bottles
+  end
+
+  total_bottles
 end
