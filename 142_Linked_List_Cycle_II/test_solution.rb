7850b1681f338f8e089a3f5b0f2559e56e35a91c
ozovalihasan <ozovalihasan@gmail.com>
2023-03-09 10:48:28 +0300

    Update the test of the "Linked List Cycle II" challenge

--- 142_Linked_List_Cycle_II/test_solution.rb
+++ 142_Linked_List_Cycle_II/test_solution.rb
@@ -9,6 +9,21 @@ describe 'Solution' do
     examples.each_slice(2).each_with_index do |(arr, pos), index|
       it 'returns the node where the cycle begins' do
         linked_list = LinkedList.new(arr)
+
+        unless pos == -1
+          cursor = linked_list.head
+          index2 = 0
+          cycle_node = nil
+
+          until cursor.next.nil?
+            cycle_node = cursor if index2 == pos
+            cursor = cursor.next
+            index2 += 1
+          end
+
+          cursor.next = cycle_node
+        end
+        
         expect(detectCycle(linked_list.head)).to eq(results[index])
       end
     end
