2308d96ff8d497456ae1caae4bfcf57ebed44eb1
ozovalihasan <ozovalihasan@gmail.com>
2023-03-11 13:24:26 +0300

    Add a solution to the "Convert Sorted List to Binary Search Tree" challenge

--- 109_Convert_Sorted_List_to_Binary_Search_Tree/solution.rb
+++ 109_Convert_Sorted_List_to_Binary_Search_Tree/solution.rb
@@ -18,5 +18,24 @@
 # @param {ListNode} head
 # @return {TreeNode}
 def sorted_list_to_bst(head)
+  values = []
+  node = head 
+  while node
+    values << node.val 
+    node = node.next
+  end
+
+  to_bst(values, 0, values.size)
+end
+
+def to_bst(arr, start_index, end_index)
+  return nil if start_index >= end_index
   
+  middle_index = (start_index + end_index) / 2
+  
+  node = TreeNode.new(arr[middle_index])
+  node.left = to_bst(arr, start_index, middle_index )
+  node.right = to_bst(arr, middle_index + 1, end_index )
+  
+  node
 end
