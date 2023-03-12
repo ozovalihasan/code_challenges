1586452c90be85a5c419b4b23048852b22f09501
ozovalihasan <ozovalihasan@gmail.com>
2023-03-12 12:32:06 +0300

    Add a solution to the "Merge k Sorted Lists" challenge

--- 23_Merge_k_Sorted_Lists/solution.rb
+++ 23_Merge_k_Sorted_Lists/solution.rb
@@ -9,5 +9,26 @@
 # @param {ListNode[]} lists
 # @return {ListNode}
 def merge_k_lists(lists)
+  lists.delete(nil)
   
+  return nil if lists.empty?
+  
+  values = []
+  lists.each do |node|
+    until node.nil?
+      values << node.val
+      node = node.next
+    end
+  end
+
+  values.sort!
+  head = ListNode.new(values.shift)
+  cursor = head
+
+  values.each do |value|
+    cursor.next = ListNode.new(value)
+    cursor = cursor.next
+  end
+
+  head
 end
