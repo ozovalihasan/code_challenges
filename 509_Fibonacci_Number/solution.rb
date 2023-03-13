3584fdfd3ffd99b509f1a8d3ff2c37f1d31cb3b9
ozovalihasan <ozovalihasan@gmail.com>
2023-03-13 11:18:31 +0300

    Update the solution of the "Fibonacci Number" challenge

--- 509_Fibonacci_Number/solution.rb
+++ 509_Fibonacci_Number/solution.rb
@@ -1,11 +1,9 @@
 # @param {Integer} n
 # @return {Integer}
 def fib(n)
-  @hash ||= {0 => 0, 1 => 1}
+  (@memo ||= {0 => 0, 1 => 1})
   
-  unless @hash[n]
-    @hash[n] = fib(n - 1) + fib(n - 2)
-  end
-
-  @hash[n]
+  @memo[n] = fib(n - 1) + fib(n - 2) unless @memo[n]
+  
+  @memo[n]
 end
