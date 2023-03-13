6500336f21adc453ff33b8099b2360a7c5904113
ozovalihasan <ozovalihasan@gmail.com>
2023-03-13 11:00:34 +0300

    Add a solution to the "Symmetric Tree" challenge

--- 101_Symmetric_Tree/solution.rb
+++ 101_Symmetric_Tree/solution.rb
@@ -10,5 +10,14 @@
 # @param {TreeNode} root
 # @return {Boolean}
 def is_symmetric(root)
-    
+  check_symmetry(root.left, root.right)  
+end
+
+def check_symmetry(left_node, right_node)
+  return true if left_node.nil? && right_node.nil?
+  return false if left_node.nil? || right_node.nil?
+
+  left_node.val == right_node.val && 
+    check_symmetry(left_node.left, right_node.right) && 
+    check_symmetry(left_node.right, right_node.left)
 end
