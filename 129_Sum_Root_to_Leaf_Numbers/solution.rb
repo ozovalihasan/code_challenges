802197ea3277796dce606aaaef7bc1eced2e7acf
ozovalihasan <ozovalihasan@gmail.com>
2023-03-14 10:45:16 +0300

    Add a solution to the "Sum Root to Leaf Numbers" challenge

--- 129_Sum_Root_to_Leaf_Numbers/solution.rb
+++ 129_Sum_Root_to_Leaf_Numbers/solution.rb
@@ -10,5 +10,26 @@
 # @param {TreeNode} root
 # @return {Integer}
 def sum_numbers(root)
-    
+  @result = 0
+  check_node(root, [])
+  @result
+end
+
+def check_node(node, previous)
+  return if node.nil?
+  
+  previous << node.val
+
+  if leaf? node
+    @result += previous.join.to_i 
+  else
+    check_node(node.left, previous)
+    check_node(node.right, previous)
+  end
+
+  previous.pop
+end
+
+def leaf?(node)
+  node.left.nil? && node.right.nil?
 end
