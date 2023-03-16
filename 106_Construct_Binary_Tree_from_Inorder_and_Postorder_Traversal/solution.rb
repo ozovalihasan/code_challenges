46b3cc0a30a99614846eed7dfdb9e81e25e0efa6
ozovalihasan <ozovalihasan@gmail.com>
2023-03-16 11:29:00 +0300

    Add a solution to the "Construct Binary Tree from Inorder and Postorder Traversal" challenge

--- 106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal/solution.rb
+++ 106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal/solution.rb
@@ -11,5 +11,20 @@
 # @param {Integer[]} postorder
 # @return {TreeNode}
 def build_tree(inorder, postorder)
-    
+  postorder_reverse = postorder.reverse
+
+  build_subtrees(inorder, postorder_reverse)
+end
+
+def build_subtrees(inorder, postorder_reverse)
+  return nil if inorder.empty?
+
+  root_val = postorder_reverse.find {|num| inorder.include? num}
+  postorder_reverse.delete(root_val)
+
+  node = TreeNode.new(root_val)
+  index = inorder.index(root_val)
+  node.left = build_subtrees(inorder[...index], postorder_reverse)
+  node.right = build_subtrees(inorder[(index + 1) ...], postorder_reverse)
+  node
 end
