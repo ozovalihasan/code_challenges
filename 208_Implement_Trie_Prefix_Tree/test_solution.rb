ce800fee1d9efc705b2a90d558df1380abd36bba
ozovalihasan <ozovalihasan@gmail.com>
2023-03-17 10:55:27 +0300

    Update the test of the "Implement Trie (Prefix Tree)" challenge

--- 208_Implement_Trie_Prefix_Tree/test_solution.rb
+++ 208_Implement_Trie_Prefix_Tree/test_solution.rb
@@ -7,8 +7,8 @@ describe 'Solution' do
   describe 'Trie' do
     examples.each_slice(2).each_with_index do |(actions, params), index|
       it "returns a class 'Trie'" do
+        trie = nil
         actions.zip(params).each_with_index do |(action, (param)), index2|
-          trie = nil
           if action == "Trie"
             trie = Trie.new
           elsif action == "insert"
@@ -16,7 +16,7 @@ describe 'Solution' do
           elsif action == "search"
             expect(trie.search(param)).to eq(results[index][index2])
           elsif action == "startsWith"
-            expect(trie.startsWith(param)).to eq(results[index][index2])
+            expect(trie.starts_with(param)).to eq(results[index][index2])
           end
         end
 
