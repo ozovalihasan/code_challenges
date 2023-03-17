8954012b29fccfd58dddda665e8dbc4784136d47
ozovalihasan <ozovalihasan@gmail.com>
2023-03-17 10:55:54 +0300

    Add a solution to the "Implement Trie (Prefix Tree)" challenge

--- 208_Implement_Trie_Prefix_Tree/solution.rb
+++ 208_Implement_Trie_Prefix_Tree/solution.rb
@@ -1,6 +1,6 @@
 class Trie
   def initialize()
-      
+    @root = NodeTrie.new
   end
 
 
@@ -9,7 +9,14 @@ class Trie
   :rtype: Void
 =end
   def insert(word)
-      
+    word = word.split("")
+    cursor = @root
+
+    while char = word.shift
+      cursor = (cursor.chars[char] ||= NodeTrie.new)
+    end
+    
+    cursor.word = true if word.empty?
   end
 
 
@@ -18,7 +25,9 @@ class Trie
   :rtype: Boolean
 =end
   def search(word)
-      
+    cursor = check_nodes(word)
+
+    cursor ? cursor.word : false
   end
 
 
@@ -27,10 +36,21 @@ class Trie
   :rtype: Boolean
 =end
   def starts_with(prefix)
-      
+    check_nodes(prefix) ? true : false
   end
 
+  def check_nodes(prefix)
+    prefix = prefix.split("")
+    cursor = @root
+    
+    while char = prefix.shift
+      cursor = cursor.chars[char]
+      
+      break if cursor.nil?
+    end
 
+    cursor
+  end
 end
 
 # Your Trie object will be instantiated and called as such:
@@ -38,3 +58,11 @@ end
 # obj.insert(word)
 # param_2 = obj.search(word)
 # param_3 = obj.starts_with(prefix)
+
+class NodeTrie
+  attr_accessor :chars, :word
+  def initialize(chars = {}, word = false)
+    @chars = chars
+    @word = word
+  end
+end
