0351fc23b82c65a3a149d572959f66b00b5611bc
ozovalihasan <ozovalihasan@gmail.com>
2023-03-20 10:18:52 +0300

    Add a solution to the "Destination City" challenge

--- 1436_Destination_City/solution.rb
+++ 1436_Destination_City/solution.rb
@@ -1,5 +1,8 @@
 # @param {String[][]} paths
 # @return {String}
 def dest_city(paths)
-    
+  paths = paths.to_h
+  entering_cities = paths.values
+  leaving_cities = paths.keys
+  (entering_cities - leaving_cities).first
 end
