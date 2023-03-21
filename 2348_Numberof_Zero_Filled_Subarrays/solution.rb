402c66e22dee013b3829fc9924e1c1e57f410f0d
ozovalihasan <ozovalihasan@gmail.com>
2023-03-21 10:33:58 +0300

    Add a solution to the "Number of Zero-Filled Subarrays" challenge

--- 2348_Numberof_Zero_Filled_Subarrays/solution.rb
+++ 2348_Numberof_Zero_Filled_Subarrays/solution.rb
@@ -1,5 +1,7 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def zero_filled_subarray(nums)
-    
+  nums.chunk(&:zero?)
+      .filter_map {|all_zeros, arr| arr.size if all_zeros}
+      .sum {|size| (size + 1) * size / 2 }
 end
