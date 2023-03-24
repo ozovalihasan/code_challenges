a9f26096be9536feade54d18b33055995c68abad
ozovalihasan <ozovalihasan@gmail.com>
2023-03-24 11:37:15 +0300

    Add a solution to the "Reorder Routes to Make All Paths Lead to the City Zero" challenge

--- 1466_Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero/solution.rb
+++ 1466_Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero/solution.rb
@@ -1,6 +1,39 @@
+# @return {Integer}
 # @param {Integer} n
 # @param {Integer[][]} connections
-# @return {Integer}
 def min_reorder(n, connections)
-    
+  cities = 0.upto(n-1).map { City.new }
+
+  connections.each do |city1, city2|
+    cities[city1].going_to_cities << cities[city2]
+    cities[city1].connected_cities << cities[city2]
+    cities[city2].connected_cities << cities[city1]
+  end
+
+  end_city = cities[0]
+  @necessary_change = end_city.going_to_cities.size
+  end_city.connected_cities.each do |starting_city|
+    check_city(end_city, starting_city)
+  end
+
+  @necessary_change
+end
+
+def check_city(end_city, starting_city)
+  @necessary_change += starting_city.going_to_cities.size
+  @necessary_change -= 1 if starting_city.going_to_cities.include?(end_city)
+
+  starting_city.connected_cities.delete(end_city)
+  starting_city.connected_cities.each do |next_city|
+    check_city(starting_city, next_city)
+  end
+end
+
+class City
+  attr_reader :connected_cities, :going_to_cities
+
+  def initialize
+    @connected_cities = []
+    @going_to_cities = []
+  end
 end
