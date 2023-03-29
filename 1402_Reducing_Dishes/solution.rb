5c03f26b6bea4ea8a4beef2ffdc83b58d10b5461
ozovalihasan <ozovalihasan@gmail.com>
2023-03-29 11:06:20 +0300

    Add a solution to the "Reducing Dishes" challenge

--- 1402_Reducing_Dishes/solution.rb
+++ 1402_Reducing_Dishes/solution.rb
@@ -1,5 +1,17 @@
 # @param {Integer[]} satisfaction
 # @return {Integer}
 def max_satisfaction(satisfaction)
-    
+  negatives, positives = satisfaction.sort!.partition(&:negative?)
+  total_satisfaction = positives.sum
+  result = positives.each_with_index.sum {|level, index| level * (index + 1)}
+  
+  negatives.reverse_each do |level|
+    total_satisfaction += level
+
+    break if total_satisfaction <= 0  
+
+    result += total_satisfaction 
+  end
+
+  result
 end
