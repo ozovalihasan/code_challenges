e21d360abcb9993554738dd9ec1ada566536404a
ozovalihasan <ozovalihasan@gmail.com>
2023-04-02 20:14:27 +0300

    Add a solution to the "Successful Pairs of Spells and Potions" challenge

--- 2300_Successful_Pairs_of_Spells_and_Potions/solution.rb
+++ 2300_Successful_Pairs_of_Spells_and_Potions/solution.rb
@@ -3,5 +3,12 @@
 # @param {Integer} success
 # @return {Integer[]}
 def successful_pairs(spells, potions, success)
-    
+  potions.sort!.reverse!
+  potions << 0
+  success = success.to_f
+
+  cache = {}
+  spells.map! do |spell |
+    cache[spell] ||= ( potions.bsearch_index {|potion| potion < (success / spell) } )
+  end
 end
