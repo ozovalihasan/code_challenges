c1ff1bcd2ca26ec9847610eca0c0f8f4eca704ef
ozovalihasan <ozovalihasan@gmail.com>
2023-04-03 12:00:14 +0300

    Add a solution to the "Boats to Save People" challenge

--- 881_Boats_to_Save_People/solution.rb
+++ 881_Boats_to_Save_People/solution.rb
@@ -2,5 +2,18 @@
 # @param {Integer} limit
 # @return {Integer}
 def num_rescue_boats(people, limit)
-    
+  people.sort!
+  
+  index1 = 0
+  index2 = people.size - 1
+
+  result = 0
+  while index1 <= index2
+    index1 += 1 if ( people[index1] + people[index2] ) <= limit
+      
+    result += 1
+    index2 -= 1
+  end
+
+  result
 end
