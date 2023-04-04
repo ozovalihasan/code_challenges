c7c1280cddc642cbed50fb258dbccb29c7f44ca3
ozovalihasan <ozovalihasan@gmail.com>
2023-04-04 11:21:06 +0300

    Add a solution to the "Optimal Partition of String" challenge

--- 2405_Optimal_Partition_of_String/solution.rb
+++ 2405_Optimal_Partition_of_String/solution.rb
@@ -1,5 +1,17 @@
 # @param {String} s
 # @return {Integer}
 def partition_string(s)
-    
+  chars = []
+  partitions_count = 1
+  
+  s.each_char do |char|
+    if chars.include? char
+      chars.clear
+      partitions_count += 1 
+    end
+
+    chars << char
+  end
+
+  partitions_count
 end
