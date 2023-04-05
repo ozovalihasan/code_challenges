8a3eff5cdce93cf42acbc5cc9671142e4a6b3a2c
ozovalihasan <ozovalihasan@gmail.com>
2023-04-05 11:29:31 +0300

    Add a solution to the "Minimize Maximum of Array" challenge

--- 2439_Minimize_Maximum_of_Array/solution.rb
+++ 2439_Minimize_Maximum_of_Array/solution.rb
@@ -1,5 +1,16 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def minimize_array_value(nums)
+  total = 0
+  max = 0
+  
+  nums.each_with_index do |num, index|
+    total += num
     
+    if total > (max * (index + 1))
+      max = ((total.to_f) / (index + 1)).ceil
+    end
+  end
+
+  max
 end
