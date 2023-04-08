2bdfec9f6b390c0d7f6fd6cd623e849145e8fab0
ozovalihasan <ozovalihasan@gmail.com>
2023-04-08 12:17:55 +0300

    Add a solution to the "Clone Graph" challenge

--- 133_Clone_Graph/solution.rb
+++ 133_Clone_Graph/solution.rb
@@ -11,5 +11,21 @@
 # @param {Node} node
 # @return {Node}
 def cloneGraph(node)
-    p node
+  return if node.nil?
+
+  nodes = {}
+  clone(node, nodes)
+end
+
+def clone(node, nodes)
+  return nodes[node.val] if nodes[node.val]
+
+  nodes[node.val] = Node.new(node.val)
+  clone_node = nodes[node.val]
+  
+  clone_node.neighbors = node.neighbors.map do |neighbor_node|
+    clone(neighbor_node, nodes)
+  end
+
+  clone_node
 end
