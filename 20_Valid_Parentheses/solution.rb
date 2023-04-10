d3c873525abc8f0375183c2b9f06b66c35c1ad89
ozovalihasan <ozovalihasan@gmail.com>
2023-04-10 10:23:31 +0300

    Add a solution to the "Valid Parentheses" challenge

--- 20_Valid_Parentheses/solution.rb
+++ 20_Valid_Parentheses/solution.rb
@@ -1,5 +1,25 @@
 # @param {String} s
 # @return {Boolean}
+PAIRS = {
+  "}" => "{",
+  ")" => "(",
+  "]" => "["
+}
+
 def is_valid(s)
-    
+  return false unless s.count(PAIRS.keys.join) == s.count(PAIRS.values.join)
+  
+  stack = []
+  
+  s.split("").each do |char|
+    if not PAIRS[char]
+      stack << char
+    elsif PAIRS[char] == stack.last
+      stack.pop
+    else
+      return false
+    end
+  end
+
+  stack.empty?
 end
