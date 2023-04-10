a6bdbc2dace6192e92acc5d7230fc829a836152f
ozovalihasan <ozovalihasan@gmail.com>
2023-04-10 10:48:48 +0300

    Add a solution to the "Reverse String II" challenge

--- 541_Reverse_String_II/solution.rb
+++ 541_Reverse_String_II/solution.rb
@@ -2,5 +2,9 @@
 # @param {Integer} k
 # @return {String}
 def reverse_str(s, k)
-    
+  (0..(s.size - 1)).step(2 * k) do |index|
+    s[index ... index + k] = s[index ... index + k].reverse!
+  end
+
+  s
 end
