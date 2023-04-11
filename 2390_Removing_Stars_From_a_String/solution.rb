55413a90098b0b7312b1e4152ffa38abd706f96a
ozovalihasan <ozovalihasan@gmail.com>
2023-04-11 10:38:52 +0300

    Add a solution to the "Removing Stars From a String" challenge

--- 2390_Removing_Stars_From_a_String/solution.rb
+++ 2390_Removing_Stars_From_a_String/solution.rb
@@ -1,5 +1,11 @@
 # @param {String} s
 # @return {String}
 def remove_stars(s)
-    
+  result = []
+
+  s.each_char do |char|
+    char == "*" ? result.pop : result << char
+  end
+
+  result.join
 end
