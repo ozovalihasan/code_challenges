dd846d7aad75dc7d1add78db78fa69f05dbfe6d2
ozovalihasan <ozovalihasan@gmail.com>
2023-04-12 13:31:48 +0300

    Add a solution to the "Simplify Path" challenge

--- 71_Simplify_Path/solution.rb
+++ 71_Simplify_Path/solution.rb
@@ -1,5 +1,13 @@
 # @param {String} path
 # @return {String}
 def simplify_path(path)
-    
+  stack = []
+  
+  parts = path.split("/") - ["", "."]
+  
+  parts.each do |part|
+    part == ".." ? stack.pop : (stack << part)
+  end
+
+  "/" << stack.join("/")
 end
