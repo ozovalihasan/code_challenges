34184389d8444d21afa4524bb0a39b517f4ecbb9
ozovalihasan <ozovalihasan@gmail.com>
2023-04-13 11:27:43 +0300

    Add a solution to the "Baseball Game" challenge

--- 682_Baseball_Game/solution.rb
+++ 682_Baseball_Game/solution.rb
@@ -1,5 +1,22 @@
 # @param {String[]} operations
 # @return {Integer}
 def cal_points(operations)
+  scores = []
+
+  operations.each do |operation|
+
+    case operation
+    when "+"
+      scores << scores.last(2).sum
+    when "D"
+      scores << (scores.last * 2)
+    when "C"
+      scores.pop
+    else
+      scores << operation.to_i
+    end
     
+  end
+
+  scores.sum
 end
