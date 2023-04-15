b6bc3d4ef1787a527b1aa034279d3321cd9530d9
ozovalihasan <ozovalihasan@gmail.com>
2023-04-15 18:53:16 +0300

    Add a solution to the "Maximum Value of K Coins From Piles" challenge

--- 2218_Maximum_Value_of_K_Coins_From_Piles/solution.rb
+++ 2218_Maximum_Value_of_K_Coins_From_Piles/solution.rb
@@ -2,5 +2,24 @@
 # @param {Integer} k
 # @return {Integer}
 def max_value_of_coins(piles, k)
-    
+  result = Array.new(k + 1) {[0]}
+
+  piles.each do |pile|
+    sum = 0
+
+    pile.unshift(0)
+    pile.map! { |cell| sum += cell }
+  end
+
+  piles.each do |pile|
+    pile.each_with_index do |cell, index|
+      0.upto(k - index) do |result_index|
+        result[result_index + index] << (cell + result[result_index].first)
+      end      
+    end
+
+    result.map! {|row| [row.max]}
+  end
+
+  result[k].max
 end
