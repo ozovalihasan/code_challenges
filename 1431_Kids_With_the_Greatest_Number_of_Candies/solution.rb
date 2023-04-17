5235374049917636c7355f6949733351a2933222
ozovalihasan <ozovalihasan@gmail.com>
2023-04-17 12:08:11 +0300

    Add a solution to the "Kids With the Greatest Number of Candies" challenge

--- 1431_Kids_With_the_Greatest_Number_of_Candies/solution.rb
+++ 1431_Kids_With_the_Greatest_Number_of_Candies/solution.rb
@@ -2,5 +2,9 @@
 # @param {Integer} extra_candies
 # @return {Boolean[]}
 def kids_with_candies(candies, extra_candies)
-    
+  max = candies.max
+
+  candies.map do |candy|
+    (candy + extra_candies) >= max
+  end
 end
