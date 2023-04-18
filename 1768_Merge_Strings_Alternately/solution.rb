7dea95466d75cfd040475705879ff4b0ed20bb48
ozovalihasan <ozovalihasan@gmail.com>
2023-04-18 10:45:36 +0300

    Add a solution to the "Merge Strings Alternately" challenge

--- 1768_Merge_Strings_Alternately/solution.rb
+++ 1768_Merge_Strings_Alternately/solution.rb
@@ -2,5 +2,9 @@
 # @param {String} word2
 # @return {String}
 def merge_alternately(word1, word2)
-    
+  word1 = word1.chars
+  word2 = word2.chars
+  word1[word2.size - 1] = nil if word1.size < word2.size
+  
+  word1.zip(word2).flatten.compact.join
 end
