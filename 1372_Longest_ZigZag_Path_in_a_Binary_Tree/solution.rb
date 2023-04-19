c17be1bf4b9c4c6a7d9e27fec75a76abd6e587b0
ozovalihasan <ozovalihasan@gmail.com>
2023-04-19 11:57:21 +0300

    Add a solution to the "Longest ZigZag Path in a Binary Tree" challenge

--- 1372_Longest_ZigZag_Path_in_a_Binary_Tree/solution.rb
+++ 1372_Longest_ZigZag_Path_in_a_Binary_Tree/solution.rb
@@ -10,5 +10,15 @@
 # @param {TreeNode} root
 # @return {Integer}
 def longest_zig_zag(root)
-    
+  @max_count = 0
+  check(root, 0)
+  @max_count
+end
+
+def check(node, count, previous_side = nil)
+  return if node.nil?
+
+  @max_count = count if count > @max_count
+  check(node.left, (previous_side != :left ? count + 1 : 1), :left)
+  check(node.right, (previous_side != :right ? count + 1 : 1), :right)
 end
