fe91ccc0066f8ee3dcc479d1cbaac2aa0e084b56
ozovalihasan <ozovalihasan@gmail.com>
2023-04-20 11:02:47 +0300

    Add a solution to the "Maximum Width of Binary Tree" challenge

--- 662_Maximum_Width_of_Binary_Tree/solution.rb
+++ 662_Maximum_Width_of_Binary_Tree/solution.rb
@@ -10,5 +10,19 @@
 # @param {TreeNode} root
 # @return {Integer}
 def width_of_binary_tree(root)
-    
+  @nodes_on_same_depth = []
+  
+  check root
+
+  @nodes_on_same_depth.map { |indices| indices.max - indices.min }.max + 1
+end
+
+def check node, depth = 0, index = 0
+  return if node.nil?
+
+  (@nodes_on_same_depth[depth] ||= []) << index
+  
+  depth += 1
+  check node.left, depth, (index * 2) + 1
+  check node.right, depth, (index * 2) + 2
 end
