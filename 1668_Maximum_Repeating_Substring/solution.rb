232518a5cc5d40e8ccfa2101b29a63aae85c0e17
ozovalihasan <ozovalihasan@gmail.com>
2023-04-22 00:22:23 +0300

    Add a solution to the "Maximum Repeating Substring" challenge

--- 1668_Maximum_Repeating_Substring/solution.rb
+++ 1668_Maximum_Repeating_Substring/solution.rb
@@ -2,5 +2,16 @@
 # @param {String} word
 # @return {Integer}
 def max_repeating(sequence, word)
-    
+  result = 0
+  
+  until sequence.empty?
+    if sequence.start_with?(word)
+      word_counts = sequence.match(/^(#{ word })+/)[0].scan(word).size
+      result = word_counts if word_counts > result 
+    end
+
+    sequence[0] = ""
+  end
+
+  result
 end
