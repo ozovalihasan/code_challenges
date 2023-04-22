e55fed28d5504dbbc3f9e0032274b7e89d94192e
ozovalihasan <ozovalihasan@gmail.com>
2023-04-22 22:45:21 +0300

    Add a solution to the "Minimum Insertion Steps to Make a String Palindrome" challenge

--- 1312_Minimum_Insertion_Steps_to_Make_a_String_Palindrome/solution.rb
+++ 1312_Minimum_Insertion_Steps_to_Make_a_String_Palindrome/solution.rb
@@ -1,5 +1,26 @@
 # @param {String} s
 # @return {Integer}
 def min_insertions(s)
+  @cache = Array.new(s.size) { [] }
+  s = s.chars
+  check( s, 0, s.size - 1)
+end
 
+def check(str, start_index, end_index)
+  cached = @cache[end_index][start_index]
+  return cached if cached
+
+  if start_index >= end_index
+    return @cache[end_index][start_index] = 0
+  end
+  
+  if str[start_index] == str[end_index]
+    return @cache[end_index][start_index] = check( str, start_index + 1, end_index - 1 )
+  else
+    return @cache[end_index][start_index] = [ 
+                                              check( str, start_index + 1, end_index ), 
+                                              check( str, start_index, end_index - 1 ) 
+                                            ].min + 1
+  end
+  
 end
