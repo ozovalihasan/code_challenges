d71dde506e1d60b26e69e3341a10f0a9560258dc
ozovalihasan <ozovalihasan@gmail.com>
2023-04-24 10:27:57 +0300

    Add a solution to the "Rearrange Spaces Between Words" challenge

--- 1592_Rearrange_Spaces_Between_Words/solution.rb
+++ 1592_Rearrange_Spaces_Between_Words/solution.rb
@@ -1,5 +1,12 @@
 # @param {String} text
 # @return {String}
 def reorder_spaces(text)
-    
+  spaces_count = text.count(" ")
+  text = text.split(/\s+/)
+  text.delete("")
+  
+  return text.join + " " * spaces_count if text.size == 1
+
+  quotient, remainder = spaces_count.divmod(text.size - 1)
+  text.join(" " * quotient) + " " * remainder
 end
