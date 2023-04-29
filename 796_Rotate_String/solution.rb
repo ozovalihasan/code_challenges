d3841c8d74e7033294bd59bcd515869a08d6dc29
ozovalihasan <ozovalihasan@gmail.com>
2023-04-30 00:41:46 +0300

    Add a solution to the "Rotate String" challenge

--- 796_Rotate_String/solution.rb
+++ 796_Rotate_String/solution.rb
@@ -2,5 +2,9 @@
 # @param {String} goal
 # @return {Boolean}
 def rotate_string(s, goal)
-    
+  0.upto(s.size - 1) do |index|
+    return true if (s[index..] + s[...index]) == goal
+  end 
+
+  false
 end
