d57a93f1aea96da7cb0a55973d172bdb5ff49c6b
ozovalihasan <ozovalihasan@gmail.com>
2023-05-03 12:15:36 +0300

    Add a solution to the "Maximum Product of Three Numbers" challenge

--- 628_Maximum_Product_of_Three_Numbers/solution.rb
+++ 628_Maximum_Product_of_Three_Numbers/solution.rb
@@ -1,5 +1,10 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def maximum_product(nums)
-    
+  nums.sort!
+
+  [ 
+    nums.last(3), 
+    nums.first(2) << nums.last
+  ].map {|arr| arr.inject(&:*) }.max
 end
