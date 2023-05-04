85588d56bf464f7b35d321fb359b8450515febf7
ozovalihasan <ozovalihasan@gmail.com>
2023-05-04 12:47:13 +0300

    Add a solution to the "Dota2 Senate" challenge

--- 649_Dota2_Senate/solution.rb
+++ 649_Dota2_Senate/solution.rb
@@ -1,5 +1,19 @@
 # @param {String} senate
 # @return {String}
 def predict_party_victory(senate)
-    
+  remove_senate = 0
+  rights = {"D" => 1, "R" => -1 }
+  senate = senate.split("").map! {|party| rights[party]}
+  
+  loop do
+    senate.each_with_index do |party, index|
+      senate[index] = 0 if (remove_senate * party) < 0
+
+      remove_senate += party
+    end
+
+    senate.delete(0)
+    return "Dire" if senate.none? -1
+    return "Radiant" if senate.none? 1
+  end
 end
