89c10ae446f20a5ff24f8b74041dec7d1c8ccc66
ozovalihasan <ozovalihasan@gmail.com>
2023-05-05 11:18:21 +0300

    Add a solution to the "Maximum Number of Vowels in a Substring of Given Length" challenge

--- 1456_Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length/solution.rb
+++ 1456_Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length/solution.rb
@@ -1,6 +1,19 @@
 # @param {String} s
 # @param {Integer} k
 # @return {Integer}
+VOWELS = {'a' => true, 'e' => true, 'i' => true, 'o' => true, 'u' => true }
+
 def max_vowels(s, k)
-    
+  vowels_count = s[0...k].count(VOWELS.keys.join)
+  max_count = vowels_count
+  s_rest = s[k..]
+  s_rest.each_char.with_index do |char, index|
+    vowels_count -= 1 if VOWELS[ s[index] ]
+    vowels_count += 1 if VOWELS[ char ]
+
+    max_count = vowels_count if max_count < vowels_count 
+    return max_count if max_count == k
+  end
+
+  max_count
 end
