066f20c96dd102320391859ed30147925f9fc83e
ozovalihasan <ozovalihasan@gmail.com>
2023-05-06 12:15:17 +0300

    Add a solution to the "Number of Subsequences That Satisfy the Given Sum Condition" challenge

--- 1498_Number_of_Subsequences_That_Satisfy_the_Given_Sum_Condition/solution.rb
+++ 1498_Number_of_Subsequences_That_Satisfy_the_Given_Sum_Condition/solution.rb
@@ -2,5 +2,16 @@
 # @param {Integer} target
 # @return {Integer}
 def num_subseq(nums, target)
-    
+  nums.sort!
+
+  result = 0
+
+  nums.each_with_index do |num, index|
+    break if (num * 2) > target
+
+    max_index = (nums.bsearch_index {|num_searched| num_searched > (target - num) } || nums.size) - 1
+    result += 2 ** (max_index - index )
+  end
+
+  result % (10 ** 9 + 7)
 end
