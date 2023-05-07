f3f915142c0c560f6983a95cfb1b0a875348e630
ozovalihasan <ozovalihasan@gmail.com>
2023-05-07 17:09:59 +0300

    Add a solution to the "Find the Longest Valid Obstacle Course at Each Position" challenge

--- 1964_Find_the_Longest_Valid_Obstacle_Course_at_Each_Position/solution.rb
+++ 1964_Find_the_Longest_Valid_Obstacle_Course_at_Each_Position/solution.rb
@@ -1,5 +1,11 @@
 # @param {Integer[]} obstacles
 # @return {Integer[]}
 def longest_obstacle_course_at_each_position(obstacles)
-    
+  cache = []
+  obstacles.map.each_with_index do |obstacle, index2|
+    index = cache.bsearch_index {|el| el > obstacle } || cache.size
+
+    cache[index] = obstacle
+    index + 1
+  end
 end
