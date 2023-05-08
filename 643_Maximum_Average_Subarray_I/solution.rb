2b088fb541294b7bec4c53a659d0067f1987cc32
ozovalihasan <ozovalihasan@gmail.com>
2023-05-08 12:16:07 +0300

    Add a solution to the "Maximum Average Subarray I" challenge

--- 643_Maximum_Average_Subarray_I/solution.rb
+++ 643_Maximum_Average_Subarray_I/solution.rb
@@ -2,5 +2,13 @@
 # @param {Integer} k
 # @return {Float}
 def find_max_average(nums, k)
-  
+  max_sum = sum = nums.first(k).sum
+
+  k.upto(nums.size - 1) do |index|
+    sum += nums[index] - nums[index - k] 
+
+    max_sum = sum if sum > max_sum
+  end
+
+  max_sum.to_f / k
 end
