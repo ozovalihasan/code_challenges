0f136d84463d6b8924d39c3ae63875f3b36feb07
ozovalihasan <ozovalihasan@gmail.com>
2023-05-09 11:01:19 +0300

    Add a solution to the "Spiral Matrix" challenge

--- 54_Spiral_Matrix/solution.rb
+++ 54_Spiral_Matrix/solution.rb
@@ -1,5 +1,14 @@
 # @param {Integer[][]} matrix
 # @return {Integer[]}
 def spiral_order(matrix)
-    
+  result = []
+
+  until matrix.empty?
+    result.concat matrix.shift
+    result.concat matrix.map(&:pop)
+    result.concat (matrix.pop || []).reverse
+    result.concat matrix.map(&:shift).reverse
+  end
+
+  result.compact
 end
