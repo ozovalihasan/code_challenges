a22914abae306614f5d6a06bdce1eb349e76e44a
ozovalihasan <ozovalihasan@gmail.com>
2023-05-11 18:07:13 +0300

    Add a solution to the "Uncrossed Lines" challenge

--- 1035_Uncrossed_Lines/solution.rb
+++ 1035_Uncrossed_Lines/solution.rb
@@ -2,5 +2,19 @@
 # @param {Integer[]} nums2
 # @return {Integer}
 def max_uncrossed_lines(nums1, nums2)
+  indices_of_nums2 = {}
+  nums2.each_with_index {|num2, index2| (indices_of_nums2[num2] ||= []) << index2 } 
+  indices_of_nums2.transform_values!(&:reverse)
+  
+  result = []
+  nums1.each_with_index do |num|
     
+    indices_of_nums2[num]&.each do |index|
+      max_index = result.rindex {|indices| indices.any? {|previous_index| previous_index < index }} || -1
+      (result[max_index + 1] ||= []) << index
+    end
+    
+  end
+
+  result.size
 end
