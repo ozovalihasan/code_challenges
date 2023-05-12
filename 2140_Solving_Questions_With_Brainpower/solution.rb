cbbf27dd997f961b70fc6dc3868b0fd3f11cb203
ozovalihasan <ozovalihasan@gmail.com>
2023-05-12 11:37:40 +0300

    Add a solution to the "Solving Questions With Brainpower" challenge

--- 2140_Solving_Questions_With_Brainpower/solution.rb
+++ 2140_Solving_Questions_With_Brainpower/solution.rb
@@ -1,5 +1,18 @@
 # @param {Integer[][]} questions
 # @return {Integer}
 def most_points(questions)
-    
+  result = [0]
+
+  questions.each_with_index do |(point, brainpower), index|
+    next_index = brainpower + index + 1
+    if result[index] + point > ( result[next_index] || 0 )
+      result[next_index] = result[index] + point 
+    end
+
+    if result[index] >= (result[index + 1] || 0)
+      result[index + 1] = result[index] 
+    end
+  end
+
+  result.compact.max
 end
