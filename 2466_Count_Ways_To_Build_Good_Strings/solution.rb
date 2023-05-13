f0f0cf662becf5fc7e9d1003554d7b3daecd84e3
ozovalihasan <ozovalihasan@gmail.com>
2023-05-13 19:29:59 +0300

    Add a solution to the "Count Ways To Build Good Strings" challenge

--- 2466_Count_Ways_To_Build_Good_Strings/solution.rb
+++ 2466_Count_Ways_To_Build_Good_Strings/solution.rb
@@ -3,6 +3,29 @@
 # @param {Integer} zero
 # @param {Integer} one
 # @return {Integer}
+MOD = (10**9 + 7)
+
 def count_good_strings(low, high, zero, one)
-    
+  
+  if zero == one
+    low = (low - 1) / zero + 1  
+    high = (high - 1) / zero + 1  
+    return ((2 ** low ) * (2 ** (high - low + 1) - 1)) % MOD
+  end
+  
+  @low, @high, @zero, @one = low, high, zero, one
+  @cache = []
+
+  check_by_starting(0) 
+end
+
+def check_by_starting(index)
+  return @cache[index] if @cache[index]
+  return @cache[index] = 0 if index > @high
+
+  subresult = ( index < @low ? 0 : 1 )
+  subresult += check_by_starting(index + @zero)
+  subresult += check_by_starting(index + @one)
+  
+  @cache[index] = subresult % MOD
 end
