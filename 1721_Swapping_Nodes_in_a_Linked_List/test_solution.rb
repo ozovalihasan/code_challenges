5bc974d25df686f22df15c6a530e2d431bddd581
ozovalihasan <ozovalihasan@gmail.com>
2023-05-15 11:28:12 +0300

    Add a test for the "Swapping Nodes in a Linked List" challenge

---
+++ 1721_Swapping_Nodes_in_a_Linked_List/test_solution.rb
@@ -0,0 +1,17 @@
+require_relative './solution'
+require_relative '../read_test_files'
+require_relative 'linked_list'
+
+describe 'Solution' do
+  examples, results = read_test_files.values_at(:examples, :results)
+
+  describe '#swap_nodes' do
+    examples.each_slice(2).each_with_index do |(arr, k), index|
+      it 'returns a linked list by swapping its left and right nodes' do
+        linked_list = LinkedList.new(arr)
+        linked_list.head = swap_nodes(linked_list.head, k)
+        expect(linked_list.to_a).to eq(results[index])
+      end
+    end
+  end
+end
