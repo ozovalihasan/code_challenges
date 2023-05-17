5a8fa13f2723aa9743b84eb94ab588fac60708e6
ozovalihasan <ozovalihasan@gmail.com>
2023-05-17 11:22:28 +0300

    Add a solution to the "Maximum Twin Sum of a Linked List" challenge

--- 2130_Maximum_Twin_Sum_of_a_Linked_List/solution.rb
+++ 2130_Maximum_Twin_Sum_of_a_Linked_List/solution.rb
@@ -9,5 +9,15 @@
 # @param {ListNode} head
 # @return {Integer}
 def pair_sum(head)
-    
+  @max = 0
+  sum_nodes(head, head)
+  @max
+end
+
+def sum_nodes(node, head)
+  return head if node.nil? 
+
+  node2 = sum_nodes(node.next, head)
+  @max = node2.val + node.val if (node2.val + node.val) > @max
+  node2.next
 end
