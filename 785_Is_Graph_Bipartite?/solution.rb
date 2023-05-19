4ace8d29c7f30c9b6c676d2b28c7fcdf23f22539
ozovalihasan <ozovalihasan@gmail.com>
2023-05-19 12:03:53 +0300

    Add a solution to the "Is Graph Bipartite?" challenge

--- 785_Is_Graph_Bipartite?/solution.rb
+++ 785_Is_Graph_Bipartite?/solution.rb
@@ -1,5 +1,23 @@
 # @param {Integer[][]} graph
 # @return {Boolean}
 def is_bipartite(graph)
-    
+  @graph = graph
+  @result = {}
+
+  graph.each_with_index do |nodes, node|
+    next if @result[node]
+     
+    return false unless check(node, :a)
+  end
+
+  true
+end
+
+def check(node, label)
+  return label == @result[node] if @result[node] 
+
+  @result[node] = label
+  
+  label = (label == :a) ? :b : :a
+  @graph[node].each { |next_node| return false unless check(next_node, label) }
 end
