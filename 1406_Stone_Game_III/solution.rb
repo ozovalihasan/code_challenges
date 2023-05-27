e9ca457cf36f0116471079ed401a663031192037
ozovalihasan <ozovalihasan@gmail.com>
2023-05-27 14:22:04 +0300

    Add one more test case to the "Stone Game III" challenge

--- 1406_Stone_Game_III/solution.rb
+++ 1406_Stone_Game_III/solution.rb
@@ -1,5 +1,20 @@
 # @param {Integer[]} stone_value
 # @return {String}
 def stone_game_iii(stone_value)
-  
+  result = Array.new(3, 0)
+  sum = 0
+
+  stone_value.reverse_each do |stone|
+    sum += stone
+    result.unshift( sum - result.min).pop
+  end
+
+  case result[0] <=> sum / 2.0 
+  when 1
+    "Alice"
+  when -1
+    "Bob"
+  else
+    "Tie"
+  end
 end
