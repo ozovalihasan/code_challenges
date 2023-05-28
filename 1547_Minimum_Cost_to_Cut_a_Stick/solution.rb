8b989c2ce1533bb3e433703e9e17e76a82c1440d
ozovalihasan <ozovalihasan@gmail.com>
2023-05-28 23:33:15 +0300

    Add a solution to the "Minimum Cost to Cut a Stick" challenge

--- 1547_Minimum_Cost_to_Cut_a_Stick/solution.rb
+++ 1547_Minimum_Cost_to_Cut_a_Stick/solution.rb
@@ -2,5 +2,27 @@
 # @param {Integer[]} cuts
 # @return {Integer}
 def min_cost(n, cuts)
-    
+  cuts.sort!
+  @cache = {0 => {}}
+  cuts.each {|cut| @cache[cut] = {}}
+  
+  check_cut(0, n, cuts)
+end
+
+def check_cut(start_index, end_index, cuts)
+  return @cache[start_index][end_index] if @cache[start_index][end_index]
+
+  selected_cuts = cuts.select {|cut_index| cut_index.between?(start_index + 1, end_index - 1)}
+  
+  return @cache[start_index][end_index] = 0 if selected_cuts.empty?
+  
+  cost = end_index - start_index
+  min_of_next_costs = selected_cuts
+                        .map do |selected_cut|
+                          check_cut(start_index, selected_cut, selected_cuts) + 
+                          check_cut(selected_cut, end_index, selected_cuts)
+                        end
+                        .min 
+                                   
+  @cache[start_index][end_index] = min_of_next_costs + cost 
 end
