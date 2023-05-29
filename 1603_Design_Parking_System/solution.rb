4a912c90fc3b8ab259bfa4d62d1a9c908e92607a
ozovalihasan <ozovalihasan@gmail.com>
2023-05-29 10:18:29 +0300

    Add a solution to the "Design Parking System" challenge

--- 1603_Design_Parking_System/solution.rb
+++ 1603_Design_Parking_System/solution.rb
@@ -1,12 +1,11 @@
 class ParkingSystem
-
 =begin
   :type big: Integer
   :type medium: Integer
   :type small: Integer
 =end
   def initialize(big, medium, small)
-
+    @slots = [0, big, medium, small]
   end
 
 
@@ -15,10 +14,11 @@ class ParkingSystem
   :rtype: Boolean
 =end
   def add_car(car_type)
-    
+    return false unless @slots[car_type].positive?
+
+    @slots[car_type] -= 1
+    true
   end
-
-
 end
 
 # Your ParkingSystem object will be instantiated and called as such:
