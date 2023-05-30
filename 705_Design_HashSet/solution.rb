041b6c54f37cd7a5eb9a66f057bb9c65f2328a1b
ozovalihasan <ozovalihasan@gmail.com>
2023-05-30 12:19:23 +0300

    Add a solution to the "Design HashSet" challenge

--- 705_Design_HashSet/solution.rb
+++ 705_Design_HashSet/solution.rb
@@ -1,36 +1,35 @@
 class MyHashSet
   def initialize()
-      
+    @hash = []
   end
-
 
 =begin
   :type key: Integer
   :rtype: Void
 =end
   def add(key)
-      
+    index = @hash.bsearch_index {|num| num >= key} || @hash.size
+    return if @hash[index] == key
+    
+    @hash.insert index, key
   end
-
 
 =begin
   :type key: Integer
   :rtype: Void
 =end
   def remove(key)
-      
+    index = @hash.bsearch_index {|num| key <=> num}
+    @hash.delete_at(index) if index
   end
-
 
 =begin
   :type key: Integer
   :rtype: Boolean
 =end
   def contains(key)
-      
+    @hash.bsearch_index {|num| key <=> num} != nil
   end
-
-
 end
 
 # Your MyHashSet object will be instantiated and called as such:
