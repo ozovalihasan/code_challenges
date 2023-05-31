c4c4fb68d978b8eec57b2d4e871a00afc2e289e8
ozovalihasan <ozovalihasan@gmail.com>
2023-05-31 10:48:41 +0300

    Add a test for the "Design Underground System" challenge

---
+++ 1396_Design_Underground_System/test_solution.rb
@@ -0,0 +1,28 @@
+require_relative './solution'
+require_relative '../read_test_files'
+
+describe 'Solution' do
+  examples, results = read_test_files.values_at(:examples, :results)
+
+  describe 'UndergroundSystem' do
+    examples.each_slice(2).each_with_index do |(actions, params), index|
+      it "returns UndergroundSystem keeping track of customer travel times between different stations" do
+        underground_system = nil
+
+        actions.zip(params).each_with_index do |(action, param), index2|
+          case action
+          when "UndergroundSystem"
+            underground_system = UndergroundSystem.new
+          when "checkIn"
+            underground_system.check_in(*param)
+          when "checkOut"
+            underground_system.check_out(*param)
+          when "getAverageTime"
+            expect(underground_system.get_average_time(*param)).to eq(results[index][index2])
+          end
+        end
+
+      end
+    end
+  end
+end
