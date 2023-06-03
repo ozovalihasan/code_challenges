bc4d9302083daf302d44f4e51431eae65677e8f8
ozovalihasan <ozovalihasan@gmail.com>
2023-06-03 13:25:13 +0300

    Add a solution to the "Time Needed to Inform All Employees" challenge

--- 1376_Time_Needed_to_Inform_All_Employees/solution.rb
+++ 1376_Time_Needed_to_Inform_All_Employees/solution.rb
@@ -4,5 +4,27 @@
 # @param {Integer[]} inform_time
 # @return {Integer}
 def num_of_minutes(n, head_id, manager, inform_time)
-    
+  @inform_time = inform_time
+  @employees = []
+  
+  manager.each_with_index do |manager_of_employee, employee|
+    next if manager_of_employee == -1
+
+    (@employees[manager_of_employee] ||= []) << employee
+  end
+  
+  @max_time = 0
+  check_manager(head_id, 0)
+  @max_time
+end
+
+def check_manager(manager_id, time)
+  return if @employees[manager_id].nil?
+
+  time += @inform_time[manager_id] 
+  @max_time = time if time > @max_time 
+  
+  @employees[manager_id].each do |employee_id|
+    check_manager(employee_id, time)
+  end
 end
