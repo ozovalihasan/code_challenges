0acd7ba93aa25935833359f13b4e820b93c659d0
ozovalihasan <ozovalihasan@gmail.com>
2023-06-04 21:16:07 +0300

    Add a solution to the "Rotate Function" challenge

--- 396_Rotate_Function/solution.rb
+++ 396_Rotate_Function/solution.rb
@@ -1,5 +1,18 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def max_rotate_function(nums)
-    
+  sum = nums.sum
+  nums_size = nums.size
+  
+  result = 0
+  nums.each_with_index { |num, index| result += num * index }
+
+  max_result = result
+
+  nums.each do |num|
+    result += (nums_size * num) - sum
+    max_result = result if result > max_result
+  end
+  
+  max_result
 end
