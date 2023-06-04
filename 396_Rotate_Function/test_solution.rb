a074042259f0f014d7f3e3b156d2cdaa0827751f
ozovalihasan <ozovalihasan@gmail.com>
2023-06-04 21:04:35 +0300

    Add a test for the "Rotate Function" challenge

---
+++ 396_Rotate_Function/test_solution.rb
@@ -0,0 +1,14 @@
+require_relative './solution'
+require_relative '../read_test_files'
+
+describe 'Solution' do
+  examples, results = read_test_files.values_at(:examples, :results)
+
+  describe '#max_rotate_function' do
+    examples.each_with_index do |nums, index|
+      it 'returns the maximum value of F(0), F(1), ..., F(n-1)' do
+        expect(max_rotate_function(nums)).to eq(results[index])
+      end
+    end
+  end
+end
