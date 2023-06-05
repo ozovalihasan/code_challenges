dbdc35573ad9f32d6fc1794c11024ca0d966ce17
ozovalihasan <ozovalihasan@gmail.com>
2023-06-05 11:22:47 +0300

    Add a solution to the "Rabbits in Forest" challenge

--- 781_Rabbits_in_Forest/solution.rb
+++ 781_Rabbits_in_Forest/solution.rb
@@ -1,5 +1,8 @@
 # @param {Integer[]} answers
 # @return {Integer}
 def num_rabbits(answers)
-    
+  rabbit_groups = answers.group_by(&:itself).transform_values {|val| val.size.to_f}
+
+  rabbit_groups.map { |key, value| ((value / (key + 1)).ceil) * (key + 1) }
+               .sum
 end
