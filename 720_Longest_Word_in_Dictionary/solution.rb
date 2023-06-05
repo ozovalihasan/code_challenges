71e100a8063b437909ff31c52bf78f0c12df057c
ozovalihasan <ozovalihasan@gmail.com>
2023-06-05 11:36:42 +0300

    Add a solution to the "Longest Word in Dictionary" challenge

--- 720_Longest_Word_in_Dictionary/solution.rb
+++ 720_Longest_Word_in_Dictionary/solution.rb
@@ -1,5 +1,10 @@
 # @param {String[]} words
 # @return {String}
 def longest_word(words)
-  
+  found_words = {"" => true}
+  words.sort!.each do |word|
+    found_words[word] = true if found_words[word[...-1]]
+  end
+
+  found_words.keys.max_by(&:length)
 end
