ba3e7735517be9eaec8300e446c8bf5ecbf7c593
ozovalihasan <ozovalihasan@gmail.com>
2023-06-05 15:04:55 +0300

    Add a solution to the "Best Sightseeing Pair" challenge

--- 1014_Best_Sightseeing_Pair/solution.rb
+++ 1014_Best_Sightseeing_Pair/solution.rb
@@ -1,5 +1,20 @@
 # @param {Integer[]} values
 # @return {Integer}
 def max_score_sightseeing_pair(values)
-    
+  minus_values = values.map.with_index { |value, index| value - index }
+
+  plus_values = values.map.with_index { |value, index| value + index }
+
+  max_upto_index = - Float::INFINITY
+  max_score = - Float::INFINITY
+
+  1.upto(values.size - 1) do |index|
+    value2 = minus_values[index]
+    max_upto_index = plus_values[index - 1] if max_upto_index < plus_values[index - 1]
+
+    score = max_upto_index + value2
+    max_score = score if score > max_score
+  end
+
+  max_score
 end
