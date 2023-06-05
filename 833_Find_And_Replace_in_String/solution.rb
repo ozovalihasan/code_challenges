360d020ee9b1a0aa27c9175e99ad5bce8a67cb30
ozovalihasan <ozovalihasan@gmail.com>
2023-06-05 11:55:02 +0300

    Add a solution to the "Find And Replace in String" challenge

--- 833_Find_And_Replace_in_String/solution.rb
+++ 833_Find_And_Replace_in_String/solution.rb
@@ -4,5 +4,12 @@
 # @param {String[]} targets
 # @return {String}
 def find_replace_string(s, indices, sources, targets)
-    
+  result = s
+  checked_sources = indices.zip(sources.zip(targets)).sort_by!(&:first)
+
+  checked_sources.reverse_each do |index, (source, target)|
+    result[index...index + source.size] = target if s[index...index + source.size] == source
+  end
+
+  result
 end
