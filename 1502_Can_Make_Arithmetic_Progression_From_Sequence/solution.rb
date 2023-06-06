43b42a53d6b7305d6afceb8f4d1f08be8e76d8e9
ozovalihasan <ozovalihasan@gmail.com>
2023-06-06 10:31:27 +0300

    Add a solution to the "Can Make Arithmetic Progression From Sequence" challenge

--- 1502_Can_Make_Arithmetic_Progression_From_Sequence/solution.rb
+++ 1502_Can_Make_Arithmetic_Progression_From_Sequence/solution.rb
@@ -1,5 +1,14 @@
 # @param {Integer[]} arr
 # @return {Boolean}
 def can_make_arithmetic_progression(arr)
+  arr.sort!
+  diff = arr[1] - arr[0]
+  result = arr.shift
+  
+  arr.each do |num|
+    result += diff
     
+    return false unless num == result
+  end
+  true
 end
