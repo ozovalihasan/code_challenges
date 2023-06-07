0620d248f5aaa560172630f72ea23448f210185a
ozovalihasan <ozovalihasan@gmail.com>
2023-06-07 11:23:14 +0300

    Add a solution to the "Minimum Flips to Make a OR b Equal to c" challenge

--- 1318_Minimum_Flips_to_Make_a_OR_b_Equal_to_c/solution.rb
+++ 1318_Minimum_Flips_to_Make_a_OR_b_Equal_to_c/solution.rb
@@ -3,5 +3,22 @@
 # @param {Integer} c
 # @return {Integer}
 def min_flips(a, b, c)
-    
+  a, b, c = set_numbers_as_reverse_binary(a, b, c)
+  max_size = [a.size, b.size, c.size].max
+  
+  result = 0
+  0.upto(max_size - 1) do |index|
+    if c[index] == "1"
+      result += 1 if (a[index] != "1") && (b[index] != "1")
+    else
+      result += 1 if a[index] == "1"
+      result += 1 if b[index] == "1"
+    end
+  end
+
+  result
+end
+
+def set_numbers_as_reverse_binary(*nums)
+  nums.map {|num| num.to_s(2).reverse}
 end
