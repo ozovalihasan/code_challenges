bc535c0aae71fe070ab2eed5dc320db391840d7c
ozovalihasan <ozovalihasan@gmail.com>
2023-06-07 12:11:16 +0300

    Add a solution to the "Integer Replacement" challenge

--- 397_Integer_Replacement/solution.rb
+++ 397_Integer_Replacement/solution.rb
@@ -1,5 +1,19 @@
 # @param {Integer} n
 # @return {Integer}
 def integer_replacement(n)
+  @cache = {1 => 0}  
+  find_number(n)
+end
+
+def find_number(n)
+  return @cache[n] if @cache[n]
     
+  if n.even?
+    @cache[n] = find_number(n / 2) + 1
+  else
+    @cache[n] = [
+      find_number(n + 1),
+      find_number(n - 1)
+    ].min + 1
+  end
 end
