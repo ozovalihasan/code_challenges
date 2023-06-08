d74f509e386527f7e332db69773b1b6b10bad5e9
ozovalihasan <ozovalihasan@gmail.com>
2023-06-08 10:24:54 +0300

    Add a solution to the "Count Negative Numbers in a Sorted Matrix" challenge

--- 1351_Count_Negative_Numbers_in_a_Sorted_Matrix/solution.rb
+++ 1351_Count_Negative_Numbers_in_a_Sorted_Matrix/solution.rb
@@ -1,5 +1,6 @@
 # @param {Integer[][]} grid
 # @return {Integer}
 def count_negatives(grid)
-    
+  row_size = grid.first.size
+  grid.sum { |row| row_size - (row.bsearch_index(&:negative?) || row_size) }
 end
