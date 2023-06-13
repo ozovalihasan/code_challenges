68218c8c7f50a02450d338d829946bdefd43f6ee
ozovalihasan <ozovalihasan@gmail.com>
2023-06-13 10:41:09 +0300

    Add a solution to the "Equal Row and Column Pairs" challenge

--- 2352_Equal_Row_and_Column_Pairs/solution.rb
+++ 2352_Equal_Row_and_Column_Pairs/solution.rb
@@ -1,5 +1,8 @@
 # @param {Integer[][]} grid
 # @return {Integer}
 def equal_pairs(grid)
-    
+  grid, grid_transpose = grid.tally, grid.transpose.tally
+  grid_transpose.default = 0
+
+  grid.sum {|row, count| grid_transpose[row] * count }
 end
