4da3e0069857035600bfa42f2cfb95ac5cb2d058
ozovalihasan <ozovalihasan@gmail.com>
2023-06-14 09:58:42 +0300

    Add a solution to the "Minimum Absolute Difference in BST" challenge

--- 530_Minimum_Absolute_Difference_in_BST/solution.rb
+++ 530_Minimum_Absolute_Difference_in_BST/solution.rb
@@ -10,5 +10,19 @@
 # @param {TreeNode} root
 # @return {Integer}
 def get_minimum_difference(root)
-    
+  @values = []
+  check_node(root)
+  
+  @values.each_cons(2)
+         .map {|first_val, second_val| (second_val - first_val).abs }
+         .min
+end
+
+def check_node(node)
+  return if node.nil?
+
+  index = @values.bsearch_index {|val| val > node.val} || @values.size
+  @values.insert(index, node.val)
+  check_node(node.left)
+  check_node(node.right)
 end
