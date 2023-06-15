d84cbbdb0acbb9506e79c2f18051a4f947fcfe22
ozovalihasan <ozovalihasan@gmail.com>
2023-06-15 10:29:02 +0300

    Add a solution to the "Maximum Level Sum of a Binary Tree" challenge

--- 1161_Maximum_Level_Sum_of_a_Binary_Tree/solution.rb
+++ 1161_Maximum_Level_Sum_of_a_Binary_Tree/solution.rb
@@ -10,5 +10,22 @@
 # @param {TreeNode} root
 # @return {Integer}
 def max_level_sum(root)
-    
+  max_level = 0
+  max_sum = -Float::INFINITY
+
+  current_nodes = [root]
+  level = 1
+
+  until current_nodes.empty?
+    level_sum = current_nodes.sum(&:val)
+    if level_sum > max_sum
+      max_level = level
+      max_sum = level_sum
+    end
+    level += 1
+    current_nodes.pop(current_nodes.size).each {|node| current_nodes << node.left <<  node.right }
+    current_nodes.compact!
+  end
+
+  max_level
 end
