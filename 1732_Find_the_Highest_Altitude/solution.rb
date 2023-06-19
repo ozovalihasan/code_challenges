34228c7e7cb373424cb6004dd20d549673e5a535
ozovalihasan <ozovalihasan@gmail.com>
2023-06-19 12:00:41 +0300

    Add a solution to the "Find the Highest Altitude" challenge

--- 1732_Find_the_Highest_Altitude/solution.rb
+++ 1732_Find_the_Highest_Altitude/solution.rb
@@ -1,5 +1,13 @@
 # @param {Integer[]} gain
 # @return {Integer}
 def largest_altitude(gain)
-    
+  net_height = 0
+  max = net_height
+
+  gain.each do |height|
+    net_height += height
+    max = net_height if net_height > max
+  end
+
+  max
 end
