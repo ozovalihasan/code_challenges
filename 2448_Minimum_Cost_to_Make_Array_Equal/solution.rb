dcd245cc2abf2a6b849e99bc9474ceddba63aaf4
ozovalihasan <ozovalihasan@gmail.com>
2023-06-21 12:34:43 +0300

    Add a solution to the "Minimum Cost to Make Array Equal" challenge

--- 2448_Minimum_Cost_to_Make_Array_Equal/solution.rb
+++ 2448_Minimum_Cost_to_Make_Array_Equal/solution.rb
@@ -1,6 +1,10 @@
 # @param {Integer[]} nums
-# @param {Integer[]} cost
+# @param {Integer[]} costs
 # @return {Integer}
-def min_cost(nums, cost)
-    
+def min_cost(nums, costs)
+  min, max = nums.minmax
+  return 0 if min == max
+
+  memo = Hash.new { |h, k| h[k] = nums.zip(costs).sum(0) { |number, cost| cost * (k - number).abs } }
+  memo[(min..max).bsearch { |number| memo[number] < memo[number + 1] }]
 end
