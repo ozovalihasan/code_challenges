658a93d6e08659b61fac5eaeec2d3d8934caa207
ozovalihasan <ozovalihasan@gmail.com>
2023-06-22 12:15:44 +0300

    Add a solution to the "Best Time to Buy and Sell Stock with Transaction Fee" challenge

--- 714_Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee/solution.rb
+++ 714_Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee/solution.rb
@@ -2,5 +2,12 @@
 # @param {Integer} fee
 # @return {Integer}
 def max_profit(prices, fee)
-    
+  bought = - Float::INFINITY
+  sold = 0
+
+  prices.each do |price|
+    bought, sold = [sold - price - fee, bought].max, [bought + price, sold].max
+  end
+
+  sold
 end
