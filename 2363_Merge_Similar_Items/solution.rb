5ed3258ea8864f65af8601dc79ff0bd30c5957dd
ozovalihasan <ozovalihasan@gmail.com>
2023-06-24 12:16:06 +0300

    Add a solution to the "Merge Similar Items" challenge

--- 2363_Merge_Similar_Items/solution.rb
+++ 2363_Merge_Similar_Items/solution.rb
@@ -2,5 +2,5 @@
 # @param {Integer[][]} items2
 # @return {Integer[][]}
 def merge_similar_items(items1, items2)
-    
+  (items1 + items2).each_with_object(Hash.new(0)) { |(key, val), result| result[key] += val }.sort
 end
