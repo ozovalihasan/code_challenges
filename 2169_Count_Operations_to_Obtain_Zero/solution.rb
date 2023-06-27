124b28348b4ff8503137ca5c56d392acf857dd89
ozovalihasan <ozovalihasan@gmail.com>
2023-06-28 01:19:32 +0300

    Add a solution to the "Count Operations to Obtain Zero" challenge

--- 2169_Count_Operations_to_Obtain_Zero/solution.rb
+++ 2169_Count_Operations_to_Obtain_Zero/solution.rb
@@ -2,5 +2,17 @@
 # @param {Integer} num2
 # @return {Integer}
 def count_operations(num1, num2)
+  count = 0
+  
+  until (num1 * num2).zero? 
+    if num1 > num2
+      count += num1 / num2
+      num1 %= num2
+    else
+      count += num2 / num1
+      num2 %= num1
+    end
+  end
 
+  count
 end
