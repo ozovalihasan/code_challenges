8dec0dcb2ba958b7a3fd535b77e417e6fa5d4af3
ozovalihasan <ozovalihasan@gmail.com>
2023-06-28 18:23:26 +0300

    Update the solution of the "Path with Maximum Probability" challenge

--- 1514_Path_with_Maximum_Probability/solution.rb
+++ 1514_Path_with_Maximum_Probability/solution.rb
@@ -8,31 +8,31 @@ def max_probability(n, edges, succ_prob, start_point, end_point)
   @points = Array.new(n) {[]}
   @end_point = end_point
 
-  edges.zip(succ_prob).each do |(point1, point2), prob|
-    @points[point1] << [point2, prob]
-    @points[point2] << [point1, prob]
+  edges.zip(succ_prob).each do |(point1, next_point), prob|
+    @points[point1] << [next_point, prob]
+    @points[next_point] << [point1, prob]
   end
 
   result = 0
   queue = [ [start_point, 1] ]
   checked = {}
   loop do
-    point, prob = queue.pop
-    while checked[point] 
-      point, prob = queue.pop
+    checked_point, checked_prob = queue.pop
+    while checked[checked_point] 
+      checked_point, checked_prob = queue.pop
     end
     
-    break unless prob
-    break if prob <= result
+    break unless checked_prob
+    break if checked_prob <= result
 
-    checked[point] = true
+    checked[checked_point] = true
 
-    @points[point].each do |point2, prob2|
-      next if checked[point2]
-      next result = [result, prob2 * prob].max if point2 == end_point
+    @points[checked_point].each do |next_point, next_prob|
+      next if checked[next_point]
+      next result = [result, next_prob * checked_prob].max if next_point == end_point
       
-      index = queue.bsearch_index {|_, prob1| prob1 > (prob2 * prob) } || queue.size
-      queue.insert(index, [point2, prob2 * prob])
+      index = queue.bsearch_index {|_, prob_from_queue| prob_from_queue > (next_prob * checked_prob) } || queue.size
+      queue.insert(index, [next_point, next_prob * checked_prob])
     end
   end
 
