3c285f8c4c1e45231bff4030fa06bc9fe12d5cd1
ozovalihasan <ozovalihasan@gmail.com>
2023-07-03 12:59:15 +0300

    Add a solution to the "Buddy Strings" challenge

--- 859_Buddy_Strings/solution.rb
+++ 859_Buddy_Strings/solution.rb
@@ -2,5 +2,11 @@
 # @param {String} goal
 # @return {Boolean}
 def buddy_strings(s, goal)
-    
+  s_chars_count = s.chars.tally
+  goal_chars_count = goal.chars.tally
+  
+  return false if s_chars_count != goal_chars_count
+  return true if s == goal && s_chars_count.size < s.length
+
+  s.chars.zip(goal.chars).count {|char1, char2| char1 != char2} == 2
 end
