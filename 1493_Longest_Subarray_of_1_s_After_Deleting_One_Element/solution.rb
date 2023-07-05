55d6e02d73e13ac71a5e9d84e612fa1651ae2551
ozovalihasan <ozovalihasan@gmail.com>
2023-07-05 14:22:06 +0300

    Add a solution to the "Longest Subarray of 1's After Deleting One Element" challenge

--- 1493_Longest_Subarray_of_1_s_After_Deleting_One_Element/solution.rb
+++ 1493_Longest_Subarray_of_1_s_After_Deleting_One_Element/solution.rb
@@ -1,5 +1,14 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def longest_subarray(nums)
-    
+  return nums.size - 1 if nums.all? 1
+  return 0 if nums.all? 0
+  
+  counts = nums.chunk(&:zero?).map {|bool, arr| arr.size * (bool ? -1 : 1)}
+  counts.unshift 0
+
+  counts.each_cons(3)
+        .filter_map { |(first, middle, last)| first + last if middle == -1 }
+        .push(counts.max)
+        .max
 end
