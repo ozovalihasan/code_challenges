3054929b2965019cb42d73255228f1e794061e8c
ozovalihasan <ozovalihasan@gmail.com>
2023-07-06 12:19:46 +0300

    Add a solution to the "Minimum Size Subarray Sum" challenge

--- 209_Minimum_Size_Subarray_Sum/solution.rb
+++ 209_Minimum_Size_Subarray_Sum/solution.rb
@@ -2,5 +2,22 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def min_sub_array_len(target, nums)
-    
+  start_index = 0
+  sum = 0
+  lengths = []
+
+  nums.each_with_index do |num, index|
+    sum += num
+
+    if sum >= target
+      until (sum - nums[start_index]) < target  do
+        sum -= nums[start_index]
+        start_index += 1
+      end
+
+      lengths << (index - start_index + 1)
+    end
+  end
+
+  lengths.min || 0
 end
