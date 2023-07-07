4c97f8dd5a933918f75f96dfc7f30271fb29ca76
ozovalihasan <ozovalihasan@gmail.com>
2023-07-07 14:27:31 +0300

    Add a solution to the "Maximize the Confusion of an Exam" challenge

--- 2024_Maximize_the_Confusion_of_an_Exam/solution.rb
+++ 2024_Maximize_the_Confusion_of_an_Exam/solution.rb
@@ -2,5 +2,29 @@
 # @param {Integer} k
 # @return {Integer}
 def max_consecutive_answers(answer_key, k)
-    
+  [check_for(answer_key, k, "F"), check_for(answer_key, k, "T")].max
+end
+
+def check_for(answer_key, k, reference_char)
+  return answer_key.length if answer_key.count(reference_char) <= k
+  
+  start_index = 0
+  result = 0
+  temp_result = 0
+  answer_key.each_char do |char|
+    k -= 1 if char == reference_char
+
+    temp_result += 1
+
+    while k < 0
+      k += 1 if answer_key[start_index] == reference_char
+
+      temp_result -= 1
+      start_index += 1
+    end
+
+    result = temp_result if temp_result > result
+  end
+
+  result
 end
