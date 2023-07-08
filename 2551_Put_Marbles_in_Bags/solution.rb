f9fd2b6fdd485d57b71f8ef3366292a089008dc2
ozovalihasan <ozovalihasan@gmail.com>
2023-07-08 14:32:45 +0300

    Add a solution to the "Put Marbles in Bags" challenge

--- 2551_Put_Marbles_in_Bags/solution.rb
+++ 2551_Put_Marbles_in_Bags/solution.rb
@@ -2,5 +2,6 @@
 # @param {Integer} k
 # @return {Integer}
 def put_marbles(weights, k)
-    
+  cons_sums = weights.each_cons(2).map(&:sum).sort
+  cons_sums.last(k - 1).sum - cons_sums.first(k - 1).sum
 end
