382b65b37d1808021b20a55075bb8c55fea57663
ozovalihasan <ozovalihasan@gmail.com>
2023-07-10 00:01:10 +0300

    Add a solution to the "Longest Nice Substring" challenge

--- 1763_Longest_Nice_Substring/solution.rb
+++ 1763_Longest_Nice_Substring/solution.rb
@@ -1,5 +1,12 @@
-# @param {String} s
+# @param {String} str
 # @return {String}
-def longest_nice_substring(s)
-    
+def longest_nice_substring(str)
+  counts_str = str.chars.tally
+
+  unpairs = counts_str.keys.reject {|chr| (chr == chr.upcase) ? counts_str[chr.downcase] : counts_str[chr.upcase]}
+  return str if unpairs.size.zero?
+  return "" if unpairs.size == counts_str.size
+  
+  substrings = str.split(Regexp.union(unpairs))
+  substrings.map { |substr| longest_nice_substring(substr) }.max_by(&:size)
 end
