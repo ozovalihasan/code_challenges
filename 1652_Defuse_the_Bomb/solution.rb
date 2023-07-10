e8513ea469c22377f5f7cbf559bd1ac369fbfe40
ozovalihasan <ozovalihasan@gmail.com>
2023-07-10 14:07:37 +0300

    Add a solution to the "Defuse the Bomb" challenge

--- 1652_Defuse_the_Bomb/solution.rb
+++ 1652_Defuse_the_Bomb/solution.rb
@@ -2,5 +2,13 @@
 # @param {Integer} k
 # @return {Integer[]}
 def decrypt(code, k)
-    
+  if k < 0
+    code.rotate!(k - 1)
+    k = -k
+  end
+
+  sum = code.first(k).sum
+  size = code.size
+  
+  code.each_index.map { |index| sum += code[(index + k) % size] - code[index] }
 end
