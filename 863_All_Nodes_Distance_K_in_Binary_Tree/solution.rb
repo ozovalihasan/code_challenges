32ce06933259d1043b5aa8512a885109ed3599b6
ozovalihasan <ozovalihasan@gmail.com>
2023-07-11 14:36:47 +0300

    Add a solution to the "All Nodes Distance K in Binary Tree" challenge

--- 863_All_Nodes_Distance_K_in_Binary_Tree/solution.rb
+++ 863_All_Nodes_Distance_K_in_Binary_Tree/solution.rb
@@ -11,5 +11,40 @@
 # @param {Integer} k
 # @return {Integer[]}
 def distance_k(root, target, k)
-    
+  @target = target
+  @max_step = k
+  @result = []
+  check_node(root)
+  @result
+end
+
+def check_node(node, step = nil)
+  return if node.nil?
+  return if step && (step < 0)
+
+  step = @max_step if node == @target
+  
+  step = check_left_and_right(node, step)
+
+  if step && step.zero?
+    @result << node.val
+  end
+
+  step -= 1 if step
+  step
+end
+
+def check_left_and_right(node, step)
+  step_left = check_node(node.left, step ? step - 1 : nil)
+  if step.nil? && step_left
+    step = step_left 
+  end
+
+  step_right = check_node(node.right, step ? step - 1 : nil)
+  if step.nil? && step_right
+    step = step_right
+    check_node(node.left, step ? step - 1 : nil)
+  end
+  
+  step
 end
