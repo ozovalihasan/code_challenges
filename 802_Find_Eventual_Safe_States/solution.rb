4ddf0231e02257a4d0fbb9d9afe6f45d44c4e25e
ozovalihasan <ozovalihasan@gmail.com>
2023-07-12 12:42:42 +0300

    Add a solution to the "Find Eventual Safe States" challenge

--- 802_Find_Eventual_Safe_States/solution.rb
+++ 802_Find_Eventual_Safe_States/solution.rb
@@ -1,5 +1,21 @@
 # @param {Integer[][]} graph
 # @return {Integer[]}
 def eventual_safe_nodes(graph)
-    
+  @graph = graph
+  @checked = {}
+  @terminal_nodes = {}
+
+  graph.each_index {|node_index| terminal_node?(node_index) unless @checked[node_index] }
+  
+  @terminal_nodes.keys.sort
+end
+
+def terminal_node?(node_index)
+  return @terminal_nodes[node_index] if @checked[node_index]
+
+  @checked[node_index] = true
+
+  if @graph[node_index].map! {|next_node_index| terminal_node?(next_node_index) }.all?(true)
+    @terminal_nodes[node_index] = true 
+  end
 end
