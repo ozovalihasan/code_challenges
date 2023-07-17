f6aa12f39ce8a159362cfb21d2ec4626c9395b07
ozovalihasan <ozovalihasan@gmail.com>
2023-07-17 13:50:19 +0300

    Add a solution to the "Add Two Numbers II" challenge

--- 445_Add_Two_Numbers_II/solution.rb
+++ 445_Add_Two_Numbers_II/solution.rb
@@ -10,5 +10,27 @@
 # @param {ListNode} l2
 # @return {ListNode}
 def add_two_numbers(l1, l2)
-    
+  digits1 = digits(l1)
+  digits2 = digits(l2)
+
+  total = digits1.to_i + digits2.to_i
+  set_link_list(total.digits)
+end
+
+def digits(list_node, digits = "")
+  return "" if list_node.nil?
+
+  list_node.val.to_s + digits(list_node.next, digits)
+end
+
+def set_link_list(arr)
+  head = ListNode.new(arr.pop)
+  cursor = head
+
+  until arr.empty?
+    cursor.next = ListNode.new(arr.pop)
+    cursor = cursor.next
+  end
+
+  head
 end
