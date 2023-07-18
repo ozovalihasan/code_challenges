639fe1c6c8cc470b22c7adccb34811737a394ea1
ozovalihasan <ozovalihasan@gmail.com>
2023-07-18 13:49:19 +0300

    Add a solution to the "Shifting Letters" challenge

--- 848_Shifting_Letters/solution.rb
+++ 848_Shifting_Letters/solution.rb
@@ -2,5 +2,12 @@
 # @param {Integer[]} shifts
 # @return {String}
 def shifting_letters(s, shifts)
-    
+  letters = ("a".."z").to_a
+
+  s = s.chars.map {|letter| letter.ord - 97}
+
+  sum = 0
+  shifts = shifts.reverse.map {|shift| sum += shift }.reverse
+  
+  s.zip(shifts).map {|index_with_shift| letters[index_with_shift.sum % 26] }.join
 end
