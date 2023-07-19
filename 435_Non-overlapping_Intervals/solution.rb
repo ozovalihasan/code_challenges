15fb3ba69838974f470a3582616699d317e823b1
ozovalihasan <ozovalihasan@gmail.com>
2023-07-19 14:36:23 +0300

    Add a solution to the "Non-overlapping Intervals" challenge

--- 435_Non-overlapping_Intervals/solution.rb
+++ 435_Non-overlapping_Intervals/solution.rb
@@ -1,5 +1,16 @@
 # @param {Integer[][]} intervals
 # @return {Integer}
 def erase_overlap_intervals(intervals)
-    
+  intervals.sort_by!(&:last)
+  
+  boundary = -Float::INFINITY
+  count = 0
+  intervals.each do |start_num, end_num|
+    if boundary <= start_num
+      boundary = end_num
+      count += 1
+    end
+  end
+
+  intervals.size - count
 end
