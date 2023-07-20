cf9593a1b516c2d16c8e9bbb256043e8e5580a13
ozovalihasan <ozovalihasan@gmail.com>
2023-07-20 12:55:52 +0300

    Add a solution to the "Asteroid Collision" challenge

--- 735_Asteroid_Collision/solution.rb
+++ 735_Asteroid_Collision/solution.rb
@@ -1,5 +1,24 @@
 # @param {Integer[]} asteroids
 # @return {Integer[]}
 def asteroid_collision(asteroids)
-    
+  index = 0
+  stack = []
+
+  asteroids.each do |asteroid|
+    stack << asteroid
+
+    loop do
+      break unless stack[-2]&.positive? && stack[-1]&.negative?
+
+      if stack.last(2).sum > 0
+        stack.pop
+      elsif stack.last(2).sum.zero?
+        stack.pop(2)
+      else
+        stack.delete_at(-2)
+      end
+    end    
+  end
+
+  stack
 end
