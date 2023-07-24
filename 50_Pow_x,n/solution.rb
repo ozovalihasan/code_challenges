35916bb57ffb1e9ff6112b22af0711f17fd7b229
ozovalihasan <ozovalihasan@gmail.com>
2023-07-24 13:32:42 +0300

    Add a solution to the "Pow(x, n)" challenge

--- 50_Pow_x,n/solution.rb
+++ 50_Pow_x,n/solution.rb
@@ -2,5 +2,13 @@
 # @param {Integer} n
 # @return {Float}
 def my_pow(x, n)
-    
+  return 1 if n.zero?
+  
+  if n.negative?
+    x = 1 / x
+    n = -n
+  end
+  
+  result = my_pow(x * x, n / 2)
+  n.odd? ? result * x : result
 end
