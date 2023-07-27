a691d1472030efb4d635854a4a919f31abd6f956
ozovalihasan <ozovalihasan@gmail.com>
2023-07-27 13:20:29 +0300

    Add a solution to the "Maximum Running Time of N Computers" challenge

--- 2141_Maximum_Running_Time_of_N_Computers/solution.rb
+++ 2141_Maximum_Running_Time_of_N_Computers/solution.rb
@@ -2,5 +2,13 @@
 # @param {Integer[]} batteries
 # @return {Integer}
 def max_run_time(n, batteries)
-    
+  batteries.sort!
+
+  total_energy = batteries.shift(batteries.size - n).sum
+  used_computers = 0
+  batteries.map do |battery|
+    total_energy += battery
+    used_computers += 1
+    total_energy / used_computers
+  end.min
 end
