a72a3c894a33b069632495468f6e113bde2193d8
ozovalihasan <ozovalihasan@gmail.com>
2023-07-29 15:21:38 +0300

    Update the test of the "Soup Servings" challenge

--- 808_Soup_Servings/test_solution.rb
+++ 808_Soup_Servings/test_solution.rb
@@ -7,7 +7,7 @@ describe 'Solution' do
   describe '#soup_servings' do
     examples.each_with_index do |n, index|
       it 'returns the probability that soup A will be empty first, plus half the probability that A and B become empty at the same time' do
-        expect(soup_servings(n)).to eq(results[index])
+        expect(soup_servings(n).round(5)).to eq(results[index])
       end
     end
   end
