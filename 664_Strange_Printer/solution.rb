f5a534341160956b2be5c3f47ea1a7c8cb1a1068
ozovalihasan <ozovalihasan@gmail.com>
2023-07-30 20:48:08 +0300

    Add a solution to the "Strange Printer" challenge

--- 664_Strange_Printer/solution.rb
+++ 664_Strange_Printer/solution.rb
@@ -1,5 +1,28 @@
 # @param {String} s
 # @return {Integer}
 def strange_printer(s)
-    
+  @s = s.gsub(/(.)\1*/, '\1').chars
+  
+  @memo = {}
+  @indices = {}
+  @s.each_with_index do |char, index|
+    (@indices[char] ||= []) << index
+  end
+  
+  check_str(0, @s.size - 1)
+end
+
+def check_str(start_index, end_index)
+  return @memo[[start_index, end_index]] if @memo[[start_index, end_index]]
+  return 0 if start_index > end_index
+  
+  char = @s[start_index]
+  
+  suitable_indices = @indices[char].select {|char_index| char_index.between?(start_index + 1, end_index) }
+  sub_results = suitable_indices.map! do |char_index|
+                  check_str(start_index + 1, char_index - 1) + check_str(char_index, end_index)
+                end
+
+  sub_results << ( check_str(start_index + 1, end_index) + 1 )
+  @memo[[start_index, end_index]] = sub_results.min
 end
