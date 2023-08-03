46ac26c60611c2aaf5bbf69f3b33f2353cdaaa3d
ozovalihasan <ozovalihasan@gmail.com>
2023-08-03 12:29:15 +0300

    Add a solution to the "Letter Combinations of a Phone Number" challenge

--- 17_Letter_Combinations_of_a_Phone_Number/solution.rb
+++ 17_Letter_Combinations_of_a_Phone_Number/solution.rb
@@ -1,5 +1,28 @@
 # @param {String} digits
 # @return {String[]}
+DIGIT_CHAR_PAIRS = {
+  "2" => ["a", "b", "c"], 
+  "3" => ["d", "e", "f"], 
+  "4" => ["g", "h", "i"], 
+  "5" => ["j", "k", "l"], 
+  "6" => ["m", "n", "o"], 
+  "7" => ["p", "q", "r", "s"], 
+  "8" => ["t", "u", "v"], 
+  "9" => ["w", "x", "y", "z"], 
+
+}
 def letter_combinations(digits)
+  if digits.length > 1
     
+    digits.chars.map { |digit| DIGIT_CHAR_PAIRS[digit] }.inject(&:product).map(&:join)
+    
+  elsif digits.length == 1
+
+    DIGIT_CHAR_PAIRS[digits]
+
+  else
+    
+    []
+
+  end
 end
