c3494e51fda15cda2c1f97354bde40dcd138c2a5
ozovalihasan <ozovalihasan@gmail.com>
2023-08-04 14:46:15 +0300

    Add a solution to the "Number of Flowers in Full Bloom" challenge

--- 2251_Number_of_Flowers_in_Full_Bloom/solution.rb
+++ 2251_Number_of_Flowers_in_Full_Bloom/solution.rb
@@ -2,5 +2,15 @@
 # @param {Integer[]} people
 # @return {Integer[]}
 def full_bloom_flowers(flowers, people)
-    
+  flowers_size = flowers.size
+  start_indices = flowers.map(&:first).sort
+  end_indices = flowers.map(&:last).sort
+
+  people.map do |num|
+    (
+      start_indices.bsearch_index {|start_index| start_index > num} || flowers_size
+    ) - (
+      end_indices.bsearch_index {|end_index| end_index >= num} || flowers_size
+    )
+  end
 end
