660f146f68da816a968ace1ecc2cf27a38fa2cb1
ozovalihasan <ozovalihasan@gmail.com>
2023-08-05 14:18:18 +0300

    Update TreeNode#to_a for the "Unique Binary Search Trees II" challenge

--- 95_Unique_Binary_Search_Trees_II/binary_tree.rb
+++ 95_Unique_Binary_Search_Trees_II/binary_tree.rb
@@ -11,43 +11,27 @@ class TreeNode
   def to_a
     node = self
     arr = []
-    queue = [[self, 1]]
+    queue = [self]
 
     until queue.empty?
       node = queue.shift
-      next if node.first.nil?
-      arr[node.last - 1] = node.first.val
+      
+      if node.nil?
+        arr << node
+        next
+      else
+        arr << node.val
+      end
 
-      queue << [node.first.left, node.last * 2 ]
-      queue << [node.first.right, node.last * 2 + 1  ]
+      queue << node.left
+      queue << node.right
     end
+
+    while arr.last.nil?
+      arr.pop
+    end
+
     arr
   end
   
-end
-
-class Tree
-  attr_accessor :root
-
-  def initialize(arr)
-    val = arr.shift
-
-    @root = (TreeNode.new val if val)
-
-    nodes = [@root]
-    until arr.empty?
-      new_nodes = []
-      nodes.each do |node|
-        val = arr.shift
-        node.left = TreeNode.new(val) if val
-        new_nodes << node.left if node.left
-
-        val = arr.shift
-        node.right = TreeNode.new(val) if val
-        new_nodes << node.right if node.right
-      end
-      nodes = new_nodes
-
-    end
-  end
 end
