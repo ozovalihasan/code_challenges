bbc42527987933276295e87e2f76a17d07877a33
ozovalihasan <ozovalihasan@gmail.com>
2023-08-06 12:55:21 +0300

    Add a test for the "Number of Music Playlists" challenge

---
+++ 920_Number_of_Music_Playlists/test_solution.rb
@@ -0,0 +1,14 @@
+require_relative './solution'
+require_relative '../read_test_files'
+
+describe 'Solution' do
+  examples, results = read_test_files.values_at(:examples, :results)
+
+  describe '#num_music_playlists' do
+    examples.each_slice(3).each_with_index do |(n, goal, k), index|
+      it 'returns the valid IP addresses in any order' do
+        expect(num_music_playlists(n, goal, k)).to match_array(results[index])
+      end
+    end
+  end
+end
