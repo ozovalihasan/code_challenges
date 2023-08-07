4d29c1f4f2b28a02ee9b25212e5aaf5415dc065d
ozovalihasan <ozovalihasan@gmail.com>
2023-08-07 11:09:22 +0300

    Add a solution to the "Search a 2D Matrix" challenge

--- 74_Search_a_2D_Matrix/solution.rb
+++ 74_Search_a_2D_Matrix/solution.rb
@@ -2,5 +2,8 @@
 # @param {Integer} target
 # @return {Boolean}
 def search_matrix(matrix, target)
-    
+  row_index = matrix.bsearch_index {|row| row.last >= target}
+  return false if row_index.nil?
+  
+  matrix[row_index].bsearch {|cell| cell >= target} == target
 end
