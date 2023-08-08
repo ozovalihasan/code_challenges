20aeabe06324d779c6a4dc57c657a748e60367b0
ozovalihasan <ozovalihasan@gmail.com>
2023-08-08 14:25:43 +0300

    Add a solution to the "Search in Rotated Sorted Array" challenge

--- 33_Search_in_Rotated_Sorted_Array/solution.rb
+++ 33_Search_in_Rotated_Sorted_Array/solution.rb
@@ -2,5 +2,12 @@
 # @param {Integer} target
 # @return {Integer}
 def search(nums, target)
-    
+  first_num = nums.first
+  rotating_index = nums.bsearch_index {|num| num < first_num } || nums.size
+  
+  if target >= nums[0]
+    nums[...rotating_index].bsearch_index {|num| target <=> num } || -1
+  else
+    rotating_index + (nums[rotating_index...].bsearch_index {|num| target <=> num } || (- index - 1))  
+  end
 end
