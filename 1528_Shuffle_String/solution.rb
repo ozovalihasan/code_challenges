6f9a94dbb1f41e44bcb6d44947a3821ed4522254
ozovalihasan <ozovalihasan@gmail.com>
2023-08-09 23:36:45 +0300

    Add a solution to the "Shuffle String" challenge

--- 1528_Shuffle_String/solution.rb
+++ 1528_Shuffle_String/solution.rb
@@ -2,5 +2,7 @@
 # @param {Integer[]} indices
 # @return {String}
 def restore_string(s, indices)
-    
+  result = s.clone
+  indices.zip(s.chars).each {|index, char| result[index] = char}
+  result
 end
