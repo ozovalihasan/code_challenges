82203187406744d0cd945e0d89cab7780ad0ce0a
ozovalihasan <ozovalihasan@gmail.com>
2023-08-11 14:58:43 +0300

    Add a test for the "Coin Change II" challenge

---
+++ 518_Coin_Change_II/test_solution.rb
@@ -0,0 +1,14 @@
+require_relative './solution'
+require_relative '../read_test_files'
+
+describe 'Solution' do
+  examples, results = read_test_files.values_at(:examples, :results)
+
+  describe '#change' do
+    examples.each_slice(2).each_with_index do |(amount, coins), index|
+      it "returns the number of combinations that make up that amount" do
+        expect(change(amount, coins)).to eq(results[index])
+      end
+    end
+  end
+end
