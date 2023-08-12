c49f6f98e9d970e642fd71233b9616c1c241b8e4
ozovalihasan <ozovalihasan@gmail.com>
2023-08-12 13:02:14 +0300

    Update the solution of the "Unique Paths II" challenge

--- 63_Unique_Paths_II/solution.rb
+++ 63_Unique_Paths_II/solution.rb
@@ -15,15 +15,11 @@ end
 STEPS = [[1, 0], [0, 1]]
 def check_cell(row_index, col_index)
   return @memo[[row_index, col_index]] if @memo[[row_index, col_index]]
-  return 0 unless on_grid?(row_index, col_index) 
+  return 0 unless row_index <= @max_row_index
   return 1 if @grid[row_index][col_index] == :finish
   return 0 unless @grid[row_index][col_index] == 0
 
   @memo[[row_index, col_index]] = STEPS.sum do |row_inc, col_inc|
                                     check_cell(row_index + row_inc, col_index + col_inc)
                                   end
-end
-
-def on_grid?(row_index, col_index) 
-  row_index <= @max_row_index && col_index <= @max_col_index
 end
