e47d559d3dbf2bf1673ffb5e48af87bf2e463e34
ozovalihasan <ozovalihasan@gmail.com>
2023-08-13 14:29:03 +0300

    Add a solution to the "Check if There is a Valid Partition For The Array" challenge

--- 2369_Check_if_There_is_a_Valid_Partition_For_The_Array/solution.rb
+++ 2369_Check_if_There_is_a_Valid_Partition_For_The_Array/solution.rb
@@ -1,5 +1,23 @@
 # @param {Integer[]} nums
 # @return {Boolean}
 def valid_partition(nums)
-    
+  @result = []
+  @result[nums.size] = true
+  @result[nums.size - 1] = false
+  @result[nums.size - 2] = nums[-1] == nums[-2]
+
+  @nums = nums
+  check(0)  
+end
+
+def check(index)
+  return @result[index] unless @result[index].nil?
+  @result[index] = false
+  
+  return true if @nums[index + 1] == @nums[index] && check(index + 2)
+  
+  diff1 = @nums[index + 1] - @nums[index]
+  return true if (diff1 == 0 || diff1 == 1) && ((@nums[index + 2] - @nums[index + 1]) === diff1) && check(index + 3)
+
+  false
 end
