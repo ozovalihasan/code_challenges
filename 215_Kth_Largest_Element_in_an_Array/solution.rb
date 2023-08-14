b0591db55eca559e78ae2a6fe137b8f9607a57ba
ozovalihasan <ozovalihasan@gmail.com>
2023-08-14 11:51:02 +0300

    Add a solution to the "Kth Largest Element in an Array" challenge

--- 215_Kth_Largest_Element_in_an_Array/solution.rb
+++ 215_Kth_Largest_Element_in_an_Array/solution.rb
@@ -2,5 +2,16 @@
 # @param {Integer} k
 # @return {Integer}
 def find_kth_largest(nums, k)
+  result = Array.new(k, -Float::INFINITY)
+
+  nums.each do |num|
+    index = result.bsearch_index {|result_num| result_num < num} 
     
+    if index
+      result.insert(index, num)
+      result.pop
+    end
+  end
+  
+  result[k - 1]
 end
