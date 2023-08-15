3dbacc3a5529fae0ba15393703736bffe5e61bbf
ozovalihasan <ozovalihasan@gmail.com>
2023-08-15 13:08:02 +0300

    Add a solution to the "Partition List" challenge

--- 86_Partition_List/solution.rb
+++ 86_Partition_List/solution.rb
@@ -10,5 +10,24 @@
 # @param {Integer} x
 # @return {ListNode}
 def partition(head, x)
-   
+  temp_head = ListNode.new(nil, head)
+  cursor1 = temp_head
+
+  while cursor1.next && cursor1.next.val < x
+    cursor1 = cursor1.next
+  end
+
+  cursor2 = cursor1.next
+  until cursor2.nil? || cursor2.next.nil?
+    
+    if cursor2.next.val < x
+      cursor1.next, cursor2.next, cursor2.next.next = cursor2.next, cursor2.next.next, cursor1.next
+      cursor1 = cursor1.next
+    elsif
+      cursor2 = cursor2.next
+    end
+    
+  end
+
+  temp_head.next
 end
