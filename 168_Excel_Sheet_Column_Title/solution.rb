6252d922460c77b4166a0893694beefd08200d50
ozovalihasan <ozovalihasan@gmail.com>
2023-08-22 11:05:22 +0300

    Add a solution to the "Excel Sheet Column Title" challenge

--- 168_Excel_Sheet_Column_Title/solution.rb
+++ 168_Excel_Sheet_Column_Title/solution.rb
@@ -1,5 +1,15 @@
 # @param {Integer} column_number
 # @return {String}
+CHARS = ("A".."Z").to_a
 def convert_to_title(column_number)
-  
+  result = ""
+
+  quotient = column_number
+  until quotient.zero?
+    quotient, remainder = (quotient - 1).divmod(26)
+    
+    result << CHARS[remainder]
+  end
+
+  result.reverse
 end
