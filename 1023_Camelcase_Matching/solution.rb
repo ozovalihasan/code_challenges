9209000b08b5581ab33e98e91a8e07d7e137e256
ozovalihasan <ozovalihasan@gmail.com>
2023-08-24 12:00:43 +0300

    Add a solution to the "Camelcase Matching" challenge

--- 1023_Camelcase_Matching/solution.rb
+++ 1023_Camelcase_Matching/solution.rb
@@ -2,5 +2,6 @@
 # @param {String} pattern
 # @return {Boolean[]}
 def camel_match(queries, pattern)
-    
+  pattern = "^[a-z]*" << pattern.split("").join("[a-z]*") << "[a-z]*$"
+  queries.map {|query| query.match(pattern) ? true : false}
 end
