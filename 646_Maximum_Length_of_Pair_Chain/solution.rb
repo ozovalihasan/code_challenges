5d5cc86bef2cc59daf84a2dc5efa2f36753a61eb
ozovalihasan <ozovalihasan@gmail.com>
2023-08-26 14:37:28 +0300

    Add a solution to the "Maximum Length of Pair Chain" challenge

--- 646_Maximum_Length_of_Pair_Chain/solution.rb
+++ 646_Maximum_Length_of_Pair_Chain/solution.rb
@@ -1,5 +1,10 @@
 # @param {Integer[][]} pairs
 # @return {Integer}
 def find_longest_chain(pairs)
-    
+  pairs.sort_by!(&:last)
+  
+  last = -Float::INFINITY
+  pairs.count do |start_num, end_num|
+    last = end_num if last < start_num
+  end
 end
