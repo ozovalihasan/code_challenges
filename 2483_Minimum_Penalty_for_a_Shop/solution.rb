7adf05b7a3b86cd1225193e0f54d9b32391967bc
ozovalihasan <ozovalihasan@gmail.com>
2023-08-29 11:50:15 +0300

    Add a solution to the "Minimum Penalty for a Shop" challenge

--- 2483_Minimum_Penalty_for_a_Shop/solution.rb
+++ 2483_Minimum_Penalty_for_a_Shop/solution.rb
@@ -1,5 +1,10 @@
 # @param {String} customers
 # @return {Integer}
 def best_closing_time(customers)
-    
+  result = 0
+  customers = customers.each_char.map do |customer|
+                result += customer == "Y" ? 1 : -1
+              end.unshift(0)
+
+  customers.index(customers.max)
 end
