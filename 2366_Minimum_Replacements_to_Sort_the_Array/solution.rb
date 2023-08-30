62edc8360f449a376733f420ae7aa374d093b322
ozovalihasan <ozovalihasan@gmail.com>
2023-08-30 12:19:30 +0300

    Add a solution to the "Minimum Replacements to Sort the Array" challenge

--- 2366_Minimum_Replacements_to_Sort_the_Array/solution.rb
+++ 2366_Minimum_Replacements_to_Sort_the_Array/solution.rb
@@ -1,5 +1,14 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def minimum_replacement(nums)
-    
+  previous = nums.last
+  replacement_count = 0  
+  (nums.size - 2).downto(0) do |index|
+    num = nums[index]
+    replacement_size = ((num - 1) / previous) + 1
+    previous = (num / replacement_size)
+    replacement_count += replacement_size - 1
+  end
+
+  replacement_count
 end
