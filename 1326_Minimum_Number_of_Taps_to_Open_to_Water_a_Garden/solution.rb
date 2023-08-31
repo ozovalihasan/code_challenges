04e636a357021884a18e5065b5631d3238186b71
ozovalihasan <ozovalihasan@gmail.com>
2023-08-31 11:33:45 +0300

    Add a solution to the "Minimum Number of Taps to Open to Water a Garden" challenge

--- 1326_Minimum_Number_of_Taps_to_Open_to_Water_a_Garden/solution.rb
+++ 1326_Minimum_Number_of_Taps_to_Open_to_Water_a_Garden/solution.rb
@@ -2,5 +2,25 @@
 # @param {Integer[]} ranges
 # @return {Integer}
 def min_taps(n, ranges)
+  garden_max_index = n 
+  intervals = ranges.map.with_index {|range, index| [index - range, index + range] }
+  intervals.sort_by!(&:first)
+
+  intervals << [garden_max_index, garden_max_index]
+  
+  count = 0
+  current_index = 0
+  next_index = 0
+  intervals.each do |start_index, end_index|
+    return -1 if start_index > next_index
     
+    if start_index > current_index
+      count += 1
+      current_index = next_index
+    end
+
+    next_index = end_index if end_index > next_index
+  end
+  
+  count
 end
