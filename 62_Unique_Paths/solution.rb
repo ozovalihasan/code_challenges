2e595f12f451b88093e2474a0449418638ea3b9d
ozovalihasan <ozovalihasan@gmail.com>
2023-09-03 13:52:36 +0300

    Add a solution to the "Unique Paths" challenge

--- 62_Unique_Paths/solution.rb
+++ 62_Unique_Paths/solution.rb
@@ -2,5 +2,12 @@
 # @param {Integer} n
 # @return {Integer}
 def unique_paths(m, n)
-    
+  total = (m - 1) + (n - 1)
+  
+  bigger_num, smaller_num = [n, m].minmax
+  range_multiply(bigger_num, total) / range_multiply(1, smaller_num-1)
+end
+
+def range_multiply(start_num, end_num)
+  (start_num..end_num).reduce(:*) || 1
 end
