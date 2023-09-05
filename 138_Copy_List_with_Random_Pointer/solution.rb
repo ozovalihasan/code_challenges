60648e2ea53e7799a6e490da165c79c40edaf4a0
ozovalihasan <ozovalihasan@gmail.com>
2023-09-05 13:06:26 +0300

    Add a solution to the "Copy List with Random Pointer" challenge

--- 138_Copy_List_with_Random_Pointer/solution.rb
+++ 138_Copy_List_with_Random_Pointer/solution.rb
@@ -11,5 +11,22 @@
 # @param {Node} node
 # @return {Node}
 def copyRandomList(head)
-  
+  nodes = []
+  clone_nodes = []
+
+  cursor = head
+  while cursor
+    nodes << cursor 
+    clone_nodes << Node.new(cursor.val) 
+    cursor = cursor.next
+  end
+
+  nodes.each_with_index do |node, index|
+    clone_node = clone_nodes[index]
+    clone_node.next = clone_nodes[index + 1]
+    random_index = nodes.index(node.random)
+    clone_node.random = clone_nodes[random_index] if random_index
+  end
+
+  clone_nodes.first
 end
