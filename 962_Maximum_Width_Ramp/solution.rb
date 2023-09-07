72bdff780b8a24c55feda9ebdc61e8339ccccce1
ozovalihasan <ozovalihasan@gmail.com>
2023-09-07 15:55:02 +0300

    Add a solution to the "Maximum Width Ramp" challenge

--- 962_Maximum_Width_Ramp/solution.rb
+++ 962_Maximum_Width_Ramp/solution.rb
@@ -1,5 +1,17 @@
-# @param {Integer[]} nums
-# @return {Integer}
 def max_width_ramp(nums)
-    
+  stack = [0]
+  nums.each_with_index do |num, index|
+    if num < nums[stack.last]
+      stack << index
+    end
+  end
+
+  max_width = 0
+  (nums.length - 1).downto(0) do |index|
+    while stack.any? && nums[index] >= nums[stack.last]
+      max_width = [max_width, index - stack.pop].max
+    end
+  end
+
+  max_width
 end
