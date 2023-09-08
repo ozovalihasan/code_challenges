1ab8a278ef555d3b0ca8ad4dd3f7dafe994065c9
ozovalihasan <ozovalihasan@gmail.com>
2023-09-08 11:51:36 +0300

    Add a solution to the "Watering Plants" challenge

--- 2079_Watering_Plants/solution.rb
+++ 2079_Watering_Plants/solution.rb
@@ -2,5 +2,17 @@
 # @param {Integer} capacity
 # @return {Integer}
 def watering_plants(plants, capacity)
-    
+  steps = plants.size
+  current_level = capacity
+
+  plants.each_with_index do |plant, index|
+    if current_level < plant
+      steps += index * 2 
+      current_level = capacity
+    end
+
+    current_level -= plant
+  end
+
+  steps
 end
