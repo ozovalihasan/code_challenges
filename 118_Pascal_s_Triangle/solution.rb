76867a88c9341f95f4f7bc89196381bb14e7d45f
ozovalihasan <ozovalihasan@gmail.com>
2023-09-08 11:25:16 +0300

    Update the solution of the "Pascal's Triangle" challenge

--- 118_Pascal_s_Triangle/solution.rb
+++ 118_Pascal_s_Triangle/solution.rb
@@ -2,8 +2,15 @@
 # @return {Integer[][]}
 def generate(num_rows)
   result = [[1]]
-  2.upto(num_rows) do |_row|
-    result << [1, *result.last.each_cons(2).map(&:sum), 1]
+
+  (1..num_rows - 1).each do |row_index|
+    last_row = result.last
+
+    result << 1.upto(row_index - 1).map do |i|
+                last_row[i - 1] + last_row[i]
+              end
+
+    result.last.unshift(1).push(1)
   end
 
   result
