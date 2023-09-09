f93f40e28b8bf4240881d7d6cfe70b9e40545a76
ozovalihasan <ozovalihasan@gmail.com>
2023-09-09 14:37:33 +0300

    Add a solution to the "Combination Sum IV" challenge

--- 377_Combination_Sum_IV/solution.rb
+++ 377_Combination_Sum_IV/solution.rb
@@ -2,5 +2,17 @@
 # @param {Integer} target
 # @return {Integer}
 def combination_sum4(nums, target)
-    
+  @nums = nums
+  @memo = [1]
+  calculate_for(target) 
+end
+
+def calculate_for(target) 
+  unless @memo[target]
+    @memo[target] = @nums.sum do |num| 
+                      target >= num ? calculate_for(target - num) : 0 
+                    end
+  end
+
+  @memo[target]
 end
