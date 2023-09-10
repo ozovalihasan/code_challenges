09ae3cb91167f2d07c1a175ac98d069fb4786346
ozovalihasan <ozovalihasan@gmail.com>
2023-09-10 12:03:05 +0300

    Add a solution to the "Count All Valid Pickup and Delivery Options" challenge

--- 1359_Count_All_Valid_Pickup_and_Delivery_Options/solution.rb
+++ 1359_Count_All_Valid_Pickup_and_Delivery_Options/solution.rb
@@ -1,5 +1,10 @@
+MODULO = (10 ** 9) + 7
+
 # @param {Integer} n
 # @return {Integer}
 def count_orders(n)
-      
+  (2..n).inject(1) do |result, num| 
+    result *= ((2 * num) - 1) * num
+    result %= MODULO 
+  end
 end
