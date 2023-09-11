24eac7e089b7ebb8f288ec3ddbd2099395f150e0
ozovalihasan <ozovalihasan@gmail.com>
2023-09-11 11:34:53 +0300

    Add a solution to the "Group the People Given the Group Size They Belong To" challenge

--- 1282_Group_the_People_Given_the_Group_Size_They_Belong_To/solution.rb
+++ 1282_Group_the_People_Given_the_Group_Size_They_Belong_To/solution.rb
@@ -1,5 +1,11 @@
 # @param {Integer[]} group_sizes
 # @return {Integer[][]}
 def group_the_people(group_sizes)
-    
+  groups = {}
+  group_sizes.uniq.each {|size| groups[size] = []}
+  group_sizes.each_with_index {|size, index| groups[size] << index}
+
+  result = []
+  groups.each {|size, group| result.concat group.each_slice(size).to_a}
+  result
 end
