47f939be5a92746247c73bb2f853b755fec31bd7
ozovalihasan <ozovalihasan@gmail.com>
2023-09-12 12:05:15 +0300

    Add a solution to the "Minimum Deletions to Make Character Frequencies Unique" challenge

--- 1647_Minimum_Deletions_to_Make_Character_Frequencies_Unique/solution.rb
+++ 1647_Minimum_Deletions_to_Make_Character_Frequencies_Unique/solution.rb
@@ -1,5 +1,25 @@
 # @param {String} s
 # @return {Integer}
 def min_deletions(s)
-    
+  char_counts = s.chars.tally
+  
+  frequencies = []
+  char_counts.values.tally.each {|num, count| frequencies[num] = count}
+
+  total = 0
+  necessary_change = 0
+
+  (frequencies.size - 1).downto(1) do |index|
+    frequency = frequencies[index]
+
+    if frequency.nil?
+      necessary_change -= 1 if necessary_change > 0
+    elsif frequency > 1
+      necessary_change += frequency - 1
+    end
+
+    total += necessary_change
+  end
+
+  total
 end
