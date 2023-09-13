77371f2e2cd8495e582a879f801e46c4e661655b
ozovalihasan <ozovalihasan@gmail.com>
2023-09-13 13:18:00 +0300

    Update the solution of the "Candy" challenge

--- 135_Candy/solution.rb
+++ 135_Candy/solution.rb
@@ -1,26 +1,24 @@
 # @param {Integer[]} ratings
 # @return {Integer}
 def candy(ratings)
-  result = Array.new(ratings.size, 1) 
-  distribute_candies(result, ratings)
+  candies = Array.new(ratings.size, 1) 
+  distribute_candies(candies, ratings)
 
-  result.reverse!
+  candies.reverse!
   ratings.reverse!
-  distribute_candies(result, ratings)
+  distribute_candies(candies, ratings)
 
-  result.sum
+  candies.sum
 end
 
-def distribute_candies(result, ratings)
-  candle = 1
+def distribute_candies(candies, ratings)
+  previous_candy = candies.first
   previous_rating = ratings.first
 
   1.upto(ratings.size - 1) do |index|
     rating = ratings[index] 
-    candle = 0 if rating <= previous_rating 
-    candle += 1
-    result[index] = candle if candle > result[index] 
-    
-    previous_rating = rating
+    previous_candy = 0 if rating <= ratings[index - 1]  
+    previous_candy += 1
+    candies[index] = previous_candy if previous_candy > candies[index] 
   end
 end
