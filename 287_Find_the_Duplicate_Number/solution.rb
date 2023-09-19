2e9ef52e569969510ce69a090b7b5c3b421520fc
ozovalihasan <ozovalihasan@gmail.com>
2023-09-19 13:20:28 +0300

    Add a solution to the "Find the Duplicate Number" challenge

--- 287_Find_the_Duplicate_Number/solution.rb
+++ 287_Find_the_Duplicate_Number/solution.rb
@@ -1,5 +1,20 @@
+# Thanks to @NikkiNicholas for the solution(https://leetcode.com/problems/find-the-duplicate-number/solutions/72929/o-nlogn-binary-search-and-o-n-circle-detect/)
+
 # @param {Integer[]} nums
 # @return {Integer}
 def find_duplicate(nums)
-    
+  slow, fast = nums[0], nums[ nums[0] ]
+
+  while slow != fast
+    slow = nums[slow]
+    fast = nums[ nums[fast] ]
+  end
+
+  tag = 0
+  while tag != slow
+    tag  = nums[tag]
+    slow = nums[slow]
+  end
+
+  return slow
 end
