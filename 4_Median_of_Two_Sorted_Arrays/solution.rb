76971636745e9785e2e30ffad7a119c4608665a2
ozovalihasan <ozovalihasan@gmail.com>
2023-09-21 14:25:03 +0300

    Add a solution to the "Median of Two Sorted Arrays" challenge

--- 4_Median_of_Two_Sorted_Arrays/solution.rb
+++ 4_Median_of_Two_Sorted_Arrays/solution.rb
@@ -2,5 +2,20 @@
 # @param {Integer[]} nums2
 # @return {Float}
 def find_median_sorted_arrays(nums1, nums2)
+  result = []
+
+  until nums1.empty? && nums2.empty?
+    result << if ((nums1.last && nums2.last && nums1.last >= nums2.last) || !nums2.last) 
+                nums1.pop 
+              else
+                nums2.pop
+              end
+  end
+
+  size = result.size
+  half_size = size / 2
+
+  return result[ half_size ] if size.odd? 
     
+  result[ half_size - 1..half_size ].sum.to_f/2
 end
