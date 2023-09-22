aa59be0409f7e4096eda819e01138569136ca9e8
ozovalihasan <ozovalihasan@gmail.com>
2023-09-22 12:02:36 +0300

    Add a solution to the "Degree of an Array" challenge

--- 697_Degree_of_an_Array/solution.rb
+++ 697_Degree_of_an_Array/solution.rb
@@ -1,5 +1,9 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def find_shortest_sub_array(nums)
-    
+  num_indices = {}
+  nums.each_with_index { |num, index| (num_indices[num] ||= []) << index }
+
+  degree = num_indices.values.map(&:size).max
+  num_indices.values.filter_map {|indices| indices.last - indices.first + 1 if indices.size == degree}.min
 end
