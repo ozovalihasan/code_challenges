a98a27b855a9b8b56182d4eec74410cdd7771af9
ozovalihasan <ozovalihasan@gmail.com>
2023-09-25 13:16:56 +0300

    Add a solution to the "Find the Width of Columns of a Grid" challenge

--- 2639_Find_the_Width_of_Columns_of_a_Grid/solution.rb
+++ 2639_Find_the_Width_of_Columns_of_a_Grid/solution.rb
@@ -1,5 +1,7 @@
 # @param {Integer[][]} grid
 # @return {Integer[]}
 def find_column_width(grid)
-    
+  grid.transpose.map do |row| 
+                   row.map {|cell| cell.to_s.size}.max
+                 end
 end
