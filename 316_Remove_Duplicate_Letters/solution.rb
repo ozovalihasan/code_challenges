87c7613555b805fab42c4b0c48d518e540bc84ae
ozovalihasan <ozovalihasan@gmail.com>
2023-09-26 20:31:28 +0300

    Add a solution to the "Remove Duplicate Letters" challenge

--- 316_Remove_Duplicate_Letters/solution.rb
+++ 316_Remove_Duplicate_Letters/solution.rb
@@ -1,5 +1,24 @@
 # @param {String} s
 # @return {String}
 def remove_duplicate_letters(s)
+  str = s.split("")
+  counts = str.tally
+  result = []
+  added = Set.new
+
+  str.each do |char|
+    while result.any? && counts[result.last].positive? && !(added.include? char) && result.last >= char
+      popped_char = result.pop
+      added.delete(popped_char)
+    end
     
+    counts[char] -= 1
+
+    unless added.include? char
+      result << char 
+      added << char
+    end
+  end
+
+  result.join
 end
