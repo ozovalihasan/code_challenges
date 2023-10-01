52f2dba6d42e2d46e6f2417880a23855334c4cec
ozovalihasan <ozovalihasan@gmail.com>
2023-10-01 12:26:09 +0300

    Add a solution to the "Largest Odd Number in String" challenge

--- 1903_Largest_Odd_Number_in_String/solution.rb
+++ 1903_Largest_Odd_Number_in_String/solution.rb
@@ -1,5 +1,11 @@
 # @param {String} num
 # @return {String}
 def largest_odd_number(num)
-    
+  (num.size - 1).downto(0) do |index|
+    if num[index].to_i.odd?
+      return num[..index] 
+    end
+  end
+  
+  ""
 end
