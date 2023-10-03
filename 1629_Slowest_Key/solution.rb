a833dffbd47a78a1b081387b8e30dffbb12fc307
ozovalihasan <ozovalihasan@gmail.com>
2023-10-03 19:43:04 +0300

    Add a solution to the "Slowest Key" challenge

--- 1629_Slowest_Key/solution.rb
+++ 1629_Slowest_Key/solution.rb
@@ -2,5 +2,10 @@
 # @param {String} keys_pressed
 # @return {Character}
 def slowest_key(release_times, keys_pressed)
-    
+  previous_time = 0
+  release_times.zip(keys_pressed.chars).max_by do |release_time, key|
+    press_time = release_time - previous_time
+    previous_time = release_time
+    [press_time, key]
+  end.last
 end
