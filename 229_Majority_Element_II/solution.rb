4b83e54ed2b45a88b36b01e676080a4e97a7d5d0
ozovalihasan <ozovalihasan@gmail.com>
2023-10-05 11:33:16 +0300

    Add a solution to the "Majority Element II" challenge

--- 229_Majority_Element_II/solution.rb
+++ 229_Majority_Element_II/solution.rb
@@ -1,5 +1,6 @@
 # @param {Integer[]} nums
 # @return {Integer[]}
 def majority_element(nums)
-    
+  necessary_count = nums.size / 3
+  nums.tally.select {|_, count| count > necessary_count }.keys
 end
