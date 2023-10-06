d7ceb1f5022c606179bbd9b15d32f0f93be885c8
ozovalihasan <ozovalihasan@gmail.com>
2023-10-06 12:20:13 +0300

    Add a solution to the "Integer Break" challenge

--- 343_Integer_Break/solution.rb
+++ 343_Integer_Break/solution.rb
@@ -1,5 +1,16 @@
 # @param {Integer} n
 # @return {Integer}
 def integer_break(n)
-    
+  return n - 1 if n < 4
+
+  result = 3 ** (n / 3)
+
+  case n % 3
+  when 0
+    result * 1
+  when 1
+    result * 4 / 3
+  when 2
+    result * 2
+  end
 end
