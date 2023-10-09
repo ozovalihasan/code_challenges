74d7a94151dcf604a5aeefa3ae2419bc6c2dc813
ozovalihasan <ozovalihasan@gmail.com>
2023-10-09 11:44:22 +0300

    Add a solution to the "Create Target Array in the Given Order" challenge

--- 1389_Create_Target_Array_in_the_Given_Order/solution.rb
+++ 1389_Create_Target_Array_in_the_Given_Order/solution.rb
@@ -2,5 +2,10 @@
 # @param {Integer[]} index
 # @return {Integer[]}
 def create_target_array(nums, index)
-    
+  result = []
+  nums.zip(index) do |num, num_index|
+    result.insert(num_index, num)
+  end
+
+  result
 end
