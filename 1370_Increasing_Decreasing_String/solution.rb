c0ad99bce0f195c4c39985dbd2e68dde7d89ed09
ozovalihasan <ozovalihasan@gmail.com>
2023-10-11 19:16:34 +0300

    Add a solution to the "Increasing Decreasing String" challenge

--- 1370_Increasing_Decreasing_String/solution.rb
+++ 1370_Increasing_Decreasing_String/solution.rb
@@ -1,5 +1,22 @@
 # @param {String} s
 # @return {String}
 def sort_string(s)
-    
+  char_counts = s.chars.tally
+  result = []
+  uniq_chars = char_counts.keys.sort
+
+  count = 0
+  until uniq_chars.empty?
+    count += 1
+    result.concat uniq_chars
+    uniq_chars.reject! {|char| char_counts[char] <= count }
+
+    break if uniq_chars.empty?
+
+    count += 1
+    result.concat uniq_chars.reverse
+    uniq_chars.reject! {|char| char_counts[char] <= count }
+  end
+
+  result.join
 end
