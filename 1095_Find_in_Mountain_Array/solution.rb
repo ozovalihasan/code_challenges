0e376aca0d0e77777cdf85ebecb38df82d0ec30d
ozovalihasan <ozovalihasan@gmail.com>
2023-10-12 12:03:44 +0300

    Add a solution to the "Find in Mountain Array" challenge

--- 1095_Find_in_Mountain_Array/solution.rb
+++ 1095_Find_in_Mountain_Array/solution.rb
@@ -14,5 +14,30 @@
 # @param {MountainArray} mountain_arr
 # @return {int}
 def findInMountainArray(target, mountain_arr)
-    
+  @mountain_arr = mountain_arr
+  @target = target
+  @length = mountain_arr.length
+  
+  max_height_index = find_max_height_index
+
+  check_increasing_part(max_height_index) || check_decreasing_part(max_height_index) || -1
+end
+
+def find_max_height_index
+  (1...@length).bsearch do |index|
+    previous_height, current_height,  = [@mountain_arr.get(index - 1), @mountain_arr.get(index)]
+    previous_height > current_height
+  end - 1
+end
+
+def check_increasing_part(max_height_index)
+  (0..max_height_index).bsearch do |index|
+    @target <=> @mountain_arr.get(index)
+  end
+end
+
+def check_decreasing_part(max_height_index)
+  (max_height_index...@length).bsearch do |index|
+    @mountain_arr.get(index) <=> @target 
+  end
 end
