23784fd2588b397adfb7f3e8c8bc6278e6d123c3
ozovalihasan <ozovalihasan@gmail.com>
2023-10-13 11:14:47 +0300

    Add a solution to the "Min Cost Climbing Stairs" challenge

--- 746_Min_Cost_Climbing_Stairs/solution.rb
+++ 746_Min_Cost_Climbing_Stairs/solution.rb
@@ -1,5 +1,9 @@
 # @param {Integer[]} cost
 # @return {Integer}
 def min_cost_climbing_stairs(cost)
-    
+  2.upto(cost.size - 1) do |index|
+    cost[index] += [cost[index - 1], cost[index - 2]].min
+  end
+
+  cost.last(2).min
 end
