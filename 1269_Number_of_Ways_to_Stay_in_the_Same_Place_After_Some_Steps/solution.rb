7dd0abe3726b3525c5ada6c5f7dd3930b36999f1
ozovalihasan <ozovalihasan@gmail.com>
2023-10-15 16:13:29 +0300

    Add a solution to the "Number of Ways to Stay in the Same Place After Some Steps" challenge

--- 1269_Number_of_Ways_to_Stay_in_the_Same_Place_After_Some_Steps/solution.rb
+++ 1269_Number_of_Ways_to_Stay_in_the_Same_Place_After_Some_Steps/solution.rb
@@ -1,6 +1,25 @@
+MODULO = (10**9) + 7
+
 # @param {Integer} steps
 # @param {Integer} arr_len
 # @return {Integer}
 def num_ways(steps, arr_len)
-  
+  @max_index = arr_len - 1
+  @memo = Array.new(steps + 1) { [] }
+  check_step(steps, 0)
+end
+
+def check_step(steps, index)
+  return @memo[steps][index] if @memo[steps][index]
+
+  if steps.zero?
+    return @memo[steps][index] = index.zero? ? 1 : 0
+  end
+
+  steps -= 1
+  result = check_step(steps, index)
+  result = (result + check_step(steps, index - 1)) % MODULO if index.positive?
+  result = (result + check_step(steps, index + 1)) % MODULO if index < @max_index
+
+  @memo[steps + 1][index] = result
 end
