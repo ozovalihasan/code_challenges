8f6359979ac12b1fdc725b86586e2b9ea12aaa44
ozovalihasan <ozovalihasan@gmail.com>
2023-10-16 11:46:55 +0300

    Add a solution to the "Check if Numbers Are Ascending in a Sentence" challenge

--- 2042_Check_if_Numbers_Are_Ascending_in_a_Sentence/solution.rb
+++ 2042_Check_if_Numbers_Are_Ascending_in_a_Sentence/solution.rb
@@ -1,5 +1,13 @@
-# @param {String} s
+# @param {String} str
 # @return {Boolean}
-def are_numbers_ascending(s)
-    
+def are_numbers_ascending(str)
+  prev = -1
+  str.scan(/\d+/) do
+    num = Regexp.last_match[0].to_i
+    return false if prev >= num
+
+    prev = num
+  end
+
+  true
 end
