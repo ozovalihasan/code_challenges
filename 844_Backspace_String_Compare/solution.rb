4630307ab7d352d94d12b9f319031e0ede1ee59d
ozovalihasan <ozovalihasan@gmail.com>
2023-10-19 23:22:08 +0300

    Add a solution to the "Backspace String Compare" challenge

--- 844_Backspace_String_Compare/solution.rb
+++ 844_Backspace_String_Compare/solution.rb
@@ -1,6 +1,20 @@
-# @param {String} s
-# @param {String} t
+# @param {String} str1
+# @param {String} str2
 # @return {Boolean}
-def backspace_compare(s, t)
-    
+def backspace_compare(str1, str2)
+  delete_chars_if_necessary(str1) == delete_chars_if_necessary(str2)
+end
+
+def delete_chars_if_necessary(str)
+  result = ''
+
+  str.each_char do |char|
+    if char == '#'
+      result[-1] = '' unless result.empty?
+    else
+      result << char
+    end
+  end
+
+  result
 end
