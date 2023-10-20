64d87b8fe3edde7319825ff573a4881c9f2db233
ozovalihasan <ozovalihasan@gmail.com>
2023-10-20 22:14:47 +0300

    Add a solution to the "String Matching in an Array" challenge

--- 1408_String_Matching_in_an_Array/solution.rb
+++ 1408_String_Matching_in_an_Array/solution.rb
@@ -1,5 +1,16 @@
 # @param {String[]} words
 # @return {String[]}
 def string_matching(words)
-    
+  res = []
+
+  words.each do |word1|
+    words.each do |word2|
+      next unless word1 != word2 && word2.include?(word1)
+
+      res << word1
+      break
+    end
+  end
+
+  res
 end
