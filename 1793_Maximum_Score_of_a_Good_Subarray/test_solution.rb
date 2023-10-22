e2bb9dfbc82823b0ec17db34c9f4d90a10d23b5c
ozovalihasan <ozovalihasan@gmail.com>
2023-10-22 10:55:00 +0300

    Add a test for the "Maximum Score of a Good Subarray" challenge

---
+++ 1793_Maximum_Score_of_a_Good_Subarray/test_solution.rb
@@ -0,0 +1,14 @@
+require_relative './solution'
+require_relative '../read_test_files'
+
+describe 'Solution' do
+  examples, results = read_test_files.values_at(:examples, :results)
+
+  describe '#maximum_score' do
+    examples.each_slice(2).each_with_index do |(nums, k), index|
+      it "returns the maximum possible score of a good subarray" do
+        expect(maximum_score(nums, k)).to eq(results[index])
+      end
+    end
+  end
+end
