9be5f228585e551ed677af3b3685e7eeed471189
ozovalihasan <ozovalihasan@gmail.com>
2023-10-23 20:08:15 +0300

    Add a solution to the "Power of Four" challenge

--- 342_Power_of_Four/solution.rb
+++ 342_Power_of_Four/solution.rb
@@ -1,5 +1,14 @@
-# @param {Integer} n
+# @param {Integer} num
 # @return {Boolean}
-def is_power_of_four(n)
+def is_power_of_four(num)
+  return false unless num.positive?
+
+  power = 0
+  until 4**power > num
+    return true if 4**power == num
     
+    power += 1
+  end
+
+  false
 end
