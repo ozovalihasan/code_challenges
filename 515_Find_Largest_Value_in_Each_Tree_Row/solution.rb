8daa679917042527c986456f7bdc1fd031a06d7d
ozovalihasan <ozovalihasan@gmail.com>
2023-10-24 19:13:18 +0300

    Add a solution to the "Find Largest Value in Each Tree Row" challenge

--- 515_Find_Largest_Value_in_Each_Tree_Row/solution.rb
+++ 515_Find_Largest_Value_in_Each_Tree_Row/solution.rb
@@ -10,5 +10,16 @@
 # @param {TreeNode} root
 # @return {Integer[]}
 def largest_values(root)
-   
+  @res = []
+  check_node(root, 0)
+  @res
+end
+
+def check_node(node, depth)
+  return if node.nil?
+
+  @res[depth] = node.val if @res[depth].nil? || node.val > @res[depth]
+    
+  check_node(node.left, depth + 1)
+  check_node(node.right, depth + 1)
 end
