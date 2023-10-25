90e4bdce4f5d589c8f3dcb0d3bcbe79cba87ca09
ozovalihasan <ozovalihasan@gmail.com>
2023-10-25 22:38:54 +0300

    Add a solution to the "K-th Symbol in Grammar" challenge

--- 779_K_th_Symbol_in_Grammar/solution.rb
+++ 779_K_th_Symbol_in_Grammar/solution.rb
@@ -2,5 +2,15 @@
 # @param {Integer} k
 # @return {Integer}
 def kth_grammar(n, k)
-    
+  @count_change = 0
+  (n - 1).times do
+    @count_change += 1 if (k % 2).zero? 
+    k = (k + 1) / 2
+  end
+
+  change_necessary? ? 1 : 0 
+end
+
+def change_necessary?
+  @count_change.odd?
 end
