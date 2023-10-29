3942fc8a655d8077d297cdfc2f2a0e4dbbe26bf3
ozovalihasan <ozovalihasan@gmail.com>
2023-10-30 00:00:32 +0300

    Add a solution to the "Delete Characters to Make Fancy String" challenge

--- 1957_Delete_Characters_to_Make_Fancy_String/solution.rb
+++ 1957_Delete_Characters_to_Make_Fancy_String/solution.rb
@@ -1,5 +1,5 @@
 # @param {String} s
 # @return {String}
-def make_fancy_string(s)
-    
+def make_fancy_string(str)
+  str.chars.slice_when {|first, second| first != second }.map {|part| part.first(2)}.join
 end
