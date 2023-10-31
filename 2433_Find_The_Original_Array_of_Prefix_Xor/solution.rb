c9ee766d459a11a2f0c29e76e3a71e2fb326b74a
ozovalihasan <ozovalihasan@gmail.com>
2023-10-31 21:15:14 +0300

    Add a solution to the "Find The Original Array of Prefix Xor" challenge

--- 2433_Find_The_Original_Array_of_Prefix_Xor/solution.rb
+++ 2433_Find_The_Original_Array_of_Prefix_Xor/solution.rb
@@ -1,5 +1,13 @@
 # @param {Integer[]} pref
 # @return {Integer[]}
 def find_array(pref)
-    
+  prev = pref.first
+  res = [prev]
+
+  1.upto(pref.size - 1) do |index|
+    res << (prev ^ pref[index])
+    prev = pref[index]
+  end
+
+  res
 end
