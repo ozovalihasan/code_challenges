ae42e36fe173c687fd2aa2f00af8de78645f9344
ozovalihasan <ozovalihasan@gmail.com>
2023-11-01 21:03:38 +0300

    Add a solution to the "Find Mode in Binary Search Tree" challenge

--- 501_Find_Mode_in_Binary_Search_Tree/solution.rb
+++ 501_Find_Mode_in_Binary_Search_Tree/solution.rb
@@ -10,5 +10,16 @@
 # @param {TreeNode} root
 # @return {Integer[]}
 def find_mode(root)
+  @counts = Hash.new(0)
+  check_node(root)
+  max_count = @counts.values.max
+  @counts.select { |_, val| val == max_count }.keys
+end
+
+def check_node(node)
+  return if node.nil?
   
+  @counts[node.val] += 1
+  check_node(node.left)
+  check_node(node.right)
 end
