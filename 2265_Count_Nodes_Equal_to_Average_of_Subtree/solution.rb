52740e5b3a7e0b98bd069fe1f4108ff506cd6df4
ozovalihasan <ozovalihasan@gmail.com>
2023-11-02 21:16:07 +0300

    Add a solution to the "Count Nodes Equal to Average of Subtree" challenge

--- 2265_Count_Nodes_Equal_to_Average_of_Subtree/solution.rb
+++ 2265_Count_Nodes_Equal_to_Average_of_Subtree/solution.rb
@@ -10,5 +10,21 @@
 # @param {TreeNode} root
 # @return {Integer}
 def average_of_subtree(root)
-    
+  @result = 0
+  check_node(root)    
+  @result
+end
+
+def check_node(node)
+  return [0, 0] if node.nil?
+  
+  left_sum, left_count = check_node(node.left)
+  right_sum, right_count = check_node(node.right)
+  
+  sum = node.val + left_sum + right_sum
+  count = 1 + left_count + right_count
+  
+  @result += 1 if (sum / count) == node.val
+  
+  [sum, count]
 end
