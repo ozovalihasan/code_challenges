c230aadb0936e81b645b634e31ac8bd79420c254
ozovalihasan <ozovalihasan@gmail.com>
2023-11-03 22:45:01 +0300

    Add a solution to the "Build an Array With Stack Operations" challenge

--- 1441_Build_an_Array_With_Stack_Operations/solution.rb
+++ 1441_Build_an_Array_With_Stack_Operations/solution.rb
@@ -2,5 +2,15 @@
 # @param {Integer} n
 # @return {String[]}
 def build_array(target, n)
-    
+  counter = 0
+
+  target.map do |num|
+    counter += 1
+
+    result = %w[Push Pop] * (num - counter)
+    result << 'Push'
+
+    counter = num
+    result
+  end.flatten
 end
