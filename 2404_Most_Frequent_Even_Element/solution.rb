2d8ce869cf94d384930808ae0f1cbf0a3306d621
ozovalihasan <ozovalihasan@gmail.com>
2023-11-04 23:58:12 +0300

    Add a solution to the "Most Frequent Even Element" challenge

--- 2404_Most_Frequent_Even_Element/solution.rb
+++ 2404_Most_Frequent_Even_Element/solution.rb
@@ -1,5 +1,11 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def most_frequent_even(nums)
-    
+  nums.select!(&:even?)
+
+  return -1 if nums.empty?
+
+  count = nums.tally
+  max = count.values.max
+  count.select { |key, val| val == max }.keys.min
 end
