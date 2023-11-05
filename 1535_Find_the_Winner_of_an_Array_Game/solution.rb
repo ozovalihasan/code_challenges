e7bd3b360831f24656dacf134b5c95f5627c489a
ozovalihasan <ozovalihasan@gmail.com>
2023-11-05 22:24:11 +0300

    Add a solution to the "Find the Winner of an Array Game" challenge

--- 1535_Find_the_Winner_of_an_Array_Game/solution.rb
+++ 1535_Find_the_Winner_of_an_Array_Game/solution.rb
@@ -2,5 +2,19 @@
 # @param {Integer} k
 # @return {Integer}
 def get_winner(arr, k)
-    
+  max = arr.first
+  counter = -1
+
+  arr.each do |num|
+    if num > max
+      max = num
+      counter = 0
+    end
+
+    counter += 1
+
+    return max if counter == k
+  end
+
+  max
 end
