0dfdbfa0b9871663ee69c190af1395529022e0b2
ozovalihasan <ozovalihasan@gmail.com>
2023-11-06 21:04:42 +0300

    Add a solution to the "Seat Reservation Manager" challenge

--- 1845_Seat_Reservation_Manager/solution.rb
+++ 1845_Seat_Reservation_Manager/solution.rb
@@ -3,23 +3,23 @@ class SeatManager
   :type n: Integer
 =end
   def initialize(n)
-      
+    @seats = (1..n).to_a.reverse
   end
 
 =begin
   :rtype: Integer
 =end
   def reserve()
-      
+    @seats.pop
   end
-
 
 =begin
   :type seat_number: Integer
   :rtype: Void
 =end
   def unreserve(seat_number)
-      
+    index = @seats.bsearch_index {|seat| seat < seat_number} || @seats.size
+    @seats.insert(index, seat_number)
   end
 
 end
