13f231a7b1d8123cb8924ee28329977db68ee10d
ozovalihasan <ozovalihasan@gmail.com>
2023-11-07 23:28:33 +0300

    Add a solution to the "Eliminate Maximum Number of Monsters" challenge

--- 1921_Eliminate_Maximum_Number_of_Monsters/solution.rb
+++ 1921_Eliminate_Maximum_Number_of_Monsters/solution.rb
@@ -2,5 +2,11 @@
 # @param {Integer[]} speed
 # @return {Integer}
 def eliminate_maximum(dist, speed)
-    
+  times = dist
+            .zip(speed)
+            .map { |local_dist, local_speed| local_dist / local_speed.to_f }.sort
+
+  times.find_index.with_index do |time, index|
+    time <= index
+  end || times.length
 end
