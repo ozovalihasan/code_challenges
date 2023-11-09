bffda73170989f1dae36684b27b80f7fe3dc9fe9
ozovalihasan <ozovalihasan@gmail.com>
2023-11-09 22:41:38 +0300

    Add a solution to the "Count Number of Homogenous Substrings" challenge

--- 1759_Count_Number_of_Homogenous_Substrings/solution.rb
+++ 1759_Count_Number_of_Homogenous_Substrings/solution.rb
@@ -1,5 +1,10 @@
+MODULO = (10**9) + 7
 # @param {String} str
 # @return {Integer}
 def count_homogenous(str)
-  
+  str.chars.chunk(&:itself).map { |arr| arr.last.count }.sum { |num| permutation(num) % MODULO } % MODULO
+end
+
+def permutation(num)
+  (num * (num + 1)) / 2
 end
