56619d94a134ac9184c23071232fce7f70c85f25
ozovalihasan <ozovalihasan@gmail.com>
2023-11-12 01:03:26 +0300

    Add a solution to the "Design Graph With Shortest Path Calculator" challenge

--- 2642_Design_Graph_With_Shortest_Path_Calculator/solution.rb
+++ 2642_Design_Graph_With_Shortest_Path_Calculator/solution.rb
@@ -4,15 +4,19 @@ class Graph
   :type edges: Integer[][]
 =end
   def initialize(n, edges)
-      
+    @nodes = Array.new(n) {{}}
+    
+    edges.each do |edge|
+      add_edge(edge)
+    end
   end
 
 =begin
   :type edge: Integer[]
   :rtype: Void
 =end
-  def add_edge(edge)
-      
+  def add_edge((start_node, end_node, weight))
+    @nodes[start_node][end_node] = weight
   end
 
 =begin
@@ -21,8 +25,32 @@ class Graph
   :rtype: Integer
 =end
   def shortest_path(node1, node2)
-      
+    stack = [[node1, 0]]
+    checked = Hash.new Float::INFINITY
+    checked[node1] = 0
+
+    until stack.empty?
+      next_stack = []
+      stack.sort_by(&:last).each do |current_node, current_weight|
+        @nodes[current_node].each do |next_node, weight|
+          next_sum = current_weight + weight
+
+          next if checked[next_node] <= next_sum
+          next if next_sum >= checked[node2]
+          next if current_node == node2
+
+          checked[next_node] = current_weight + weight
+          next_stack.push([next_node, next_sum])
+        end
+      end
+
+      stack = next_stack
+    end
+
+    checked[node2] == Float::INFINITY ? -1 : checked[node2]
   end
+
+  
 end
 
 # Your Graph object will be instantiated and called as such:
