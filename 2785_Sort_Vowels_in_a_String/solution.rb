555ec1cbd42f9f6959350c03614c11f9852f6df3
ozovalihasan <ozovalihasan@gmail.com>
2023-11-13 22:21:52 +0300

    Add a solution to the "Sort Vowels in a String" challenge

--- 2785_Sort_Vowels_in_a_String/solution.rb
+++ 2785_Sort_Vowels_in_a_String/solution.rb
@@ -1,5 +1,20 @@
 # @param {String} s
 # @return {String}
 def sort_vowels(str)
-    
+  vowels = []
+  indices = [] 
+  str.each_char.with_index do |char, index|
+    if char.match?(/[aeiou]/i)
+      vowels << char
+      indices << index
+    end
+  end
+
+  vowels.sort!
+
+  indices.zip(vowels).each do |index, char|
+    str[index] = char
+  end
+
+  str
 end
