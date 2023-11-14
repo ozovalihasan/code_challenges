39884fd52220c402ea96fd1096ecd8ead17a7136
ozovalihasan <ozovalihasan@gmail.com>
2023-11-14 22:11:34 +0300

    Add a solution to the "Unique Length-3 Palindromic Subsequences" challenge

--- 1930_Unique_Length-3_Palindromic_Subsequences/solution.rb
+++ 1930_Unique_Length-3_Palindromic_Subsequences/solution.rb
@@ -1,5 +1,17 @@
 # @param {String} s
 # @return {Integer}
 def count_palindromic_subsequence(str)
+  indices = {}
+  str = str.chars
   
+  str.each_with_index do |char, index|
+    indices[char] ||= [index, index]
+    indices[char][-1] = index
+  end
+
+  indices.values.sum do |char_indices|
+    next 0 if char_indices.first == char_indices.last
+    
+    str[char_indices.first + 1..char_indices.last - 1].uniq.size
+  end
 end
