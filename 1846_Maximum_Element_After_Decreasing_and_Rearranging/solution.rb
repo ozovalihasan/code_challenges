38df9af347ad994e65aa0010eb33bb6673b11036
ozovalihasan <ozovalihasan@gmail.com>
2023-11-16 00:26:00 +0300

    Add a solution to the "Maximum Element After Decreasing and Rearranging" challenge

--- 1846_Maximum_Element_After_Decreasing_and_Rearranging/solution.rb
+++ 1846_Maximum_Element_After_Decreasing_and_Rearranging/solution.rb
@@ -1,5 +1,11 @@
 # @param {Integer[]} arr
 # @return {Integer}
 def maximum_element_after_decrementing_and_rearranging(arr)
-  
+  result = 1
+  arr.sort.each do |num|
+    result += 1 
+    result = num if num <= result
+  end
+
+  result
 end
