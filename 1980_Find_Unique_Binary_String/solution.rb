fbd2a4395e7e7d9886f1ca482508b883b74cd468
ozovalihasan <ozovalihasan@gmail.com>
2023-11-16 23:28:37 +0300

    Add a solution to the "Find Unique Binary String" challenge

--- 1980_Find_Unique_Binary_String/solution.rb
+++ 1980_Find_Unique_Binary_String/solution.rb
@@ -1,5 +1,12 @@
 # @param {String[]} nums
 # @return {String}
 def find_different_binary_string(nums)
-    
+  nums.map! { |num| num.to_i(2) } 
+  result = nil
+  (0...(2**nums.size)).each do |num|
+    break (result = num) unless nums.include?(num)
+  end
+
+  result = result.to_s(2)
+  ('0' * (nums.size - result.size)) << result
 end
