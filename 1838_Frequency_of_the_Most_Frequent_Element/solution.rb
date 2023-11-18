7c733883b421e5b2097baa919cbe82951781fade
ozovalihasan <ozovalihasan@gmail.com>
2023-11-19 00:29:54 +0300

    Add a solution to the "Frequency of the Most Frequent Element" challenge

--- 1838_Frequency_of_the_Most_Frequent_Element/solution.rb
+++ 1838_Frequency_of_the_Most_Frequent_Element/solution.rb
@@ -2,5 +2,22 @@
 # @param {Integer} k
 # @return {Integer}
 def max_frequency(nums, k)
-    
+  nums.sort!
+
+  left = 0
+  right = 0
+  sum = 0
+  
+  until nums[right].nil?
+    sum += nums[right]
+
+    unless (nums[right] * (right - left + 1)) - sum <= k
+      sum -= nums[left]
+      left += 1
+    end
+ 
+    right += 1
+  end
+
+  right - left
 end
