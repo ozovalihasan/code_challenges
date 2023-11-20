b7cc022e319b5bf8e09319d626e316401a807b76
ozovalihasan <ozovalihasan@gmail.com>
2023-11-20 23:05:01 +0300

    Add a solution to the "Minimum Amount of Time to Collect Garbage" challenge

--- 2391_Minimum_Amount_of_Time_to_Collect_Garbage/solution.rb
+++ 2391_Minimum_Amount_of_Time_to_Collect_Garbage/solution.rb
@@ -2,5 +2,12 @@
 # @param {Integer[]} travel
 # @return {Integer}
 def garbage_collection(garbage, travel)
-    
+  total_time_to_collect = garbage.join.size
+
+  necessary_time_to_travel = %w[P G M].sum do |char|
+    index = garbage.rindex { |garb| garb.include?(char) } || 0
+    travel.first(index).sum
+  end
+
+  necessary_time_to_travel + total_time_to_collect
 end
