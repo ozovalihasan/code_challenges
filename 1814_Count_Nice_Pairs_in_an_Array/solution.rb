4b035ee240a30281c0a13a29cfdfdcf16c3361a1
ozovalihasan <ozovalihasan@gmail.com>
2023-11-22 00:30:27 +0300

    Add a solution to the "Count Nice Pairs in an Array" challenge

--- 1814_Count_Nice_Pairs_in_an_Array/solution.rb
+++ 1814_Count_Nice_Pairs_in_an_Array/solution.rb
@@ -1,5 +1,9 @@
+MODULO = (10**9) + 7
 # @param {Integer[]} nums
 # @return {Integer}
 def count_nice_pairs(nums)
-    
+  differences = nums.map { |num| num - num.to_s.reverse.to_i }
+  diff_counts = differences.tally.values.tally
+
+  diff_counts.sum { |size, count| ((size * (size - 1)) / 2 * count) % MODULO } % MODULO 
 end
