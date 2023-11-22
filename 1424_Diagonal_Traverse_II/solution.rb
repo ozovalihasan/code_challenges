14289c23341fe312cc29f15140bb32c6ab37f55e
ozovalihasan <ozovalihasan@gmail.com>
2023-11-22 20:31:15 +0300

    Add a solution to the "Diagonal Traverse II" challenge

--- 1424_Diagonal_Traverse_II/solution.rb
+++ 1424_Diagonal_Traverse_II/solution.rb
@@ -1,5 +1,13 @@
 # @param {Integer[][]} nums
 # @return {Integer[]}
 def find_diagonal_order(nums)
-    
+  result = []
+  
+  nums.each_with_index do |arr, row_index|
+    arr.each_with_index do |num, col_index|
+      (result[row_index + col_index] ||= []) << num
+    end
+  end
+
+  result.map(&:reverse).flatten
 end
