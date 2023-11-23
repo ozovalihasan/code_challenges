f31d1b1990ed19aef8a858c4758311de1a3ca31d
ozovalihasan <ozovalihasan@gmail.com>
2023-11-23 22:32:41 +0300

    Add a solution to the "Arithmetic Subarrays" challenge

--- 1630_Arithmetic_Subarrays/solution.rb
+++ 1630_Arithmetic_Subarrays/solution.rb
@@ -1,7 +1,23 @@
 # @param {Integer[]} nums
-# @param {Integer[]} l
-# @param {Integer[]} r
+# @param {Integer[]} left_indices
+# @param {Integer[]} right_indices
 # @return {Boolean[]}
-def check_arithmetic_subarrays(nums, l, r)
+def check_arithmetic_subarrays(nums, left_indices, right_indices)
+  left_indices.zip(right_indices).map do |left_index, right_index|
+    arithmetic?(nums[left_index..right_index])
+  end
+end
 
+def arithmetic?(arr)
+  arr.sort!
+  diff = arr[-1] - arr[-2]
+  previous_num = arr.pop - diff
+
+  until arr.empty?
+    return false unless arr.pop == previous_num
+
+    previous_num -= diff
+  end
+
+  true
 end
