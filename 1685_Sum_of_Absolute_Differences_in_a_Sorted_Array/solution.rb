b6d1b8f0d100c64e2477b34282998e1a95032247
ozovalihasan <ozovalihasan@gmail.com>
2023-11-25 20:25:37 +0300

    Add a solution to the "Sum of Absolute Differences in a Sorted Array" challenge

--- 1685_Sum_of_Absolute_Differences_in_a_Sorted_Array/solution.rb
+++ 1685_Sum_of_Absolute_Differences_in_a_Sorted_Array/solution.rb
@@ -1,5 +1,13 @@
 # @param {Integer[]} nums
 # @return {Integer[]}
 def get_sum_absolute_differences(nums)
-    
+  sum_right_part = nums.sum
+  sum_left_part = 0
+
+  (1..nums.size).map do |index|
+    num = nums[index - 1]
+    sum_left_part += num
+    sum_right_part -= num
+    (sum_right_part - (num * (nums.size - index))).abs + (sum_left_part - (num * index)).abs
+  end
 end
