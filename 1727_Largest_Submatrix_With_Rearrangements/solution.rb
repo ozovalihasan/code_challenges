101fc8b9bcb7161b8b2e0886ff77df7a4c7c691f
ozovalihasan <ozovalihasan@gmail.com>
2023-11-26 22:14:07 +0300

    Add a solution to the "Largest Submatrix With Rearrangements" challenge

--- 1727_Largest_Submatrix_With_Rearrangements/solution.rb
+++ 1727_Largest_Submatrix_With_Rearrangements/solution.rb
@@ -1,5 +1,29 @@
 # @param {Integer[][]} matrix
 # @return {Integer}
 def largest_submatrix(matrix)
+  result = 0
+  zeros = []
+  matrix.each_with_index do |row, row_index|
+    zeros[row_index] = []
     
+    row.each_with_index do |cell, col_index| 
+      zeros[row_index] << col_index if cell.zero?
+    end
+  end
+
+  max_row_index = matrix.size - 1
+  col_size = matrix.first.size
+  matrix.each_index do |row_index|
+    valid_columns = (0...col_size).to_a
+
+    row_index.upto(max_row_index) do |row_index2|
+      break if result / (matrix.size - row_index) >= valid_columns.size
+      
+      valid_columns -= zeros[row_index2]
+      new_result = valid_columns.size * (row_index2 - row_index + 1)
+      result = new_result if new_result > result
+    end
+  end
+
+  result
 end
