dee1a349df4c36192b942c0a751539d4ecb1b6f3
ozovalihasan <ozovalihasan@gmail.com>
2023-11-28 22:56:33 +0300

    Add a solution to the "Number of Ways to Divide a Long Corridor" challenge

--- 2147_Number_of_Ways_to_Divide_a_Long_Corridor/solution.rb
+++ 2147_Number_of_Ways_to_Divide_a_Long_Corridor/solution.rb
@@ -1,5 +1,10 @@
+MODULO = (10**9) + 7
+
 # @param {String} corridor
 # @return {Integer}
 def number_of_ways(corridor)
-    
+  count = corridor.count('S')
+  return 0 if count.odd? || count < 2
+  
+  corridor[/S.*S/].scan(/SP*S(P*)/).inject(1) { |result, plants| (result * plants[0].size.next) % MODULO }
 end
