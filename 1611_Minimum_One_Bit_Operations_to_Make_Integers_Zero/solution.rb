5fd53ac7c3f5f8be36a8255d76d331351c252c36
ozovalihasan <ozovalihasan@gmail.com>
2023-11-30 22:32:44 +0300

    Add a solution to the "Minimum One Bit Operations to Make Integers Zero" challenge

--- 1611_Minimum_One_Bit_Operations_to_Make_Integers_Zero/solution.rb
+++ 1611_Minimum_One_Bit_Operations_to_Make_Integers_Zero/solution.rb
@@ -1,5 +1,17 @@
 # @param {Integer} num
 # @return {Integer}
 def minimum_one_bit_operations(num)
-  
+  str = num.to_s(2)
+
+  result = 0
+  total_ones = 0
+  str.each_char do |char|
+    if char == '1'
+      result = (result * 2) + (total_ones.even? ? 1 : 0)
+      total_ones += 1
+    else
+      result = (result * 2) + (total_ones.odd? ? 1 : 0)
+    end
+  end
+  result
 end
