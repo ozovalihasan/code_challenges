5b96c64bc1455f2ccb8c01f6c5be991e285f22f0
ozovalihasan <ozovalihasan@gmail.com>
2023-12-02 20:35:09 +0300

    Add a solution to the "Find Words That Can Be Formed by Characters" challenge

--- 1160_Find_Words_That_Can_Be_Formed_by_Characters/solution.rb
+++ 1160_Find_Words_That_Can_Be_Formed_by_Characters/solution.rb
@@ -2,5 +2,18 @@
 # @param {String} chars
 # @return {Integer}
 def count_characters(words, chars)
-    
+  char_counts = count(chars)
+  char_counts.default = 0
+
+  words
+    .select { |word| good_word?(word, char_counts) }
+    .sum(&:size)
+end
+
+def good_word?(word, char_counts)
+  count(word).all? { |char, count| char_counts[char] >= count }
+end
+
+def count(word)
+  word.chars.tally
 end
