940593f306f15c2acd4791b9107850092d15b81e
ozovalihasan <ozovalihasan@gmail.com>
2023-12-03 11:14:22 +0300

    Add a solution to the "Minimum Time Visiting All Points" challenge

--- 1266_Minimum_Time_Visiting_All_Points/solution.rb
+++ 1266_Minimum_Time_Visiting_All_Points/solution.rb
@@ -1,5 +1,13 @@
 # @param {Integer[][]} points
 # @return {Integer}
 def min_time_to_visit_all_points(points)
-    
+  points.each_cons(2).sum do |start_point, end_point|
+    start_x, start_y = start_point
+    end_x, end_y = end_point
+
+    x_diff = (start_x - end_x).abs
+    y_diff = (start_y - end_y).abs
+
+    [x_diff, y_diff].max
+  end
 end
