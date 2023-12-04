578f043b4058909606c70e2dad2537df0166dc91
ozovalihasan <ozovalihasan@gmail.com>
2023-12-04 20:14:35 +0300

    Add a solution to the "Largest 3-Same-Digit Number in String" challenge

--- 2264_Largest_3-Same-Digit_Number_in_String/solution.rb
+++ 2264_Largest_3-Same-Digit_Number_in_String/solution.rb
@@ -1,5 +1,9 @@
 # @param {String} num
 # @return {String}
 def largest_good_integer(num)
-    
+  result = 0.upto(num.size - 3).filter_map do |index|
+    num[index...index + 3] if (num[index] == num[index + 1]) && (num[index] == num[index + 2])
+  end
+  
+  result.max || ''
 end
