4c69c3f429b4777f9f0594ea7b004743b279cf72
ozovalihasan <ozovalihasan@gmail.com>
2023-12-05 20:27:33 +0300

    Add a solution to the "How Many Numbers Are Smaller Than the Current Number" challenge

--- 1365_How_Many_Numbers_Are_Smaller_Than_the_Current_Number/solution.rb
+++ 1365_How_Many_Numbers_Are_Smaller_Than_the_Current_Number/solution.rb
@@ -1,5 +1,13 @@
 # @param {Integer[]} nums
 # @return {Integer[]}
 def smaller_numbers_than_current(nums)
-    
+  result = {}
+  total_count = 0
+
+  nums.tally.sort.each do |num, count|
+    result[num] = total_count  
+    total_count += count
+  end
+
+  nums.map { |num| result[num] }
 end
