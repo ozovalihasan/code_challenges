ddbb68a4e3746dd66ead19ddd71b38e0d5f5a343
ozovalihasan <ozovalihasan@gmail.com>
2023-12-06 23:12:08 +0300

    Add a solution to the "Calculate Money in Leetcode Bank" challenge

--- 1716_Calculate_Money_in_Leetcode_Bank/solution.rb
+++ 1716_Calculate_Money_in_Leetcode_Bank/solution.rb
@@ -1,5 +1,10 @@
 # @param {Integer} num
 # @return {Integer}
 def total_money(num)
-    
+  div = num / 7
+  result = (28 * div) + (7 * ((div * (div - 1)) / 2))
+
+  remainder = num % 7
+  result += (remainder * (remainder + 1) / 2) + (div * remainder)
+  result
 end
