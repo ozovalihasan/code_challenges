876129acf6406e5da21efc54b9213c7b020258b9
ozovalihasan <ozovalihasan@gmail.com>
2023-12-07 18:05:40 +0300

    Add a solution to the "Three Consecutive Odds" challenge

--- 1550_Three_Consecutive_Odds/solution.rb
+++ 1550_Three_Consecutive_Odds/solution.rb
@@ -1,5 +1,11 @@
 # @param {Integer[]} arr
 # @return {Boolean}
 def three_consecutive_odds(arr)
-    
+  result = 0
+  arr.each do |num|
+    num.odd? ? result += 1 : result = 0
+    return true if result == 3
+  end
+  
+  false
 end
