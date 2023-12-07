e0ede4fefe8aac80bc18834844ee63408197e8fb
ozovalihasan <ozovalihasan@gmail.com>
2023-12-07 18:01:24 +0300

    Add a test for the "Three Consecutive Odds" challenge

---
+++ 1550_Three_Consecutive_Odds/test_solution.rb
@@ -0,0 +1,14 @@
+require_relative './solution'
+require_relative '../read_test_files'
+
+describe 'Solution' do
+  examples, results = read_test_files.values_at(:examples, :results)
+
+  describe '#three_consecutive_odds' do
+    examples.each_with_index do |arr, index|
+      it 'returns true if there are three consecutive odd numbers in the array' do
+        expect(three_consecutive_odds(arr)).to eq(results[index])
+      end
+    end
+  end
+end
