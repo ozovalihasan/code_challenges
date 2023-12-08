808f90c67ee1b53b830c0b9d45a53b4abd60b506
ozovalihasan <ozovalihasan@gmail.com>
2023-12-09 00:01:52 +0300

    Add a solution to the "Row With Maximum Ones" challenge

--- 2643_Row_With_Maximum_Ones/solution.rb
+++ 2643_Row_With_Maximum_Ones/solution.rb
@@ -1,5 +1,16 @@
 # @param {Integer[][]} mat
 # @return {Integer[]}
 def row_and_maximum_ones(mat)
-    
+  count = 0
+  result = [0, count]
+  mat.each_with_index do |row, index|
+    row_count = row.count(1)
+
+    if row_count > count
+      count = row_count
+      result = [index, count]
+    end
+  end
+
+  result
 end
