feb79cc87a6189392cb9373337d490663dec6c34
ozovalihasan <ozovalihasan@gmail.com>
2023-12-11 21:12:03 +0300

    Add a solution to the "Element Appearing More Than 25% In Sorted Array" challenge

--- 1287_Element_Appearing_More_Than_25%_In_Sorted_Array/solution.rb
+++ 1287_Element_Appearing_More_Than_25%_In_Sorted_Array/solution.rb
@@ -1,5 +1,8 @@
 # @param {Integer[]} arr
 # @return {Integer}
 def find_special_integer(arr)
-    
+  necessary_count = arr.length / 4
+  arr.each_with_index do |num, index|
+    return num if arr[index + necessary_count] == num
+  end
 end
