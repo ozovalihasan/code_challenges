f8c5edc119e51cc7195207eefab641eb366a44c9
ozovalihasan <ozovalihasan@gmail.com>
2023-12-13 21:58:26 +0300

    Add a solution to the "Special Positions in a Binary Matrix" challenge

--- 1582_Special_Positions_in_a_Binary_Matrix/solution.rb
+++ 1582_Special_Positions_in_a_Binary_Matrix/solution.rb
@@ -1,5 +1,15 @@
 # @param {Integer[][]} mat
 # @return {Integer}
 def num_special(mat)
-    
+  col_count = mat.transpose.map! { |row| row.count(1) }
+
+  count = 0
+  mat.each do |row|
+    next unless row.count(1) == 1
+
+    col_index = row.index(1)
+    count += 1 if col_count[col_index] == 1
+  end
+  
+  count
 end
