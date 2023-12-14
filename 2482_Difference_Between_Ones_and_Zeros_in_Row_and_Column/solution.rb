5a04b172d0622606fe61c13871ad9339c1e05608
ozovalihasan <ozovalihasan@gmail.com>
2023-12-14 21:35:04 +0300

    Update the solution of the "Difference Between Ones and Zeros in Row and Column" challenge

--- 2482_Difference_Between_Ones_and_Zeros_in_Row_and_Column/solution.rb
+++ 2482_Difference_Between_Ones_and_Zeros_in_Row_and_Column/solution.rb
@@ -4,22 +4,13 @@ def ones_minus_zeros(grid)
   row_diff = find_diff(grid)
   col_diff = find_diff(grid.transpose)
   
-  result = []
-  grid.each_index do |row_index|
-    result[row_index] = []
-    
-    grid.first.each_index do |col_index|
-      result[row_index][col_index] = row_diff[row_index] + col_diff[col_index]
+  (0...grid.size).map do |row_index|
+    (0...grid.first.size).map do |col_index|
+      row_diff[row_index] + col_diff[col_index]
     end
   end
-
-  result
 end
 
 def find_diff(grid)
-  row_diff = []
-  grid.each_with_index do |row, index|
-    row_diff[index] = row.count(1) - row.count(0)
-  end
-  row_diff
+  grid.map { |row| grid.first.size - (2 * row.count(0)) }
 end
