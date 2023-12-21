3a031d6483d8f1258f5a64656cfdc5472ce2704a
ozovalihasan <ozovalihasan@gmail.com>
2023-12-21 23:55:19 +0300

    Add a solution to the "Widest Vertical Area Between Two Points Containing No Points" challenge

--- 1637_Widest_Vertical_Area_Between_Two_Points_Containing_No_Points/solution.rb
+++ 1637_Widest_Vertical_Area_Between_Two_Points_Containing_No_Points/solution.rb
@@ -1,5 +1,5 @@
 # @param {Integer[][]} points
 # @return {Integer}
 def max_width_of_vertical_area(points)
-    
+  points.map(&:first).sort!.each_cons(2).map { |first, second| second - first }.max
 end
