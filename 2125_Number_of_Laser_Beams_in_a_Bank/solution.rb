ef86b0bcd718e2136ccf647d2ae35f80fb759924
ozovalihasan <ozovalihasan@gmail.com>
2023-12-22 22:15:23 +0300

    Add a solution to the "Number of Laser Beams in a Bank" challenge

--- 2125_Number_of_Laser_Beams_in_a_Bank/solution.rb
+++ 2125_Number_of_Laser_Beams_in_a_Bank/solution.rb
@@ -1,5 +1,7 @@
 # @param {String[]} bank
 # @return {Integer}
 def number_of_beams(bank)
-    
+  bank.map { |row| row.count('1') }
+    .reject(&:zero?)
+    .each_cons(2).sum { |first, second| first * second }
 end
