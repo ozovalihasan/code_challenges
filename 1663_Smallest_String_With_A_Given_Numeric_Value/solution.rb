ae7276739af0e47ec5355457a3ba6d6b981f545d
ozovalihasan <ozovalihasan@gmail.com>
2023-12-24 00:11:15 +0300

    Add a solution to the "Smallest String With A Given Numeric Value" challenge

--- 1663_Smallest_String_With_A_Given_Numeric_Value/solution.rb
+++ 1663_Smallest_String_With_A_Given_Numeric_Value/solution.rb
@@ -2,5 +2,11 @@
 # @param {Integer} k
 # @return {String}
 def get_smallest_string(n, k)
-    
+  result = 'a' * n
+  k -= n
+  quotient, reminder = k.divmod(25)
+  
+  result[-quotient..] = 'z' * quotient if quotient.positive?
+  result[-quotient - 1] = (reminder + 97).chr if quotient < n
+  result
 end
