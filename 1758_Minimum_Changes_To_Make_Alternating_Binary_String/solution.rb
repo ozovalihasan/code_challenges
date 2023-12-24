f6763811dd59a4f26f5aa0458bac25418ce5e6a3
ozovalihasan <ozovalihasan@gmail.com>
2023-12-24 13:14:18 +0300

    Add a solution to the "Minimum Changes To Make Alternating Binary String" challenge

--- 1758_Minimum_Changes_To_Make_Alternating_Binary_String/solution.rb
+++ 1758_Minimum_Changes_To_Make_Alternating_Binary_String/solution.rb
@@ -1,5 +1,11 @@
 # @param {String} str
 # @return {Integer}
 def min_operations(str)
-    
+  counts = [0, 0]
+
+  str.each_char.with_index do |chr, index|
+    counts[(index + chr.to_i) % 2] += 1
+  end
+
+  counts.min
 end
