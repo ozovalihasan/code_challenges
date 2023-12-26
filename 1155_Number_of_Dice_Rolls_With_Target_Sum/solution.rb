e9490ccb5dd4e6399601c2ff284b2d89347effbe
ozovalihasan <ozovalihasan@gmail.com>
2023-12-26 20:40:41 +0300

    Add a solution to the "Number of Dice Rolls With Target Sum" challenge

--- 1155_Number_of_Dice_Rolls_With_Target_Sum/solution.rb
+++ 1155_Number_of_Dice_Rolls_With_Target_Sum/solution.rb
@@ -1,7 +1,23 @@
-# @param {Integer} n
-# @param {Integer} k
+MOD = (10**9) + 7
+# @param {Integer} units
+# @param {Integer} sides
 # @param {Integer} target
 # @return {Integer}
-def num_rolls_to_target(n, k, target)
-    
+def num_rolls_to_target(units, sides, target)
+  @memo = {}
+  @sides = sides
+  check(units, target)
+
+  @memo[[units, target]] % MOD
+end
+
+def check(units, target)
+  return @memo[[units, target]] if @memo[[units, target]]
+  return @memo[[units, target]] = 1 if units.zero? && target.zero?
+  return @memo[[units, target]] = 0 if units <= 0 || target <= 0
+  return @memo[[units, target]] = 0 if (units * @sides) < target
+  
+  @memo[[units, target]] = 1.upto(@sides).sum do |side|
+    check(units - 1, target - side) 
+  end % MOD
 end
