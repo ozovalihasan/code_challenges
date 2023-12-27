ecf3ee9c58243f1463e74b4800d31ac2b55bdfe5
ozovalihasan <ozovalihasan@gmail.com>
2023-12-27 20:36:23 +0300

    Add a solution to the "Minimum Time to Make Rope Colorful" challenge

--- 1578_Minimum_Time_to_Make_Rope_Colorful/solution.rb
+++ 1578_Minimum_Time_to_Make_Rope_Colorful/solution.rb
@@ -2,5 +2,22 @@
 # @param {Integer[]} needed_time
 # @return {Integer}
 def min_cost(colors, needed_time)
-    
+  last_color = ''
+  max = 0
+
+  total_time = needed_time.sum
+  colors.each_char.with_index do |color, index|
+    time = needed_time[index]
+
+    if color == last_color
+      max = [max, time].max
+    else
+      total_time -= max
+      max = time
+    end
+
+    last_color = color
+  end
+
+  total_time -= max
 end
