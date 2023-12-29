2b3e03b13d18edc89e9a8590991eddcf332a2d2b
ozovalihasan <ozovalihasan@gmail.com>
2023-12-29 23:47:03 +0300

    Add a solution to the "Minimum Difficulty of a Job Schedule" challenge

--- 1335_Minimum_Difficulty_of_a_Job_Schedule/solution.rb
+++ 1335_Minimum_Difficulty_of_a_Job_Schedule/solution.rb
@@ -1,6 +1,24 @@
 # @param {Integer[]} job_difficulty
-# @param {Integer} d
+# @param {Integer} total_days
 # @return {Integer}
-def min_difficulty(job_difficulty, d)
-    
+def min_difficulty(job_difficulty, total_days)
+  result = Array.new(total_days + 1) { {} }
+
+  result[0] = { 0 => 0 }
+
+  result[0...total_days].each_with_index do |possible_totals, day_index|
+    possible_totals.each do |index, total|
+      max = job_difficulty[index]
+
+      index.upto(job_difficulty.size - 1) do |index2|
+        max = [max, job_difficulty[index2]].max
+
+        if (result[day_index + 1][index2 + 1] || Float::INFINITY) > (total + max)
+          result[day_index + 1][index2 + 1] = total + max 
+        end
+      end
+    end
+  end
+
+  result.last[job_difficulty.size] || -1
 end
