7651aebd2d1ea955778f42326b41274671fad0a1
ozovalihasan <ozovalihasan@gmail.com>
2023-12-31 13:47:24 +0300

    Add a solution to the "Largest Substring Between Two Equal Characters" challenge

--- 1624_Largest_Substring_Between_Two_Equal_Characters/solution.rb
+++ 1624_Largest_Substring_Between_Two_Equal_Characters/solution.rb
@@ -1,5 +1,9 @@
-# @param {String} s
+# @param {String} str
 # @return {Integer}
-def max_length_between_equal_characters(s)
+def max_length_between_equal_characters(str)
+  indices = {}
   
+  str.each_char.with_index { |char, index| (indices[char] ||= []) << index }
+
+  indices.values.map { |char_indices| char_indices.last - char_indices.first }.max - 1
 end
