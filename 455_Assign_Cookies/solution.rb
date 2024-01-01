fe07ddf4800f18eb18080b6bd639204a4bfc04cf
ozovalihasan <ozovalihasan@gmail.com>
2024-01-01 14:08:40 +0300

    Add a solution to the "Assign Cookies" challenge

--- 455_Assign_Cookies/solution.rb
+++ 455_Assign_Cookies/solution.rb
@@ -2,5 +2,15 @@
 # @param {Integer[]} cookie_sizes
 # @return {Integer}
 def find_content_children(greed_factors, cookie_sizes)
+  greed_factors.sort!
+  cookie_sizes.sort!
 
+  children_count = 0
+  cookie_sizes.each do |cookie_size|
+    break unless greed_factors[children_count]
+
+    children_count += 1 if cookie_size >= greed_factors[children_count]
+  end
+
+  children_count
 end
