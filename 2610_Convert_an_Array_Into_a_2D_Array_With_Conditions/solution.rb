11980d021bddbe5bde6ba206cb766ac89e3d2128
ozovalihasan <ozovalihasan@gmail.com>
2024-01-02 22:13:21 +0300

    Add a solution to the "Convert an Array Into a 2D Array With Conditions" challenge

--- 2610_Convert_an_Array_Into_a_2D_Array_With_Conditions/solution.rb
+++ 2610_Convert_an_Array_Into_a_2D_Array_With_Conditions/solution.rb
@@ -1,5 +1,12 @@
 # @param {Integer[]} nums
 # @return {Integer[][]}
 def find_matrix(nums)
-    
+  count = Hash.new(0)
+  result = []
+  nums.each do |num|
+    (result[count[num]] ||= []) << num
+    count[num] = count[num] + 1
+  end
+  
+  result
 end
