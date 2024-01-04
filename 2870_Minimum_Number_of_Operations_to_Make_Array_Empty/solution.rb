f3c0d1a4bf504426326e8c7fcedad7ce2af4d426
ozovalihasan <ozovalihasan@gmail.com>
2024-01-04 20:58:50 +0300

    Add a solution to the "Minimum Number of Operations to Make Array Empty" challenge

--- 2870_Minimum_Number_of_Operations_to_Make_Array_Empty/solution.rb
+++ 2870_Minimum_Number_of_Operations_to_Make_Array_Empty/solution.rb
@@ -1,5 +1,8 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def min_operations(nums)
-    
+  counts = nums.tally.values
+  return -1 if counts.any?(1)
+
+  counts.sum { |count| ((count + 2) / 3) }
 end
