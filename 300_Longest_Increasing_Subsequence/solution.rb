63e4883d35299c376603f3c353906f5f7de64066
ozovalihasan <ozovalihasan@gmail.com>
2024-01-05 21:41:35 +0300

    Add a solution to the "Longest Increasing Subsequence" challenge

--- 300_Longest_Increasing_Subsequence/solution.rb
+++ 300_Longest_Increasing_Subsequence/solution.rb
@@ -1,5 +1,11 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def length_of_lis(nums)
-    
+  result = []
+  nums.each do |num|
+    index = result.bsearch_index { |checked_num| checked_numq >= num } || result.size
+    result[index] = num
+  end
+
+  result.size
 end
