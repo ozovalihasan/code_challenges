b1ee6ebf03a628a580b259bc58de5b06c63cb38a
ozovalihasan <ozovalihasan@gmail.com>
2024-01-06 16:07:29 +0300

    Update the solution of the "Maximum Profit in Job Scheduling" challenge

--- 1235_Maximum_Profit_in_Job_Scheduling/solution.rb
+++ 1235_Maximum_Profit_in_Job_Scheduling/solution.rb
@@ -3,14 +3,16 @@
 # @param {Integer[]} profit
 # @return {Integer}
 def job_scheduling(start_time, end_time, profit)
-  jobs = start_time.zip(end_time, profit).sort_by(&:first).reverse
-  previous_total_profits = [[0, 0]]
+  return profit.max if start_time.max < end_time.min
 
-  while (time_now, end_time, profit = jobs.pop)
-    max_profit_until_now = previous_total_profits.select { |end_time, _| end_time <= time_now }.max_by(&:last).last
-    previous_total_profits.select! {|end_time, _| end_time > time_now}
-    previous_total_profits << [ time_now, max_profit_until_now ] << [ end_time, max_profit_until_now + profit ]
+  jobs = start_time.zip(end_time, profit).sort_by(&:first)
+  total_profits = [[0, 0]]
+
+  jobs.each do |current_time, current_end_time, current_profit|
+    max_profit_until_now = total_profits.select { |checked_end_time, _| checked_end_time <= current_time }.max_by(&:last).last
+    total_profits.select! { |checked_end_time, _| checked_end_time > current_time }
+    total_profits << [current_time, max_profit_until_now] << [current_end_time, max_profit_until_now + current_profit]
   end
 
-  previous_total_profits.max_by(&:last).last
+  total_profits.max_by(&:last).last
 end
