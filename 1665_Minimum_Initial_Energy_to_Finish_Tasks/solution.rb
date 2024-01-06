2dd6b419be907b3f7a79cedfa0e6575ed9bb8da6
ozovalihasan <ozovalihasan@gmail.com>
2024-01-06 17:15:09 +0300

    Add a solution to the "Minimum Initial Energy to Finish Tasks" challenge

--- 1665_Minimum_Initial_Energy_to_Finish_Tasks/solution.rb
+++ 1665_Minimum_Initial_Energy_to_Finish_Tasks/solution.rb
@@ -1,5 +1,12 @@
 # @param {Integer[][]} tasks
 # @return {Integer}
 def minimum_effort(tasks)
-    
+  tasks.sort_by! { |task| task[1] - task[0] }
+  
+  necessary_effort = 0
+  tasks.each_index do |actual_effort, minimum_effort|
+    necessary_effort = [necessary_effort + actual_effort, minimum_effort].max
+  end
+
+  necessary_effort
 end
