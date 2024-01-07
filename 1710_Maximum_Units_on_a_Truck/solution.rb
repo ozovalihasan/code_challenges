d3f85dbb02efe327d6ccdd618e90b87fd0c43e2d
ozovalihasan <ozovalihasan@gmail.com>
2024-01-07 19:32:27 +0300

    Add a solution to the "Maximum Units on a Truck" challenge

--- 1710_Maximum_Units_on_a_Truck/solution.rb
+++ 1710_Maximum_Units_on_a_Truck/solution.rb
@@ -2,5 +2,19 @@
 # @param {Integer} truck_size
 # @return {Integer}
 def maximum_units(box_types, truck_size)
-    
+  box_types.sort_by! { |_number_of_boxes, units| units }.reverse!
+
+  @result = 0
+  box_types.sum do |number_of_boxes, units|
+    if truck_size >= number_of_boxes
+      truck_size -= number_of_boxes
+      
+      @result += number_of_boxes * units
+    else
+      @result += truck_size * units
+      break 
+    end
+  end
+
+  @result
 end
