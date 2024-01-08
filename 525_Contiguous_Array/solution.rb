475c1121fb878381973443b5f8e6c613f34b3893
ozovalihasan <ozovalihasan@gmail.com>
2024-01-08 20:38:11 +0300

    Add a solution to the "Contiguous Array" challenge

--- 525_Contiguous_Array/solution.rb
+++ 525_Contiguous_Array/solution.rb
@@ -1,5 +1,13 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def find_max_length(nums)
+  total = 0
+  indices = { 0 => -1 }
+
+  nums.map.with_index do |num, index| 
+    total += num.zero? ? -1 : 1
     
+    indices[total] ||= index
+    index - indices[total] 
+  end.max
 end
