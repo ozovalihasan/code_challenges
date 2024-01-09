b688ff842a8fd547db25041bb11342268fb3132e
ozovalihasan <ozovalihasan@gmail.com>
2024-01-09 21:52:53 +0300

    Add a solution to the "Custom Sort String" challenge

--- 791_Custom_Sort_String/solution.rb
+++ 791_Custom_Sort_String/solution.rb
@@ -2,5 +2,7 @@
 # @param {String} str
 # @return {String}
 def custom_sort_string(order, str)
-    
+  chars_count = str.chars.tally
+  result = order.chars.map { |char| char * (chars_count.delete(char) || 0) }.join
+  result + chars_count.map { |char, count| char * count }.join
 end
