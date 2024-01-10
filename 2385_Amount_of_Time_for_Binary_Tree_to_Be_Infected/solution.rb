62706817d389eba99037b2f98541c8a2292cc54c
ozovalihasan <ozovalihasan@gmail.com>
2024-01-10 23:58:30 +0300

    Add a solution to the "Amount of Time for Binary Tree to Be Infected" challenge

--- 2385_Amount_of_Time_for_Binary_Tree_to_Be_Infected/solution.rb
+++ 2385_Amount_of_Time_for_Binary_Tree_to_Be_Infected/solution.rb
@@ -11,5 +11,28 @@
 # @param {Integer} start
 # @return {Integer}
 def amount_of_time(root, start)
-    
+  @start_val = start
+  @max = 0
+  check_node(root)
+  @max
+end
+
+def check_node(node)
+  return [0, false] if node.nil? 
+
+  left_count, left_infected = check_node(node.left)
+  right_count, right_infected = check_node(node.right)
+  
+  @max = [@max, left_count + right_count + 1].max if left_infected || right_infected
+
+  if left_infected
+    [left_count + 1, true]
+  elsif right_infected
+    [right_count + 1, true]
+  elsif node.val == @start_val
+    @max = [left_count, right_count].max
+    [0, true]
+  else
+    [[left_count, right_count].max + 1, false]
+  end
 end
