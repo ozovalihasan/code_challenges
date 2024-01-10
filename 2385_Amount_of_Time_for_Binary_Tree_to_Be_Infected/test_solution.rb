a4d63c8a94a9f2b40f97f1ab0a6dfcc0b4d3ff21
ozovalihasan <ozovalihasan@gmail.com>
2024-01-10 22:08:33 +0300

    Add a test for the "Amount of Time for Binary Tree to Be Infected" challenge

---
+++ 2385_Amount_of_Time_for_Binary_Tree_to_Be_Infected/test_solution.rb
@@ -0,0 +1,16 @@
+require_relative './solution'
+require_relative '../read_test_files'
+require_relative './binary_tree'
+
+describe 'Solution' do
+  examples, results = read_test_files.values_at(:examples, :results)
+
+  describe '#is_same_tree' do
+    examples.each_slice(2).each_with_index do |(root, start), index|
+      it 'returns whether two binary trees are same or not' do
+        tree = Tree.new(root)
+        expect(amount_of_time(tree, start)).to eq(results[index])
+      end
+    end
+  end
+end
