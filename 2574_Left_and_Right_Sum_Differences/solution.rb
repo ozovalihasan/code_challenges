92c1ec625de5516b5d657b99a5e6fa4d20934f38
ozovalihasan <ozovalihasan@gmail.com>
2024-01-11 22:10:38 +0300

    Add a solution to the "Left and Right Sum Differences" challenge

--- 2574_Left_and_Right_Sum_Differences/solution.rb
+++ 2574_Left_and_Right_Sum_Differences/solution.rb
@@ -1,5 +1,12 @@
 # @param {Integer[]} nums
 # @return {Integer[]}
 def left_right_difference(nums)
-    
+  left_sum = 0
+  right_sum = nums.sum
+  
+  nums.map do |num| 
+    right_sum -= num
+    left_sum += num 
+    (left_sum - num - right_sum).abs
+  end
 end
