55681247330b7ce361aa2a83aca7d23b8fcd3268
ozovalihasan <ozovalihasan@gmail.com>
2024-01-12 22:07:25 +0300

    Add a solution to the "Minimum Add to Make Parentheses Valid" challenge

--- 921_Minimum_Add_to_Make_Parentheses_Valid/solution.rb
+++ 921_Minimum_Add_to_Make_Parentheses_Valid/solution.rb
@@ -1,5 +1,18 @@
 # @param {String} str
 # @return {Integer}
 def min_add_to_make_valid(str)
-    
+  necessary_open = 0
+  necessary_closed = 0
+  
+  str.each_char do |char|
+    if char == '('
+      necessary_closed += 1
+    elsif necessary_closed.zero?
+      necessary_open += 1
+    else
+      necessary_closed -= 1
+    end
+  end
+
+  necessary_open + necessary_closed
 end
