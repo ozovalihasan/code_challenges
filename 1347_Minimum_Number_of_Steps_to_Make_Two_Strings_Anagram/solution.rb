65a570159454efe14dac29be2ec047d6f3ad553d
ozovalihasan <ozovalihasan@gmail.com>
2024-01-13 21:25:37 +0300

    Add a solution to the "Minimum Number of Steps to Make Two Strings Anagram" challenge

--- 1347_Minimum_Number_of_Steps_to_Make_Two_Strings_Anagram/solution.rb
+++ 1347_Minimum_Number_of_Steps_to_Make_Two_Strings_Anagram/solution.rb
@@ -2,5 +2,11 @@
 # @param {String} t
 # @return {Integer}
 def min_steps(s, t)
-    
+  s_bytes = s.bytes.tally
+  t_bytes = t.bytes.tally
+  t_bytes.default = 0
+  
+  s_bytes.sum do |byte, count| 
+    [count - t_bytes[byte], 0].max
+  end
 end
