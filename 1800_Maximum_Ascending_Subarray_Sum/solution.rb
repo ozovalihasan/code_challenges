ddde23691aa1a59ea80edde428a7b48187c9ecc0
ozovalihasan <ozovalihasan@gmail.com>
2024-01-14 19:07:15 +0300

    Add a solution to the "Maximum Ascending Subarray Sum" challenge

--- 1800_Maximum_Ascending_Subarray_Sum/solution.rb
+++ 1800_Maximum_Ascending_Subarray_Sum/solution.rb
@@ -1,5 +1,14 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def max_ascending_sum(nums)
-    
+  previous = 0 
+  result = [0]
+
+  nums.each do |num|
+    result << 0 if num <= previous
+    result[-1] += num
+    previous = num
+  end
+  
+  result.max
 end
