eb4d2e2631ed822f36a199b50a57943dde91795a
ozovalihasan <ozovalihasan@gmail.com>
2024-01-15 22:56:26 +0300

    Add a solution to the "Minimum Increment to Make Array Unique" challenge

--- 945_Minimum_Increment_to_Make_Array_Unique/solution.rb
+++ 945_Minimum_Increment_to_Make_Array_Unique/solution.rb
@@ -1,5 +1,19 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def min_increment_for_unique(nums)
+  nums.sort!
+
+  possible_num = 0
+  moves = 0
+  nums.each do |num|
+    if possible_num >= num
+      moves += possible_num - num
+    else
+      possible_num = num
+    end
     
+    possible_num += 1
+  end
+
+  moves
 end
