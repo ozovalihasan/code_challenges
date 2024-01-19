f1d54274108508bdb350b6c2e540523fe4f7efe8
ozovalihasan <ozovalihasan@gmail.com>
2024-01-19 19:45:53 +0300

    Add a solution to the "Longest Square Streak in an Array" challenge

--- 2501_Longest_Square_Streak_in_an_Array/solution.rb
+++ 2501_Longest_Square_Streak_in_an_Array/solution.rb
@@ -1,5 +1,11 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def longest_square_streak(nums)
-    
+  nums.sort!
+  
+  result = Hash.new(0)
+  nums.each { |num| result[num**2] = result[num] + 1 }
+
+  max = result.values.max
+  max == 1 ? -1 : max
 end
