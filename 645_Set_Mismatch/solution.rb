30e4093465b9992fead40c622bd1bb02471256ce
ozovalihasan <ozovalihasan@gmail.com>
2024-01-22 23:19:44 +0300

    Add a solution to the "Set Mismatch" challenge

--- 645_Set_Mismatch/solution.rb
+++ 645_Set_Mismatch/solution.rb
@@ -1,5 +1,7 @@
 # @param {Integer[]} nums
 # @return {Integer[]}
 def find_error_nums(nums)
-    
+  missing = ((1..nums.size).to_a - nums).first
+  extra = nums.tally.find { |_, count| count == 2 }.first
+  [extra, missing]
 end
