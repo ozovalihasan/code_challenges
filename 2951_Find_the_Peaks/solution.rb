25a0dedb43e20aab669ea86e3de062c9156ddbe3
ozovalihasan <ozovalihasan@gmail.com>
2024-01-22 23:07:21 +0300

    Add a solution to the "Find the Peaks" challenge

--- 2951_Find_the_Peaks/solution.rb
+++ 2951_Find_the_Peaks/solution.rb
@@ -1,5 +1,7 @@
 # @param {Integer[]} mountain
 # @return {Integer[]}
 def find_peaks(mountain)
-
+  mountain.each_cons(3).filter_map.with_index do |(first, second, third), index|
+    index + 1 if second > first && second > third
+  end 
 end
