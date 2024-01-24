7d03ce0acfd07ec8423230676be3b423d652c401
ozovalihasan <ozovalihasan@gmail.com>
2024-01-24 22:08:36 +0300

    Add a solution to the "Pseudo-Palindromic Paths in a Binary Tree" challenge

--- 1457_Pseudo_Palindromic_Paths_in_a_Binary_Tree/solution.rb
+++ 1457_Pseudo_Palindromic_Paths_in_a_Binary_Tree/solution.rb
@@ -10,5 +10,24 @@
 # @param {TreeNode} root
 # @return {Integer}
 def pseudo_palindromic_paths(root)
+  @counts = Hash.new(0)
+  @counts_odd = 0
+  @result = 0
+  check_node(root)
+  @result
+end
 
+def check_node(node)
+  return if node.nil?
+
+  @counts[node.val] += 1
+  @counts_odd += @counts[node.val].odd? ? 1 : -1
+  
+  @result += 1 if node.left.nil? && node.right.nil? && @counts_odd <= 1
+
+  check_node(node.left)
+  check_node(node.right)
+
+  @counts_odd += @counts[node.val].odd? ? -1 : 1
+  @counts[node.val] -= 1
 end
