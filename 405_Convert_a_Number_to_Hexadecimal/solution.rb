e6f093d95886d3292bb4166571cf81eef7dd4849
ozovalihasan <ozovalihasan@gmail.com>
2024-01-26 00:16:32 +0300

    Add a solution to the "Convert a Number to Hexadecimal" challenge

--- 405_Convert_a_Number_to_Hexadecimal/solution.rb
+++ 405_Convert_a_Number_to_Hexadecimal/solution.rb
@@ -1,5 +1,18 @@
+CHARS = ("0".."9").to_a + ("a".."f").to_a
+
 # @param {Integer} num
 # @return {String}
 def to_hex(num)
-    
+  return '0' if num.zero?
+
+  hex = ''
+
+  num = (2**32) + num if num < 0
+  
+  while num != 0
+    hex << CHARS[num % 16]
+    num /= 16
+  end
+
+  hex.reverse
 end
