be2a0ddd9b327d66d5d60d5924242e29b474d8c3
ozovalihasan <ozovalihasan@gmail.com>
2024-01-26 23:22:29 +0300

    Add a solution to the "Out of Boundary Paths" challenge

--- 576_Out_of_Boundary_Paths/solution.rb
+++ 576_Out_of_Boundary_Paths/solution.rb
@@ -1,3 +1,5 @@
+MODULO = (10 ** 9) + 7
+
 # @param {Integer} m
 # @param {Integer} n
 # @param {Integer} max_move
@@ -5,5 +7,22 @@
 # @param {Integer} start_column
 # @return {Integer}
 def find_paths(m, n, max_move, start_row, start_column)
-    
+  @max_row_index = m - 1
+  @max_col_index = n - 1
+  @result = Array.new(max_move + 1) { Array.new(m) { [] } }
+  check(start_row, start_column, max_move)
+end
+
+def check(row_index, col_index, move)
+  return 0 if move.negative?
+  return 1 unless row_index.between?(0, @max_row_index) && col_index.between?(0, @max_col_index)
+  return @result[move][row_index][col_index] if @result[move][row_index][col_index]
+  
+  move -= 1
+  @result[move + 1][row_index][col_index] = (
+    check(row_index + 1, col_index, move) +
+    check(row_index - 1, col_index, move) +
+    check(row_index, col_index + 1, move) +
+    check(row_index, col_index - 1, move)
+  ) % MODULO
 end
