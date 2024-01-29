5efcdba1e69b5ef190b385331639a092bd096dca
ozovalihasan <ozovalihasan@gmail.com>
2024-01-29 21:11:08 +0300

    Add a solution to the "Check Whether Two Strings are Almost Equivalent" challenge

--- 2068_Check_Whether_Two_Strings_are_Almost_Equivalent/solution.rb
+++ 2068_Check_Whether_Two_Strings_are_Almost_Equivalent/solution.rb
@@ -2,5 +2,9 @@
 # @param {String} word2
 # @return {Boolean}
 def check_almost_equivalent(word1, word2)
-    
+  chars = word1.chars.tally
+  chars.default = 0
+  word2.each_char { |char| chars[char] -= 1 }
+
+  chars.values.all? { |count| count.between?(-3, 3) }
 end
