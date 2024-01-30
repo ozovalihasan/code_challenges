40232ca9a5522b228f03a96ec7e4ff361e6bb4ce
ozovalihasan <ozovalihasan@gmail.com>
2024-01-30 22:31:19 +0300

    Add a solution to the "Minimum Time Difference" challenge

--- 539_Minimum_Time_Difference/solution.rb
+++ 539_Minimum_Time_Difference/solution.rb
@@ -1,5 +1,12 @@
 # @param {String[]} time_points
 # @return {Integer}
 def find_min_difference(time_points)
-    
+  time_points = time_points.map { |time| convert_to_minutes(time) }.sort
+  time_points << (time_points.first + (24 * 60))
+  time_points.each_cons(2).map { |first, second| second - first }.min
+end
+
+def convert_to_minutes(time)
+  hour, min = time.split(':').map!(&:to_i)
+  (hour * 60) + min
 end
