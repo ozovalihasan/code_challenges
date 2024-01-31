b517e30e5f8cf4890233ec509692b3e8a724e059
ozovalihasan <ozovalihasan@gmail.com>
2024-01-31 23:44:07 +0300

    Add a solution to the "Partition Labels" challenge

--- 763_Partition_Labels/solution.rb
+++ 763_Partition_Labels/solution.rb
@@ -1,5 +1,22 @@
 # @param {String} str
 # @return {Integer[]}
 def partition_labels(str)
-    
+  last_indices = {}
+  str.each_char.with_index do |char, index|
+    last_indices[char] = index
+  end
+
+  parts = []
+  last_index = 0
+  first_index = 0
+  str.each_char.with_index do |char, index|
+    last_index = last_indices[char] if last_indices[char] > last_index
+
+    if last_index == index
+      parts << (last_index - first_index + 1)
+      first_index = last_index = index + 1
+    end
+  end
+
+  parts
 end
