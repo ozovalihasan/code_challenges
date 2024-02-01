439ff10c107e27fed40d371ab7c04e122dc9c421
ozovalihasan <ozovalihasan@gmail.com>
2024-02-01 23:09:57 +0300

    Add a solution to the "Divide Array Into Arrays With Max Difference" challenge

--- 2966_Divide_Array_Into_Arrays_With_Max_Difference/solution.rb
+++ 2966_Divide_Array_Into_Arrays_With_Max_Difference/solution.rb
@@ -2,5 +2,7 @@
 # @param {Integer} k
 # @return {Integer[][]}
 def divide_array(nums, k)
-    
+  nums = nums.sort.each_slice(3).to_a
+  
+  nums.all? { |arr| (arr.last - arr.first) <= k } ? nums : []
 end
