b5005deea2e1fa6f85fd6fac91251c0478c921e6
ozovalihasan <ozovalihasan@gmail.com>
2024-02-04 01:57:44 +0300

    Add a solution to the "Most Common Word" challenge

--- 819_Most_Common_Word/solution.rb
+++ 819_Most_Common_Word/solution.rb
@@ -2,5 +2,9 @@
 # @param {String[]} banned
 # @return {String}
 def most_common_word(paragraph, banned)
-    
+  paragraph = paragraph.downcase
+
+  paragraph.gsub!(/[^a-z]/, ' ')
+  words = paragraph.split(' ')
+  words.tally.reject { |word, frequency| banned.include? word }.max_by(&:last).first
 end
