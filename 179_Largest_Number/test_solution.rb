36ee14c0112e1d1bb8ce10bc9c65cece79dd7517
ozovalihasan <ozovalihasan@gmail.com>
2024-02-06 21:48:52 +0300

    Add a test for the "Largest Number" challenge

---
+++ 179_Largest_Number/test_solution.rb
@@ -0,0 +1,14 @@
+require_relative './solution'
+require_relative '../read_test_files'
+
+describe 'Solution' do
+  examples, results = read_test_files.values_at(:examples, :results)
+
+  describe '#largest_number' do
+    examples.each_with_index do |nums, index|
+      it "returns the largest number" do
+        expect(largest_number(nums)).to eq(results[index])
+      end
+    end
+  end
+end
