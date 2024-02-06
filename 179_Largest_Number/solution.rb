8797a8cd926a4ac003144850b25d94821f9e493e
ozovalihasan <ozovalihasan@gmail.com>
2024-02-06 22:29:43 +0300

    Add a solution to the "Largest Number" challenge

--- 179_Largest_Number/solution.rb
+++ 179_Largest_Number/solution.rb
@@ -1,5 +1,11 @@
 # @param {Integer[]} nums
 # @return {String}
 def largest_number(nums)
-    
+  return "0" if nums.all? 0
+
+  largest_size = nums.max.to_s.size
+  nums.map(&:to_s)
+    .sort_by { |num| num * ((largest_size / num.size) + 1)}
+    .reverse
+    .join
 end
