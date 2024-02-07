531f741667d66272ecaae850440c6934532de6ae
ozovalihasan <ozovalihasan@gmail.com>
2024-02-07 23:37:39 +0300

    Add a solution to the "Brick Wall" challenge

--- 554_Brick_Wall/solution.rb
+++ 554_Brick_Wall/solution.rb
@@ -1,5 +1,13 @@
 # @param {Integer[][]} wall
 # @return {Integer}
 def least_bricks(wall)
-    
+  rows_count = wall.size
+  wall_with_edge_points = wall.map do |row|
+    total = 0
+    row.map! {|width| total += width}
+    row[...-1]
+  end
+  uncrossed_bricks_count = wall_with_edge_points.flatten.tally.values.max || 0
+
+  rows_count - uncrossed_bricks_count
 end
