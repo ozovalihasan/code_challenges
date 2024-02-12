89b26cef9212849c19d18ab92544813e603a259b
ozovalihasan <ozovalihasan@gmail.com>
2024-02-12 23:52:27 +0300

    Add a solution to the "Majority Element" challenge

--- 169_Majority_Element/solution.rb
+++ 169_Majority_Element/solution.rb
@@ -1,5 +1,7 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def majority_element(nums)
-    
+  counts = nums.tally
+  max = counts.values.max
+  counts.find { |_, count| count == max}.first
 end
