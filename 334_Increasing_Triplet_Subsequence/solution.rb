efc8857c1a754ddef2e12a40094815a86f92d47e
ozovalihasan <ozovalihasan@gmail.com>
2024-02-13 22:04:02 +0300

    Add a solution to the "Increasing Triplet Subsequence" challenge

--- 334_Increasing_Triplet_Subsequence/solution.rb
+++ 334_Increasing_Triplet_Subsequence/solution.rb
@@ -1,5 +1,12 @@
 # @param {Integer[]} nums
 # @return {Boolean}
 def increasing_triplet(nums)
-    
+  nums.each_with_object([]) do |num, result|
+    index = result.bsearch_index {|checked_num| checked_num >= num} || (result.size )
+    result[index] = num
+
+    return true if index == 2
+  end    
+
+  false
 end
