e7712f37009435ed686f23cb500e448639da9931
ozovalihasan <ozovalihasan@gmail.com>
2024-02-15 22:07:15 +0300

    Add a solution to the "Find Polygon With the Largest Perimeter" challenge

--- 2971_Find_Polygon_With_the_Largest_Perimeter/solution.rb
+++ 2971_Find_Polygon_With_the_Largest_Perimeter/solution.rb
@@ -1,5 +1,14 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def largest_perimeter(nums)
-    
+  nums.sort!
+
+  total = nums.first(2).sum
+  result = -1
+  nums[2..].each do |num|
+    result = total + num if total > num
+    total += num
+  end
+
+  result
 end
