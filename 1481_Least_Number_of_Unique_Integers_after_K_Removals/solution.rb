c37020967021b3321fd47082cc847f388a3dcba0
ozovalihasan <ozovalihasan@gmail.com>
2024-02-16 23:58:45 +0300

    Add a solution to the "Least Number of Unique Integers after K Removals" challenge

--- 1481_Least_Number_of_Unique_Integers_after_K_Removals/solution.rb
+++ 1481_Least_Number_of_Unique_Integers_after_K_Removals/solution.rb
@@ -2,5 +2,15 @@
 # @param {Integer} k
 # @return {Integer}
 def find_least_num_of_unique_ints(arr, k)
-    
+  counts = arr.tally.values.sort
+  return counts.size if k.zero?
+
+  sum = 0
+  counts.each_with_index do |count, index|
+    sum += count
+    return counts.size - index - 1 if sum == k
+    return counts.size - index  if sum > k
+  end
+
+  0
 end
