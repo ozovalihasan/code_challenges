e95bbaec8943dc18b503c7a7a80a7596e726e6b9
ozovalihasan <ozovalihasan@gmail.com>
2024-02-17 23:29:15 +0300

    Add a solution to the "Furthest Building You Can Reach" challenge

--- 1642_Furthest_Building_You_Can_Reach/solution.rb
+++ 1642_Furthest_Building_You_Can_Reach/solution.rb
@@ -3,5 +3,24 @@
 # @param {Integer} ladders
 # @return {Integer}
 def furthest_building(heights, bricks, ladders)
+  used_ladders = []
+  used_bricks = 0
+  
+  heights.each_cons(2).with_index do |(previous_height, current_height), index|
     
+    if current_height > previous_height
+      diff_height = current_height - previous_height
+      ladder_index = used_ladders.bsearch_index {|checked_height| checked_height < diff_height } || used_ladders.size
+      used_ladders.insert(ladder_index, diff_height)
+
+      if used_ladders.size > ladders
+        used_bricks += used_ladders.pop
+
+        return index if used_bricks > bricks
+      end
+    end
+    
+  end
+
+  heights.size - 1
 end
