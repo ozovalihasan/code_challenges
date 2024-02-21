c947291c5a9fb233377e3e26e9f78af1c0209f33
ozovalihasan <ozovalihasan@gmail.com>
2024-02-21 23:52:28 +0300

    Add a solution to the "Bitwise AND of Numbers Range" challenge

--- 201_Bitwise_AND_of_Numbers_Range/solution.rb
+++ 201_Bitwise_AND_of_Numbers_Range/solution.rb
@@ -2,5 +2,15 @@
 # @param {Integer} right
 # @return {Integer}
 def range_bitwise_and(left, right)
-    
+  left = left.to_s(2).chars.reverse
+  right = right.to_s(2).chars.reverse
+  zipped_nums = right.zip(left)
+
+  result = 0
+  (zipped_nums.size - 1).downto(0) do |index|
+    right_digit, left_digit = zipped_nums[index]
+    right_digit == left_digit ? result += (2 ** index) * left_digit.to_i : break
+  end
+
+  result
 end
