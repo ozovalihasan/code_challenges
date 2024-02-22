37e4e2b2a96c7e3bec93bcb21088d071fd4a987c
ozovalihasan <ozovalihasan@gmail.com>
2024-02-22 21:19:01 +0300

    Add a solution to the "Relative Ranks" challenge

--- 506_Relative_Ranks/solution.rb
+++ 506_Relative_Ranks/solution.rb
@@ -1,5 +1,12 @@
+MEDALS = ["Gold Medal","Silver Medal","Bronze Medal"]
+
 # @param {Integer[]} score
 # @return {String[]}
 def find_relative_ranks(score)
-    
+  result = {}
+  score.sort.reverse.each_with_index do |checked_score, index|
+    result[checked_score] = MEDALS[index] || (index + 1).to_s
+  end
+
+  score.map {|checked_score| result[checked_score] }
 end
