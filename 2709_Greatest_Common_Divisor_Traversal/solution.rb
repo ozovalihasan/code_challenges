ce9481558cf7bc2803b5b728330a551a64ff3aaa
ozovalihasan <ozovalihasan@gmail.com>
2024-02-25 21:51:55 +0300

    Add a solution to the "Greatest Common Divisor Traversal" challenge

--- 2709_Greatest_Common_Divisor_Traversal/solution.rb
+++ 2709_Greatest_Common_Divisor_Traversal/solution.rb
@@ -1,5 +1,34 @@
 # @param {Integer[]} nums
 # @return {Boolean}
 def can_traverse_all_pairs(nums)
-    
+  return true if nums.size == 1
+  return false if nums.any? 1
+  return true if nums.all?(nums.first)
+
+  @connected = {}
+  nums = nums.uniq
+  nums.each_with_index do |num1, index1|
+    (index1 + 1).upto(nums.size - 1) do |index2|
+      num2 = nums[index2]
+      if num1.gcd(num2) > 1
+        (@connected[num1] ||= []) << num2
+        (@connected[num2] ||= []) << num1
+      end
+    end
+
+    return false unless @connected[num1]
+  end
+  
+  @checked = {}
+  check_next_nums(nums.first)
+  @checked.size == nums.size
+end
+
+def check_next_nums(num)
+  return if @checked[num]
+
+  @checked[num] = true
+  @connected[num]&.each do |next_num|
+    check_next_nums(next_num)
+  end
 end
