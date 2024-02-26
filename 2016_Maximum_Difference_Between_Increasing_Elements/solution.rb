d8e6be297117deff6fd28b81b3da64c6191de735
ozovalihasan <ozovalihasan@gmail.com>
2024-02-27 00:09:43 +0300

    Add a solution to the "Maximum Difference Between Increasing Elements" challenge

--- 2016_Maximum_Difference_Between_Increasing_Elements/solution.rb
+++ 2016_Maximum_Difference_Between_Increasing_Elements/solution.rb
@@ -1,5 +1,16 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def maximum_difference(nums)
-    
+  max_diff = -1
+  min = nums.first 
+
+  nums.each do |num|
+    if num < min
+      min = num
+    elsif (min + max_diff) < num
+      max_diff = num - min
+    end
+  end
+
+  max_diff.zero? ? -1 : max_diff
 end
