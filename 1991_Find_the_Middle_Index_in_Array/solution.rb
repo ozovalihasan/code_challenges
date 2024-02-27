b700c0f4243e80e7322a453cf69543c1db92c7b8
ozovalihasan <ozovalihasan@gmail.com>
2024-02-27 22:47:07 +0300

    Add a solution to the "Find the Middle Index in Array" challenge

--- 1991_Find_the_Middle_Index_in_Array/solution.rb
+++ 1991_Find_the_Middle_Index_in_Array/solution.rb
@@ -1,5 +1,17 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def find_middle_index(nums)
-    
+  right_sum = nums.sum
+  left_sum = 0
+  left_num = 0
+
+  nums.each_with_index do |middle_num, index|
+    right_sum -= middle_num
+    left_sum += left_num
+    return index if right_sum == left_num
+
+    left_num += middle_num
+  end
+
+  -1
 end
