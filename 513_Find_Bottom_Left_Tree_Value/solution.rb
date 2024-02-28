2d2e065133ca05f200f0ef6d96f92c23a52fb9f5
ozovalihasan <ozovalihasan@gmail.com>
2024-02-28 21:14:01 +0300

    Add a solution to the "Find Bottom Left Tree Value" challenge

--- 513_Find_Bottom_Left_Tree_Value/solution.rb
+++ 513_Find_Bottom_Left_Tree_Value/solution.rb
@@ -10,5 +10,18 @@
 # @param {TreeNode} root
 # @return {Integer}
 def find_bottom_left_value(root)
-    
+  @result = [root.val, 0]
+
+  check_node(root, 0)
+
+  @result.first
+end
+
+def check_node(node, depth)
+  return if node.nil?
+
+  @result = [node.val, depth] if depth > @result.last
+
+  check_node(node.left, depth + 1) if node.left
+  check_node(node.right, depth + 1) if node.right
 end
