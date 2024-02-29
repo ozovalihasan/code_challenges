95f89a2d4c5fd28b8c10317aec2390ba4c0abf23
ozovalihasan <ozovalihasan@gmail.com>
2024-02-29 22:46:26 +0300

    Add a solution to the "Even Odd Tree" challenge

--- 1609_Even_Odd_Tree/solution.rb
+++ 1609_Even_Odd_Tree/solution.rb
@@ -10,5 +10,33 @@
 # @param {TreeNode} root
 # @return {Boolean}
 def is_even_odd_tree(root)
-    
+  @levels = []
+  check_node(root, 0)
+end
+
+def check_node(node, level)
+  return true if node.nil?
+
+  if level.odd?  
+    return false if node.val.odd?
+  else
+    return false if node.val.even?
+  end
+
+  if @levels[level]
+    if level.odd?
+      return false unless @levels[level].last > node.val
+    else
+      return false unless @levels[level].last < node.val
+    end
+  else
+    @levels[level] = []
+  end
+
+  @levels[level] << node.val
+
+  return false unless check_node(node.left, level + 1)
+  return false unless check_node(node.right, level + 1)
+
+  true
 end
