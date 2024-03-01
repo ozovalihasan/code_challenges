0ff3b67f253d33f56592959c425c9454ac277f82
ozovalihasan <ozovalihasan@gmail.com>
2024-03-02 01:11:27 +0300

    Add a solution to the "Maximum Odd Binary Number" challenge

--- 2864_Maximum_Odd_Binary_Number/solution.rb
+++ 2864_Maximum_Odd_Binary_Number/solution.rb
@@ -1,5 +1,7 @@
 # @param {String} s
 # @return {String}
 def maximum_odd_binary_number(s)
-    
+  counts_one = s.count('1')
+  counts_zero = s.count('0')
+  "1" * (counts_one - 1) + "0" * (counts_zero) + "1"
 end
