ea1aed4c6c97d3ca04fffcbe259c46af88ecc672
ozovalihasan <ozovalihasan@gmail.com>
2024-03-05 02:24:48 +0300

    Add a solution to the "Bag of Tokens" challenge

--- 948_Bag_of_Tokens/solution.rb
+++ 948_Bag_of_Tokens/solution.rb
@@ -2,5 +2,22 @@
 # @param {Integer} power
 # @return {Integer}
 def bag_of_tokens_score(tokens, power)
-    
+  tokens = tokens.sort
+  score = 0
+  max_score = 0
+
+  until tokens.empty?
+    if power >= tokens.first
+      power -= tokens.shift
+      score += 1
+      max_score = score if score > max_score
+    else 
+      power += tokens.pop
+      score -= 1
+  
+      break if score.negative?
+    end
+  end
+
+  max_score
 end
