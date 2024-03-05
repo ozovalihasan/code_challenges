2915c0df18a1b954a1376720c5f8ab5786481693
ozovalihasan <ozovalihasan@gmail.com>
2024-03-05 23:59:46 +0300

    Add a solution to the "Minimum Length of String After Deleting Similar Ends" challenge

--- 1750_Minimum_Length_of_String_After_Deleting_Similar_Ends/solution.rb
+++ 1750_Minimum_Length_of_String_After_Deleting_Similar_Ends/solution.rb
@@ -1,5 +1,18 @@
 # @param {String} str
 # @return {Integer}
 def minimum_length(str)
-    
+  chunks = str.chars.chunk_while {|first, second| first == second}.to_a
+  while chunks.first.first == chunks.last.first && chunks.size > 1
+    chunks.pop
+    chunks.shift
+  end
+
+  return 0 if chunks.empty?
+  
+  chars = chunks.flatten(1)
+  if chars.last == chars.first
+    chars.size == 1 ? 1 : 0
+  else
+    chars.size
+  end
 end
