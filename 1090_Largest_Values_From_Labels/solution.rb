9db25f3c39eb413bac2b7fb29e3153cb3c268045
ozovalihasan <ozovalihasan@gmail.com>
2024-03-07 21:33:29 +0300

    Add a solution to the "Largest Values From Labels" challenge

--- 1090_Largest_Values_From_Labels/solution.rb
+++ 1090_Largest_Values_From_Labels/solution.rb
@@ -4,5 +4,19 @@
 # @param {Integer} use_limit
 # @return {Integer}
 def largest_vals_from_labels(values, labels, num_wanted, use_limit)
+  values_with_labels = values.zip(labels).sort_by(&:first).reverse
+
+  sum = 0
+  used = Hash.new(0)
+  values_with_labels.each do |val, label|
+    if used[label] < use_limit
+      used[label] += 1
+      num_wanted -= 1
+      sum += val
+    end
     
+    break if num_wanted.zero?
+  end
+
+  sum
 end
