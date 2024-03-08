61772c3e76680a0492199a995da7e40376993666
ozovalihasan <ozovalihasan@gmail.com>
2024-03-08 22:50:14 +0300

    Add a solution to the "Remove Covered Intervals" challenge

--- 1288_Remove_Covered_Intervals/solution.rb
+++ 1288_Remove_Covered_Intervals/solution.rb
@@ -1,5 +1,17 @@
 # @param {Integer[][]} intervals
 # @return {Integer}
 def remove_covered_intervals(intervals)
-    
+  intervals = intervals.sort_by {|first, second| [first, - second]}
+
+  upper_limit = -Float::INFINITY
+  remaining = intervals.size
+  intervals.each do |first, second|
+    if second <= upper_limit
+      remaining -= 1
+    else
+      upper_limit = second
+    end
+  end
+
+  remaining
 end
