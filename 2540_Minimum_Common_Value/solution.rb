701baf672c187d1dbd99e1dba05d47f3c396e76d
ozovalihasan <ozovalihasan@gmail.com>
2024-03-09 23:35:21 +0300

    Add a solution to the "Minimum Common Value" challenge

--- 2540_Minimum_Common_Value/solution.rb
+++ 2540_Minimum_Common_Value/solution.rb
@@ -2,5 +2,7 @@
 # @param {Integer[]} nums2
 # @return {Integer}
 def get_common(nums1, nums2)
-    
+  return -1 if nums1.first > nums2.last || nums2.first > nums1.last
+  
+  (nums1 & nums2).min || -1
 end
