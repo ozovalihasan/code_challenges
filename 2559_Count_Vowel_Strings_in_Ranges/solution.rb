84f43d98e7ed480a94d51595d0ff82dcab288cb7
ozovalihasan <ozovalihasan@gmail.com>
2024-03-10 23:35:13 +0300

    Add a solution to the "Count Vowel Strings in Ranges" challenge

--- 2559_Count_Vowel_Strings_in_Ranges/solution.rb
+++ 2559_Count_Vowel_Strings_in_Ranges/solution.rb
@@ -1,6 +1,22 @@
+VOWELS = Set["a", "e", "i", "o", "u"]
+
 # @param {String[]} words
 # @param {Integer[][]} queries
 # @return {Integer[]}
 def vowel_strings(words, queries)
-    
+  count = 0
+  increased = []
+  words = words.map.with_index do |word, index|
+    start_and_end_with_vowel = VOWELS.include?(word[0]) && VOWELS.include?(word[-1])
+    if start_and_end_with_vowel
+      count += 1 
+      increased[index] = true
+    end
+
+    count
+  end
+
+  queries.map do |start_index, end_index| 
+    words[end_index] - words[start_index] + (increased[start_index] ? 1 : 0)
+  end
 end
