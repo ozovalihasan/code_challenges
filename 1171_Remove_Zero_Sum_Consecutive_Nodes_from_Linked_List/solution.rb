a250c2322707782bfbb741f81493a5f931195ed3
ozovalihasan <ozovalihasan@gmail.com>
2024-03-12 23:00:52 +0300

    Add a solution to the "Remove Zero Sum Consecutive Nodes from Linked List" challenge

--- 1171_Remove_Zero_Sum_Consecutive_Nodes_from_Linked_List/solution.rb
+++ 1171_Remove_Zero_Sum_Consecutive_Nodes_from_Linked_List/solution.rb
@@ -9,5 +9,19 @@
 # @param {ListNode} head
 # @return {ListNode}
 def remove_zero_sum_sublists(head)
-    
+  new_head_node = ListNode.new(0, head)
+  
+  @results = { 0 => new_head_node}
+  check_node(head, 0)
+  new_head_node.next
+end
+
+def check_node(node, sum)
+  return if node.nil?
+
+  sum += node.val
+  @results[sum].next = node.next if @results[sum]
+  @results[sum] = node
+
+  check_node(node.next, sum)
 end
