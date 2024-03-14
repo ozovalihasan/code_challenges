e0466f4982c04167e043327b1b7a718f3b6f5125
ozovalihasan <ozovalihasan@gmail.com>
2024-03-15 02:27:58 +0300

    Add a solution to the "Binary Subarrays With Sum" challenge

--- 930_Binary_Subarrays_With_Sum/solution.rb
+++ 930_Binary_Subarrays_With_Sum/solution.rb
@@ -2,5 +2,18 @@
 # @param {Integer} goal
 # @return {Integer}
 def num_subarrays_with_sum(nums, goal)
-    
+  sum = 0
+  nums = nums.map {|num| sum += num}
+  freq = nums.tally
+  freq.default = 0
+  freq[0] += 1
+  
+  freq.sum do |num, val|
+    if goal.zero?
+      (val * (val - 1)) / 2
+    else
+      goal_val = freq[num + goal]
+      goal_val * val
+    end
+  end
 end
