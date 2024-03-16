148baf790893897194128862279d463310bd106a
ozovalihasan <ozovalihasan@gmail.com>
2024-03-16 22:26:23 +0300

    Add a solution to the "Capitalize the Title" challenge

--- 2129_Capitalize_the_Title/solution.rb
+++ 2129_Capitalize_the_Title/solution.rb
@@ -1,5 +1,7 @@
 # @param {String} title
 # @return {String}
 def capitalize_title(title)
-    
+  title.split(" ")
+       .map {|word| word.size > 2 ? word.capitalize : word.downcase }
+       .join(" ")
 end
