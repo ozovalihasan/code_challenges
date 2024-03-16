f108f96236966b67970d3227135a897445ee0055
ozovalihasan <ozovalihasan@gmail.com>
2024-03-16 22:12:42 +0300

    Add a solution to the "Interval List Intersections" challenge

--- 986_Interval_List_Intersections/solution.rb
+++ 986_Interval_List_Intersections/solution.rb
@@ -2,5 +2,23 @@
 # @param {Integer[][]} second_list
 # @return {Integer[][]}
 def interval_intersection(first_list, second_list)
-    
+  return [] if second_list.empty?
+
+  result = []
+  first_list.reverse_each do |first_el|
+    while second_list.last.first > first_el.last
+      second_list.pop
+
+      return result.reverse if second_list.empty?
+    end
+
+    second_list.reverse_each do |second_el|
+      break if second_el.last < first_el.first
+
+      concatenated_sub_result = (second_el + first_el).sort
+      result << concatenated_sub_result[1..2]
+    end
+  end
+
+  result.reverse
 end
