751b0ce4b3dd8bd16e539b2d1ac83d1021628ed3
ozovalihasan <ozovalihasan@gmail.com>
2024-03-17 19:46:19 +0300

    Add a solution to the "Get Equal Substrings Within Budget" challenge

--- 1208_Get_Equal_Substrings_Within_Budget/solution.rb
+++ 1208_Get_Equal_Substrings_Within_Budget/solution.rb
@@ -3,5 +3,22 @@
 # @param {Integer} max_cost
 # @return {Integer}
 def equal_substring(s, t, max_cost)
-    
+  s = s.chars.map(&:ord)
+  t = t.chars.map(&:ord)
+  differences = s.zip(t).map {|char1, char2| (char1 - char2).abs}
+
+  count = 0
+  sum = 0
+  s.length.times do |index|
+    loop do
+      return count unless s[index + count]
+      
+      sum += differences[index + count]
+
+      sum <= max_cost ? (count += 1) : break
+    end 
+    sum -= differences[index]
+  end
+  
+  count
 end
