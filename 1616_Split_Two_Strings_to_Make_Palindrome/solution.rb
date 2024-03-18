d848ba63ae4d6ff01d48ea4070b5e0b2d990ae25
ozovalihasan <ozovalihasan@gmail.com>
2024-03-18 23:25:28 +0300

    Add a solution to the "Split Two Strings to Make Palindrome" challenge

--- 1616_Split_Two_Strings_to_Make_Palindrome/solution.rb
+++ 1616_Split_Two_Strings_to_Make_Palindrome/solution.rb
@@ -2,5 +2,25 @@
 # @param {String} b
 # @return {Boolean}
 def check_palindrome_formation(a, b)
-    
+  b = b.reverse
+  check_strings(a, b) || check_strings(b, a)
+end
+
+def check_strings(str1, str2)
+  half_size = str1.size / 2
+  left_index = half_size - 1
+  right_index = half_size * -1
+  left_part = str1[..left_index]
+  right_part = str1[right_index..].reverse
+
+  found_index = (0..left_index + 1).to_a.bsearch_index do |index|
+    left_part[index] == right_part[index] && left_part[index..] == right_part[index..]
+  end
+
+  if found_index
+    return str1[...found_index] == str2[...found_index] ||
+              str2[-found_index..] == str1[-found_index..]
+  end 
+  
+  false
 end
