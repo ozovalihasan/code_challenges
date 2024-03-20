28e699b111af563af405d62b84f5e5624b8d66e6
ozovalihasan <ozovalihasan@gmail.com>
2024-03-21 00:40:18 +0300

    Add a solution to the "Merge Sorted Array" challenge

--- 88_Merge_Sorted_Array/solution.rb
+++ 88_Merge_Sorted_Array/solution.rb
@@ -4,5 +4,11 @@
 # @param {Integer} n
 # @return {Void} Do not return anything, modify nums1 in-place instead.
 def merge(nums1, m, nums2, n)
-    
+  nums1.pop(n)
+  (m + n).times do |index|
+    num = nums1[index]
+    if num.nil? || (nums2.first && num >= nums2.first)
+      nums1.insert(index, nums2.shift)
+    end
+  end 
 end
