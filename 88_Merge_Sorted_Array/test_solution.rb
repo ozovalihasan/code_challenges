5ed61edffb7bb28b1d1210c47a2a4882056f9797
ozovalihasan <ozovalihasan@gmail.com>
2024-03-21 00:02:09 +0300

    Add a test for the "Merge Sorted Array" challenge

---
+++ 88_Merge_Sorted_Array/test_solution.rb
@@ -0,0 +1,14 @@
+require_relative './solution'
+require_relative '../read_test_files'
+
+describe 'Solution' do
+  examples, results = read_test_files.values_at(:examples, :results)
+
+  describe '#merge' do
+    examples.each_slice(4).each_with_index do |(nums1, m, nums2, n), index|
+      it 'modifies the original array as a single array sorted in non-decreasing order' do
+        expect(merge(nums1, m, nums2, n)).to eq(results[index])
+      end
+    end
+  end
+end
