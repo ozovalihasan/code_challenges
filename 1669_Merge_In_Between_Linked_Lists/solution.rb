1ce5bbe8c208521599e428faff2f74f54193bd62
ozovalihasan <ozovalihasan@gmail.com>
2024-03-21 00:23:40 +0300

    Add a solution to the "Merge In Between Linked Lists" challenge

--- 1669_Merge_In_Between_Linked_Lists/solution.rb
+++ 1669_Merge_In_Between_Linked_Lists/solution.rb
@@ -12,5 +12,25 @@
 # @param {ListNode} list2
 # @return {ListNode}
 def merge_in_between(list1, a, b, list2)
-    
+  head = list1
+
+  change_start = nil
+  change_end = nil
+  a -= 1
+  b += 1
+  counter = 0
+  loop do
+    change_start = list1 if counter == a
+    break change_end = list1 if counter == b
+      
+    list1 = list1.next
+    counter += 1
+  end
+
+  change_start.next = list2
+  result_list = change_start
+  result_list = result_list.next until result_list.next.nil?
+  result_list.next = change_end
+
+  head
 end
