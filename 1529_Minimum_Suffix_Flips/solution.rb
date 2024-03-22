8b0a5ad36f6abf164c84801b07d40c0d1032f296
ozovalihasan <ozovalihasan@gmail.com>
2024-03-22 21:12:45 +0300

    Add a solution to the "Minimum Suffix Flips" challenge

--- 1529_Minimum_Suffix_Flips/solution.rb
+++ 1529_Minimum_Suffix_Flips/solution.rb
@@ -1,5 +1,13 @@
 # @param {String} target
 # @return {Integer}
 def min_flips(target)
-    
+  previous = "0"
+
+  count = 0
+  target.each_char do |char|
+    count += 1 unless char == previous
+    previous = char
+  end
+
+  count
 end
