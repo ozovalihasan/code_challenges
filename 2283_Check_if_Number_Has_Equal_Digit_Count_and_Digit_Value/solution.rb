360b6804158903495a6dfe34f80be9441d0e67a0
ozovalihasan <ozovalihasan@gmail.com>
2024-03-24 12:13:30 +0300

    Add a solution to the "Check if Number Has Equal Digit Count and Digit Value" challenge

--- 2283_Check_if_Number_Has_Equal_Digit_Count_and_Digit_Value/solution.rb
+++ 2283_Check_if_Number_Has_Equal_Digit_Count_and_Digit_Value/solution.rb
@@ -1,5 +1,11 @@
 # @param {String} num
 # @return {Boolean}
 def digit_count(num)
-    
+  counts = num.chars.tally
+  counts.transform_keys!(&:to_i)
+  counts.default = 0
+
+  num.each_char.with_index.all? do |char, index|
+    counts[index] == char.to_i
+  end
 end
