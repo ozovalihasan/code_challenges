eae6743290242143afbb0f1165b996dbb843c6ed
ozovalihasan <ozovalihasan@gmail.com>
2024-03-26 23:53:45 +0300

    Add a solution to the "First Missing Positive" challenge

--- 41_First_Missing_Positive/solution.rb
+++ 41_First_Missing_Positive/solution.rb
@@ -1,5 +1,12 @@
+# Another one's solution is implemented
+
 # @param {Integer[]} nums
 # @return {Integer}
 def first_missing_positive(nums)
-    
+  nums << 0
+  size = nums.size
+  limit = size - 1
+  nums.map! { |num| num.between?(0, limit) ? num : 0 }
+  nums.each { |num| nums[num % size] += size}
+  nums.index { |num| (num / size) == 0 } || nums.size
 end
