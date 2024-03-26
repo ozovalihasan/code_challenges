f8d7790260890aa9ecd5c39fdb14d22ddddd47c0
ozovalihasan <ozovalihasan@gmail.com>
2024-03-26 23:56:33 +0300

    Update the solution of the "First Missing Positive" challenge

--- 41_First_Missing_Positive/solution.rb
+++ 41_First_Missing_Positive/solution.rb
@@ -1,4 +1,4 @@
-# Another one's solution is implemented
+# Another one's solution(https://leetcode.com/problems/first-missing-positive/solutions/17080/python-o-1-space-o-n-time-solution-with-explanation) is implemented
 
 # @param {Integer[]} nums
 # @return {Integer}
