d44631468ac8e63adb20cd071ea6c2047c076e7b
ozovalihasan <ozovalihasan@gmail.com>
2024-03-27 23:49:12 +0300

    Add a solution to the "Subarray Product Less Than K" challenge

--- 713_Subarray_Product_Less_Than_K/solution.rb
+++ 713_Subarray_Product_Less_Than_K/solution.rb
@@ -2,5 +2,22 @@
 # @param {Integer} k
 # @return {Integer}
 def num_subarray_product_less_than_k(nums, k)
+  return 0 if k <= 1
+
+  count = 0
+  left = -1
+  product = 1
+  
+  nums.size.times do |right|
+    product *= nums[right]
     
+    until product < k
+      left += 1
+      product /= nums[left]
+    end
+    
+    count += right - left
+  end
+
+  count
 end
