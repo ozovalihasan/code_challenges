f8a810511e670dbf38a472357aa640fed34c9a12
ozovalihasan <ozovalihasan@gmail.com>
2024-03-29 01:41:50 +0300

    Add a solution to the "Length of Longest Subarray With at Most K Frequency" challenge

--- 2958_Length_of_Longest_Subarray_With_at_Most_K_Frequency/solution.rb
+++ 2958_Length_of_Longest_Subarray_With_at_Most_K_Frequency/solution.rb
@@ -2,5 +2,16 @@
 # @param {Integer} k
 # @return {Integer}
 def max_subarray_length(nums, k)
-    
+  counts = Hash.new(0)
+  left_index = -1
+  nums.map.with_index do |num, right_index|
+    counts[num] += 1
+    while counts[num] > k
+      left_index += 1
+      left_num = nums[left_index]
+      counts[left_num] -= 1
+    end
+
+    right_index - left_index
+  end.max
 end
