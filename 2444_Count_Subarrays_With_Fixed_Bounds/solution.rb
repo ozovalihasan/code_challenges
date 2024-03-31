c07a3716c856ecf7aeb0d8164e12945821e56d64
ozovalihasan <ozovalihasan@gmail.com>
2024-03-31 17:28:09 +0300

    Update the solution of the "Count Subarrays With Fixed Bounds" challenge

--- 2444_Count_Subarrays_With_Fixed_Bounds/solution.rb
+++ 2444_Count_Subarrays_With_Fixed_Bounds/solution.rb
@@ -3,29 +3,32 @@
 # @param {Integer} max_k
 # @return {Integer}
 def count_subarrays(nums, min_k, max_k)
+  @min_k = min_k
+  @max_k = max_k
   chunks = nums.chunk {|num| num.between? min_k, max_k }
                .select {|statement, chunk| statement && chunk.include?(min_k) && chunk.include?(max_k)}
                .map(&:last)
       
+  chunks.sum {|chunk| check_chunk(chunk)}
+end
+
+def check_chunk(chunk)
   counter = 0
-  chunks.each do |chunk| 
-    next counter += (chunk.size * (chunk.size + 1) / 2) if min_k == max_k
+  size = chunk.size
+  return counter += (size * (size + 1) / 2) if @min_k == @max_k
 
-    last_bound = nil
-    last_processed_index = -1
-    last_bound = ( chunk.index(min_k) < chunk.index(max_k) ) ? [min_k, -1] : last_bound = [max_k, -1]
-    
-    chunk.each_with_index do |num, index|
-      if (num == min_k) || (num == max_k)
+  right_index = -1
+  counts = Hash.new(0)
+  chunk.each_with_index do |left_num, left_index|
+    until counts[@min_k].positive? && counts[@max_k].positive?
+      right_index += 1
+      return counter if right_index == size
 
-        unless num == last_bound.first
-          counter += (chunk.size - index) * (last_bound.last - last_processed_index) 
-          last_processed_index = last_bound.last
-        end
-
-        last_bound = [num, index]
-      end
+      right_num = chunk[right_index]
+      counts[right_num] += 1
     end
-  end
-  counter
+
+    counter += size - right_index
+    counts[left_num] -= 1
+  end     
 end
