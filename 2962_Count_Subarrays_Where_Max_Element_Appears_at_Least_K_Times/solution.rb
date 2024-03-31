a261b64f04db8820ea8187e73a57faeefa9eec5f
ozovalihasan <ozovalihasan@gmail.com>
2024-03-31 21:21:46 +0300

    Add a solution to the "Count Subarrays Where Max Element Appears at Least K Times" challenge

--- 2962_Count_Subarrays_Where_Max_Element_Appears_at_Least_K_Times/solution.rb
+++ 2962_Count_Subarrays_Where_Max_Element_Appears_at_Least_K_Times/solution.rb
@@ -2,5 +2,19 @@
 # @param {Integer} k
 # @return {Integer}
 def count_subarrays(nums, k)
+  max_num = nums.max
+  max_counts = 0
+  left_index = 0
+
+  nums.sum do |num|
+    max_counts += 1 if num == max_num
+
+    while max_counts >= k
+      max_counts -= 1 if nums[left_index] == max_num
+      
+      left_index += 1
+    end
     
+    left_index
+  end
 end
