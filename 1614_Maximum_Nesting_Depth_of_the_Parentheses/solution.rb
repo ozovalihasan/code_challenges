4eb3516b128f06092e39de244bbfed995a0b1773
ozovalihasan <ozovalihasan@gmail.com>
2024-04-04 23:05:00 +0300

    Add a solution to the "Maximum Nesting Depth of the Parentheses" challenge

--- 1614_Maximum_Nesting_Depth_of_the_Parentheses/solution.rb
+++ 1614_Maximum_Nesting_Depth_of_the_Parentheses/solution.rb
@@ -1,5 +1,16 @@
 # @param {String} str
 # @return {Integer}
 def max_depth(str)
-    
+  depth = 0
+  max = 0
+  str.each_char do |char|
+    if char == "("
+      depth += 1
+      max = depth if depth > max
+    elsif char == ")"
+      depth -= 1
+    end
+  end
+
+  max
 end
