65ce51563b74f6e0413e3f88e076d8883f574912
ozovalihasan <ozovalihasan@gmail.com>
2024-04-06 20:38:49 +0300

    Add a solution to the "Minimum Remove to Make Valid Parentheses" challenge

--- 1249_Minimum_Remove_to_Make_Valid_Parentheses/solution.rb
+++ 1249_Minimum_Remove_to_Make_Valid_Parentheses/solution.rb
@@ -1,5 +1,21 @@
 # @param {String} s
 # @return {String}
 def min_remove_to_make_valid(s)
-    
+  s = check_str(s.reverse, ")", "(")
+  s = check_str(s.reverse, "(", ")")
+
+  s.delete("-")
+end
+
+def check_str(str, increasing_char, decreasing_char)
+  counter = 0
+  str.each_char.with_index do |char, index|
+    if char == increasing_char
+      counter += 1
+    elsif char == decreasing_char
+      counter.zero? ? ( str[index] = "-" ) : ( counter -= 1 )
+    end
+  end
+  
+  str
 end
