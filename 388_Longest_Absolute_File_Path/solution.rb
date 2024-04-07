0d11c4ff9ad0ce6c7c39c1066a1cfb5dc78b5a0b
ozovalihasan <ozovalihasan@gmail.com>
2024-04-07 21:09:14 +0300

    Add a solution to the "Longest Absolute File Path" challenge

--- 388_Longest_Absolute_File_Path/solution.rb
+++ 388_Longest_Absolute_File_Path/solution.rb
@@ -1,5 +1,16 @@
 # @param {String} input
 # @return {Integer}
 def length_longest_path(input)
-    
+  input = input.split("\n")
+  nested_input = []
+  counts = [0]
+  max = 1
+  
+  input.each do |sub_path|
+    count = sub_path.count("\t")
+    counts[count + 1] = counts[count] + sub_path.size - count + 1
+    max = counts[count + 1] if max < counts[count + 1] && sub_path.include?('.')
+  end
+
+  max - 1
 end
