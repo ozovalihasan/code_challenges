a2494c3597a5d02d3ff8cd32a381b23aaaa90889
ozovalihasan <ozovalihasan@gmail.com>
2024-04-07 23:14:21 +0300

    Add a solution to the "Minimum Operations to Make All Array Elements Equal" challenge

--- 2602_Minimum_Operations_to_Make_All_Array_Elements_Equal/solution.rb
+++ 2602_Minimum_Operations_to_Make_All_Array_Elements_Equal/solution.rb
@@ -2,5 +2,35 @@
 # @param {Integer[]} queries
 # @return {Integer[]}
 def min_operations(nums, queries)
-    
+  @nums = nums.sort!
+  
+  sum = 0
+  @sums = [sum]
+  nums.each do |num|
+    @sums << (sum += num)
+  end
+
+  queries.map do |query|
+    index = nums.bsearch_index {|num| num > query}
+    result = calculate_upper(query, index)
+
+    index = nums.size unless index
+    result += calculate_lower(query, index)
+  end
+end
+
+def calculate_upper(query, index)
+  result = 0
+  if index
+    query_total = (@nums.size - index) * query
+    sub_total = @sums.last - @sums[index]
+    result = sub_total - query_total
+  end
+  result
+end
+
+def calculate_lower(query, index)
+  query_total = index * query
+  sub_total = @sums[index]
+  query_total - sub_total
 end
