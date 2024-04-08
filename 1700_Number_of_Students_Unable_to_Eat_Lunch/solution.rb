e97611ab54e32fa3bcf8a0da97a07883339b6df5
ozovalihasan <ozovalihasan@gmail.com>
2024-04-08 23:51:37 +0300

    Add a solution to the "Number of Students Unable to Eat Lunch" challenge

--- 1700_Number_of_Students_Unable_to_Eat_Lunch/solution.rb
+++ 1700_Number_of_Students_Unable_to_Eat_Lunch/solution.rb
@@ -2,5 +2,13 @@
 # @param {Integer[]} sandwiches
 # @return {Integer}
 def count_students(students, sandwiches)
-    
+  students = students.tally
+  students.default = 0
+  while (sandwich = sandwiches.shift)
+    students[sandwich] -= 1
+
+    return sandwiches.size + 1 if students[sandwich].negative?
+  end
+
+  0
 end
