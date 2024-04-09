b1de14a2030f4118c70e42271232d989ffb0065b
ozovalihasan <ozovalihasan@gmail.com>
2024-04-09 20:44:40 +0300

    Add a solution to the "Time Needed to Buy Tickets" challenge

--- 2073_Time_Needed_to_Buy_Tickets/solution.rb
+++ 2073_Time_Needed_to_Buy_Tickets/solution.rb
@@ -2,5 +2,11 @@
 # @param {Integer} k
 # @return {Integer}
 def time_required_to_buy(tickets, k)
-    
+  ref_ticket = tickets[k]
+  
+  tickets.map.with_index.sum do |ticket, index|
+    time = [ref_ticket, ticket].min
+    ref_ticket -= 1 if k == index
+    time
+  end
 end
