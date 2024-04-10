21a00d5d86bdf0e9ac580b5e50eb77c58a09546c
ozovalihasan <ozovalihasan@gmail.com>
2024-04-10 21:08:33 +0300

    Add a solution to the "Reveal Cards In Increasing Order" challenge

--- 950_Reveal_Cards_In_Increasing_Order/solution.rb
+++ 950_Reveal_Cards_In_Increasing_Order/solution.rb
@@ -1,5 +1,13 @@
 # @param {Integer[]} deck
 # @return {Integer[]}
 def deck_revealed_increasing(deck)
-    
+  deck.sort!
+  result = []
+  indices = (0...deck.size).to_a
+  while indices.first
+    result[indices.shift] = deck.shift
+    indices.push indices.shift
+  end
+  
+  result
 end
