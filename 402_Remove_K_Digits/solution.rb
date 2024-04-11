af86bb894a589d04991fb1c6443e4c8b56dc7eb0
ozovalihasan <ozovalihasan@gmail.com>
2024-04-12 01:55:37 +0300

    Add a solution to the "Remove K Digits" challenge

--- 402_Remove_K_Digits/solution.rb
+++ 402_Remove_K_Digits/solution.rb
@@ -2,5 +2,18 @@
 # @param {Integer} k
 # @return {String}
 def remove_kdigits(num, k)
+  return "0" if num.size <= k
+  
+  result = ["0"]
+  num.each_char do |char|
+    while k.positive? && char < result.last
+      result.pop
+      k -= 1
+    end
     
+    result << char
+  end
+  
+  result.pop(k)
+  result.join.to_i.to_s
 end
