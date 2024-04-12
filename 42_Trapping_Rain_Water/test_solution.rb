4770c2b33b09bdfc2cba660f768c8f1fd2ee75d6
ozovalihasan <ozovalihasan@gmail.com>
2024-04-12 16:08:23 +0300

    Add a test for the "Trapping Rain Water" challenge

---
+++ 42_Trapping_Rain_Water/test_solution.rb
@@ -0,0 +1,14 @@
+require_relative './solution'
+require_relative '../read_test_files'
+
+describe 'Solution' do
+  examples, results = read_test_files.values_at(:examples, :results)
+
+  describe '#trap' do
+    examples.each_with_index do |height, index|
+      it 'returns how much water it can trap after raining' do
+        expect(trap(height)).to eq(results[index])
+      end
+    end
+  end
+end
