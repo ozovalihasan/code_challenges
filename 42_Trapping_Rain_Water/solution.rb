ee42a6ca251326567fe1828278d4326e6ec822cf
ozovalihasan <ozovalihasan@gmail.com>
2024-04-12 16:50:18 +0300

    Add a solution to the "Trapping Rain Water" challenge

--- 42_Trapping_Rain_Water/solution.rb
+++ 42_Trapping_Rain_Water/solution.rb
@@ -1,5 +1,15 @@
 # @param {Integer[]} height
 # @return {Integer}
 def trap(height)
-    
+  max_height = height.max
+  index = height.index max_height
+  check_columns(height[...index]) + check_columns(height[index + 1..].reverse)
+end
+
+def check_columns(heights)
+  previous = 0
+  heights.sum do |height|
+    previous = height if height >= previous
+    previous - height
+  end
 end
