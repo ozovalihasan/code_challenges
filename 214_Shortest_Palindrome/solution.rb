8cbe5e028a679e0bae1bc11a849fdb44aa082d0d
ozovalihasan <ozovalihasan@gmail.com>
2024-04-12 19:58:50 +0300

    Add a solution to the "Shortest Palindrome" challenge

--- 214_Shortest_Palindrome/solution.rb
+++ 214_Shortest_Palindrome/solution.rb
@@ -1,5 +1,14 @@
 # @param {String} s
 # @return {String}
 def shortest_palindrome(s)
+  s_original = s.clone
+  s_reverse = s.clone.reverse
+  s.size.times do
+    break if s == s_reverse
     
+    s[-1] = ""
+    s_reverse[0] = ""
+  end
+
+  s_original[s.size..].reverse + s_original
 end
