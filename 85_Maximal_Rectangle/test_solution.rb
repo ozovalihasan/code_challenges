386ca2f32f08f64d4b5ea2715b8c1ba75896c6e6
ozovalihasan <ozovalihasan@gmail.com>
2024-04-13 14:15:06 +0300

    Add a test for the "Maximal Rectangle" challenge

---
+++ 85_Maximal_Rectangle/test_solution.rb
@@ -0,0 +1,14 @@
+require_relative './solution'
+require_relative '../read_test_files'
+
+describe 'Solution' do
+  examples, results = read_test_files.values_at(:examples, :results)
+
+  describe '#maximal_rectangle' do
+    examples.each_with_index do |matrix, index|
+      it "returns the largest rectangle containing only 1's and return its area" do
+        expect(maximal_rectangle(matrix)).to match_array(results[index])
+      end
+    end
+  end
+end
