b618d3c164eb1891f39367d504a7eae6e73e08b4
ozovalihasan <ozovalihasan@gmail.com>
2024-04-14 19:15:33 +0300

    Add a solution to the "Find Right Interval" challenge

--- 436_Find_Right_Interval/solution.rb
+++ 436_Find_Right_Interval/solution.rb
@@ -1,5 +1,12 @@
 # @param {Integer[][]} intervals
 # @return {Integer[]}
 def find_right_interval(intervals)
-    
+  starts = intervals.map.with_index do |(start_point, _end_point), index|
+    [start_point, index]
+  end
+  starts.sort_by!(&:first)
+  
+  intervals.map do |_start_point, end_point|
+    starts.bsearch {|start_point, _index| start_point >= end_point}&.last || -1
+  end
 end
