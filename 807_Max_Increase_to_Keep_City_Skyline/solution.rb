251429c1e94147a8b21ff28c1db2be9df7715f91
ozovalihasan <ozovalihasan@gmail.com>
2024-04-16 00:20:36 +0300

    Add a solution to the "Max Increase to Keep City Skyline" challenge

--- 807_Max_Increase_to_Keep_City_Skyline/solution.rb
+++ 807_Max_Increase_to_Keep_City_Skyline/solution.rb
@@ -1,5 +1,8 @@
 # @param {Integer[][]} grid
 # @return {Integer}
 def max_increase_keeping_skyline(grid)
-    
+  rows = grid.map(&:max)
+  columns = grid.transpose.map(&:max)
+  
+  rows.product(columns).sum(&:min) - grid.flatten.sum
 end
