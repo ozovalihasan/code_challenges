71de406367137f1a6e93609e259b439be053f11c
ozovalihasan <ozovalihasan@gmail.com>
2024-04-16 22:31:18 +0300

    Add a solution to the "Add One Row to Tree" challenge

--- 623_Add_One_Row_to_Tree/solution.rb
+++ 623_Add_One_Row_to_Tree/solution.rb
@@ -12,5 +12,26 @@
 # @param {Integer} depth
 # @return {TreeNode}
 def add_one_row(root, val, depth)
+  temp_node = TreeNode.new(0, root)
+  @val = val
+  @depth = depth
+  check_node(temp_node, 1)
+  temp_node.left
+  root
+end
+
+def check_node(node, checked_depth)
+  return if node.nil?
   
+  if checked_depth == @depth
+    new_node_left = TreeNode.new(@val, node.left) 
+    node.left = new_node_left
+
+    new_node_right = TreeNode.new(@val, nil, node.right) 
+    node.right = new_node_right
+  else
+    check_node(node.left, checked_depth + 1)
+    check_node(node.right, checked_depth + 1)
+  end
+
 end
