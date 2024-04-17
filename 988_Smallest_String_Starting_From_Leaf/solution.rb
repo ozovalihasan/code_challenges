b55de4661bc69832574187e462720f847bab805b
ozovalihasan <ozovalihasan@gmail.com>
2024-04-17 23:54:10 +0300

    Add a solution to the "Smallest String Starting From Leaf" challenge

--- 988_Smallest_String_Starting_From_Leaf/solution.rb
+++ 988_Smallest_String_Starting_From_Leaf/solution.rb
@@ -1,3 +1,5 @@
+CHARS = ("a".."z").to_a
+
 # Definition for a binary tree node.
 # class TreeNode
 #     attr_accessor :val, :left, :right
@@ -10,5 +12,18 @@
 # @param {TreeNode} root
 # @return {String}
 def smallest_from_leaf(root)
-    
+  @min_word = ("z".ord + 1).chr
+  check_node(root, "")
+  @min_word
+end
+
+def check_node(node, word)
+  word = CHARS[node.val] + word
+
+  if node.left.nil? && node.right.nil?
+    return @min_word = word if word < @min_word
+  end
+
+  check_node(node.left, word) if node.left
+  check_node(node.right, word) if node.right
 end
