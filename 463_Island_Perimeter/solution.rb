14c8556ff68a57c82eb34b72a55047e667eb2142
ozovalihasan <ozovalihasan@gmail.com>
2024-04-18 22:11:30 +0300

    Update the solution of the "Island Perimeter" challenge

--- 463_Island_Perimeter/solution.rb
+++ 463_Island_Perimeter/solution.rb
@@ -1,16 +1,12 @@
 # @param {Integer[][]} grid
 # @return {Integer}
 def island_perimeter(grid)
-  result = 0
-  
-  grid.each_with_index do |row, index|
-    row.each_with_index do |cell, index2|
-      next unless cell == 1      
-      result += 4
-      result -= 2 if index >= 1 && grid[ index - 1 ][ index2 ] == 1
-      result -= 2 if index2 >= 1 && grid[ index ][ index2 - 1 ] == 1
-    end
-  end
+  count_neighbor_borders(grid) + count_neighbor_borders(grid.transpose)
+end
 
-  result
+def count_neighbor_borders(matrix)
+  matrix.map do |row|
+    row << 0
+    row.each_cons(2).count([1,0])
+  end.sum * 2
 end
