bdac3a56b0c54fc0d343b03269275b24bcef4b39
ozovalihasan <ozovalihasan@gmail.com>
2024-04-20 00:13:40 +0300

    Add a solution to the "Count the Number of Fair Pairs" challenge

--- 2563_Count_the_Number_of_Fair_Pairs/solution.rb
+++ 2563_Count_the_Number_of_Fair_Pairs/solution.rb
@@ -3,5 +3,21 @@
 # @param {Integer} upper
 # @return {Integer}
 def count_fair_pairs(nums, lower, upper)
-    
+  nums.sort!
+  nums.unshift(-Float::INFINITY)
+  lower_index = nums.size - 1
+  upper_index = nums.size - 1
+  
+  count = 0
+  nums.each_with_index do |num, index|
+    lower_index -= 1 while num + nums[lower_index] >= lower
+    upper_index -= 1 while num + nums[upper_index] > upper
+
+    count += upper_index - lower_index
+    count -= 1 if (num * 2).between?(lower, upper)
+
+    break if upper_index.zero?
+  end
+
+  count / 2
 end
