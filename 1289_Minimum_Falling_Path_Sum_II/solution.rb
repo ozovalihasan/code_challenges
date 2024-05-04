7651754380fe7c63daa36f415db32f39752558b5
ozovalihasan <ozovalihasan@gmail.com>
2024-05-04 20:54:18 +0300

    Add a solution to the "Minimum Falling Path Sum II" challenge

--- 1289_Minimum_Falling_Path_Sum_II/solution.rb
+++ 1289_Minimum_Falling_Path_Sum_II/solution.rb
@@ -1,5 +1,16 @@
 # @param {Integer[][]} grid
 # @return {Integer}
 def min_falling_path_sum(grid)
-    
+  size = grid.first.size
+  return grid.last.first if size == 1
+  result = Array.new(size, 0)
+  grid.each do |row|
+    min, second = result.min(2)
+    index = result.index(min)
+    result = Array.new(size, result[index])
+    result[index] = second
+    result = result.zip(row).map(&:sum)
+  end
+
+  result.min
 end
