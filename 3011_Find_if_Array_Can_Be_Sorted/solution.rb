3424dfc68f5ec3b75d9ce10c6f88b0e1c0214ceb
Hasan Özovalı <ozovalihasan@gmail.com>
2024-11-06 18:46:18 +0300

    Add a solution to the "Find if Array Can Be Sorted" challenge

--- 3011_Find_if_Array_Can_Be_Sorted/solution.rb
+++ 3011_Find_if_Array_Can_Be_Sorted/solution.rb
@@ -1,5 +1,6 @@
 # @param {Integer[]} nums
 # @return {Boolean}
 def can_sort_array(nums)
-    
+  minmax_chunks = nums.chunk { |num| num.to_s(2).count('1') }.map { |_count, arr| arr }.map(&:minmax)
+  minmax_chunks.each_cons(2).all? { |(_, max1), (min2, _)| max1 <= min2 }
 end
