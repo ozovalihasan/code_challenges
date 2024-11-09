871302df1a7da5bd99c8a42a2db37e99ccd9743a
Hasan Özovalı <ozovalihasan@gmail.com>
2024-11-09 14:36:12 +0300

    Add a solution to the "Minimum Array End" challenge

--- 3133_Minimum_Array_End/solution.rb
+++ 3133_Minimum_Array_End/solution.rb
@@ -1,6 +1,17 @@
-# @param {Integer} n
-# @param {Integer} x
+# @param {Integer} num
+# @param {Integer} arr_size
 # @return {Integer}
-def min_end(n, x)
+def min_end(arr_size, num)
+  str = num.to_s(2)
+  added_num = (arr_size - 1).to_s(2).chars
     
+  updated_num = str.reverse.chars.map do |char|  
+    if added_num.any? && char == '0'
+      added_num.pop
+    else
+      char
+    end
+  end 
+  result = added_num + updated_num.reverse
+  result.join.to_i(2)
 end
