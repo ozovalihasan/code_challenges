c015bbc98dd72e9d536fc71256f18193129428b9
Hasan Özovalı <ozovalihasan@gmail.com>
2024-11-20 10:56:04 +0300

    Add a solution to the "Take K of Each Character From Left and Right" challenge

--- 2516_Take_K_of_Each_Character_From_Left_and_Right/solution.rb
+++ 2516_Take_K_of_Each_Character_From_Left_and_Right/solution.rb
@@ -1,6 +1,28 @@
 # @param {String} str
 # @param {Integer} chr_count
 # @return {Integer}
+CHECKED_CHARACTERS = %w[a b c].freeze
 def take_characters(str, chr_count)
+  chars = str.chars
+  char_counts = chars.tally
+  char_counts.default = 0
   
+  return -1 unless CHECKED_CHARACTERS.all? { |char| char_counts[char] >= chr_count } 
+
+  min_time = chars.count
+  right_index = 0
+  (chars.count - 1).downto(0) do |left_index|
+    left_char = chars[left_index]
+    char_counts[left_char] -= 1
+
+    until char_counts[left_char] >= chr_count
+      right_index -= 1
+      right_char = chars[right_index]
+      char_counts[right_char] += 1
+    end
+
+    min_time = [min_time, left_index - right_index].min
+  end
+
+  min_time
 end
