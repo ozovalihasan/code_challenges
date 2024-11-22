8f81e5efe3f97ff29dfefc0709c01ca3be8a8645
Hasan Özovalı <ozovalihasan@gmail.com>
2024-11-22 11:24:22 +0300

    Add a solution to the "Flip Columns For Maximum Number of Equal Rows" challenge

--- 1072_Flip_Columns_For_Maximum_Number_of_Equal_Rows/solution.rb
+++ 1072_Flip_Columns_For_Maximum_Number_of_Equal_Rows/solution.rb
@@ -1,5 +1,14 @@
 # @param {Integer[][]} matrix
 # @return {Integer}
+FLIPS = { 0 => 1, 1 => 0 }.freeze
 def max_equal_rows_after_flips(matrix)
-    
+  matrix.map! do |row|
+    if row[0].zero?
+      row
+    else
+      row.map! { |cell| FLIPS[cell] }
+    end
+  end
+  
+  matrix.tally.values.max
 end
