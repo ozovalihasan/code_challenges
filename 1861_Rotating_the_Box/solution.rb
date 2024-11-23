c6a21c4d8f2bef86a3edae551fcca85760d2ca0f
Hasan Özovalı <ozovalihasan@gmail.com>
2024-11-23 15:34:00 +0300

    Add a solution to the "Rotating the Box" challenge

--- 1861_Rotating_the_Box/solution.rb
+++ 1861_Rotating_the_Box/solution.rb
@@ -1,5 +1,19 @@
 # @param {Character[][]} box
 # @return {Character[][]}
 def rotate_the_box(box)
-    
+  box.each do |row|
+    placed_index = row.size - 1
+
+    (row.size - 1).downto(0) do |index|
+      case row[index]
+      when '#'
+        row[placed_index], row[index] = row[index], row[placed_index]
+        placed_index -= 1
+      when '*'
+        placed_index = index - 1
+      end
+    end
+  end
+
+  box.reverse.transpose
 end
