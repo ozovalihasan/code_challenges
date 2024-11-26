1e4a36f4e9230454b2ac1063434e821337974af3
Hasan Özovalı <ozovalihasan@gmail.com>
2024-11-26 21:16:44 +0300

    Add a solution to the "Find Champion II" challenge

--- 2924_Find_Champion_II/solution.rb
+++ 2924_Find_Champion_II/solution.rb
@@ -1,6 +1,8 @@
-# @param {Integer} n
+# @param {Integer} total_teams_number
 # @param {Integer[][]} edges
 # @return {Integer}
-def find_champion(n, edges)
-    
+def find_champion(total_teams_number, edges)
+  result = (0...total_teams_number).to_a - edges.map(&:last)
+  
+  result.size == 1 ? result.first : -1
 end
