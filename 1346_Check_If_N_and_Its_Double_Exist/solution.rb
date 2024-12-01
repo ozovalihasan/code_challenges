d961f85fc0e65f4847cccda3ad2b067ed3d83cf2
Hasan Özovalı <ozovalihasan@gmail.com>
2024-12-01 10:43:57 +0300

    Add a solution to the "Check If N and Its Double Exist" challenge

--- 1346_Check_If_N_and_Its_Double_Exist/solution.rb
+++ 1346_Check_If_N_and_Its_Double_Exist/solution.rb
@@ -1,3 +1,17 @@
 # @param {Integer[]} arr
 # @return {Boolean}
-def check_if_exist(arr); end
+def check_if_exist(arr)
+  counts = arr.tally
+  
+  arr.each do |num|
+    if num.zero? 
+      return true if counts[num] > 1
+      
+      next
+    end
+
+    return true if counts[num * 2]
+  end
+
+  false
+end
