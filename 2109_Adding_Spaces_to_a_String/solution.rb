8e0fc8eac534651543442a1759975fb8df4f0107
Hasan Özovalı <ozovalihasan@gmail.com>
2024-12-03 12:52:36 +0300

    Add a solution to the "Adding Spaces to a String" challenge

--- 2109_Adding_Spaces_to_a_String/solution.rb
+++ 2109_Adding_Spaces_to_a_String/solution.rb
@@ -1,4 +1,13 @@
 # @param {String} str
 # @param {Integer[]} spaces
 # @return {String}
-def add_spaces(str, spaces); end
+def add_spaces(str, spaces)
+  spaces.unshift(0)
+  spaces << str.size
+  
+  result = spaces.each_cons(2).map do |start_point, end_point|
+    str.slice(start_point, end_point - start_point)
+  end
+  
+  result.join(' ')
+end
