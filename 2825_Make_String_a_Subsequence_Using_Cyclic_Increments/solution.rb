45d86d42c9edf9f3d4453bdc82c4de74095fd8c3
Hasan Özovalı <ozovalihasan@gmail.com>
2024-12-04 12:00:17 +0300

    Add a solution to the "Make String a Subsequence Using Cyclic Increments" challenge

--- 2825_Make_String_a_Subsequence_Using_Cyclic_Increments/solution.rb
+++ 2825_Make_String_a_Subsequence_Using_Cyclic_Increments/solution.rb
@@ -1,4 +1,16 @@
 # @param {String} str1
 # @param {String} str2
 # @return {Boolean}
-def can_make_subsequence(str1, str2); end
+NEXT_CHARS = ('a'..'z').zip(('a'..'z').to_a.rotate).to_h
+def can_make_subsequence(str1, str2)
+  index = 0
+  target_index = str2.size
+
+  str1.each_char do |char|
+    index += 1 if (char == str2[index]) || (NEXT_CHARS[char] == str2[index])
+
+    return true if target_index == index
+  end
+
+  false
+end
