8feec9d5b7e684f6e9a4f008959c86c66e34f63b
Hasan Özovalı <ozovalihasan@gmail.com>
2024-12-06 10:41:03 +0300

    Add a solution to the "Maximum Number of Integers to Choose From a Range I" challenge

--- 2554_Maximum_Number_of_Integers_to_Choose_From_a_Range_I/solution.rb
+++ 2554_Maximum_Number_of_Integers_to_Choose_From_a_Range_I/solution.rb
@@ -2,4 +2,20 @@
 # @param {Integer} range_max
 # @param {Integer} max_sum
 # @return {Integer}
-def max_count(banned, range_max, max_sum); end
+def max_count(banned, range_max, max_sum)
+  sum = 0
+  count = 0
+  banned_nums = {}
+  banned.each { |num| banned_nums[num] = true }
+  
+  1.upto(range_max) do |num|
+    next if banned_nums[num]    
+
+    sum += num
+    break if sum > max_sum
+
+    count += 1
+  end
+
+  count
+end
