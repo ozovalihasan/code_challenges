6af575133bebe85a4e1e8501c61597ad8942124a
Hasan Özovalı <ozovalihasan@gmail.com>
2024-12-07 21:22:35 +0300

    Add a solution to the "Minimum Limit of Balls in a Bag" challenge

--- 1760_Minimum_Limit_of_Balls_in_a_Bag/solution.rb
+++ 1760_Minimum_Limit_of_Balls_in_a_Bag/solution.rb
@@ -1,4 +1,14 @@
 # @param {Integer[]} nums
 # @param {Integer} max_operations
 # @return {Integer}
-def minimum_size(nums, max_operations); end
+def minimum_size(nums, max_operations) 
+  max = nums.max
+
+  (1..max).bsearch do |size|
+    sum = nums.sum do |num|
+      ((num - 1) / size)
+    end
+    
+    sum <= max_operations
+  end
+end
