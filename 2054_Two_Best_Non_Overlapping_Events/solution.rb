cdac3f59986f7c3136b7b5c345b99615988facf8
Hasan Özovalı <ozovalihasan@gmail.com>
2024-12-08 21:59:30 +0300

    Add a solution to the "Two Best Non-Overlapping Events" challenge

--- 2054_Two_Best_Non_Overlapping_Events/solution.rb
+++ 2054_Two_Best_Non_Overlapping_Events/solution.rb
@@ -1,3 +1,22 @@
 # @param {Integer[][]} events
 # @return {Integer}
-def max_two_events(events); end
+def max_two_events(events)
+  events.sort_by!(&:first)
+
+  max_value = 0
+  previous_max_value = 0
+  end_time_values = []
+
+  events.each do |start_time, end_time, value|
+    end_time_index = end_time_values.bsearch_index { |pair| pair.first >= end_time } || end_time_values.size
+    end_time_values.insert(end_time_index, [end_time, value])
+
+    while end_time_values.first.first < start_time
+      previous_max_value = [end_time_values.shift.last, previous_max_value].max
+    end
+    
+    max_value = [max_value, previous_max_value + value].max
+  end
+
+  max_value
+end
