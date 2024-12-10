8f0231422d30f9d43ede8dcd607b8ba454b96595
Hasan Özovalı <ozovalihasan@gmail.com>
2024-12-10 13:50:36 +0300

    Add a solution to the "Find Longest Special Substring That Occurs Thrice I" challenge

--- 2981_Find_Longest_Special_Substring_That_Occurs_Thrice_I/solution.rb
+++ 2981_Find_Longest_Special_Substring_That_Occurs_Thrice_I/solution.rb
@@ -1,3 +1,22 @@
 # @param {String} str
 # @return {Integer}
-def maximum_length(str); end
+def maximum_length(str)
+  groups = str.chars.slice_when { |before, after| before != after }
+             .group_by(&:first)
+             .transform_values { |group| group.map(&:size) }
+
+  max_lengths = groups.values.map do |sizes|
+    first, second, third = sizes.max(3)
+    second ||= 0
+    third ||= 0
+
+    [
+      first - 2,
+      second == first ? second - 1 : second,
+      third
+    ].max
+  end
+
+  result = max_lengths.max
+  result.zero? ? -1 : result
+end
