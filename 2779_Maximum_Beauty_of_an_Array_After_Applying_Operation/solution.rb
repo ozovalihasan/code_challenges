ac379af4a582eade52fd7d68301b5be09c05fc5f
Hasan Özovalı <ozovalihasan@gmail.com>
2024-12-11 13:12:40 +0300

    Add a solution to the "Maximum Beauty of an Array After Applying Operation" challenge

--- 2779_Maximum_Beauty_of_an_Array_After_Applying_Operation/solution.rb
+++ 2779_Maximum_Beauty_of_an_Array_After_Applying_Operation/solution.rb
@@ -1,4 +1,21 @@
 # @param {Integer[]} nums
 # @param {Integer} range_diff
 # @return {Integer}
-def maximum_beauty(nums, range_diff); end
+def maximum_beauty(nums, range_diff)
+  nums.sort!
+  
+  cursor = 0
+  max = 0
+  range_diff *= 2
+  
+  nums.each_with_index do |num, index|
+    cursor += 1 while nums[cursor] && nums[cursor] <= (num + range_diff)
+    
+    max = cursor - index if cursor - index > max
+    cursor += 1
+    
+    break if nums[cursor].nil?
+  end
+
+  max
+end
