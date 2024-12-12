58e9cd192fe9716a2c17b25fed434ec48e3b22d3
Hasan Özovalı <ozovalihasan@gmail.com>
2024-12-12 21:54:28 +0300

    Add a solution to the "Take Gifts From the Richest Pile" challenge

--- 2558_Take_Gifts_From_the_Richest_Pile/solution.rb
+++ 2558_Take_Gifts_From_the_Richest_Pile/solution.rb
@@ -1,4 +1,15 @@
 # @param {Integer[]} gifts
-# @param {Integer} k
+# @param {Integer} total_second
 # @return {Integer}
-def pick_gifts(gifts, k); end
+def pick_gifts(gifts, total_second) 
+  gifts.sort!
+
+  total_second.times do
+    gift = Integer.sqrt(gifts.pop)
+    
+    index = gifts.bsearch_index { |checked_gift| checked_gift >= gift } || gifts.size
+    gifts.insert(index, gift)
+  end
+
+  gifts.sum
+end
