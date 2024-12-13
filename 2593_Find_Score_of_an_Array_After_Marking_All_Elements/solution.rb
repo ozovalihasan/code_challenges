ba0c94183b1ed2f2b4f31970bb882b28396b3d6f
Hasan Özovalı <ozovalihasan@gmail.com>
2024-12-13 21:56:14 +0300

    Add a solution to the "Find Score of an Array After Marking All Elements" challenge

--- 2593_Find_Score_of_an_Array_After_Marking_All_Elements/solution.rb
+++ 2593_Find_Score_of_an_Array_After_Marking_All_Elements/solution.rb
@@ -1,3 +1,30 @@
 # @param {Integer[]} nums
 # @return {Integer}
-def find_score(nums); end
+def find_score(nums)
+  checked = Array.new(nums.size, false)
+  checked << true
+  
+  checked_count = 0
+  total_score = 0
+
+  nums.zip(0...nums.size).sort_by(&:first).each do |num, index|
+    next if checked[index]
+
+    checked[index] = true
+    checked_count += 1
+    total_score += num
+
+    unless checked[index - 1]
+      checked[index - 1] = true
+      checked_count += 1
+    end
+    
+    unless checked[index + 1]
+      checked[index + 1] = true
+      checked_count += 1
+    end
+
+    return total_score if checked_count >= nums.size
+  end
+  
+end
