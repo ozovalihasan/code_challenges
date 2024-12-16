75eba021c563c03691648554167b14efe51a90f5
Hasan Özovalı <ozovalihasan@gmail.com>
2024-12-16 20:17:26 +0300

    Add a solution to the "Final Array State After K Multiplication Operations I" challenge

--- 3264_Final_Array_State_After_K_Multiplication_Operations_I/solution.rb
+++ 3264_Final_Array_State_After_K_Multiplication_Operations_I/solution.rb
@@ -2,4 +2,11 @@
 # @param {Integer} total_operation
 # @param {Integer} multiplier
 # @return {Integer[]}
-def get_final_state(nums, total_operation, multiplier); end
+def get_final_state(nums, total_operation, multiplier) 
+  total_operation.times do
+    min_index = nums.index(nums.min)
+    nums[min_index] *= multiplier
+  end
+
+  nums
+end
