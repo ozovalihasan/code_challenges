31648d4cabc18a2733a34c6f76ea8a68dcd7fe67
Hasan Özovalı <ozovalihasan@gmail.com>
2024-12-17 20:18:10 +0300

    Add a solution to the "Construct String With Repeat Limit" challenge

--- 2182_Construct_String_With_Repeat_Limit/solution.rb
+++ 2182_Construct_String_With_Repeat_Limit/solution.rb
@@ -1,4 +1,31 @@
 # @param {String} str
 # @param {Integer} repeat_limit
 # @return {String}
-def repeat_limited_string(str, repeat_limit); end
+def repeat_limited_string(str, repeat_limit)
+  result = ''
+  counts = str.chars.tally.sort_by(&:first)
+
+  loop do
+    break if counts.empty?
+
+    char, count = counts.pop
+    used_count = [repeat_limit, count].min
+    result << (char * used_count)
+    count -= used_count
+
+    next if count.zero?
+    break if counts.empty?
+
+    result << counts.last[0]
+
+    if counts.last[1] == 1
+      counts.pop
+    else
+      counts.last[1] -= 1
+    end
+
+    counts << [char, count]
+  end
+
+  result
+end
