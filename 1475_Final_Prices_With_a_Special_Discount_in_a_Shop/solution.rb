cf69d3f68a9cff667689118732846f10b1819903
Hasan Özovalı <ozovalihasan@gmail.com>
2024-12-18 20:55:24 +0300

    Add a solution to the "Final Prices With a Special Discount in a Shop" challenge

--- 1475_Final_Prices_With_a_Special_Discount_in_a_Shop/solution.rb
+++ 1475_Final_Prices_With_a_Special_Discount_in_a_Shop/solution.rb
@@ -1,3 +1,12 @@
 # @param {Integer[]} prices
 # @return {Integer[]}
-def final_prices(prices); end
+def final_prices(prices)
+  discounts = [0]
+  
+  prices.reverse.map do |price|
+    discounts.pop while discounts.last > price
+    
+    discounts << price
+    price - discounts[-2]
+  end.reverse
+end
