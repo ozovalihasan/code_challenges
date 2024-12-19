079e8b65811a27a91db958126bbe967f98369c9f
Hasan Özovalı <ozovalihasan@gmail.com>
2024-12-19 16:32:43 +0300

    Add a solution to the "Max Chunks To Make Sorted" challenge

--- 769_Max_Chunks_To_Make_Sorted/solution.rb
+++ 769_Max_Chunks_To_Make_Sorted/solution.rb
@@ -1,3 +1,20 @@
 # @param {Integer[]} arr
 # @return {Integer}
-def max_chunks_to_sorted(arr); end
+def max_chunks_to_sorted(arr)
+  sorted_arr = arr.sort
+
+  count = 0
+  cursor_sorted = 0
+  cursor = 0
+  checked = {}
+
+  while cursor < arr.size
+    checked[arr[cursor]] = true
+    cursor += 1
+
+    cursor_sorted += 1 while checked[sorted_arr[cursor_sorted]]
+    count += 1 if cursor_sorted == cursor
+  end
+
+  count
+end
