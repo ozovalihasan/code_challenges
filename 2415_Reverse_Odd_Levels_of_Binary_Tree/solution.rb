9667d2c42b0e0f37b3fdff9e6d340a9094da80f8
Hasan Özovalı <ozovalihasan@gmail.com>
2024-12-20 15:18:16 +0300

    Add a solution to the "Reverse Odd Levels of Binary Tree" challenge

--- 2415_Reverse_Odd_Levels_of_Binary_Tree/solution.rb
+++ 2415_Reverse_Odd_Levels_of_Binary_Tree/solution.rb
@@ -9,4 +9,16 @@
 # end
 # @param {TreeNode} root
 # @return {TreeNode}
-def reverse_odd_levels(root); end
+def reverse_odd_levels(root) 
+  swap_values(root.left, root.right, 1)
+  root
+end
+
+def swap_values(cursor1, cursor2, level)
+  return if cursor1.nil? || cursor2.nil?
+
+  cursor1.val, cursor2.val = cursor2.val, cursor1.val if level.odd?
+  
+  swap_values(cursor1.left, cursor2.right, level + 1)
+  swap_values(cursor1.right, cursor2.left, level + 1)
+end
