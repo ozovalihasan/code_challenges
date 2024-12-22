ed56b867df24fb7e446e9be2f51ccda7bd35ab93
Hasan Özovalı <ozovalihasan@gmail.com>
2024-12-22 22:07:06 +0300

    Add a solution to the "Find Building Where Alice and Bob Can Meet" challenge

--- 2940_Find_Building_Where_Alice_and_Bob_Can_Meet/solution.rb
+++ 2940_Find_Building_Where_Alice_and_Bob_Can_Meet/solution.rb
@@ -1,6 +1,33 @@
-# @param {Integer[]} heights
-# @param {Integer[][]} queries
-# @return {Integer[]}
 def leftmost_building_queries(heights, queries)
-    
+  sorted = []
+  last_height_index = heights.size - 1
+  result = {}
+
+  grouped_queries = queries.map(&:sort!).group_by(&:last).transform_values! { |values| values.map(&:first) }
+  sorted_grouped_queries = grouped_queries.sort_by(&:first).reverse
+
+  sorted_grouped_queries.each do |max, mins|
+    last_height_index.downto(max) do |index|
+      height = heights[index]
+      sorted.shift while !sorted.empty? && sorted.first.first <= height
+      sorted.unshift [height, index]
+    end
+    last_height_index = max
+
+    mins.each do |min|
+      result[max] ||= {}
+      result[max][min] = if min == max || heights[max] > heights[min]
+                           max
+                         elsif heights[max] <= heights[min]
+                           if sorted.last.first <= heights[min]
+                             -1
+                           else
+                             sorted.bsearch { |(sorted_height, _)| sorted_height > heights[min] }.last
+                           end
+                         
+                         end
+    end
+  end
+
+  queries.map { |min, max| result[max][min] }
 end
