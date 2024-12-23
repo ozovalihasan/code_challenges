13a9e914c99693b8866918d641479be0ba6cebba
Hasan Özovalı <ozovalihasan@gmail.com>
2024-12-23 15:44:16 +0300

    Remove an unnecessary print

--- 706_Design_HashMap/test_solution.rb
+++ 706_Design_HashMap/test_solution.rb
@@ -1,4 +1,4 @@
-require_relative './solution'
+require_relative 'solution'
 require_relative '../read_test_files'
 
 describe 'Solution' do
@@ -9,18 +9,16 @@ describe 'Solution' do
       it "returns a class 'MyHashMap'" do
         my_hash_map = nil
         actions.zip(params).each_with_index do |(action, (*param)), index2|
-          p param
-          if action == "MyHashMap"
+          if action == 'MyHashMap'
             my_hash_map = MyHashMap.new
-          elsif action == "put"
+          elsif action == 'put'
             my_hash_map.put(*param)
-          elsif action == "remove"
+          elsif action == 'remove'
             my_hash_map.remove(*param)
-          elsif action == "get"
+          elsif action == 'get'
             expect(my_hash_map.get(*param)).to eq(results[index][index2])
           end
         end
-
       end
     end
   end
