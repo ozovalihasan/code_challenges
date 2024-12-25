4055f6df839268dafb50529e1a86a6accb7d3e77
Hasan Özovalı <ozovalihasan@gmail.com>
2024-12-25 17:31:02 +0300

    Add a solution to the "Check If a Word Occurs As a Prefix of Any Word in a Sentence" challenge

--- 1455_Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence/solution.rb
+++ 1455_Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence/solution.rb
@@ -1,4 +1,7 @@
 # @param {String} sentence
 # @param {String} search_word
 # @return {Integer}
-def is_prefix_of_word(sentence, search_word); end
+def is_prefix_of_word(sentence, search_word)
+  index = sentence.split.find_index { |word| word.start_with? search_word }
+  index ? index + 1 : -1
+end
