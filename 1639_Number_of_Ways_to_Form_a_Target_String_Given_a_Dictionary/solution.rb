31e0035de88d8c9cec8cb5a35ae5a5fcaf67cc69
Hasan Özovalı <ozovalihasan@gmail.com>
2024-12-29 21:14:49 +0300

    Add a solution to the "Number of Ways to Form a Target String Given a Dictionary" challenge

--- 1639_Number_of_Ways_to_Form_a_Target_String_Given_a_Dictionary/solution.rb
+++ 1639_Number_of_Ways_to_Form_a_Target_String_Given_a_Dictionary/solution.rb
@@ -2,46 +2,21 @@
 # @param {String} target
 # @return {Integer}
 
-# I couldn't solve the challenge
-
+MODULO = (10**9) + 7
 def num_ways(words, target)
-  @letters_frequency = {}
-  @result = 0
+  counts = words.map(&:chars).transpose.map(&:tally)
   
-  words.each do |word|
-    word.each_char.with_index do |letter, index|
-      @letters_frequency[letter] ||= Hash.new(0)
-      @letters_frequency[letter][index] += 1
+  previous = Array.new(counts.size + 1, 1)
+  target.chars.each do |char|
+    current = Array.new(counts.size + 1, 0)
+    
+    counts.each_with_index do |sub_counts, index|
+      total = (previous[index] * (sub_counts[char] || 0)) + current[index]
+      current[index + 1] = total % MODULO
     end
+
+    previous = current
   end
 
-  @letters_frequency.transform_values! {|value| value.sort_by(&:first)}
-
-  @cache = []
-
-  0.upto(target.size) do |index|
-    @cache[index] = {}
-  end
-  (check target, 0, -1) % (10**9 + 7) 
-  
-end
-
-def check target, target_index, word_previous_index
-  cached_sub_result = @cache[target_index][word_previous_index] 
-  return cached_sub_result if cached_sub_result
-  
-  return @cache[target_index][word_previous_index]  = 1 unless target[target_index]
-
-  sub_result = 0
-
-  (@letters_frequency[target[target_index]] || {}).each do |index, count|
-    next unless index > word_previous_index
-
-    result = (check target, target_index + 1, index)
-    break if result.zero?
-
-    sub_result += result * count
-  end
-
-  @cache[target_index][word_previous_index]  = sub_result % (10**9 + 7) 
+  previous.last
 end
