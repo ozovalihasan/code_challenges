91a162c0c24515780c763d5c799a6048f36f109a
Hasan Özovalı <ozovalihasan@gmail.com>
2025-01-01 18:32:35 +0300

    Update the solution of the "Maximum Score After Splitting a String" challenge

--- 1422_Maximum_Score_After_Splitting_a_String/solution.rb
+++ 1422_Maximum_Score_After_Splitting_a_String/solution.rb
@@ -1,10 +1,10 @@
-# @param {String} s
+# @param {String} str
 # @return {Integer}
-def max_score(s)
+def max_score(str)
   result = 0
-  previous = s.count("1")
-  0.upto(s.size - 2) do |index|
-    previous += s[index] == "0" ? 1 : -1
+  previous = str.count('1')
+  0.upto(str.size - 2) do |index|
+    previous += str[index] == '0' ? 1 : -1
 
     result = previous if previous > result
   end
