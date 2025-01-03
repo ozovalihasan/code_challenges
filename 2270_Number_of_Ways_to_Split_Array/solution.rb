966e86571a55312ed1af02bdcc672b7c572110a4
Hasan Özovalı <ozovalihasan@gmail.com>
2025-01-03 17:45:41 +0300

    Add a solution to the "Number of Ways to Split Array" challenge

--- 2270_Number_of_Ways_to_Split_Array/solution.rb
+++ 2270_Number_of_Ways_to_Split_Array/solution.rb
@@ -1,3 +1,11 @@
 # @param {Integer[]} nums
 # @return {Integer}
-def ways_to_split_array(nums); end
+def ways_to_split_array(nums) 
+  diff = 1 - nums.sum
+  
+  nums[...-1].count do |num|
+    diff += 2 * num
+
+    diff.positive?
+  end
+end
