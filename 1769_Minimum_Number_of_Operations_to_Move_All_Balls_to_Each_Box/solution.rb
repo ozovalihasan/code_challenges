315dbf2403ff4db6e667f882907d55d3ba24188d
Hasan Özovalı <ozovalihasan@gmail.com>
2025-01-06 17:14:21 +0300

    Add a solution to the "Minimum Number of Operations to Move All Balls to Each Box" challenge

--- 1769_Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box/solution.rb
+++ 1769_Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box/solution.rb
@@ -1,3 +1,29 @@
 # @param {String} boxes
 # @return {Integer[]}
-def min_operations(boxes); end
+def min_operations(boxes)
+  left_count = 0
+  left_sum = 0
+
+  right_count = 0
+  right_sum = 0
+  
+  boxes = boxes.chars
+  boxes.each.with_index do |char, index|
+    if char == '1'
+      right_count += 1
+      right_sum += index
+    end
+  end
+
+  boxes.each.map.with_index do |char, index|
+    if char == '1'
+      right_count -= 1
+      right_sum -= index
+
+      left_count += 1
+      left_sum += index
+    end
+
+    right_sum - (right_count * index) + (left_count * index) - left_sum
+  end
+end
