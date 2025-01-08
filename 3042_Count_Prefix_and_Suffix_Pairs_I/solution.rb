c387aeb5a423ba4c43a113dae74c3d6a2d53e048
Hasan Özovalı <ozovalihasan@gmail.com>
2025-01-08 21:24:14 +0300

    Add a solution to the "Count Prefix and Suffix Pairs I" challenge

--- 3042_Count_Prefix_and_Suffix_Pairs_I/solution.rb
+++ 3042_Count_Prefix_and_Suffix_Pairs_I/solution.rb
@@ -1,3 +1,18 @@
 # @param {String[]} words
 # @return {Integer}
-def count_prefix_suffix_pairs(words); end
+def count_prefix_suffix_pairs(words) 
+  pairs = {}
+  count = 0
+  
+  words.each do |word|
+    pair_words = (pairs[word[0]] ||= []) 
+    
+    pair_words.each do |word2|
+      count += 1 if word.start_with?(word2) && word.end_with?(word2)
+    end
+    
+    pair_words << word
+  end
+
+  count
+end
