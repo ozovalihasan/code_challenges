d101ffea4bde4cdfe39a18bf4dbfd77ce7c3dac1
Hasan Özovalı <ozovalihasan@gmail.com>
2025-01-11 20:56:55 +0300

    Add a solution to the "Construct K Palindrome Strings" challenge

--- 1400_Construct_K_Palindrome_Strings/solution.rb
+++ 1400_Construct_K_Palindrome_Strings/solution.rb
@@ -1,4 +1,9 @@
 # @param {String} str
 # @param {Integer} palindrome_count
 # @return {Boolean}
-def can_construct(str, palindrome_count); end
+def can_construct(str, palindrome_count) 
+  palindrome_count.between?(
+    str.chars.tally.values.count(&:odd?), 
+    str.length
+  )
+end
--- 1400_Construct_K_Palindrome_Strings/test_solution.rb
+++ 1400_Construct_K_Palindrome_Strings/test_solution.rb
@@ -7,7 +7,7 @@ describe 'Solution' do
   describe '#can_construct' do
     examples.each_slice(2).with_index do |(str, palindrome_count), index|
       it 'returns true if you can use all the characters in s to construct k palindrome strings or false otherwise' do
-        expect(can_construct(str, palindrome_count).intersection(results[index])).to eq(results[index])
+        expect(can_construct(str, palindrome_count)).to eq(results[index])
       end
     end
   end
