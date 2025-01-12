f3b0c75d96a02642f11fdef2def1dfeb0af77438
Hasan Özovalı <ozovalihasan@gmail.com>
2025-01-12 20:50:02 +0300

    Add a solution to the "Check if a Parentheses String Can Be Valid" challenge

--- 2116_Check_if_a_Parentheses_String_Can_Be_Valid/solution.rb
+++ 2116_Check_if_a_Parentheses_String_Can_Be_Valid/solution.rb
@@ -1,4 +1,30 @@
 # @param {String} str
 # @param {String} locked
 # @return {Boolean}
-def can_be_valid(str, locked); end
+def can_be_valid(str, locked)
+  return false if str.length.odd?
+
+  min = 0
+  max = 0
+  str.chars.zip(locked.chars) do |char, locked_index|
+    
+    if locked_index == '1'
+      if char == '('
+        min += 1
+        max += 1
+      else
+        min -= 1
+        max -= 1
+      end
+    else
+      max += 1
+      min -= 1
+    end
+
+    min = 0 if min.negative?
+    return false if max.negative?
+  end
+  return false if min.positive?
+  
+  true
+end
