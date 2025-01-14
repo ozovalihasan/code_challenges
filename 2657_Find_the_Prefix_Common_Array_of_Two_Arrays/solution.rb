27a1eb25de0ee07d56577f6cecae71223cd012ff
Hasan Özovalı <ozovalihasan@gmail.com>
2025-01-14 13:18:14 +0300

    Add a solution to the "Find the Prefix Common Array of Two Arrays" challenge

--- 2657_Find_the_Prefix_Common_Array_of_Two_Arrays/solution.rb
+++ 2657_Find_the_Prefix_Common_Array_of_Two_Arrays/solution.rb
@@ -1,4 +1,17 @@
-# @param {Integer[]} a
-# @param {Integer[]} b
+# @param {Integer[]} arr1
+# @param {Integer[]} arr2
 # @return {Integer[]}
-def find_the_prefix_common_array(a, b); end
+def find_the_prefix_common_array(arr1, arr2)
+  counts = Hash.new(0)
+  result = 0
+  
+  arr1.zip(arr2).map do |num1, num2|
+    counts[num1] += 1
+    result += 1 if counts[num1] == 2
+    
+    counts[num2] += 1
+    result += 1 if counts[num2] == 2
+    
+    result
+  end
+end
