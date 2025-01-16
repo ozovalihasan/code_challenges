eca2eb54c437fb3b9d6d7dd7263c986cb78d2a4e
Hasan Özovalı <ozovalihasan@gmail.com>
2025-01-16 22:43:33 +0300

    Add a solution to the "Bitwise XOR of All Pairings" challenge

--- 2425_Bitwise_XOR_of_All_Pairings/solution.rb
+++ 2425_Bitwise_XOR_of_All_Pairings/solution.rb
@@ -1,4 +1,11 @@
 # @param {Integer[]} nums1
 # @param {Integer[]} nums2
 # @return {Integer}
-def xor_all_nums(nums1, nums2); end
+def xor_all_nums(nums1, nums2)
+  result = 0
+
+  result ^= nums1.inject(&:^) if nums2.length.odd?
+  result ^= nums2.inject(&:^) if nums1.length.odd?
+
+  result
+end
