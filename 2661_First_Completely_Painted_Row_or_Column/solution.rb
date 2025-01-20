e3cdd1d11f08adf7544bf6f6613be180d3388628
Hasan Özovalı <ozovalihasan@gmail.com>
2025-01-20 03:24:47 +0300

    Add a solution to the "First Completely Painted Row or Column" challenge

--- 2661_First_Completely_Painted_Row_or_Column/solution.rb
+++ 2661_First_Completely_Painted_Row_or_Column/solution.rb
@@ -1,4 +1,25 @@
 # @param {Integer[]} arr
 # @param {Integer[][]} mat
 # @return {Integer}
-def first_complete_index(arr, mat); end
+def first_complete_index(arr, mat)
+  coordinates = []
+  mat.each_with_index do |row, row_index|
+    row.each_with_index do |cell, col_index|
+      coordinates[cell] = [row_index, col_index]
+    end
+  end
+
+  row_num = mat.size
+  col_num = mat[0].size
+
+  row_count = Array.new(row_num, col_num)
+  col_count = Array.new(col_num, row_num)
+  
+  arr.each_with_index do |num, index|
+    row_index, col_index = coordinates[num]
+    row_count[row_index] -= 1  
+    col_count[col_index] -= 1  
+
+    return index if row_count[row_index].zero? || col_count[col_index].zero?
+  end
+end
