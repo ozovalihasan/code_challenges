c1538fa46fe967898839ea2174032b72552bc22f
Hasan Özovalı <ozovalihasan@gmail.com>
2025-01-22 00:34:19 +0300

    Add a solution to the "Grid Game" challenge

--- 2017_Grid_Game/solution.rb
+++ 2017_Grid_Game/solution.rb
@@ -1,5 +1,15 @@
 # @param {Integer[][]} grid
 # @return {Integer}
 def grid_game(grid)
-    
+  sums = [grid.first.sum, - grid.last.last]
+
+  min_result = Float::INFINITY
+  grid.first.length.times do |index|
+    sums[0] -= grid[0][index]
+    sums[1] += grid[1][index - 1]
+
+    min_result = [sums.max, min_result].min
+  end
+  
+  min_result
 end
