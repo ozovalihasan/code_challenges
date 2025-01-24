89014a3b81d58275077f57e720544599c9ce8b37
Hasan Özovalı <ozovalihasan@gmail.com>
2025-01-24 04:05:46 +0300

    Update the solution of the "Count Servers that Communicate" challenge again

--- 1267_Count_Servers_that_Communicate/solution.rb
+++ 1267_Count_Servers_that_Communicate/solution.rb
@@ -4,22 +4,17 @@ def count_servers(grid)
   row_counts = grid.map(&:sum)
   col_counts = grid.transpose.map(&:sum)
   
-  selected_cols = []
-  col_counts.each_with_index do |count, index|
-    selected_cols << index if count == 1
-  end
+  selected_cols = col_counts.filter_map.with_index { |count, col_index| col_index if count == 1 }
 
-  result = (row_counts.sum + col_counts.sum) / 2
-
+  result = row_counts.sum
   return result if selected_cols.empty?
 
-  grid.each_with_index do |row, row_index|
-    next unless row_counts[row_index] == 1
-
-    result -= selected_cols.count do |col_index|
-      row[col_index] == 1 && col_counts[col_index] == 1
+  selected_rows = row_counts.filter_map.with_index { |count, rows_index| rows_index if count == 1 }
+  alone_servers = selected_cols.count do |col_index|
+    selected_rows.any? do |row_index|
+      grid[row_index][col_index] == 1
     end
   end
 
-  result
+  result - alone_servers
 end
