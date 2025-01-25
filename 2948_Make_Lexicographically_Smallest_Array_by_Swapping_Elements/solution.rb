97f11de2d885508566637f9c096d89820e100cfd
Hasan Özovalı <ozovalihasan@gmail.com>
2025-01-25 19:38:04 +0300

    Add a solution to the "Make Lexicographically Smallest Array by Swapping Elements" challenge

--- 2948_Make_Lexicographically_Smallest_Array_by_Swapping_Elements/solution.rb
+++ 2948_Make_Lexicographically_Smallest_Array_by_Swapping_Elements/solution.rb
@@ -2,5 +2,19 @@
 # @param {Integer} limit
 # @return {Integer[]}
 def lexicographically_smallest_array(nums, limit)
-    
+  sorted_nums = nums.sort.reverse
+
+  chunks = sorted_nums.chunk_while { |elt_before, elt_after| elt_before - elt_after <= limit }.to_a
+  
+  chunk_numbers = {}
+  chunks.map.with_index do |chunk, index|
+    chunk.each do |num|
+      chunk_numbers[num] = index
+    end
+  end
+
+  nums.map do |num|
+    chunk_index = chunk_numbers[num]
+    chunks[chunk_index].pop
+  end
 end
