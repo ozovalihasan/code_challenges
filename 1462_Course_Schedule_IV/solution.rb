c650c2bda0d7a0d2a32643341a5b5eb439b8b086
Hasan Özovalı <ozovalihasan@gmail.com>
2025-01-27 16:36:44 +0300

    Update the solution of the "Course Schedule IV" challenge

--- 1462_Course_Schedule_IV/solution.rb
+++ 1462_Course_Schedule_IV/solution.rb
@@ -4,15 +4,15 @@
 # @return {Boolean[]}
 def check_if_prerequisite(num_courses, prerequisites, queries)
   starting_points = num_courses.times.to_a - prerequisites.map(&:last)
-  @previous_courses = Array.new(num_courses) { 0 }
+  @total_of_prerequisite_courses = Array.new(num_courses) { 0 }
   @following_courses = Array.new(num_courses) { [] }
 
   prerequisites.each do |prereq, course|
-    @previous_courses[course] += 1
+    @total_of_prerequisite_courses[course] += 1
     @following_courses[prereq] << course
   end
-  @courses = Array.new(num_courses) { |index| Set.new << index }
-
+  
+  @courses = Array.new(num_courses) { |index| Set.new([index]) }
   starting_points.each do |course|
     add_prerequisites(course)
   end
@@ -23,11 +23,11 @@ def check_if_prerequisite(num_courses, prerequisites, queries)
 end
 
 def add_prerequisites(prereq)
-  return unless @previous_courses[prereq].zero?
+  return unless @total_of_prerequisite_courses[prereq].zero?
 
   @following_courses[prereq].each do |following_course|
     @courses[following_course] += @courses[prereq]
-    @previous_courses[following_course] -= 1
+    @total_of_prerequisite_courses[following_course] -= 1
     add_prerequisites(following_course)
   end
 end
