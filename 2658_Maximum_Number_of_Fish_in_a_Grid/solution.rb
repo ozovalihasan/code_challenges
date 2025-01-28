11cecfec719462755e506bdfe6c130f7ffa9c48c
Hasan Özovalı <ozovalihasan@gmail.com>
2025-01-28 16:54:03 +0300

    Add a solution to the "Maximum Number of Fish in a Grid" challenge

--- 2658_Maximum_Number_of_Fish_in_a_Grid/solution.rb
+++ 2658_Maximum_Number_of_Fish_in_a_Grid/solution.rb
@@ -1,3 +1,44 @@
 # @param {Integer[][]} grid
 # @return {Integer}
-def find_max_fish(grid); end
+STEPS = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+def find_max_fish(grid)
+  @grid = grid
+  @max_row_index = @grid.size - 1
+  @max_col_index = @grid[0].size - 1
+
+  max_result = 0
+  @checked = Array.new(@grid.size) { Array.new(@grid[0].size, false) }
+  grid.size.times do |row_index|
+    grid.first.size.times do |col_index|
+      next if @checked[row_index][col_index]   
+
+      @result = 0
+      check_cell(row_index, col_index) 
+      max_result = [max_result, @result].max
+    end
+  end
+
+  max_result
+end
+
+private
+
+def check_cell(row_index, col_index)
+  return unless in_boundary?(row_index, col_index)
+  return if @checked[row_index][col_index]
+
+  @checked[row_index][col_index] = true
+
+  cell = @grid[row_index][col_index]
+  return if cell.zero?
+
+  @result += cell
+
+  STEPS.each do |row_step, col_step|
+    check_cell(row_index + row_step, col_index + col_step)
+  end
+end
+
+def in_boundary?(row, col)
+  row >= 0 && row <= @max_row_index && col >= 0 && col <= @max_col_index
+end
