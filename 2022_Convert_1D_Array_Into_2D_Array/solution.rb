fd40ac05e990a861eb38db4cd29a75f02cb49ff3
Hasan Özovalı <ozovalihasan@gmail.com>
2025-02-02 20:32:55 +0300

    Add a solution to the "Convert 1D Array Into 2D Array" challenge

--- 2022_Convert_1D_Array_Into_2D_Array/solution.rb
+++ 2022_Convert_1D_Array_Into_2D_Array/solution.rb
@@ -2,6 +2,12 @@
 # @param {Integer} m
 # @param {Integer} n
 # @return {Integer[][]}
-def construct2_d_array(original, _m, _n)
-  original
+def construct2_d_array(original, m, n)
+  return [] if (m * n) != original.size
+
+  result = []
+  m.times do |i|
+    result << original[i * n, n]
+  end
+  result
 end
