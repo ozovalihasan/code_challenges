192ea1a42d7be118ddfc099710efd1a4c28d74d5
Hasan Özovalı <ozovalihasan@gmail.com>
2025-02-03 16:40:49 +0300

    Add a test for the "Length of Longest Subarray With at Most K Frequency" challenge again

--- 2958_Length_of_Longest_Subarray_With_at_Most_K_Frequency/solution.rb
+++ 2958_Length_of_Longest_Subarray_With_at_Most_K_Frequency/solution.rb
@@ -1,4 +1,17 @@
-# @param {Integer} n
-# @param {Integer[][]} edges
+# @param {Integer[]} nums
+# @param {Integer} k
 # @return {Integer}
-def magnificent_sets(n, edges); end
+def max_subarray_length(nums, k)
+  counts = Hash.new(0)
+  left_index = -1
+  nums.map.with_index do |num, right_index|
+    counts[num] += 1
+    while counts[num] > k
+      left_index += 1
+      left_num = nums[left_index]
+      counts[left_num] -= 1
+    end
+
+    right_index - left_index
+  end.max
+end
