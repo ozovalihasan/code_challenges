81d2e82365700ee947aa8fbd00fa301f5c740031
Hasan Özovalı <ozovalihasan@gmail.com>
2025-02-03 16:29:42 +0300

    Add a solution to the "Longest Strictly Increasing or Strictly Decreasing Subarray" challenge

--- 3105_Longest_Strictly_Increasing_or_Strictly_Decreasing_Subarray/solution.rb
+++ 3105_Longest_Strictly_Increasing_or_Strictly_Decreasing_Subarray/solution.rb
@@ -1,3 +1,23 @@
 # @param {Integer[]} nums
 # @return {Integer}
-def longest_monotonic_subarray(nums); end
+def longest_monotonic_subarray(nums)
+  result_decreasing = 1
+  result_increasing = 1
+  results = []
+  nums.each_cons(2).map do |start_point, end_point|
+    if start_point > end_point
+      result_decreasing += 1
+      result_increasing = 1
+    elsif start_point == end_point
+      result_decreasing = 1
+      result_increasing = 1
+    else 
+      result_decreasing = 1
+      result_increasing += 1
+    end
+
+    results << result_increasing << result_decreasing
+  end
+  
+  results.max || 1
+end
