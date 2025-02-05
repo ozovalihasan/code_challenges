91b72846bc434bef7b3c0659bd867ffb1167a38f
Hasan Özovalı <ozovalihasan@gmail.com>
2025-02-05 03:47:34 +0300

    Add a solution to the "Check if One String Swap Can Make Strings Equal" challenge

--- 1790_Check_if_One_String_Swap_Can_Make_Strings_Equal/solution.rb
+++ 1790_Check_if_One_String_Swap_Can_Make_Strings_Equal/solution.rb
@@ -1,4 +1,7 @@
 # @param {String} str1
 # @param {String} str2
 # @return {Boolean}
-def are_almost_equal(str1, str2); end
+def are_almost_equal(str1, str2) 
+  diff_chars = str1.chars.zip(str2.chars).reject { |char1, char2| char1 == char2 }
+  diff_chars.empty? || (diff_chars.size == 2 && diff_chars[0] == diff_chars[1].reverse)
+end
