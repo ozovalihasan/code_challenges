b02c655f5732e86ac59c0d1883b158ecc533f1db
Hasan Özovalı <ozovalihasan@gmail.com>
2025-02-06 22:13:44 +0300

    Add a solution to the "Tuple with Same Product" challenge

--- 1726_Tuple_with_Same_Product/solution.rb
+++ 1726_Tuple_with_Same_Product/solution.rb
@@ -1,3 +1,6 @@
 # @param {Integer[]} nums
 # @return {Integer}
-def tuple_same_product(nums); end
+def tuple_same_product(nums)
+  products = nums.combination(2).map { |first_num, second_num| first_num * second_num }.tally
+  products.values.sum { |count| (count - 1) * count } * 4
+end
