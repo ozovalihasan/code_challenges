dd56a4f5977c440953b7296fdcb3ad12fac7b822
Hasan Özovalı <ozovalihasan@gmail.com>
2025-02-07 15:56:24 +0300

    Add a solution to the "Find the Number of Distinct Colors Among the Balls" challenge

--- 3160_Find_the_Number_of_Distinct_Colors_Among_the_Balls/solution.rb
+++ 3160_Find_the_Number_of_Distinct_Colors_Among_the_Balls/solution.rb
@@ -1,4 +1,19 @@
 # @param {Integer} limit
 # @param {Integer[][]} queries
 # @return {Integer[]}
-def query_results(limit, queries); end
+def query_results(_limit, queries)
+  colors = {}
+  color_counts = Hash.new(0)
+  count = 0
+  queries.map do |ball, color|
+    if (ball_color = colors[ball])
+      color_counts[ball_color] -= 1
+      count -= 1 if color_counts[ball_color].zero?
+    end
+    
+    colors[ball] = color
+    color_counts[color] += 1
+    count += 1 if color_counts[color] == 1
+    count
+  end
+end
