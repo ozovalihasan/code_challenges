841d8e8da5568970c8ce0b2e0416aae378b9a767
Hasan Özovalı <ozovalihasan@gmail.com>
2025-02-10 10:37:23 +0300

    Add a solution to the "Clear Digits" challenge

--- 3174_Clear_Digits/solution.rb
+++ 3174_Clear_Digits/solution.rb
@@ -1,3 +1,18 @@
 # @param {String} str
 # @return {String}
-def clear_digits(str); end
+DIGITS = Set.new %w[0 1 2 3 4 5 6 7 8 9]
+def clear_digits(str)
+  chars_to_remove_count = 0
+  result = ''
+  str.reverse.each_char do |char|
+    if DIGITS.include?(char) 
+      chars_to_remove_count += 1
+    elsif chars_to_remove_count.positive?
+      chars_to_remove_count -= 1
+    else
+      result << char
+    end
+  end  
+
+  result.reverse
+end
