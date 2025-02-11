6feb61a69ad2e276ab3944f7340266909a20d0d6
Hasan Özovalı <ozovalihasan@gmail.com>
2025-02-11 16:16:38 +0300

    Add a solution to the "Remove All Occurrences of a Substring" challenge

--- 1910_Remove_All_Occurrences_of_a_Substring/solution.rb
+++ 1910_Remove_All_Occurrences_of_a_Substring/solution.rb
@@ -1,4 +1,7 @@
 # @param {String} str
 # @param {String} part
 # @return {String}
-def remove_occurrences(str, part); end
+def remove_occurrences(str, part)
+  str = str.sub(part, '') while str.match? part
+  str
+end
