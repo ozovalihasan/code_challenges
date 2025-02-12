826362abe5799ba06fec9622395e811996eb863c
Hasan Özovalı <ozovalihasan@gmail.com>
2025-02-12 04:20:36 +0300

    Add a solution to the "Max Sum of a Pair With Equal Sum of Digits" challenge

--- 2342_Max_Sum_of_a_Pair_With_Equal_Sum_of_Digits/solution.rb
+++ 2342_Max_Sum_of_a_Pair_With_Equal_Sum_of_Digits/solution.rb
@@ -1,3 +1,14 @@
 # @param {Integer[]} nums
 # @return {Integer}
-def maximum_sum(nums); end
+def maximum_sum(nums)
+  digit_sums = Hash.new { |hash, key| hash[key] = [] }
+
+  nums.each do |num|
+    sum = num.digits.sum
+    digit_sums[sum] << num
+  end
+
+  digit_sums.values.filter_map do |checked_nums|
+    checked_nums.sort!.last(2).sum if checked_nums.size > 1
+  end.max || -1
+end
