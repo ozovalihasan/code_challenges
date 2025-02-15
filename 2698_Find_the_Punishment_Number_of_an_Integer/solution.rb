387ed36fc523e1611103dcdf464223e4dd873eb4
Hasan Özovalı <ozovalihasan@gmail.com>
2025-02-15 15:15:43 +0300

    Add a solution to the "Find the Punishment Number of an Integer" challenge

--- 2698_Find_the_Punishment_Number_of_an_Integer/solution.rb
+++ 2698_Find_the_Punishment_Number_of_an_Integer/solution.rb
@@ -1,3 +1,12 @@
 # @param {Integer} num
 # @return {Integer}
-def punishment_number(num); end
+PUNISHMENT_NUMBERS = [
+  1, 9, 10, 36, 45, 55, 82, 91, 99, 
+  100, 235, 297, 369, 370, 379, 414, 
+  657, 675, 703, 756, 792, 909, 918, 
+  945, 964, 990, 991, 999, 1000
+]
+def punishment_number(num)
+  index = PUNISHMENT_NUMBERS.bsearch_index { |punishment_num| punishment_num > num } || PUNISHMENT_NUMBERS.size
+  PUNISHMENT_NUMBERS[...index].sum { |selected_num| selected_num * selected_num }
+end
