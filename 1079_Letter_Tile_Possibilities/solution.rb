dd6855980707eba1a84cbf7b228d5f383ecdc37a
Hasan Özovalı <ozovalihasan@gmail.com>
2025-02-17 21:03:37 +0300

    Add a solution to the "Letter Tile Possibilities" challenge

--- 1079_Letter_Tile_Possibilities/solution.rb
+++ 1079_Letter_Tile_Possibilities/solution.rb
@@ -1,5 +1,7 @@
 # @param {String} tiles
 # @return {Integer}
 def num_tile_possibilities(tiles)
-    
+  1.upto(tiles.size).sum do |size|
+    tiles.chars.permutation(size).map(&:join).uniq.size
+  end
 end
