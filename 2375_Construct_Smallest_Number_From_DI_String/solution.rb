d94ae6601a15f805dcff655270ed4b52e8d7424d
Hasan Özovalı <ozovalihasan@gmail.com>
2025-02-18 13:51:49 +0300

    Add a solution to the "Construct Smallest Number From DI String" challenge

--- 2375_Construct_Smallest_Number_From_DI_String/solution.rb
+++ 2375_Construct_Smallest_Number_From_DI_String/solution.rb
@@ -1,3 +1,19 @@
 # @param {String} pattern
 # @return {String}
-def smallest_number(pattern); end
+def smallest_number(pattern)
+  result = (1..(pattern.size + 1)).to_a.join
+  
+  groups = pattern.scan(/I+|D+/)
+  index = 0
+  groups.each do |group|
+    group_size = group.size
+    if group[0] == 'D'
+      range = index..(index + group_size)
+      result[range] = result[range].reverse 
+    end
+
+    index += group_size
+  end
+
+  result
+end
