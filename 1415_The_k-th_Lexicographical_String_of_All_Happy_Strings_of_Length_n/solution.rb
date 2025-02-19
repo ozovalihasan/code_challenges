ff638cf9b7f17eba76f219e55b8eee4a5d6136c5
Hasan Özovalı <ozovalihasan@gmail.com>
2025-02-19 10:56:39 +0300

    Add a solution to the "The k-th Lexicographical String of All Happy Strings of Length n" challenge

--- 1415_The_k-th_Lexicographical_String_of_All_Happy_Strings_of_Length_n/solution.rb
+++ 1415_The_k-th_Lexicographical_String_of_All_Happy_Strings_of_Length_n/solution.rb
@@ -1,6 +1,28 @@
 # @param {Integer} num
 # @param {Integer} order
 # @return {String}
-def get_happy_string(num, order)
-    
+AVAILABLE_CHARS = ['a', 'b', 'c'].freeze
+VALID_NEXT_CHARS = {
+  'a' => ['b', 'c'],
+  'b' => ['a', 'c'],
+  'c' => ['a', 'b']
+}.freeze
+
+def get_happy_string(length, order)
+  max_possible_strings = 3 * 2**(length - 1)
+  return '' if order > max_possible_strings
+
+  order -= 1
+  next_char_choices = (length - 1).times.map do 
+    choice = order % 2
+    order /= 2
+    choice
+  end
+
+  result = ""
+  result += AVAILABLE_CHARS[order % 3]
+  next_char_choices.reverse_each do |choice|
+    result << VALID_NEXT_CHARS[result[-1]][choice]
+  end
+  result
 end
