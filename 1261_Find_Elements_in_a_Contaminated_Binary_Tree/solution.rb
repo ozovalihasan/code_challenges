e0a2400a8c1bb2fab50c812709b840cd3c9154ae
Hasan Özovalı <ozovalihasan@gmail.com>
2025-02-21 11:09:28 +0300

    Add a solution to the "Find Elements in a Contaminated Binary Tree" challenge

--- 1261_Find_Elements_in_a_Contaminated_Binary_Tree/solution.rb
+++ 1261_Find_Elements_in_a_Contaminated_Binary_Tree/solution.rb
@@ -11,17 +11,30 @@ class FindElements
 =begin
     :type root: TreeNode
 =end
-    def initialize(root)
-        
+  def initialize(root)
+    @tree = root  
+    @nodes_found = Set.new
+    stack = [[root, 0]]
+    until stack.empty?
+      node, num = stack.pop
+      if node
+        @nodes_found << num
+      else
+        next
+      end
+      
+      stack << [node.left, num * 2 + 1] 
+      stack << [node.right, num * 2 + 2] 
     end
+  end
 
 =begin
     :type target: Integer
     :rtype: Boolean
 =end
-    def find(target)
-        
-    end
+  def find(target)
+    @nodes_found.include? target
+  end
 end
 
 # Your FindElements object will be instantiated and called as such:
