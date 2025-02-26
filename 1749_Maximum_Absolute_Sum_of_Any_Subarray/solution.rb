3f8b01d8db1a322e43f9148eb8c3d1f7c12412df
Hasan Özovalı <ozovalihasan@gmail.com>
2025-02-27 01:03:54 +0300

    Add a solution to the "Maximum Absolute Sum of Any Subarray" challenge

--- 1749_Maximum_Absolute_Sum_of_Any_Subarray/solution.rb
+++ 1749_Maximum_Absolute_Sum_of_Any_Subarray/solution.rb
@@ -1,5 +1,15 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def max_absolute_sum(nums)
-    
+  negative_result = 0
+  positive_result = 0
+  max = 0
+
+  nums.each do |num|
+    positive_result = [num, positive_result + num].max
+    negative_result = [num, negative_result + num].min
+    max = [positive_result, negative_result.abs, max].max
+  end
+
+  max
 end
