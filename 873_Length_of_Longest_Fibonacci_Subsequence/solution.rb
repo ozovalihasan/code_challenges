3b53cc7fd0eccae03bac277beec93e3b15b2bfe6
Hasan Özovalı <ozovalihasan@gmail.com>
2025-02-27 04:13:21 +0300

    Add a solution to the "Length of Longest Fibonacci Subsequence" challenge

--- 873_Length_of_Longest_Fibonacci_Subsequence/solution.rb
+++ 873_Length_of_Longest_Fibonacci_Subsequence/solution.rb
@@ -1,5 +1,19 @@
 # @param {Integer[]} arr
 # @return {Integer}
 def len_longest_fib_subseq(arr)
-    
+  max_length = 0
+  arr_set = arr.to_h { |num| [num, true] }
+  arr.combination(2) do |(first, second)|
+    count = 2
+    sum = first + second
+    while arr_set[sum]
+      first, second = second, sum
+      count += 1
+      sum = first + second
+    end
+
+    max_length = count if count > max_length
+  end
+
+  max_length > 2 ? max_length : 0
 end
