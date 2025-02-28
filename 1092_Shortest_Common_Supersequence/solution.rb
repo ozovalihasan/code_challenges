ad7382745e0631728000b960a649958bca025931
Hasan Özovalı <ozovalihasan@gmail.com>
2025-03-01 01:05:46 +0300

    Update the solution of the "Shortest Common Supersequence" challenge

--- 1092_Shortest_Common_Supersequence/solution.rb
+++ 1092_Shortest_Common_Supersequence/solution.rb
@@ -6,21 +6,23 @@ def shortest_common_supersequence(str1, str2)
   str1.each_char.with_index do |char1, index1|
     str2.each_char.with_index do |char2, index2|
       memo[index1 + 1][index2 + 1] = if char1 == char2
-                                     memo[index1][index2] + 1
-                                   else
-                                     [memo[index1][index2 + 1], memo[index1 + 1][index2]].max
-                                   end
+                                       memo[index1][index2] + 1
+                                     else
+                                       [memo[index1][index2 + 1], memo[index1 + 1][index2]].max
+                                     end
     end
   end
   
   memo[str1.size][str2.size]
-  result = ""
-  index1, index2 = str1.size, str2.size
-  while index1 > 0 || index2 > 0
-    if index1 > 0 && memo[index1][index2] == memo[index1 - 1][index2]
+  result = ''
+  index1 = str1.size
+  index2 = str2.size
+
+  while index1.positive? || index2.positive?
+    if index1.positive? && memo[index1][index2] == memo[index1 - 1][index2]
       result = str1[index1 - 1] + result
       index1 -= 1
-    elsif index2 > 0 && memo[index1][index2] == memo[index1][index2 - 1]
+    elsif index2.positive? && memo[index1][index2] == memo[index1][index2 - 1]
       result = str2[index2 - 1] + result
       index2 -= 1
     else
