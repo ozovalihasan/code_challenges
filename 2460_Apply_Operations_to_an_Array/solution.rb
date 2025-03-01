fa25b9f5bbe77467963d26295317887bf5b6d418
Hasan Özovalı <ozovalihasan@gmail.com>
2025-03-01 14:24:30 +0300

    Add a solution to the "Apply Operations to an Array" challenge

--- 2460_Apply_Operations_to_an_Array/solution.rb
+++ 2460_Apply_Operations_to_an_Array/solution.rb
@@ -1,5 +1,14 @@
 # @param {Integer[]} nums
 # @return {Integer[]}
 def apply_operations(nums)
-    
+  (nums.size - 1).times do |index|
+    if nums[index] == nums[index + 1]
+      nums[index] *= 2
+      nums[index.next] = 0
+    end
+  end
+
+  zero_count = nums.count(0)
+  nums.delete(0)
+  nums + [0] * zero_count
 end
