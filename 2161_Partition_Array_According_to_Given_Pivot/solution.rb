008183e88f14d4df1eb7e2bcd644529e80be0cfa
Hasan Özovalı <ozovalihasan@gmail.com>
2025-03-03 14:27:36 +0300

    Add a solution to the "Partition Array According to Given Pivot" challenge

--- 2161_Partition_Array_According_to_Given_Pivot/solution.rb
+++ 2161_Partition_Array_According_to_Given_Pivot/solution.rb
@@ -2,5 +2,7 @@
 # @param {Integer} pivot
 # @return {Integer[]}
 def pivot_array(nums, pivot)
-    
+  result = nums.group_by { |num| num <=> pivot }.to_h
+  result.default = []
+  result[-1] + result[0] + result[1]
 end
