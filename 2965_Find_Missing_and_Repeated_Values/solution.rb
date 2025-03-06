a3d914a78aa2ffadc709829d74650eaeb3dfa496
Hasan Özovalı <ozovalihasan@gmail.com>
2025-03-06 14:57:58 +0300

    Add a solution to the "Find Missing and Repeated Values" challenge

--- 2965_Find_Missing_and_Repeated_Values/solution.rb
+++ 2965_Find_Missing_and_Repeated_Values/solution.rb
@@ -1,5 +1,8 @@
 # @param {Integer[][]} grid
 # @return {Integer[]}
 def find_missing_and_repeated_values(grid)
-    
+  nums = grid.flatten
+  repeated_num = nums.tally.find { |k, v| v == 2 }.first
+  missing_num = ((1..nums.size).to_a - nums).first
+  [repeated_num, missing_num]
 end
