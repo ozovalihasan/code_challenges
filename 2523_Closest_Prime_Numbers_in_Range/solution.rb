160e36b5fb2f0907b7099eb27d36492fd850baeb
Hasan Özovalı <ozovalihasan@gmail.com>
2025-03-07 16:11:11 +0300

    Add a solution to the "Closest Prime Numbers in Range" challenge

--- 2523_Closest_Prime_Numbers_in_Range/solution.rb
+++ 2523_Closest_Prime_Numbers_in_Range/solution.rb
@@ -1,6 +1,25 @@
+require 'prime'
 # @param {Integer} left
 # @param {Integer} right
 # @return {Integer[]}
 def closest_primes(left, right)
+  last_prime = -Float::INFINITY
+  min_diff = Float::INFINITY
+
+  result = [-1, -1]
+  left.upto(right) do |num|
+    next unless num.prime?
+
+    last_diff = num - last_prime
+    if last_diff < min_diff
+      min_diff = last_diff 
+      
+      result = [last_prime, num]
+      return result if min_diff <= 2
+    end
     
+    last_prime = num
+  end
+
+  result  
 end
