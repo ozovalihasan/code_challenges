b7378759285f75491440e39386447b4511846f15
Hasan Özovalı <ozovalihasan@gmail.com>
2025-03-08 10:18:14 +0300

    Add a solution to the "Minimum Recolors to Get K Consecutive Black Blocks" challenge

--- 2379_Minimum_Recolors_to_Get_K_Consecutive_Black_Blocks/solution.rb
+++ 2379_Minimum_Recolors_to_Get_K_Consecutive_Black_Blocks/solution.rb
@@ -2,5 +2,14 @@
 # @param {Integer} k
 # @return {Integer}
 def minimum_recolors(blocks, k)
+  min_count = white_count = blocks[...k].count('W')
+  k.upto(blocks.size - 1) do |index|
+    white_count += 1 if blocks[index] == 'W'
+    white_count -= 1 if blocks[index - k] == 'W'
+    min_count = white_count if white_count < min_count
     
+    break if min_count.zero?
+  end
+
+  min_count
 end
