bbe222ed221036abb848dff46b87e1b4ea36cf79
Hasan Özovalı <ozovalihasan@gmail.com>
2025-03-09 23:52:05 +0300

    Add a solution to the "Alternating Groups II" challenge

--- 3208_Alternating_Groups_II/solution.rb
+++ 3208_Alternating_Groups_II/solution.rb
@@ -2,5 +2,12 @@
 # @param {Integer} k
 # @return {Integer}
 def number_of_alternating_groups(colors, k)
-    
+  groups = colors.chunk_while { |previous_el, next_el| previous_el != next_el }.map(&:size)
+  groups[0] += groups.pop if colors.first != colors.last && groups.size > 1
+
+  if groups.first == colors.size && colors.size.even?
+    colors.size
+  else
+    groups.filter_map { |group| (group - k + 1) if group >= k }.sum
+  end
 end
