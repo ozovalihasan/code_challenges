44078ca05fb09d6216ac8fd34177b3196e3bf939
Hasan Özovalı <ozovalihasan@gmail.com>
2025-03-11 17:26:26 +0300

    Add a solution to the "Number of Substrings Containing All Three Characters" challenge

--- 1358_Number_of_Substrings_Containing_All_Three_Characters/solution.rb
+++ 1358_Number_of_Substrings_Containing_All_Three_Characters/solution.rb
@@ -1,5 +1,23 @@
 # @param {String} s
 # @return {Integer}
 def number_of_substrings(str)
+  counts = Hash.new(0)
+  counts_valid_chars = 0
+  left_index = -1
+  result = 0
+  str_size = str.size
+
+  str.each_char.with_index do |char, index|
+    counts[char] += 1
+    counts_valid_chars += 1 if counts[char] == 1
     
+    while counts_valid_chars == 3
+      result += str_size - index
+      left_index += 1
+      counts[str[left_index]] -= 1
+      counts_valid_chars -= 1 if counts[str[left_index]].zero?
+    end
+  end
+  
+  result
 end
