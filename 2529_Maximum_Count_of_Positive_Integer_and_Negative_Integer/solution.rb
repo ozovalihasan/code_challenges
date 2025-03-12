3480ff9d3f444607c1a44be07bd9efe6f753df44
Hasan Özovalı <ozovalihasan@gmail.com>
2025-03-12 14:29:33 +0300

    Add a solution to the "Maximum Count of Positive Integer and Negative Integer" challenge

--- 2529_Maximum_Count_of_Positive_Integer_and_Negative_Integer/solution.rb
+++ 2529_Maximum_Count_of_Positive_Integer_and_Negative_Integer/solution.rb
@@ -1,5 +1,16 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def maximum_count(nums)
+  positive_count = 0
+  negative_count = 0
+
+  nums.each do |num|
+    if num.positive?
+      positive_count += 1
+    elsif num.negative?
+      negative_count += 1
+    end  
+  end
   
+  [positive_count, negative_count].max
 end
