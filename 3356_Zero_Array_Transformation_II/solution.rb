6b4591d172c11203b4acbf29adcbd30c0eb0ed1f
Hasan Özovalı <ozovalihasan@gmail.com>
2025-03-13 23:41:48 +0300

    Add a solution to the "Zero Array Transformation II" challenge

--- 3356_Zero_Array_Transformation_II/solution.rb
+++ 3356_Zero_Array_Transformation_II/solution.rb
@@ -2,5 +2,28 @@
 # @param {Integer[][]} queries
 # @return {Integer}
 def min_zero_array(nums, queries)
+  query_index = -1
+  sum_arr = Array.new(nums.size + 1, 0)
+  
+  nums.each_with_index do |num, index|
+    until sum_arr[index] >= num
+      return - 1 if query_index == queries.length - 1
+
+      query_index += 1
+      
+      start_index, end_index, val = queries[query_index]
+
+      if start_index <= index && index <= end_index
+        sum_arr[index] += val
+      elsif start_index > index
+        sum_arr[start_index] += val
+      end
+
+      sum_arr[end_index + 1] -= val if end_index >= index
+    end
     
+    sum_arr[index + 1] += sum_arr[index]
+  end
+
+  query_index + 1
 end
--- 3356_Zero_Array_Transformation_II/test_solution.rb
+++ 3356_Zero_Array_Transformation_II/test_solution.rb
@@ -6,7 +6,7 @@ describe 'Solution' do
 
   describe '#min_zero_array' do
     examples.each_slice(2).each_with_index do |(nums, queries), index|
-      it 'returns the minimum possible diameter of the resulting tree' do
+      it 'returns the minimum possible non-negative value of k, such that after processing the first k queries in sequence, nums becomes a Zero Array' do
         expect(min_zero_array(nums, queries)).to eq(results[index])
       end
     end
