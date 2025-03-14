75b569937f8ad02db1b36e3504158fdc8c09c234
Hasan Özovalı <ozovalihasan@gmail.com>
2025-03-14 08:59:05 +0300

    Add a solution to the "Maximum Candies Allocated to K Children" challenge

--- 2226_Maximum_Candies_Allocated_to_K_Children/solution.rb
+++ 2226_Maximum_Candies_Allocated_to_K_Children/solution.rb
@@ -1,6 +1,12 @@
 # @param {Integer[]} candies
 # @param {Integer} k
 # @return {Integer}
-def maximum_candies(candies, k)
-    
+def maximum_candies(candies, num_children)
+  candies_sum = candies.sum
+  
+  result = (1..((candies_sum / num_children) + 1)).bsearch do |candies_per_child|
+    num_children > (candies.sum { |candy| candy / candies_per_child })
+  end || 1
+
+  result - 1
 end
