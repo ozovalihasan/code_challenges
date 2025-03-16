3aa7bc66852278e1994773b055df27db53504bbb
Hasan Özovalı <ozovalihasan@gmail.com>
2025-03-16 14:38:00 +0300

    Add a solution to the "Minimum Time to Repair Cars" challenge

--- 2594_Minimum_Time_to_Repair_Cars/solution.rb
+++ 2594_Minimum_Time_to_Repair_Cars/solution.rb
@@ -2,5 +2,9 @@
 # @param {Integer} cars
 # @return {Integer}
 def repair_cars(ranks, cars)
-    
+  max_car = (cars.to_f / ranks.size).ceil
+  
+  (((max_car ** 2) * ranks.min)..((max_car ** 2) * ranks.max)).bsearch do |max_time|
+    ranks.sum {|rank| Math.sqrt((max_time / rank)).to_i} >= cars
+  end
 end
