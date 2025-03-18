5fc44d1c9033c3a8bfd1c1308e615985fb44efc0
Hasan Özovalı <ozovalihasan@gmail.com>
2025-03-18 21:14:24 +0300

    Add a solution to the "Longest Nice Subarray" challenge

--- 2401_Longest_Nice_Subarray/solution.rb
+++ 2401_Longest_Nice_Subarray/solution.rb
@@ -1,5 +1,17 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def longest_nice_subarray(nums)
-    
+  result = 0
+  
+  nums.each_with_index do |num, index|
+    index2 = index
+    suitable_nums = 0
+    while index2 < nums.size && (suitable_nums & nums[index2]).zero? 
+      suitable_nums = suitable_nums | nums[index2]
+      index2 += 1
+    end
+
+    result = index2 - index if index2 - index > result
+  end
+  result
 end
