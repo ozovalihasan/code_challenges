07d018a607241ec06fea46e3d389f2e9d05eee9d
Hasan Özovalı <ozovalihasan@gmail.com>
2025-03-19 17:30:19 +0300

    Add a solution to the "Minimum Operations to Make Binary Array Elements Equal to One I" challenge

--- 3191_Minimum_Operations_to_Make_Binary_Array_Elements_Equal_to_One_I/solution.rb
+++ 3191_Minimum_Operations_to_Make_Binary_Array_Elements_Equal_to_One_I/solution.rb
@@ -1,5 +1,18 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def min_operations(nums)
-    
+  return 0 if nums.none? 0
+
+  result = 0
+  (nums.size - 1).downto(2) do |index|
+    num = nums.pop
+    if num.even?
+      nums[-1] += 1
+      nums[-2] += 1
+
+      result += 1
+    end
+  end 
+
+  nums.all?(&:odd?) ? result : -1
 end
