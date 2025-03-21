c9b9d5311f44833d3da685609605fdcab64b4fdc
Hasan Özovalı <ozovalihasan@gmail.com>
2025-03-21 17:14:42 +0300

    Add a solution to the "Find All Possible Recipes from Given Supplies" challenge

--- 2115_Find_All_Possible_Recipes_from_Given_Supplies/solution.rb
+++ 2115_Find_All_Possible_Recipes_from_Given_Supplies/solution.rb
@@ -3,5 +3,21 @@
 # @param {String[]} supplies
 # @return {String[]}
 def find_all_recipes(recipes, ingredients, supplies)
-    
+  @items = recipes.zip(ingredients).to_h
+  supplies.each do |supply|
+    @items[supply] = true
+  end
+
+  recipes.select do |recipe|
+    check(recipe)
+  end
+end
+
+def check(ingredient)
+  return false unless @items[ingredient]
+  return true if @items[ingredient] == true
+
+  ingredients = @items[ingredient]
+  @items[ingredient] = false
+  @items[ingredient] = ingredients.all? { |item| check(item) }
 end
