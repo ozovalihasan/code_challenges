3a4c8f93c6db6e86979a692a934e7eaf691aa615
Hasan Özovalı <ozovalihasan@gmail.com>
2025-03-24 03:48:03 +0300

    Add a solution to the "Count Days Without Meetings" challenge

--- 3169_Count_Days_Without_Meetings/solution.rb
+++ 3169_Count_Days_Without_Meetings/solution.rb
@@ -2,5 +2,16 @@
 # @param {Integer[][]} meetings
 # @return {Integer}
 def count_days(days, meetings)
-    
+  meetings = meetings.sort_by(&:first)
+
+  last_day = meetings.first.first
+  count = last_day - 1
+  meetings.each do |start_day, end_date|
+    count += start_day - last_day - 1 if start_day > last_day
+    last_day = end_date if end_date > last_day
+  end 
+
+  count += days - last_day if days >= last_day
+  
+  count
 end
