e919f465bd102e1be82c002ccd3ce37fcfed3e80
Hasan Özovalı <ozovalihasan@gmail.com>
2025-03-26 01:22:01 +0300

    Add a solution to the "Check if Grid can be Cut into Sections" challenge

--- 3394_Check_if_Grid_can_be_Cut_into_Sections/solution.rb
+++ 3394_Check_if_Grid_can_be_Cut_into_Sections/solution.rb
@@ -2,5 +2,35 @@
 # @param {Integer[][]} rectangles
 # @return {Boolean}
 def check_valid_cuts(num, rectangles)
+  vertical = []
+  horizontal = []
+  rectangles.each do |x_start, y_start, x_end, y_end|
+    vertical << x_start << (-x_end)
+    horizontal << y_start << (-y_end)
+  end
+
+  vertical = sort_by_num(vertical)
+  return true if count_pairs(vertical)
+
+  horizontal = sort_by_num(horizontal)
+  return true if count_pairs(horizontal)
+
+  false
+end
+
+def sort_by_num(arr)
+  arr.group_by(&:abs).sort.map(&:last).map(&:sort).flatten
+end
+
+def count_pairs(arr)
+  count = 0
+  overlap = 0
+  arr.each do |num|
+    overlap += num.negative? ? (-1) : 1
     
+    count += 1 if overlap.zero?
+    return true if count >= 3
+  end
+
+  false
 end
