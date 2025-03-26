90294e164465ef1b8f1f4f32b882f4d8a975703b
Hasan Özovalı <ozovalihasan@gmail.com>
2025-03-27 02:57:53 +0300

    Add a solution to the "Minimum Operations to Make a Uni-Value Grid" challenge

--- 2033_Minimum_Operations_to_Make_a_Uni_Value_Grid/solution.rb
+++ 2033_Minimum_Operations_to_Make_a_Uni_Value_Grid/solution.rb
@@ -2,5 +2,14 @@
 # @param {Integer} x
 # @return {Integer}
 def min_operations(grid, x)
-    
+  nums = grid.flatten.sort
+  median = nums[nums.size / 2]
+
+  nums.each do |num|
+    return -1 unless ((num - median).abs % x).zero? 
+  end
+  
+  nums.sum do |num| 
+    (num - median).abs / x
+  end
 end
