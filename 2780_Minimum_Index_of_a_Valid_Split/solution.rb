0ef4b4e72dc69f79b6935dce4460b4e3f9e636c3
Hasan Özovalı <ozovalihasan@gmail.com>
2025-03-28 02:57:59 +0300

    Add a solution to the "Minimum Index of a Valid Split" challenge

--- 2780_Minimum_Index_of_a_Valid_Split/solution.rb
+++ 2780_Minimum_Index_of_a_Valid_Split/solution.rb
@@ -1,5 +1,17 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def minimum_index(nums)
+  counts_left = Hash.new(0)
+  counts_right = nums.tally
+
+  nums[...-1].each_with_index do |num, index|
+    counts_left[num] += 1
+    counts_right[num] -= 1
+    if counts_left[num] * 2 > index + 1 && counts_right[num] * 2 > nums.size - index - 1
+      return index
+    end
     
+  end
+  
+  -1
 end
