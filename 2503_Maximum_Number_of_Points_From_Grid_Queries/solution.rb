491de3a2bd2f7d4ec3888b5c85c7700a57b2aaac
Hasan Özovalı <ozovalihasan@gmail.com>
2025-03-29 02:44:25 +0300

    Update the solution of the "Maximum Number of Points From Grid Queries" challenge

--- 2503_Maximum_Number_of_Points_From_Grid_Queries/solution.rb
+++ 2503_Maximum_Number_of_Points_From_Grid_Queries/solution.rb
@@ -6,12 +6,11 @@ def max_points(grid, queries)
   @max_row_index = grid.size - 1
   @max_col_index = grid.first.size - 1
   
-  @checked = Array.new(grid.size) { Array.new(grid.first.size) }
   @queries = queries.uniq.sort.reverse
   @results = {}
   
   @cells_needed_check = [[0, 0, grid[0][0]]]
-  @checked[0][0] = true
+  grid[0][0] *= -1
 
   ref = @queries.pop
   count = 0
@@ -24,17 +23,13 @@ def max_points(grid, queries)
         next_row_index = row_index + step_row
         next_col_index = col_index + step_col
         
-        if in_boundary?(next_row_index, next_col_index) && !@checked[next_row_index][next_col_index]
-          @checked[next_row_index][next_col_index] = true
-
+        if in_boundary?(next_row_index, next_col_index)
           next_val = grid[next_row_index][next_col_index] 
-          index = @cells_needed_check.bsearch_index {|_, _, checked_val| next_val > checked_val  } 
-          
-          if index 
-            @cells_needed_check.insert(index, [next_row_index, next_col_index, next_val])  
-          else
-            @cells_needed_check << [next_row_index, next_col_index, next_val]
-          end
+          next if next_val.negative?
+
+          grid[next_row_index][next_col_index] *= -1 
+          index = @cells_needed_check.bsearch_index {|_, _, checked_val| next_val > checked_val  } || @cells_needed_check.size
+          @cells_needed_check.insert(index, [next_row_index, next_col_index, next_val])  
         end
       end
     else
@@ -46,12 +41,8 @@ def max_points(grid, queries)
   end
   
   @results[ref] = count if ref
-  @queries.each do |query|
-    @results[query] = count
-  end
-  queries.map do |query|
-    @results[query]
-  end
+  @queries.each { |query| @results[query] = count }
+  queries.map { |query| @results[query] }
 end
 
 def in_boundary?(row_index, col_index)
