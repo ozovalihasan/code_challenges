a17c846ccfdd91e487fc8ae6685321c12f0dce3c
Hasan Özovalı <ozovalihasan@gmail.com>
2025-04-02 18:15:25 +0300

    Add a solution to the "Maximum Value of an Ordered Triplet I" challenge

--- 2873_Maximum_Value_of_an_Ordered_Triplet_I/solution.rb
+++ 2873_Maximum_Value_of_an_Ordered_Triplet_I/solution.rb
@@ -1,5 +1,23 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def maximum_triplet_value(nums)
-    
+  max = -Float::INFINITY
+  max_differences = []
+  nums.each_with_index do |num, index|
+    max = num if num > max
+    max_differences << max - num
+  end
+
+  max_differences.pop
+  max_differences.unshift(nil)
+  
+  max = -Float::INFINITY
+  max_result = -Float::INFINITY
+  (nums.size - 1).downto(1) do |index|
+    num = nums[index]
+    max = num if num > max
+    max_result = max * max_differences[index] if (max * max_differences[index]) > max_result
+  end
+
+  max_result
 end
