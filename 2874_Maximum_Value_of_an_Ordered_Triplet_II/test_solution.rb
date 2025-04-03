15d9915ca5f5a9c274a947f9ef15efb9b337b1df
Hasan Özovalı <ozovalihasan@gmail.com>
2025-04-03 17:55:49 +0300

    Add a test for the "Maximum Value of an Ordered Triplet II" challenge

---
+++ 2874_Maximum_Value_of_an_Ordered_Triplet_II/test_solution.rb
@@ -0,0 +1,14 @@
+require_relative 'solution'
+require_relative '../read_test_files'
+
+describe 'Solution' do
+  examples, results = read_test_files.values_at(:examples, :results)
+
+  describe '#maximum_triplet_value' do
+    examples.each_with_index do |nums, index|
+      it 'returns the maximum value over all triplets of indices (i, j, k) such that i < j < k' do
+        expect(maximum_triplet_value(nums)).to eq(results[index])
+      end
+    end
+  end
+end
