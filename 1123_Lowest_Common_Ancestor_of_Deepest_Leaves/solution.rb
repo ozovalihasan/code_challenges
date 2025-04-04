d72da13058f2a7ee3a2075bcaea7bc0ada0d1694
Hasan Özovalı <ozovalihasan@gmail.com>
2025-04-04 21:03:05 +0300

    Add a solution to the "Lowest Common Ancestor of Deepest Leaves" challenge

--- 1123_Lowest_Common_Ancestor_of_Deepest_Leaves/solution.rb
+++ 1123_Lowest_Common_Ancestor_of_Deepest_Leaves/solution.rb
@@ -10,5 +10,26 @@
 # @param {TreeNode} root
 # @return {TreeNode}
 def lca_deepest_leaves(root)
+  @max_depth = -1
+  @result = nil
+  check_node(root, 0)
+  @result
+end
+
+def check_node(node, depth = 0)
+  return -1 if node.nil?
+
+  @max_depth = depth if depth > @max_depth
   
+  if node.right.nil? && node.left.nil?
+    @result = node if depth == @max_depth
+    
+    return depth
+  end
+
+  right_depth = check_node(node.right, depth + 1)
+  left_depth = check_node(node.left, depth + 1)
+  @result = node if right_depth == @max_depth && left_depth == @max_depth
+
+  [right_depth, left_depth].max
 end
