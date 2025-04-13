2f92be52101c5c7611750fd20371def9743f7cf4
Hasan Özovalı <ozovalihasan@gmail.com>
2025-04-14 01:09:26 +0300

    Add a solution to the "Count Good Numbers" challenge

--- 1922_Count_Good_Numbers/solution.rb
+++ 1922_Count_Good_Numbers/solution.rb
@@ -1,5 +1,21 @@
 # @param {Integer} num
 # @return {Integer}
+MODULO = 10**9 + 7
 def count_good_numbers(num)
-    
+  remainder = num % 2
+  
+  result = 1
+  num /= 2
+  base = 20
+  while num > 0 
+    if num.odd?
+      result = (result * base) % MODULO
+    end
+   
+    base = (base * base) % MODULO
+    num = num / 2
+  end
+  
+  result *= 5 ** remainder
+  result %= MODULO
 end
