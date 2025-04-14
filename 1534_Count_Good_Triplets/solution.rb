9d3f7dc61661fbfb3f3a4f792e2f4e59494ac7e9
Hasan Özovalı <ozovalihasan@gmail.com>
2025-04-14 18:00:02 +0300

    Add a solution to the "Count Good Triplets" challenge

--- 1534_Count_Good_Triplets/solution.rb
+++ 1534_Count_Good_Triplets/solution.rb
@@ -4,5 +4,7 @@
 # @param {Integer} c
 # @return {Integer}
 def count_good_triplets(arr, int1, int2, int3)
-    
+  arr.combination(3).count do |num1, num2, num3|
+    (num1 - num2).abs <= int1 && (num2 - num3).abs <= int2 && (num1 - num3).abs <= int3
+  end
 end
