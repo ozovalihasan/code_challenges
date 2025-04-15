54dede5c2fe31bc48bec98975e1b77db77c8d5d1
Hasan Özovalı <ozovalihasan@gmail.com>
2025-04-16 01:03:35 +0300

    Add a solution to the "Count Good Triplets in an Array" challenge

--- 2179_Count_Good_Triplets_in_an_Array/solution.rb
+++ 2179_Count_Good_Triplets_in_an_Array/solution.rb
@@ -2,5 +2,20 @@
 # @param {Integer[]} nums2
 # @return {Integer}
 def good_triplets(nums1, nums2)
+  num_to_position_in_nums2 = []
+  nums2.each_with_index do |value, index|
+    num_to_position_in_nums2[value] = index
+  end
+
+  ordered_indices_from_left = []
+  nums_size = nums1.size
+  nums1.each_with_index.sum do |num, index1|
+    position_in_nums2 = num_to_position_in_nums2[num]
     
+    insert_at = ordered_indices_from_left.bsearch_index { |position| position >= position_in_nums2 } || 
+                ordered_indices_from_left.size
+    
+    ordered_indices_from_left.insert(insert_at, position_in_nums2)
+    insert_at * (nums_size - index1 - 1 - (position_in_nums2 - insert_at))
+  end
 end
