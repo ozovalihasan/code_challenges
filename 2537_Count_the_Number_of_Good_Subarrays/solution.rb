1566f4a6ca0709279b00a0b8c957758d5d943528
Hasan Özovalı <ozovalihasan@gmail.com>
2025-04-17 00:19:25 +0300

    Add a solution to the "Count the Number of Good Subarrays" challenge

--- 2537_Count_the_Number_of_Good_Subarrays/solution.rb
+++ 2537_Count_the_Number_of_Good_Subarrays/solution.rb
@@ -2,5 +2,24 @@
 # @param {Integer} k
 # @return {Integer}
 def count_good(nums, k)
+  available_pairs = 0
+  counts = Hash.new(0)
+  left_index = 0
+  result = 0
+  
+  nums.each do |right_num|
+    available_pairs += counts[right_num]
+    counts[right_num] += 1
+
+    while available_pairs >= k
+      left_num = nums[left_index]
+      counts[left_num] -= 1
+      available_pairs -= counts[left_num]
+      left_index += 1
+    end
     
+    result += left_index
+  end
+
+  result
 end
