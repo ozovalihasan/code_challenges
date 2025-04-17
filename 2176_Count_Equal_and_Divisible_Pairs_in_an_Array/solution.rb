4ca55369bf9e40f7333e676a115e92bd131de383
Hasan Özovalı <ozovalihasan@gmail.com>
2025-04-17 15:11:35 +0300

    Add a solution to the "Count Equal and Divisible Pairs in an Array" challenge

--- 2176_Count_Equal_and_Divisible_Pairs_in_an_Array/solution.rb
+++ 2176_Count_Equal_and_Divisible_Pairs_in_an_Array/solution.rb
@@ -2,5 +2,13 @@
 # @param {Integer} k
 # @return {Integer}
 def count_pairs(nums, k)
-    
+  indices = Hash.new { |h, k| h[k] = [] }
+  nums.map.with_index.sum do |num, index|
+    sum = indices[num].sum do |prev_index|
+          (prev_index * index) % k == 0 ? 1 : 0
+        end
+
+    indices[num] << index
+    sum
+  end
 end
