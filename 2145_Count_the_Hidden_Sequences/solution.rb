c17925b67878da0e8bfe676b0b3547403f4574a9
Hasan Özovalı <ozovalihasan@gmail.com>
2025-04-21 14:58:38 +0300

    Add a solution to the "Count the Hidden Sequences" challenge

--- 2145_Count_the_Hidden_Sequences/solution.rb
+++ 2145_Count_the_Hidden_Sequences/solution.rb
@@ -3,5 +3,13 @@
 # @param {Integer} upper
 # @return {Integer}
 def number_of_arrays(differences, lower, upper)
-    
+  arr = [0]
+  differences.each do |diff|
+    arr << (arr[-1] + diff)
+  end      
+  min = arr.min
+  max = arr.max
+  range = max - min
+
+  [(upper - lower) - range + 1, 0].max
 end
