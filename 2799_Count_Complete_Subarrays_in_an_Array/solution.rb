adc6f92923a30327391e3846a8c43661279367a0
Hasan Özovalı <ozovalihasan@gmail.com>
2025-04-24 14:07:07 +0300

    Add a solution to the "Count Complete Subarrays in an Array" challenge

--- 2799_Count_Complete_Subarrays_in_an_Array/solution.rb
+++ 2799_Count_Complete_Subarrays_in_an_Array/solution.rb
@@ -1,5 +1,28 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def count_complete_subarrays(nums)
+  distinct_nums_count = nums.uniq.count
+  counts = Hash.new(0)
+  current_distinct_count = 0
+  right_index = -1
+  left_index = -1
+  result = 0
+  until right_index == nums.size
+    until current_distinct_count == distinct_nums_count 
+      right_index += 1
+      break if right_index == nums.size
+
+      current_distinct_count += 1 if counts[nums[right_index]] == 0
+      counts[nums[right_index]] += 1
+    end
+
+    result += nums.size - right_index
     
+    left_index += 1
+    counts[nums[left_index]] -= 1
+    current_distinct_count -= 1 if counts[nums[left_index]] == 0
+    break if right_index == nums.size
+  end
+
+  result
 end
