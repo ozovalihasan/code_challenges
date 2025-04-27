c417f0926006b33a0c6dec533d19bc38a9a0225f
Hasan Özovalı <ozovalihasan@gmail.com>
2025-04-27 15:24:49 +0300

    Add a solution to the "Count Subarrays of Length Three With a Condition" challenge

--- 3392_Count_Subarrays_of_Length_Three_With_a_Condition/solution.rb
+++ 3392_Count_Subarrays_of_Length_Three_With_a_Condition/solution.rb
@@ -1,5 +1,9 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def count_subarrays(nums)
-    
+  nums.each_cons(3).count do |first, second, third|
+    next if second.odd?
+
+    (second / 2) == (first + third)
+  end
 end
