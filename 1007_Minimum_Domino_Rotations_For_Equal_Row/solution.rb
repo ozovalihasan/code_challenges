689dafb71647d5777511dde6b8f7ce6d10124f34
Hasan Özovalı <ozovalihasan@gmail.com>
2025-05-04 00:46:17 +0300

    Add a solution to the "Minimum Domino Rotations For Equal Row" challenge

--- 1007_Minimum_Domino_Rotations_For_Equal_Row/solution.rb
+++ 1007_Minimum_Domino_Rotations_For_Equal_Row/solution.rb
@@ -2,5 +2,25 @@
 # @param {Integer[]} bottoms
 # @return {Integer}
 def min_domino_rotations(tops, bottoms)
-    
+  [
+    count_row(tops, bottoms, tops[0]), 
+    count_row(tops, bottoms, bottoms[0]), 
+    count_row(bottoms, tops, tops[0]),
+    count_row(bottoms, tops, bottoms[0])
+  ].compact.min || -1
+end
+
+def count_row(row, opposite_row, ref_num)
+  count = 0
+  row.zip(opposite_row) do |num1, num2|
+    next if num1 == ref_num
+
+    if num2 == ref_num 
+      count += 1
+    else
+      return nil
+    end
+  end
+
+  count
 end
