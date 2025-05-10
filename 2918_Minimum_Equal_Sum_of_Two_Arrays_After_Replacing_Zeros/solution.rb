a949146b18ec93f7a179815962771570b475b7d0
Hasan Özovalı <ozovalihasan@gmail.com>
2025-05-10 14:10:22 +0300

    Add a solution to the "Minimum Equal Sum of Two Arrays After Replacing Zeros" challenge

--- 2918_Minimum_Equal_Sum_of_Two_Arrays_After_Replacing_Zeros/solution.rb
+++ 2918_Minimum_Equal_Sum_of_Two_Arrays_After_Replacing_Zeros/solution.rb
@@ -2,5 +2,13 @@
 # @param {Integer[]} nums2
 # @return {Integer}
 def min_sum(nums1, nums2)
-    
+  if valid_pair(nums1, nums2) && valid_pair(nums2, nums1)
+    return [nums1.sum + nums1.count(0), nums2.sum + nums2.count(0)].max
+  end
+  
+  -1
+end
+
+def valid_pair(nums1, nums2)
+  !(nums1.count(0) == 0 && (nums2.sum + nums2.count(0) > nums1.sum))
 end
