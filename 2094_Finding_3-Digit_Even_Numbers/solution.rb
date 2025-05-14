37f1032724e8849730da613da4d7169bb38900f6
Hasan Özovalı <ozovalihasan@gmail.com>
2025-05-14 22:13:41 +0300

    Add a solution to the "Finding 3-Digit Even Numbers" challenge

--- 2094_Finding_3-Digit_Even_Numbers/solution.rb
+++ 2094_Finding_3-Digit_Even_Numbers/solution.rb
@@ -1,5 +1,30 @@
 # @param {Integer[]} digits
 # @return {Integer[]}
 def find_even_numbers(digits)
-    
+  return [] if digits.none?(&:even?)
+  counts = digits.tally
+  counts.default = 0
+
+  results = []
+  1.upto(9) do |first_digit|
+    next if counts[first_digit] == 0
+
+    counts[first_digit] -= 1
+    0.upto(9) do |second_digit|
+      next if counts[second_digit] == 0
+      
+      counts[second_digit] -= 1
+      (0..8).step(2) do |third_digit|
+        next if counts[third_digit] == 0
+
+        results << (first_digit * 100 + second_digit * 10 + third_digit)
+      end
+      counts[second_digit] += 1
+
+    end
+    counts[first_digit] += 1
+
+  end
+
+  results
 end
