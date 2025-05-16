fa744d290410ecc755264ebf35ae079bc481c035
Hasan Özovalı <ozovalihasan@gmail.com>
2025-05-17 02:11:08 +0300

    Add a solution to the "Longest Unequal Adjacent Groups Subsequence I" challenge

--- 2900_Longest_Unequal_Adjacent_Groups_Subsequence_I/solution.rb
+++ 2900_Longest_Unequal_Adjacent_Groups_Subsequence_I/solution.rb
@@ -2,5 +2,13 @@
 # @param {Integer[]} groups
 # @return {String[]}
 def get_longest_subsequence(words, groups)
-    
+  current_group = nil
+  result = []
+  words.zip(groups).map do |word, group|
+    if group != current_group
+      result << word 
+      current_group = group
+    end
+  end
+  result
 end
